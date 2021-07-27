dd8b1856fa2a62f0a77426c89e2b16853b657726
RenzoSev <sevilharenzo@gmail.com>
2021-07-27 19:18:51 -0300

    feat: desafio13

---
+++ desafio13.sql
@@ -0,0 +1,8 @@
+SELECT productName AS `Produto`, price `Preço`
+FROM w3schools.products AS p
+WHERE EXISTS (
+	SELECT * FROM order_details AS od
+  	WHERE od.productId = p.productId 
+  	AND od.quantity > 80
+)
+ORDER BY `Produto`;

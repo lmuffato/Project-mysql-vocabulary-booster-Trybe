327813dd1eeb9f46d05fb8e4fe26e6eaac66c19c
Ana Ventura <anaventura1811@gmail.com>
2021-07-27 20:46:42 -0300

    feat: desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,13 @@
+-- 11. Exibe todas as pessoas clientes que possuem compatriotas, mostrando a quantidade de compatriotas para cada pessoa cliente
+SELECT
+	c.ContactName
+AS 'Nome',
+	c.Country
+AS 'País',
+COUNT(c.Country)
+AS 'Número de compatriotas'
+FROM w3schools.customers AS c,
+w3schools.customers AS c2
+WHERE c.Country = c2.Country AND c.ContactName<>c2.ContactName
+GROUP BY c.ContactName, c2.Country
+ORDER BY c.ContactName ASC;

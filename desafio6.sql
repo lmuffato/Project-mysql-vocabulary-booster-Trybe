def949f362172a09475128cefff8dfbad1076aa9
Ana Ventura <anaventura1811@gmail.com>
2021-07-27 20:38:14 -0300

    feat: desafio 6

---
+++ desafio6.sql
@@ -0,0 +1,23 @@
+-- 6. Faça um relatório que mostra o histórico de cargos das pessoas empregadas
+SELECT
+	CONCAT(e.first_name," ", e.last_name)
+AS 'Nome completo',
+j.job_title
+AS 'Cargo',
+jh.start_date
+AS 'Data de início do cargo',
+d.department_name
+AS 'Departamento'
+FROM
+hr.employees
+AS e
+INNER JOIN hr.jobs
+AS j
+ON j.job_id = e.job_id
+INNER JOIN hr.departments
+AS d
+ON d.department_id = e.department_id
+INNER JOIN hr.job_history
+as jh
+ON jh.employee_id = e.employee_id
+ORDER BY CONCAT(e.first_name," ", e.last_name) DESC, j.job_title ASC;

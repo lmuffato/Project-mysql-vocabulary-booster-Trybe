9c64aa15358fc237f855fab550a3bc520e62d58d
Ana Ventura <anaventura1811@gmail.com>
2021-07-27 20:41:47 -0300

    feat: desafio 9

---
+++ desafio9.sql
@@ -0,0 +1,20 @@
+-- 9. Exibe todos as pessoas funcionárias que já realizaram algum pedido, mostrando também seu total de pedidos feitos
+SELECT
+	CONCAT(e.firstName, ' ', e.lastName)
+AS
+	'Nome completo',
+COUNT(o.employeeID)
+AS 
+	'Total de pedidos'
+FROM
+	w3schools.employees
+AS e
+INNER JOIN
+	w3schools.orders
+AS o
+ON 
+	e.employeeID = o.employeeID
+GROUP BY
+	e.employeeID
+ORDER BY
+	COUNT(o.employeeID) ASC;

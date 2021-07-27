77639f347ade2d813b0228be5bf3d86a8be0a6ee
RenzoSev <sevilharenzo@gmail.com>
2021-07-27 19:17:55 -0300

    feat: desafio8

---
+++ desafio8.sql
@@ -0,0 +1,10 @@
+SELECT c.contactName AS `Nome de contato`,
+s.shipperName AS `Empresa que fez o envio`,
+o.orderDate AS `Data do pedido`
+FROM w3schools.orders AS o
+JOIN w3schools.customers AS c
+ON c.customerId = o.customerId
+JOIN w3schools.shippers AS s
+ON s.shipperId = o.shipperId 
+AND s.shipperName IN ('Speedy Express', 'United Package')
+ORDER BY `Nome de contato`, `Empresa que fez o envio`; 

a7fbe544bba413b8b79da4fe830f564eec7cd90b
RenzoSev <sevilharenzo@gmail.com>
2021-07-27 19:18:26 -0300

    feat: desafio11

---
+++ desafio11.sql
@@ -0,0 +1,9 @@
+SELECT c1.contactName AS `Nome`, 
+c1.country AS`País`, 
+COUNT(c1.country) AS `Número de compatriotas`
+FROM w3schools.customers AS c1
+JOIN w3schools.customers AS c2
+ON c1.country = c2.country
+AND c1.customerID <> c2.customerId
+GROUP BY `Nome`, c2.country
+ORDER BY `Nome`;

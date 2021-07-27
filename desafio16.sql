fa00af4438ce422cc32b3581f47dc76461accae5
Ana Ventura <anaventura1811@gmail.com>
2021-07-27 20:50:26 -0300

    feat: desafio 16

---
+++ desafio16.sql
@@ -0,0 +1,18 @@
+-- 16. Crie uma função chamada buscar_quantidade_de_empregos_por_funcionario no banco de dados hr que,
+-- ao receber o email de uma pessoa funcionária, retorne a quantidade de empregos presentes em seu
+-- histórico
+USE hr;
+DELIMITER $$
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email varchar(25))
+RETURNS DOUBLE READS SQL DATA
+BEGIN
+	DECLARE quantity DOUBLE;
+	SELECT COUNT(jh.employee_ID)
+    FROM job_history AS jh
+    INNER JOIN employees AS e
+    WHERE e.employee_ID = jh.employee_ID AND e.email LIKE CONCAT('%', email, '%')
+    INTO quantity;
+RETURN quantity;
+END $$
+DELIMITER ;

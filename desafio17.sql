4be6a1b3de9b198331742ec708bf2fe1eeff3a71
Ana Ventura <anaventura1811@gmail.com>
2021-07-27 20:52:07 -0300

    feat: desafio 17, adiciona referências

---
+++ desafio17.sql
@@ -0,0 +1,14 @@
+-- 17. Crie uma TRIGGER que, a cada nova inserção realizada na tabela orders,
+-- insira automaticamente a data atual na coluna OrderDate
+USE w3schools;
+DELIMITER $$
+CREATE TRIGGER dataDeInsercao
+	BEFORE INSERT ON orders
+    FOR EACH ROW
+BEGIN
+	SET NEW.OrderDate = CURRENT_DATE();
+END $$
+DELIMITER ;
+
+-- Source:
+-- https://doc.splicemachine.com/sqlref_builtinfcns_currentdate.html

cf721ee86b23e907549e33608f5ab573549c3dbd
Ana Ventura <anaventura1811@gmail.com>
2021-07-27 20:36:49 -0300

    feat: desafio 5

---
+++ desafio5.sql
@@ -0,0 +1,14 @@
+-- 5. Exiba os cargos com sua variação salarial e suas médias máxima e mínima mensal,
+-- considerando salários máximo e minímo como anuais
+SELECT
+	job_title
+AS 'Cargo',
+(max_salary - min_salary)
+AS 'Variação Salarial',
+ROUND((min_salary / 12), 2)
+AS 'Média mínima mensal',
+ROUND((max_salary / 12), 2)
+AS 'Média máxima mensal'
+FROM
+hr.jobs
+ORDER BY (max_salary - min_salary) ASC, job_title ASC;

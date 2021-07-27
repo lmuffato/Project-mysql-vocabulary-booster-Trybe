3f19a9557ca0ceedd7a940bb7a54b8a0868a0a36
Ana Ventura <anaventura1811@gmail.com>
2021-07-27 20:43:50 -0300

    feat: desafio 10

---
+++ desafio10.sql
@@ -0,0 +1,18 @@
+-- 10. Exibe todos os produtos que já foram pedidos, que possuem uma média de quantidade nos pedidos registrados acima de 20.00
+SELECT
+	p.ProductName
+AS 'Produto',
+MIN(od.Quantity)
+AS 'Mínima',
+MAX(od.Quantity)
+AS 'Máxima',
+ROUND(AVG(od.Quantity), 2)
+AS 'Média'
+FROM w3schools.products
+AS p
+INNER JOIN w3schools.order_details
+AS od
+ON ((od.ProductID = p.ProductID))
+GROUP BY p.ProductID
+HAVING AVG(od.Quantity)>20.00
+ORDER BY AVG(od.Quantity) ASC, p.ProductName ASC;

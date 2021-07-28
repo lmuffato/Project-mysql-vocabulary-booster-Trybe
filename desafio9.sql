e0ffdef20b3704cd2a7610f2c2d2ba140282da51
Eduardo Seije <eduardoseije@gmail.com>
2021-07-28 16:01:25 -0300

    Desafio 09 - Exibe todos as pessoas funcionárias que já realizaram algum pedido, mostrando também seu total de pedidos feitos

---
+++ desafio9.sql
@@ -0,0 +1,8 @@
+SELECT CONCAT(e.firstName, '' , e.lastName) AS 'Nome completo',
+COUNT(o.EmployeeID) AS 'Total de pedidos'
+FROM w3schools.employees AS e
+INNER JOIN 
+w3schools.orders AS o 
+ON e.EmployeeID = o.EmployeeID
+GROUP BY e.EmployeeID
+ORDER BY COUNT(o.employeeID) ASC;

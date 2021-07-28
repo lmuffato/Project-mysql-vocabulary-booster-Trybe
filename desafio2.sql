f8f6e9475421b0229f1b08f22979840b5c938d00
WillianMendes <willianmendesbr@gmail.com>
2021-07-28 16:57:48 -0300

    feat: requisito 2

---
+++ desafio2.sql
@@ -0,0 +1,12 @@
+SELECT
+  j.JOB_TITLE as Cargo,
+    CASE
+      WHEN j.MAX_SALARY >= 5000 AND j.MAX_SALARY <= 10000 THEN 'Baixo'
+      WHEN j.MAX_SALARY >= 10001 AND j.MAX_SALARY <= 20000 THEN 'Médio'
+      WHEN j.MAX_SALARY >= 20001 AND j.MAX_SALARY <= 30000 THEN 'Alto'
+      WHEN j.MAX_SALARY > 30000 THEN 'Altíssimo'
+  END AS Nível
+FROM
+  jobs as j
+ORDER BY 
+  j.JOB_TITLE;

544a2961424aa335fa380a2f19b0d66a7871f322
Mateus Santiago <mateussantiago.victor@gmail.com>
2021-07-28 15:28:35 -0300

    Fix lint req3

--- desafio3.sql
+++ desafio3.sql
@@ -1,6 +1,4 @@
-SELECT
-	JOB_TITLE AS Cargo,
-    (MAX_SALARY - MIN_SALARY) AS 'Diferença entre salários máximo e mínimo'
+SELECT JOB_TITLE AS Cargo, (MAX_SALARY - MIN_SALARY) AS 'Diferença entre salários máximo e mínimo'
 FROM hr.jobs
 WHERE MAX_SALARY <> MIN_SALARY
 ORDER BY (MAX_SALARY - MIN_SALARY), JOB_TITLE;

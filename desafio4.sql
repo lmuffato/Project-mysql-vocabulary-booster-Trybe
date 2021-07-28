e642564e530fcd9f8d6f872465772abae262fe4d
Luciano Amâncio <madruga665@hotmail.com>
2021-07-28 13:09:48 -0300

    fix: Requisito 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,11 +1,11 @@
 SELECT jobs.JOB_TITLE AS 'Cargo',
 ROUND(AVG(employees.SALARY), 2) AS 'Média salarial',
- CASE
+CASE
   WHEN AVG(SALARY) BETWEEN 2000 AND 5800 THEN 'Júnior'
   WHEN AVG(SALARY) BETWEEN 5801 AND 7500 THEN 'Pleno'
   WHEN AVG(SALARY) BETWEEN 7501 AND 10500 THEN 'Sênior'
   WHEN AVG(SALARY) > 10500 THEN 'CEO'
- END AS 'Senioridade'
+END AS 'Senioridade'
 FROM employees AS employees
 INNER JOIN jobs AS jobs
 ON jobs.JOB_ID = employees.JOB_ID

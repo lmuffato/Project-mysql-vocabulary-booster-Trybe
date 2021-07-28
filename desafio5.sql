c9fce42bdfdb4a46e0e9336740756f6fab9f809c
Lucas André <lucas.cancado89@gmail.com>
2021-07-28 15:27:17 -0300

    desafio6 - Show job history by employee

--- desafio5.sql
+++ desafio5.sql
@@ -2,7 +2,7 @@ SELECT * FROM hr.jobs;
 
 SELECT JOB_TITLE AS 'Cargo',
 (MAX_SALARY - MIN_SALARY) AS 'Variação Salarial',
-(MIN_SALARY/12) AS 'Média mínima mensal',
-(MAX_SALARY/12) AS 'Média máxima mensal'
+ROUND(MIN_SALARY/12, 2) AS 'Média mínima mensal',
+ROUND(MAX_SALARY/12, 2) AS 'Média máxima mensal'
 FROM hr.jobs
 ORDER BY `Variação Salarial`, Cargo;

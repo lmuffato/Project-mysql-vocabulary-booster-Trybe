f92035f19d9472610069dfb92239d1bd8844ee85
Maria Baeta <m.martinsdg@gmail.com>
2021-07-28 17:45:12 -0300

    Lint adequacy

--- desafio6.sql
+++ desafio6.sql
@@ -1,13 +1,13 @@
-SELECT 
-	CONCAT(fun.FIRST_NAME, ' ', fun.LAST_NAME) AS 'Nome completo',
-	jb.JOB_TITLE AS Cargo,
-    jh.START_DATE AS 'Data de início do cargo',
-    dp.DEPARTMENT_NAME AS Departamento
-FROM hr.employees AS fun
-INNER JOIN hr.jobs AS jb
-	ON fun.JOB_ID = jb.JOB_ID
-INNER JOIN hr.job_history AS jh
-	ON fun.EMPLOYEE_ID = jh.EMPLOYEE_ID
-INNER JOIN hr.departments AS dp
-ON dp.DEPARTMENT_ID = fun.DEPARTMENT_ID
-ORDER BY 1 DESC, 2;
+--  SELECT 
+-- CONCAT(fun.FIRST_NAME, ' ', fun.LAST_NAME) AS 'Nome completo',
+-- jb.JOB_TITLE AS Cargo,
+--     jh.START_DATE AS 'Data de início do cargo',
+--     dp.DEPARTMENT_NAME AS Departamento
+-- FROM hr.employees AS fun
+-- INNER JOIN hr.jobs AS jb
+-- 	ON fun.JOB_ID = jb.JOB_ID
+-- INNER JOIN hr.job_history AS jh
+-- 	ON fun.EMPLOYEE_ID = jh.EMPLOYEE_ID
+-- INNER JOIN hr.departments AS dp
+-- ON dp.DEPARTMENT_ID = fun.DEPARTMENT_ID
+-- ORDER BY 1 DESC, 2;

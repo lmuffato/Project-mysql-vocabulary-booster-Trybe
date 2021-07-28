9dfd02fde8c304bfa9cd261507501b905363431b
guilhermemd <guilherme.md@gmail.com>
2021-07-28 19:47:43 -0300

    fix: :bug: req 08

--- desafio8.sql
+++ desafio8.sql
@@ -1,13 +1,13 @@
 SELECT 
-customers.customerName AS 'Nome de contato', 
-shippers.ShipperName AS 'Empresa que fez o envio',
-orders.orderDate AS 'Data do pedido'
-FROM w3schools.customers AS customers
-INNER JOIN 
-w3schools.orders AS orders 
-ON orders.CustomerID = customers.CustomerID
-INNER JOIN 
-w3schools.shippers AS shippers
-ON shippers.ShipperID = orders.ShipperID
-WHERE shippers.ShipperName IN ('Speedy Express', 'United Package')
+customers.ContactName 'Nome de contato',
+shippers.ShipperName 'Empresa que fez o envio',
+orders.OrderDate 'Data do pedido'
+FROM
+w3schools.customers AS customers
+INNER JOIN
+w3schools.orders AS orders ON customers.CustomerID = orders.CustomerID
+INNER JOIN
+w3schools.shippers AS shippers ON orders.ShipperID = shippers.ShipperID
+WHERE
+shippers.ShipperName IN ('Speedy Express', 'United Package')
 ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;

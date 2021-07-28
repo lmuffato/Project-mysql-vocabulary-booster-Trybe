89683d763427a241723dc53d18d53a83e17a76dd
Elisa <elisafranca.carvalho@gmail.com>
2021-07-28 19:55:59 -0300

    feat: requirement 8

---
+++ desafio8.sql
@@ -0,0 +1,13 @@
+-- Exibe todas as pessoas consumidoras cujos pedidos já foram enviados pelas empresas 
+-- "Speedy Express" ou "United Package"
+SELECT cust.ContactName AS `Nome de contato`,
+sh.ShipperName AS `Empresa que fez o envio`,
+ord.OrderDate AS `Data do pedido`
+FROM w3schools.customers AS cust
+INNER JOIN w3schools.orders AS ord
+ON cust.CustomerID = ord.CustomerID
+INNER JOIN w3schools.shippers AS sh
+ON ord.ShipperID = sh.ShipperID
+WHERE sh.ShipperName
+IN('Speedy Express','United Package')
+ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;

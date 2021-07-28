7cc14eca810362ec7527e693b6f96e6f93cdbfa5
Lucas André <lucas.cancado89@gmail.com>
2021-07-28 13:33:10 -0300

    desafio1-Show countries and indicate the ones from europe

--- desafio1.sql
+++ desafio1.sql
@@ -2,4 +2,5 @@ SELECT c.COUNTRY_NAME AS 'País',
 IF(r.REGION_NAME = 'Europe', 'incluido', 'não incluido') AS 'Status Inclusão'
 FROM hr.countries AS c
 INNER JOIN hr.regions AS r
-ON c.REGION_ID = r.REGION_ID;
+ON c.REGION_ID = r.REGION_ID
+ORDER BY c.COUNTRY_NAME;

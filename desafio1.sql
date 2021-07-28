478e148759c4f0cb4c90e32d0c19a34795a464de
Patrick Morais <ppternunes@gmail.com>
2021-07-28 16:56:29 -0300

    requisito 1

---
+++ desafio1.sql
@@ -0,0 +1,13 @@
+SELECT 
+    COUNTRY_NAME AS 'País',
+    IF(REGION_ID = (SELECT 
+                REGION_ID
+            FROM
+                hr.regions
+            WHERE
+                REGION_NAME LIKE '%Europe%'),
+        'incluído',
+        'não incluído') AS 'Status Inclusão'
+FROM
+    hr.countries
+ORDER BY COUNTRY_NAME ASC;

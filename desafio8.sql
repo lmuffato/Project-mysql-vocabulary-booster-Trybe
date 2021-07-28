86c4459a46743cc638c45a139cd4bf61b8792b8b
Debora Passos <deborinhapassos@gmail.com>
2021-07-28 17:42:20 -0300

    Req 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,6 +1,4 @@
-SELECT c.ContactName AS 'Nome de contato',
-	s.ShipperName AS 'Empresa que fez o envio',
-  o.OrderDate AS 'Data do pedido'
+SELECT c.ContactName AS 'Nome de contato', s.ShipperName AS 'Empresa que fez o envio', o.OrderDate AS 'Data do pedido'
 FROM customers AS c
 INNER JOIN orders AS o
 ON c.customerID = o.customerID

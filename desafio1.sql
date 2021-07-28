c7213f76ad6708066ee95ef519af8da36b9f83c3
Giovanni <giovannitadeu08@gmail.com>
2021-07-27 21:57:36 -0300

    fix: climate

--- desafio1.sql
+++ desafio1.sql
@@ -1,11 +1,4 @@
-SELECT
-  country_name
-AS
-  'País',
+SELECT country_name AS 'País',
 IF (region_id = 1, 'incluído', 'não incluído')
-AS
-  'Status Inclusão'
-FROM
-  hr.countries
-ORDER BY
-  country_name ASC;
+AS 'Status Inclusão' FROM hr.countries
+ORDER BY country_name ASC;

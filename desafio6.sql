aeaa77bc32ddb896f514d35395ed17edb0742e39
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2021-07-28 18:07:03 -0300

    Fix: desafio6

--- desafio6.sql
+++ desafio6.sql
@@ -1,6 +1,6 @@
 -- Faça um relatório que mostra o histórico de cargos das pessoas empregadas
 
-SELECT CONCAT(e.FIRST_NAME, e.LAST_NAME) `Nome completo`,
+SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) `Nome completo`,
 j.JOB_TITLE `Cargo`,
 h.START_DATE `Data de início do cargo`,
 d.DEPARTMENT_NAME `Departamento`

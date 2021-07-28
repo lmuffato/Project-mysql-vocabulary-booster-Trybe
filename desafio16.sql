41c3c6b1ff6a8a1fb2630672873450404ae9aab1
Hernanne <hernannedomiciano@gmail.com>
2021-07-28 18:43:55 -0300

    fix: req16 V2

--- desafio16.sql
+++ desafio16.sql
@@ -3,9 +3,9 @@ CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(25))
 RETURNS DOUBLE READS SQL DATA
 BEGIN
 DECLARE qty DOUBLE;
-SELECT COUNT(j.employee_id) FROM hr.job_history AS j
+SELECT COUNT(j.employee_ID) FROM hr.job_history AS j
 INNER JOIN hr.employees AS e
-WHERE e.employee_id = j.employeed_id AND e.email LIKE CONCAT('%', email, '%')
+WHERE e.employee_ID = j.employee_ID AND e.email LIKE CONCAT('%', email, '%')
 INTO qty;
 RETURN qty;
 END $$

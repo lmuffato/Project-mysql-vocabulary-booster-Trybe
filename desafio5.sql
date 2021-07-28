40006e26ad0852eb31bc87da0470ec59999a99f0
mallmann02 <leomallmann020302@gmail.com>
2021-07-28 00:09:32 -0300

    challenges[1-5]: c5

---
+++ desafio5.sql
@@ -0,0 +1,7 @@
+SELECT
+	JOB_TITLE AS `Cargo`,
+  ROUND((MAX_SALARY - MIN_SALARY), 2) AS `Variação Salarial`,
+  ROUND((MIN_SALARY / 12), 2) AS `Média mínima mensal`,
+  ROUND((MAX_SALARY / 12), 2) AS `Média máxima mensal`
+FROM hr.jobs
+ORDER BY ROUND((MAX_SALARY - MIN_SALARY), 2), JOB_TITLE;

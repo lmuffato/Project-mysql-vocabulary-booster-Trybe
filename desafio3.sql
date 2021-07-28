4b21f44ab3c1e303972cebaaea1f35199e831d32
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-07-28 17:37:46 -0300

    Desafio 2 e 3 test

--- desafio3.sql
+++ desafio3.sql
@@ -1,6 +1,6 @@
-SELECT JOB_TITLE AS 'Cargo',
+SELECT JOB_TITLE AS Cargo,
   MAX_SALARY -  MIN_SALARY
     AS 'Diferença entre salários máximo e mínimo' 
       FROM hr.jobs
-  ORDER BY JOB_TITLE ASC, 
-    'Diferença entre salários máximo e mínimo' ASC;
+  ORDER BY 'Diferença entre salários máximo e mínimo' ASC,
+   JOB_TITLE ASC;

21b08f72055d5590c742e9fb4dbed722c05a4b0a
guilhermemd <guilherme.md@gmail.com>
2021-07-28 19:08:13 -0300

    feat: :sparkles: req 08 implementation

---
+++ desafio8.sql
@@ -0,0 +1,13 @@
+SELECT 
+customers.customerName AS 'Nome de contato', 
+shippers.ShipperName AS 'Empresa que fez o envio',
+orders.orderDate AS 'Data do pedido'
+FROM w3schools.customers AS customers
+INNER JOIN 
+w3schoolS.orders AS orders 
+ON orders.CustomerID = customers.CustomerID
+INNER JOIN 
+w3schools.shippers AS shippers
+ON shippers.ShipperID = orders.ShipperID
+WHERE shippers.ShipperName IN ('Speedy Express', 'United Package')
+ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;

c37daf496acf00dc7e5d7555d390939d15e7aac1
Debora Passos <deborinhapassos@gmail.com>
2021-07-28 19:30:59 -0300

    Req 11

---
+++ desafio11.sql
@@ -0,0 +1,6 @@
+SELECT A.ContactName AS 'Nome', A.Country AS 'País', COUNT(A.Country) AS 'Número de compatriotas'
+FROM customers AS A, customers AS B
+WHERE A.ContactName <> B.ContactName
+AND A.Country = B.Country
+GROUP BY A.ContactName, B.Country
+ORDER BY A.ContactName ASC;

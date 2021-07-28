549cb4d5148fa9012ab61c80f2e371bda6f38ec9
Debora Passos <deborinhapassos@gmail.com>
2021-07-28 18:30:20 -0300

    Req 9 and 10

---
+++ desafio9.sql
@@ -0,0 +1,7 @@
+SELECT CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome completo', 
+  COUNT(o.EmployeeID) AS 'Total de pedidos'
+FROM employees AS e
+INNER JOIN orders AS o
+ON e.EmployeeID = o.EmployeeID
+GROUP BY `Nome Completo`
+ORDER BY `Total de pedidos`;

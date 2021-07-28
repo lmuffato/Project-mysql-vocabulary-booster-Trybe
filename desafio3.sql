ca857e1ec9b86ef9770f615cc07407572fcaa8fc
Pedro Ramos <pedro.ramos@betrybe.com>
2021-07-28 15:02:00 -0300

    FIX: Remove all challenges

--- desafio3.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    j.JOB_TITLE AS `Cargo`,
-    (j.MAX_SALARY - j.MIN_SALARY) AS `Diferença entre salários máximo e mínimo`
-FROM
-    jobs AS j
-ORDER BY
-    `Diferença entre salários máximo e mínimo` ASC,
-    j.JOB_TITLE ASC;

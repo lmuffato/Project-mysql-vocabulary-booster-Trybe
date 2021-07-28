432f1e92758562599939213596d4e4ea2a0862c2
Giovanni <giovannitadeu08@gmail.com>
2021-07-28 00:11:47 -0300

    fix: desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT(e.first_name, ' ', e.last_name) as 'Nome completo',
+SELECT CONCAT(e.first_name,' ', e.last_name) as 'Nome completo',
 j.job_title AS 'Cargo',
 jh.start_date AS 'Data de inicio do cargo',
 d.department_name AS 'Departamento'
@@ -8,4 +8,4 @@ INNER JOIN hr.departments AS d
 INNER JOIN hr.job_history AS jh
 ON ((d.department_id = jh.department_id)
 AND (j.job_id = jh.job_id) AND (jh.employee_id = e.employee_id))
-ORDER BY CONCAT(e.first_name, ' ', e.last_name) DESC, j.job_title ASC;
+ORDER BY CONCAT(e.first_name,' ', e.last_name) DESC, j.job_title ASC;

ee864435994f784306c3f9393ab13ee4a451d95a
lucas-chave <lcchavesr150@gmail.com>
2021-07-28 16:51:17 -0300

    correcting challenge 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,5 +1,5 @@
 -- challenge 3;
 
-SELECT job_title AS 'Cargo', max_salary - min_salary AS 'Diferença entre salários máximo e mínimo'
+SELECT job_title AS 'Cargo', max_salary - min_salary AS `Diferença entre salários máximo e mínimo`
 FROM hr.jobs
-ORDER BY 'Diferença entre salários máximo e mínimo' OR job_title;
+ORDER BY `Diferença entre salários máximo e mínimo` ASC, job_title;

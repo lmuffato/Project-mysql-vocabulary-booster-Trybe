38f1fbe2395224edfcfb84b6c22faf686b345962
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-07-28 15:11:18 -0300

    Challeng 1: Add Alias Pais

--- desafio1.sql
+++ desafio1.sql
@@ -1,2 +1,2 @@
-SELECT country_name, IF(region_id = 1, 'incluído', 'não incluído') AS 'Status Inclusão' FROM hr.countries
+SELECT country_name AS País, IF(region_id = 1, 'incluído', 'não incluído') AS 'Status Inclusão' FROM hr.countries
 ORDER BY country_name;

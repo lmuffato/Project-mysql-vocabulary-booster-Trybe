8629d1868b1b0ccef57697e74588f536f83ceef0
Rodrigo Rech <rodrigorech92@gmail.com>
2021-07-28 14:25:33 -0300

    Feat: show countries and indicate if them are from europe or not

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,1 @@
-SELECT COUNTRY_NAME AS 'País', IF (REGION_ID = 1, 'incluído', 'não incluído') AS 'Status Inclusão' FROM hr.countries;
+SELECT COUNTRY_NAME AS 'País', IF (REGION_ID = 1, 'incluído', 'não incluído') AS 'Status Inclusão' FROM hr.countries ORDER BY 'País';

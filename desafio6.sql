735227546c9d557ad095ddc0d69315919b079cd4
Felipe Muller <felipeavmuller@gmail.com>
2021-07-28 18:50:13 -0300

    Requisito 6 - inner JOIN SQL

---
+++ desafio6.sql
@@ -0,0 +1,7 @@
+SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome Completo', j.JOB_TITLE AS 'Cargo',
+jh.START_DATE AS 'Data de início do cargo', d.DEPARTMENT_NAME AS 'Departamento'
+FROM hr.job_history jh
+INNER JOIN hr.employees e ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+INNER JOIN hr.jobs j ON jh.JOB_ID = j.JOB_ID
+INNER JOIN hr.departments d ON jh.DEPARTMENT_ID = d.DEPARTMENT_ID
+ORDER BY `Nome Completo` DESC, `Cargo`;

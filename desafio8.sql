606eff7ce8b0d86777107ae3e112bed76d9421e6
Pedro Ramos <pedro.ramos@betrybe.com>
2021-07-28 10:36:58 -0300

    NEW: Add 8th challenge

---
+++ desafio8.sql
@@ -0,0 +1,21 @@
+SELECT
+	c.ContactName AS `Nome de contato`,
+    s.ShipperName AS `Empresa que fez o envio`,
+    o.OrderDate AS `Data do pedido`
+FROM
+	customers AS c
+JOIN
+	orders AS o
+ON
+	c.CustomerID = o.CustomerID
+JOIN
+	shippers AS s
+ON
+	o.ShipperID = s.ShipperID
+WHERE
+	s.ShipperName LIKE 'Speedy Express' 
+    OR s.ShipperName LIKE 'United Package'
+ORDER BY
+	c.ContactName ASC,
+    s.ShipperName ASC,
+    o.OrderDate ASC;

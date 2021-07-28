2524072f08cdf16f7c87a4abebc634e2ecc34ac3
Eduardo Seije <eduardoseije@gmail.com>
2021-07-28 16:58:28 -0300

    Desafio 11 - ajustes

--- desafio11.sql
+++ desafio11.sql
@@ -1,5 +1,5 @@
 SELECT c1.contactName AS 'Nome',
-c1.Country AS País,
+c1.Country AS 'País',
 COUNT(c1.Country) AS 'Números de compatriotas'
 FROM w3schools.customers AS c1, w3schools.customers AS c2
 WHERE c1.Country = c2.Country AND c1.ContactName <> c2.ContactName

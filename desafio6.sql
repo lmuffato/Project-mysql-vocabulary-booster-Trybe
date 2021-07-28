302b96d04f34da8e8a8bccca1e04f5c807d3203c
Eduardo Seije <eduardoseije@gmail.com>
2021-07-28 15:24:22 -0300

    Desafio 06 - refatorado

--- desafio6.sql
+++ desafio6.sql
@@ -1,15 +1,12 @@
-SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
-j.JOB_TITLE AS 'Cargo',
-jh.START_DATE AS 'Data de início do cargo',
-d.DEPARTMENT_NAME AS 'Departamento' 
+SELECT CONCAT(e.first_name," ", e.last_name) AS 'Nome completo',
+j.job_title AS 'Cargo',
+jh.start_date AS 'Data de início do cargo',
+d.department_name AS 'Departamento'
 FROM hr.employees AS e
-INNER JOIN 
-hr.jobs AS j
-ON e.JOB_ID = j.job_id
-INNER JOIN 
-hr.job_history AS jh
-ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
-INNER JOIN
-hr.departments AS d 
-ON d.DEPARTMENT_ID = e.DEPARTMENT_ID
-ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) DESC, j.JOB_TITLE ASC;
+INNER JOIN hr.jobs AS j
+ON j.job_id = e.job_id 
+INNER JOIN hr.departments AS d
+ON d.department_id = e.department_id
+INNER JOIN hr.job_history as jh
+ON jh.employee_id = e.employee_id
+ORDER BY CONCAT(e.first_name," ", e.last_name) DESC, j.job_title ASC;

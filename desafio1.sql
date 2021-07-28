5cf8233c1752fdb5dcca0aa20a80f37be1b5a77f
Rodrigo Queiroz <rodrigoandradequeiroz@gmail.com>
2021-07-28 10:25:28 -0300

    feat: req 01

--- desafio1.sql
+++ desafio1.sql
@@ -1,7 +1,4 @@
 SELECT COUNTRY_NAME AS País,
-	CASE
-		WHEN REGION_ID = 1 THEN 'incluído'
-	ELSE 'não incluído'
-	END AS 'Status Inclusão'
-FROM hr.countries
-ORDER BY País;
+	IF(REGION_ID = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
+FROM hr.countries ORDER BY País;
+

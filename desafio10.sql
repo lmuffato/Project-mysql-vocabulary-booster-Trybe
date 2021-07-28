cfe4d788a0bc6fda1016f1b7306debb0621db856
rafael <rafittu@outlook.com>
2021-07-28 18:38:25 -0300

    feat: req 10

---
+++ desafio10.sql
@@ -0,0 +1,9 @@
+SELECT p.ProductName AS "Produto",
+MIN(od.Quantity) AS "Mínima",
+MAX(od.Quantity) AS "Máxima",
+ROUND(AVG(od.Quantity), 2) AS "Média"
+FROM w3schools.order_details AS od
+JOIN w3schools.products AS p
+ON p.ProductID = od.ProductID
+GROUP BY p.ProductName HAVING `Média` > 20.00
+ORDER BY `Média`, `Produto`;

bc2346265532522e0cfb50e1e11d032a22551525
rogerio-lambert <rogerio.cl@gmail.com>
2021-07-28 20:23:22 -0300

    consertando a ordenação no desafio 7

--- desafio7.sql
+++ desafio7.sql
@@ -8,4 +8,4 @@ SELECT
     WHERE E.EMPLOYEE_ID = H.EMPLOYEE_ID) AS 'Salário'
 FROM hr.job_history AS H
 WHERE MONTH(START_DATE) IN (1, 2, 3)
-ORDER BY `Nome completo` DESC, `Data de início` ASC;
+ORDER BY `Nome completo` ASC, `Data de início` ASC;

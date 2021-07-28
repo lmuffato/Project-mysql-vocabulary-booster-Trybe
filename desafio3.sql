91f0bdb82707543d3b7cdcd2500ba2f94251ebcc
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-07-27 21:47:38 -0400

    desafio 3 done

---
+++ desafio3.sql
@@ -0,0 +1,9 @@
+-- Exiba os cargos com a diferença entre seus salários máximo e mínimo
+-- Crie uma query que exiba 02 colunas:
+-- Use o banco hr como referência
+-- A primeira deve possuir o alias "Cargo" e exibir o nome do cargo.
+-- A segunda deve possuir o alias "Diferença entre salários máximo e mínimo" e exibir a diferença entre os salários máximo e mínimo para o cargo em questão.
+-- Os resultados devem estar ordenados pela diferença entre salários máximo e mínimo em ordem crescente.
+-- caso de empate nessa diferença os resultados devem ser ordenados pelo nome do cargo em ordem alfabética.
+
+SELECT job_title AS 'Cargo', max_salary - min_salary AS 'Diferença entre salários máximo e mínimo' FROM hr.jobs ORDER BY max_salary - min_salary, Cargo;

109b0d3f705e9be566f58830e3a118e01e4b3eed
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-07-28 19:17:23 -0300

    fix: requisito 7,C = c

--- desafio7.sql
+++ desafio7.sql
@@ -1,8 +1,8 @@
-SELECT UCASE(CONCAT(e.FIRST_NAME,' ',e.LAST_NAME)) AS 'Nome Completo',
+SELECT UCASE(CONCAT(e.FIRST_NAME,' ',e.LAST_NAME)) AS 'Nome completo',
 h.START_DATE AS 'Data de início',
 e.SALARY AS 'Salário'
 FROM hr.job_history AS h
 INNER JOIN hr.employees AS e
 ON h.EMPLOYEE_ID = e.EMPLOYEE_ID
 WHERE month(h.START_DATE) IN (1, 2, 3)
-ORDER BY `Nome completo` ASC, h.START_DATE ASC;
+ORDER BY `Nome completo`, h.START_DATE;

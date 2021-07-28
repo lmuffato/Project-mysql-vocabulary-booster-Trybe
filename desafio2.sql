6626ca9dbe2da6d0301e058d4439b86f75f9540f
Giovanni <giovannitadeu08@gmail.com>
2021-07-27 21:57:10 -0300

    feat: desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,8 @@
+SELECT job_title AS 'Cargo',
+CASE
+WHEN max_salary >= 5000 AND max_salary <= 10000 THEN 'Baixo'
+WHEN max_salary >= 10001 AND max_salary <= 20000 THEN 'Médio'
+WHEN max_salary >= 20001 AND max_salary <= 30000 THEN 'Alto'
+ELSE 'Altíssimo'
+END AS 'Nível' FROM hr.jobs
+ORDER BY job_title ASC;

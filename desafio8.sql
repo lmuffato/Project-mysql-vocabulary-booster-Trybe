2c3e15920149f5d7eb74a2c0ab83ffe1cd14feb8
Giovanni <giovannitadeu08@gmail.com>
2021-07-27 23:13:07 -0300

    feat: desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,9 @@
+SELECT c.contactName AS 'Nome de contato',
+s.shipperName AS 'Empresa que fez o envio',
+o.orderDate AS 'Data do pedido'
+FROM w3schools.customers AS c
+INNER JOIN w3schools.orders AS o
+INNER JOIN w3schools.shippers AS s
+ON (c.customerID = o.customerID)
+AND ((o.shipperID=1 AND s.shipperID = o.shipperID) OR (o.shipperID=2 AND s.shipperID = o.shipperID))
+ORDER BY c.contactName ASC, s.shipperName ASC, o.orderDate;

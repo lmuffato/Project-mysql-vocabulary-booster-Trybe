c043889e95e4c5ac485d2455e82b95e5d72ba8df
Jodiel Briesemeister <jodielll29@gmail.com>
2021-07-28 17:58:03 -0300

    fix: req10

--- desafio10.sql
+++ desafio10.sql
@@ -1,11 +1,10 @@
-SELECT p.ProductName AS 'Produto', 
-  MIN(o_d.Quantity) AS 'Mínima', 
-  MAX(o_d.Quantity) AS 'Máxima', 
+SELECT p.ProductName AS 'Produto',
+  MIN(o_d.Quantity) AS 'Mínima',
+  MAX(o_d.Quantity) AS 'Máxima',
   ROUND(AVG(o_d.Quantity), 2) AS 'Média'
- FROM products AS p
- INNER JOIN order_details AS o_d
- ON p.ProductID = o_d.ProductID
- GROUP BY `Produto`
- HAVING `Média` > 20
- ORDER BY `Média`, `Produto`;
- 
+FROM products AS p
+INNER JOIN order_details AS o_d
+ON p.ProductID = o_d.ProductID
+GROUP BY `Produto`
+HAVING `Média` > 20
+ORDER BY `Média`, `Produto`;

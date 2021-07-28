3dc807c633b959318bf788e31e0b27a4e5be5430
João Nasc <oi@joaonasc.dev>
2021-07-28 20:19:11 -0300

    feat: challenge 8

---
+++ desafio8.sql
@@ -0,0 +1,14 @@
+SELECT
+  `customers`.ContactName AS `Nome de contato`,
+  `shippers`.ShipperName AS `Empresa que fez o envio`,
+  `orders`.OrderDate AS `Data do pedido`
+FROM
+  `w3schools`.orders AS `orders`
+  INNER JOIN `w3schools`.customers AS `customers` ON `customers`.CustomerID = `orders`.CustomerID
+  INNER JOIN `w3schools`.shippers AS `shippers` ON `shippers`.ShipperID = `orders`.ShipperID
+WHERE
+  `shippers`.ShipperName IN ('Speedy Express', 'United Package')
+ORDER BY
+  `Nome de contato` ASC,
+  `Empresa que fez o envio` ASC,
+  `Data do pedido` ASC;

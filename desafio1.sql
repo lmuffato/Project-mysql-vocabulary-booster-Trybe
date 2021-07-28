4f88cfdd8923214004b5c37c67d8efb3cb4a815e
iago <iago.pferreiravr@gmail.com>
2021-07-28 09:31:29 -0300

    Fix: Req 1 & Feat: Req 3

--- desafio1.sql
+++ desafio1.sql
@@ -1,7 +1,7 @@
 SELECT country_name AS País,
 CASE
-  WHEN region_id = 1 THEN 'incluido'
-  ELSE 'não incluido'
+  WHEN region_id = 1 THEN 'incluído'
+  ELSE 'não incluído'
 END AS 'Status Inclusão'
 FROM hr.countries
 ORDER BY País ASC;

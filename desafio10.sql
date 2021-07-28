38dc597d5a374fec041723e1fd88135732124566
Gui-lira <gui.lira.martins@gmail.com>
2021-07-28 18:25:01 -0300

    correção 10 e 11

--- desafio10.sql
+++ desafio10.sql
@@ -1,5 +1,5 @@
 SELECT p.ProductName AS Produto, MIN(o.Quantity) AS Mínima,
-MAX(o.Quantity) AS Máxima, AVG(o.Quantity) AS Média
+MAX(o.Quantity) AS Máxima, ROUND(AVG(o.Quantity), 2) AS Média
 FROM w3schools.products AS p
 INNER JOIN w3schools.order_details AS o ON p.ProductID = o.ProductID
 GROUP BY o.ProductID
--- desafio11.sql
+++ desafio11.sql
@@ -1,7 +1,8 @@
 SELECT c.ContactName AS Nome, c.Country AS País,
   (
     SELECT (COUNT(*) - 1) FROM w3schools.customers
-    WHERE Country = c.Country
+    WHERE Country = c.Country 
 ) AS `Número de compatriotas` 
 FROM w3schools.customers AS c
+HAVING `Número de compatriotas` > 0
 ORDER BY Nome;

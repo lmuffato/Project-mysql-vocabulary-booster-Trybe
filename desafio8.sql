12b19a97adc8a0afb72ec7eb44a394fe90d1335c
Tiago Santos <tiago881@gmail.com>
2021-07-28 17:45:59 -0300

    requirement: 8

---
+++ desafio8.sql
@@ -0,0 +1,8 @@
+SELECT cust.ContactName AS 'Nome de contato', ship.ShipperName AS 'Empresa que fez o envio', orDATE.OrderDate AS 'Data do pedido'
+FROM customers AS cust
+INNER JOIN orders AS orDATE
+ON cust.CustomerID = oRDATE.CustomerID
+INNER JOIN Shippers AS ship
+ON orDATE.ShipperID = ship.ShipperID
+WHERE ship.ShipperName IN ('Speedy Express', 'United Package')
+ORDER BY cust.ContactName ASC, ship.ShipperName ASC, orDATE.OrderDate ASC;

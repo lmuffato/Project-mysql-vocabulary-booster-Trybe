683195f8f91d682f78f55803803c45ccdfc647c4
maricegalla <maricegalla@gmail.com>
2021-07-28 20:46:54 -0300

    Desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,7 @@
+SELECT c1.ContactName AS 'Nome',
+c1.Country AS 'País',
+count(c1.Country) AS 'Número de compatriotas'
+FROM w3schools.customers AS c1, w3schools.customers AS c2
+WHERE c1.Country = c2.Country AND c1.ContactName <> c2.ContactName
+GROUP BY c1.ContactName, c2.Country
+ORDER BY c1.ContactName ASC;

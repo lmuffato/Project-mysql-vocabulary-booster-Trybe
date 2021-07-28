69ec6c538a1c87d7d4a5d837e3d415a9d05439be
luanlsr <luanfswd@gmail.com>
2021-07-28 00:31:19 -0300

    requisito 4 finalizado

--- desafio4.sql
+++ desafio4.sql
@@ -1,14 +1,19 @@
 -- 4 - Exiba a média salarial e o nível de senioridade de todas as pessoas empregadas, agrupadas pelo cargo;
 -- Precisei consultar o PR da Ana Ventura para perceber que era necessário usar o INNER JOIN para buscar a coluna SALARY
-SELECT j.JOB_TITLE AS 'Cargo', ROUND(AVG(e.SALARY),2) AS 'Média Salarial',
+SELECT j.JOB_TITLE 
+AS 'Cargo',
+ROUND(AVG(e.SALARY), 2) 
+AS 'Média Salarial',
 CASE
-  WHEN ROUND(AVG(e.SALARY),2) BETWEEN 2000 AND 5800 THEN 'Júnior'
-  WHEN ROUND(AVG(e.SALARY),2) BETWEEN 5801 AND 7500 THEN 'Pleno'
-  WHEN ROUND(AVG(e.SALARY),2) BETWEEN 7501 AND 10500 THEN 'Sênior'
-  WHEN ROUND(AVG(e.SALARY),2) > 10500 THEN 'CEO'
+  WHEN ROUND(AVG(e.SALARY), 2) >= 2000 AND ROUND(AVG(e.SALARY), 2) <= 5800 THEN 'Júnior'
+  WHEN ROUND(AVG(e.SALARY), 2) >= 5801 AND ROUND(AVG(e.SALARY), 2) <= 7500 THEN 'Pleno'
+  WHEN ROUND(AVG(e.SALARY), 2) >= 7501 AND ROUND(AVG(e.SALARY), 2) <= 10500 THEN 'Sênior'
+  WHEN ROUND(AVG(e.SALARY), 2) > 10500 THEN 'CEO'
 END AS 'Senioridade'
-FROM hr.jobs AS j
-INNER JOIN hr.employees AS e
+FROM hr.jobs 
+AS j
+INNER JOIN hr.employees 
+AS e
 ON e.JOB_ID = j.JOB_ID
-GROUP BY `Cargo`
-ORDER BY `Média Salarial`, `Cargo`;
+GROUP BY e.job_id
+ORDER BY ROUND(AVG(e.salary), 2) ASC, j.job_title ASC;

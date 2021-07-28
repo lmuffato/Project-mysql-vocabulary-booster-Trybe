937e77631752414d57531f94f464fd6d9a15bbf7
Felipe Muller <felipeavmuller@gmail.com>
2021-07-28 19:28:02 -0300

    Correçao requisito 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome Completo', j.JOB_TITLE AS 'Cargo',
+SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo', j.JOB_TITLE AS 'Cargo',
 jh.START_DATE AS 'Data de início do cargo', d.DEPARTMENT_NAME AS 'Departamento'
 FROM hr.job_history jh
 INNER JOIN hr.employees e ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID

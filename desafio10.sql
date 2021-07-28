b2fe410cab8aff377ffd656f515132c3cd1a3eac
Flavio Franco <flavio.win@gmail.com>
2021-07-28 13:41:19 -0300

    desafio 10

---
+++ desafio10.sql
@@ -0,0 +1,12 @@
+SELECT
+  ProductName AS 'Produto',
+  MIN(o.Quantity) AS 'Mínima',
+  MAX(o.Quantity) AS 'Máxima',
+  ROUND(AVG(o.Quantity), 2) AS 'Média'
+FROM
+  w3schools.products p
+INNER JOIN
+  w3schools.order_details o ON p.ProductID = o.ProductID
+GROUP BY p.ProductID
+HAVING Média > 20
+ORDER BY 4, 1;

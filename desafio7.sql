98e3105a4b2f32d298adf3f86433cafd700c62dd
Felipe Muller <felipeavmuller@gmail.com>
2021-07-28 19:45:05 -0300

    requisito 7 - inner join SQL

---
+++ desafio7.sql
@@ -0,0 +1,6 @@
+SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
+jh.START_DATE AS 'Data de início', e.SALARY AS 'Salário'
+FROM hr.job_history jh
+INNER JOIN hr.employees e ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+WHERE MONTH(jh.START_DATE) BETWEEN 01 AND 06
+ORDER BY `Nome completo`, `Data de início`;

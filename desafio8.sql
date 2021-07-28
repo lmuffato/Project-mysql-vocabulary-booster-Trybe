3e580cc90c67031989649c3c78c17b4dca423156
Lucas André <lucas.cancado89@gmail.com>
2021-07-28 15:59:52 -0300

    desafio8 - Show customer, shipper and order date

---
+++ desafio8.sql
@@ -0,0 +1,9 @@
+SELECT c.contactName AS 'Nome de contato',
+s.ShipperName AS 'Empresa que fez o envio',
+o.OrderDate AS 'Data do pedido'
+FROM w3schools.customers AS c
+RIGHT JOIN w3schools.orders AS o
+ON c.CustomerID = o.CustomerID
+LEFT JOIN w3schools.shippers AS s
+ON s.ShipperID = o.ShipperID
+ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;

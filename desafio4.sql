b9f6216f5cef9d773adac4a13213b39ff83b5a5d
Rodrigo Rech <rodrigorech92@gmail.com>
2021-07-28 16:21:21 -0300

    Fix CodeClimate desafio4.sql

--- desafio4.sql
+++ desafio4.sql
@@ -1,10 +1,10 @@
 SELECT jobs.JOB_TITLE AS `Cargo`,
-	ROUND(AVG(employees.SALARY),2) AS `Média salarial`,
+ROUND(AVG(employees.SALARY),2) AS `Média salarial`,
 CASE 
-	WHEN ROUND(AVG(employees.SALARY),2) BETWEEN 2000 AND 5800 THEN 'Júnior'
-  WHEN ROUND(AVG(employees.SALARY),2) BETWEEN 5801 AND 7500 THEN 'Pleno'
-  WHEN ROUND(AVG(employees.SALARY),2) BETWEEN 7501 AND 10500 THEN 'Sênior'
-  WHEN ROUND(AVG(employees.SALARY),2) > 10500 THEN 'CEO'
+WHEN ROUND(AVG(employees.SALARY),2) BETWEEN 2000 AND 5800 THEN 'Júnior'
+WHEN ROUND(AVG(employees.SALARY),2) BETWEEN 5801 AND 7500 THEN 'Pleno'
+WHEN ROUND(AVG(employees.SALARY),2) BETWEEN 7501 AND 10500 THEN 'Sênior'
+WHEN ROUND(AVG(employees.SALARY),2) > 10500 THEN 'CEO'
 END AS "Senioridade"
 FROM hr.jobs AS jobs
 INNER JOIN hr.employees AS employees

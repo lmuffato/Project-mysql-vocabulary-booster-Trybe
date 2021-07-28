d6d866f0dfe94a7f3e2dcb269d7795d412c1513c
rogerio-lambert <rogerio.cl@gmail.com>
2021-07-28 19:13:41 -0300

    implemementação do requisito 6

---
+++ desafio6.sql
@@ -0,0 +1,11 @@
+SELECT
+  (SELECT CONCAT(FIRST_NAME, ' ', LAST_NAME) 
+    FROM hr.employees AS E
+    WHERE E.EMPLOYEE_ID = H.EMPLOYEE_ID) AS 'Nome completo',
+  (SELECT JOB_TITLE FROM hr.jobs AS J
+    WHERE J.JOB_ID = H.JOB_ID) AS Cargo,
+  START_DATE AS 'Data de início do cargo',
+  (SELECT DEPARTMENT_NAME FROM hr.departments AS D
+    WHERE D.DEPARTMENT_ID = H.DEPARTMENT_ID) AS Departamento
+FROM hr.job_history AS H
+ORDER BY `Nome completo` DESC, Cargo ASC;

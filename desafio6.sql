6924c88981ebc90b6aa5bddea71c56aee9a7633d
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-07-27 21:29:32 -0300

    detalhes corrigidos

--- desafio6.sql
+++ desafio6.sql
@@ -1,9 +1,9 @@
-SELECT CONCAT(FIRST_NAME, " ", LAST_NAME) AS "Nome completo", 
-jobs.JOB_TITLE AS "Cargo",
+SELECT CONCAT_WS(FIRST_NAME, " ", LAST_NAME) AS "Nome completo", 
+jobs.JOB_TITLE AS Cargo,
 DATE(START_DATE) AS "Data de início do cargo",
 department.DEPARTMENT_NAME AS "Departamento"
 FROM hr.job_history AS `history`
 INNER JOIN hr.jobs AS jobs ON `history`.JOB_ID = jobs.JOB_ID
 INNER JOIN hr.employees AS employees ON employees.EMPLOYEE_ID = `history`.EMPLOYEE_ID
 INNER JOIN hr.departments AS department ON `history`.DEPARTMENT_ID = department.DEPARTMENT_ID
-ORDER BY "Nome completo" ASC, JOB_TITLE ASC;
+ORDER BY CONCAT_WS(FIRST_NAME, " ", LAST_NAME) ASC, Cargo ASC;

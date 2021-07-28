0e63fedbd48b64b3cc4f45bbaf8ab301cd41fdc8
Rodrigo Rech <rodrigorech92@gmail.com>
2021-07-28 17:12:44 -0300

    Feat: Show consumers who orders where delivered by Speedy Express and United Package

---
+++ desafio8.sql
@@ -0,0 +1,8 @@
+SELECT customers.ContactName AS `Nome de contato`,
+shippers.ShipperName AS  `Empresa que fez o envio`,
+orders.OrderDate AS `Data do pedido`
+FROM w3schools.orders AS orders
+INNER JOIN w3schools.shippers AS shippers ON shippers.ShipperId = orders.ShipperID
+INNER JOIN w3schools.customers AS customers ON customers.CustomerID = orders.CustomerID
+WHERE shippers.ShipperName IN ('Speedy Express', 'United Package')
+ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;

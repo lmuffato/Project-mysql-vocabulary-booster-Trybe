24375a3e558ca3d0498133b4ef538d9d3e4397db
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-07-28 06:59:40 +0000

    feat: desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,13 @@
+# 11 - Exibe todas as pessoas clientes que possuem compatriotas, mostrando a quantidade de compatriotas para cada pessoa cliente
+SELECT
+    c1.ContactName AS Nome,
+    c1.Country AS País,
+    COUNT(c2.ContactName) AS `Número de compatriotas`
+FROM
+    w3schools.customers c1,
+    w3schools.customers c2
+WHERE
+    c1.Country = c2.Country
+        AND c1.ContactName <> c2.ContactName
+GROUP BY Nome
+ORDER BY Nome;

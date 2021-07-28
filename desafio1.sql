2cb71d63d2166de722d0eac08621fec78ed40d1a
iago <iago.pferreiravr@gmail.com>
2021-07-28 08:50:15 -0300

    Feat: Req 1

---
+++ desafio1.sql
@@ -0,0 +1,11 @@
+SELECT country_name AS País,
+CASE
+  WHEN region_id = 1 THEN 'incluido'
+  ELSE 'não incluido'
+END AS 'Status Inclusão'
+FROM hr.countries
+ORDER BY País ASC;
+
+
+SELECT * FROM hr.countries;
+SELECT * FROM ;

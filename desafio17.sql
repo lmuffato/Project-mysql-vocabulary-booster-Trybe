f54dd661e3fb2334eaeef7d58dbe34de1c989f54
RenzoSev <sevilharenzo@gmail.com>
2021-07-27 21:08:53 -0300

    fix?: climate

--- desafio17.sql
+++ desafio17.sql
@@ -3,10 +3,10 @@ USE w3schools;
 DELIMITER $$
 
 CREATE TRIGGER trigger_insert_orderDate
-	BEFORE INSERT ON orders
+    BEFORE INSERT ON orders
     FOR EACH ROW
 BEGIN
-	SET NEW.orderDate = NOW();
+    SET NEW.orderDate = NOW();
 END $$
 
 DELIMITER ;
--- desafio4.sql
+++ desafio4.sql
@@ -1,7 +1,7 @@
 SELECT j.job_title AS `Cargo`,
 ROUND(AVG(e.salary), 2) AS `Média salarial`,
 CASE
-	WHEN ROUND(AVG(e.salary), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
+    WHEN ROUND(AVG(e.salary), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
     WHEN ROUND(AVG(e.salary), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
     WHEN ROUND(AVG(e.salary), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
     WHEN ROUND(AVG(e.salary), 2) > 10500 THEN 'CEO'

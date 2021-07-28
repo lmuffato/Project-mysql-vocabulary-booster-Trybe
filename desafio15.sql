8147c7fe7f97bcbd97f02ef90d91d2114d77288b
Gui-lira <gui.lira.martins@gmail.com>
2021-07-28 20:36:08 -0300

    correção codeclimate

--- desafio15.sql
+++ desafio15.sql
@@ -3,12 +3,11 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN job VARCHAR(50))
 BEGIN 
-   SELECT ROUND(AVG(SALARY),2) FROM employees 
-   WHERE JOB_ID = (
-     SELECT JOB_ID FROM jobs
-     WHERE JOB_TITLE = job
-     );
+    SELECT ROUND(AVG(SALARY),2) FROM employees 
+    WHERE JOB_ID = (
+      SELECT JOB_ID FROM jobs
+      WHERE JOB_TITLE = job
+      );
 END$$
 
 DELIMITER $$
-

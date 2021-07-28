b26f50a41fc620086935e68dc034fa65f1d9f290
Hernanne <hernannedomiciano@gmail.com>
2021-07-28 15:39:25 -0300

    feat: req11

---
+++ desafio11.sql
@@ -0,0 +1,7 @@
+SELECT T1.contactName AS `Nome`,
+T1.country AS `País`,
+COUNT(T1.customerID) AS `Número de compatriotas`
+FROM w3schools.customers AS T1, w3schools.customers AS T2
+WHERE T1.country = T2.country
+GROUP BY T1.customerID
+ORDER BY `Nome` ASC;

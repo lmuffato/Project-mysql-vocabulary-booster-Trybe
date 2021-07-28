65518e1bd0032b406c5639f4f1fbb7aff2e5fff0
phlopes <phlopes93@gmail.com>
2021-07-28 16:16:24 -0300

    challenge 4 completed

---
+++ desafio4.sql
@@ -0,0 +1,14 @@
+SELECT J.JOB_TITLE AS 'Cargo',
+ROUND(AVG(SALARY),2) AS `Média salarial`,
+    CASE 
+        WHEN AVG(SALARY) BETWEEN 2000 AND 5800 THEN 'Júnior'
+        WHEN AVG(SALARY) BETWEEN 5801 AND 7500 THEN 'Pleno'
+        WHEN AVG(SALARY) BETWEEN 7501 AND 10500 THEN 'Sênior'
+        WHEN AVG(SALARY) > 10500 THEN 'CEO'
+    END AS `Senioridade`
+FROM hr.jobs AS J
+INNER JOIN 
+hr.employees AS S
+ON J.JOB_ID = S.JOB_ID
+GROUP BY `Cargo`
+ORDER BY `Média Salarial`, `Cargo`;

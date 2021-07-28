5c0f9112ffcfe3d199a1175ae6b552f600404734
biazidioti <bzferreira.bio@gmail.com>
2021-07-28 10:00:30 -0300

    Challenge 1 - era digitaçao

--- desafio1.sql
+++ desafio1.sql
@@ -1,7 +1,7 @@
 -- 1 - Exiba os países e indicando se cada um deles se encontra ou não na região formada pela Europa
-USE hr;
+
 SELECT 
   COUNTRY_NAME AS 'País',
     IF (REGION_ID = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
-FROM countries
+FROM hr.countries
 ORDER BY COUNTRY_NAME ASC;

5c27d82c0c70a0d178d952957836bf6b03549c6e
lucas-chave <lcchavesr150@gmail.com>
2021-07-28 17:07:17 -0300

    correcting challenge 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,12 +1,11 @@
 -- challenge 2;
 
-SELECT 
-job_title AS 'Cargo', 
-  CASE
+SELECT job_title AS 'Cargo',
+CASE
 WHEN max_salary BETWEEN 5000 AND 10000 THEN 'Baixo'
 WHEN max_salary BETWEEN 10001 AND 20000 THEN 'Medio'
 WHEN max_salary BETWEEN 20001 AND 30000 THEN 'Alto'
 WHEN max_salary > 30000 THEN 'Altíssimo'
-  END AS 'Nível'
-FROM hr.jobs 
+END AS 'Nível'
+FROM hr.jobs
 ORDER BY job_title;

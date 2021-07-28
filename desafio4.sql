835e57be7fd2f0d3f0079ba87ef480f61d4524eb
Bruno Mendes <mendessbruno@gmail.com>
2021-07-27 22:51:29 -0300

    tentativa de resolver ate ex 7

--- desafio4.sql
+++ desafio4.sql
@@ -5,7 +5,7 @@ SELECT j.JOB_TITLE AS `Cargo`, ROUND(AVG(e.SALARY), 2) AS `Média salarial`,
         WHEN AVG(SALARY) >= 7501 AND AVG(SALARY) <= 10500 THEN 'Sênior'
         WHEN AVG(SALARY) > 10500 THEN 'CEO'
         ELSE 'Sem classificação'
-    END AS `Nível`
+    END AS `Senioridade`
 FROM hr.employees AS e
 INNER JOIN hr.jobs AS j
 ON e.JOB_ID = j.JOB_ID

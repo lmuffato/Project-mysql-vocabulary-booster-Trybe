bc3a1f0c2584133c49c0d0896e9c54d9e89a4dd6
Giovanni <giovannitadeu08@gmail.com>
2021-07-28 12:35:45 -0300

    fix: climate

--- desafio16.sql
+++ desafio16.sql
@@ -6,9 +6,9 @@ RETURNS DOUBLE READS SQL DATA
 BEGIN
 DECLARE quantity DOUBLE;
 SELECT  COUNT(jh.employee_ID) FROM job_history AS jh
-	INNER JOIN employees AS e
-	WHERE e.employee_ID = jh.employee_ID AND e.email LIKE CONCAT('%', email, '%')
-	INTO quantity;
+INNER JOIN employees AS e
+WHERE e.employee_ID = jh.employee_ID AND e.email LIKE CONCAT('%', email, '%')
+INTO quantity;
 RETURN quantity;
 END $$
 DELIMITER ;

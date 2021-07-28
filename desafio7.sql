a55544a00f49337695b759cf2f7cdb766abbe1a6
rogerio-lambert <rogerio.cl@gmail.com>
2021-07-28 20:19:06 -0300

    implementação do desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,11 @@
+SELECT
+  (SELECT UCASE(CONCAT(FIRST_NAME, ' ', LAST_NAME)) 
+    FROM hr.employees AS E
+    WHERE E.EMPLOYEE_ID = H.EMPLOYEE_ID) AS 'Nome completo',
+  START_DATE AS 'Data de início',
+  (SELECT SALARY
+    FROM hr.employees AS E
+    WHERE E.EMPLOYEE_ID = H.EMPLOYEE_ID) AS 'Salário'
+FROM hr.job_history AS H
+WHERE MONTH(START_DATE) IN (1, 2, 3)
+ORDER BY `Nome completo` DESC, `Data de início` ASC;

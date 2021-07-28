e2d89e0776b0e08859e664a2edb7a652329fc396
hheloisa <hhackenhaar@gmail.com>
2021-07-27 23:01:43 -0300

    Corrige erro do requisito 2

--- desafio2.sql
+++ desafio2.sql
@@ -2,7 +2,7 @@ SELECT job_title AS `Cargo`,
 CASE
 WHEN max_salary BETWEEN 5000 AND 10000 THEN 'Baixo'
 WHEN max_salary BETWEEN 10001 AND 20000 THEN 'Médio'
-WHEN max_salary BETWEEN 30000 THEN 'Alto'
+WHEN max_salary BETWEEN 2000 AND 30000 THEN 'Alto'
 WHEN max_salary > 30000 THEN 'Altíssimo'
 END AS `Nível`
 FROM hr.jobs

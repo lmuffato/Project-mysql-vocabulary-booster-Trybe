ad1833640a0833a6840265ff793775c8f8cef54f
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-07-28 19:04:44 -0300

    fix: requisito 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,8 +1,8 @@
-SELECT UPPER(CONCAT(e.FIRST_NAME, ' ',e.LAST_NAME)) AS 'Nome Completo',
+SELECT UCASE(CONCAT(e.FIRST_NAME,' ',e.LAST_NAME)) AS 'Nome Completo',
 h.START_DATE AS 'Data de início',
 e.SALARY AS 'Salário'
-FROM hr.job_history h
-INNER JOIN hr.employees e
+FROM hr.job_history AS h
+INNER JOIN hr.employees AS e
 ON h.EMPLOYEE_ID = e.EMPLOYEE_ID
-WHERE MONTH(h.START_DATE) BETWEEN 01 AND 03
+WHERE month(h.START_DATE) BETWEEN 01 AND 03
 ORDER BY `Nome completo` ASC, h.START_DATE ASC;

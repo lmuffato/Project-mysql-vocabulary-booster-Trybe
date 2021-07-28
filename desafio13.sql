44a8e37ccf464ab855659feb853d05d7b886d037
Giovanni <giovannitadeu08@gmail.com>
2021-07-28 12:06:16 -0300

    feat: desafio 13

---
+++ desafio13.sql
@@ -0,0 +1,6 @@
+SELECT p.ProductName AS 'Produto',
+p.price AS 'Preço'
+FROM w3schools.products AS p
+INNER JOIN w3schools.order_details AS od
+ON od.ProductID = p.ProductID AND od.Quantity>80
+ORDER BY p.ProductName ASC;

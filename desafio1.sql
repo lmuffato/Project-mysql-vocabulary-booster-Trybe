045f4f5a7a16a983bfb2a6e44ae5ba8c4aaf38d6
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-07-28 14:58:27 -0300

    fix: requisito 1,acentuaçao

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,1 @@
-SELECT COUNTRY_NAME AS 'País',IF (REGION_ID = 1, 'incluido', 'não incluido') FROM hr.countries ORDER BY COUNTRY_NAME ASC;
+SELECT COUNTRY_NAME AS 'País',IF (REGION_ID = 1, 'incluído', 'não incluído') FROM hr.countries ORDER BY COUNTRY_NAME ASC;

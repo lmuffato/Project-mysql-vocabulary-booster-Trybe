51da92488d7059cd06554f5a9aec70a440fb6aef
Bruno Mendes <mendessbruno@gmail.com>
2021-07-28 17:45:06 -0300

    resolvido problema do formato da data

--- desafio18.sql
+++ desafio18.sql
@@ -1,6 +1,6 @@
 SELECT concat(e.FIRST_NAME, ' ', e.LAST_NAME) `Nome completo`,
-jh.START_DATE AS `Data de início`,
-jh.END_DATE AS `Data de rescisão`,
+concat(day(jh.START_DATE),'/',month(jh.START_DATE),'/',year(jh.START_DATE)) AS `Data de início`,
+concat(day(jh.END_DATE),'/',month(jh.END_DATE),'/',year(jh.END_DATE)) AS `Data de rescisão`,
 ROUND(DATEDIFF(jh.END_DATE, jh.START_DATE)/365, 2) AS `Anos trabalhados`
 FROM hr.job_history AS jh
 INNER JOIN hr.employees AS e

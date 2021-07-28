b9bf441b14e6be72ba4efc48fbc459542950512a
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-07-28 18:36:32 -0300

    Fiz o 1! Tive que usar um ORDER BY para ordenar por nome de pais, estava agrupando por continente pelo que entendi

---
+++ desafio1.sql
@@ -0,0 +1,6 @@
+SELECT c.country_name AS 'País',
+IF(r.region_name = 'Europe', 'incluído', 'não incluído') AS 'Status Inclusão'
+FROM hr.countries AS c
+INNER JOIN hr.regions AS r
+ON c.REGION_ID = r.REGION_ID
+ORDER BY c.country_name;

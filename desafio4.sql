8f5194f517ef70489d3854b853ff0d5e374b0c88
Bruno Mendes <mendessbruno@gmail.com>
2021-07-27 21:43:51 -0300

    resolvido desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,13 @@
+SELECT j.JOB_TITLE AS `Cargo`, AVG(e.SALARY) AS `Média salarial`,
+    CASE
+        WHEN AVG(SALARY) >= 2000 AND AVG(SALARY) <= 5800 THEN 'Júnior'
+        WHEN AVG(SALARY) >= 5801 AND AVG(SALARY) <= 7500 THEN 'Pleno'
+        WHEN AVG(SALARY) >= 7501 AND AVG(SALARY) <= 10500 THEN 'Sênior'
+        WHEN AVG(SALARY) > 10500 THEN 'CEO'
+        ELSE 'Sem classificação'
+    END AS `Nível`
+FROM hr.employees AS e
+INNER JOIN hr.jobs AS j
+ON e.JOB_ID = j.JOB_ID
+GROUP BY Cargo
+ORDER BY `Média salarial`, Cargo;

3fc9f5f8e993bce80753d61f3ee66ab2eaf4e2fd
Pedro Ramos <pedro.ramos@betrybe.com>
2021-07-28 10:35:33 -0300

    NEW: Add 7th challenge

---
+++ desafio7.sql
@@ -0,0 +1,15 @@
+SELECT
+	UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS `Nome completo`,
+    jh.START_DATE AS `Data de início`,
+    e.SALARY AS `Salário`
+FROM
+	employees AS e
+JOIN
+	job_history AS jh
+ON
+	jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+HAVING
+	MONTH(jh.START_DATE) IN (1,2,3)
+ORDER BY
+	`Nome completo` ASC,
+    `Data de início` ASC;

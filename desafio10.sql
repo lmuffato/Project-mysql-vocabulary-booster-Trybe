78835ab225cbc77f0059892eb59f06d72f339d1b
guilhermemd <guilherme.md@gmail.com>
2021-07-28 20:12:33 -0300

    feat: :sparkles: REQ 10 IMPLEMENTATION

---
+++ desafio10.sql
@@ -0,0 +1,13 @@
+SELECT 
+products.ProductName AS 'Produto',
+MIN(order_details.Quantity) AS 'Mínima',
+MAX(order_details.Quantity) AS 'MáximA',
+ROUND(AVG(order_details.Quantity), 2) AS 'Média'
+FROM 
+w3schools.products AS products
+INNER JOIN
+w3schools.order_details AS order_details
+ON products.ProductID = order_details.ProductID
+GROUP BY products.ProductName
+HAVING `Média` > 20 
+ORDER BY `Média`, `Produto`;

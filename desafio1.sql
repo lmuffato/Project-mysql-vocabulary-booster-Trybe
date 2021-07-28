dcd8ed1a45648930879b38ca3e8ed0fbbe059ced
Marcus C. H. Filho <mchusemann@gmail.com>
2021-07-28 15:44:17 -0300

    feat: challenge 1

---
+++ desafio1.sql
@@ -0,0 +1,16 @@
+SELECT
+  c.COUNTRY_NAME AS 'País',
+  IF(
+    c.REGION_ID = (
+      SELECT
+        r.REGION_ID
+      FROM
+        hr.regions r
+      WHERE
+        r.REGION_NAME = 'Europe'
+    ),
+    'incluído',
+    'não incluído'
+  ) AS 'Status Inclusão'
+FROM
+  hr.countries c;

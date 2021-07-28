37479fd66759cd6ff8b17267075c73aed45e0e0f
rogerio-lambert <rogerio.cl@gmail.com>
2021-07-28 18:41:05 -0300

    correção do requisito 5 puchando agora da tabela jobs

--- desafio5.sql
+++ desafio5.sql
@@ -1,9 +1,8 @@
 SELECT 
-  (SELECT JOB_TITLE FROM hr.jobs AS J
-    WHERE E.JOB_ID = J.JOB_ID) AS Cargo,
-  MAX(SALARY) - MIN(SALARY) AS 'Variação Salarial',
-  ROUND(MIN(SALARY)/12, 2) AS 'Média mínima mensal',
-  ROUND(MAX(SALARY)/12, 2) AS 'Média máxima mensal'
-FROM hr.employees AS E
+  JOB_TITLE  AS Cargo,
+  MAX_SALARY - MIN_SALARY AS 'Variação Salarial',
+  ROUND(MIN_SALARY/12, 2) AS 'Média mínima mensal',
+  ROUND(MAX_SALARY/12, 2) AS 'Média máxima mensal'
+FROM hr.jobs
 GROUP BY Cargo
-ORDER BY MAX(SALARY) - MIN(SALARY) ASC, Cargo ASC
+ORDER BY MAX_SALARY - MIN_SALARY ASC, Cargo ASC

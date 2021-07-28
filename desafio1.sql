19e6a2a79434035a39a59846a4440eaac1c0c717
luanlsr <luanfswd@gmail.com>
2021-07-27 23:25:10 -0300

    requisito 2 finalizado

--- desafio1.sql
+++ desafio1.sql
@@ -1,6 +1,7 @@
+-- 1 - Exiba os países e indicando se cada um deles se encontra ou não na região formada pela Europa;
 SELECT pais.COUNTRY_NAME AS 'País', 
 -- POSIÇÃO DE ONDE COLOCAR O IF RETIRADA DO PR DO GUILHERME DORNELLES
-IF(status.REGION_NAME = 'Europe', 'incluído', 'não incluído')  AS 'Status Inclusão'
+IF(status.REGION_NAME = 'Europe', 'incluído', 'não incluído') AS 'Status Inclusão'
 FROM hr.countries AS pais
 INNER JOIN hr.regions AS status
 ON status.REGION_ID = pais.REGION_ID

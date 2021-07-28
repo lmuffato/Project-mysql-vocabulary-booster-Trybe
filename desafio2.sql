01466502ab1d008f01f4c628da98703ac0a95bd0
Marina Alane <marina.alane@outlook.com>
2021-07-28 14:34:42 -0300

    fix req 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,8 +1,8 @@
 SELECT job_title AS 'Cargo',
 CASE
-WHEN MAX_SALARY <= 10000 THEN 'Baixo'
-WHEN MAX_SALARY <= 20000 THEN 'Médio'
-WHEN MAX_SALARY <= 30000 THEN 'Alto'
-ELSE 'Altíssimo'
-END AS Nível
-FROM hr.jobs ORDER BY 'Cargo';
+WHEN max_salary BETWEEN 5000 AND 10000 THEN 'Baixo'
+WHEN max_salary BETWEEN 10001 AND 20000 THEN 'Médio'
+WHEN max_salary BETWEEN 20001 AND 30000 THEN 'Alto'
+WHEN max_salary > 30000 THEN 'Altíssimo'
+END AS 'Nível'
+FROM hr.jobs ORDER BY Cargo;

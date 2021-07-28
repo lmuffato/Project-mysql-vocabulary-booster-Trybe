ff128bc9194432f5636a283cf55124cd092fbe35
Giovanni <giovannitadeu08@gmail.com>
2021-07-27 23:03:01 -0300

    feat: desafio 7*

--- desafio7.sql
+++ desafio7.sql
@@ -1,1 +1,6 @@
-SELECT U
+SELECT UCASE(CONCAT(first_name,' ',last_name)) AS 'Nome completo',
+jh.start_date AS 'Data de início',
+salary AS 'Salário'
+FROM hr.employees AS e INNER JOIN hr.job_history AS jh ON e.employee_id = jh.employee_id
+WHERE MONTH(jh.start_date) IN (1, 2, 3)
+ORDER BY CONCAT(first_name,' ',last_name) ASC, jh.start_date ASC;

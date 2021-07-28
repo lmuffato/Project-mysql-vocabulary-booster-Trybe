b048170361985614bf5595f758267560dc646585
Eduardo Seije <eduardoseije@gmail.com>
2021-07-28 16:51:23 -0300

    Desafio 11 - Exibe todas as pessoas clientes que possuem compatriotas, mostrando a quantidade de compatriotas para cada pessoa cliente

---
+++ desafio11.sql
@@ -0,0 +1,7 @@
+SELECT c1.contactName AS 'Nome',
+c1.Country AS País,
+COUNT(c1.Country) AS 'Números de compatriotas'
+FROM w3schools.customers AS c1, w3schools.customers AS c2
+WHERE c1.Country = c2.Country AND c1.ContactName <> c2.ContactName
+GROUP BY c1.ContactName, c2.Country
+ORDER BY c1.ContactName ASC;

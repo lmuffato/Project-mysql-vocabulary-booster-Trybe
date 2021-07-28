3ecdc744b0228c0f11146e2b583f43e3c809d1ad
luanlsr <luanfswd@gmail.com>
2021-07-27 22:57:45 -0300

    requisito 1 finalizado

--- desafio1.sql
+++ desafio1.sql
@@ -1,4 +1,5 @@
 SELECT pais.COUNTRY_NAME AS 'País', 
+-- POSIÇÃO DE ONDE COLOCAR O IF RETIRADA DO PR DO GUILHERME DORNELLES
 IF(status.REGION_NAME = 'Europe', 'incluído', 'não incluído')  AS 'Status Inclusão'
 FROM hr.countries AS pais
 INNER JOIN hr.regions AS status

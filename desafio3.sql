96b5b461775a7ed7ecb8c8c2a9f1c6ec85752c8c
MarcioFujita <hanker78@gmail.com>
2021-07-28 15:27:07 -0300

    Requisitos 1 - 3 V3

--- desafio3.sql
+++ desafio3.sql
@@ -1,5 +1,3 @@
-SELECT
-	job_title AS 'Cargo',
-	(max_salary - min_salary) AS 'Diferença entre salários máximo e mínimo'
+SELECT job_title AS 'Cargo', (max_salary - min_salary) AS 'Diferença entre salários máximo e mínimo'
 FROM hr.jobs
 ORDER BY (max_salary - min_salary) ASC, Cargo ASC;

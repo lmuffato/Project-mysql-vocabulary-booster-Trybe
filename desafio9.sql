746f84854b523ba58b664bd73aad9aec0006ab58
gabrieldeori <deori.pessoal@gmail.com>
2021-07-28 19:12:01 -0300

    Add desafio9.sql not tested

---
+++ desafio9.sql
@@ -0,0 +1,13 @@
+SELECT
+	concat(emp.FirstName, ' ', emp.LastName) AS 'Nome completo',
+	count(ods.OrderID) AS 'Total de pedidos'
+FROM
+	w3schools.employees AS emp
+INNER JOIN
+	w3schools.orders AS ods
+ON
+	emp.EmployeeID = ods.EmployeeID
+GROUP BY
+	emp.EmployeeID
+ORDER BY
+	`Total de pedidos`;

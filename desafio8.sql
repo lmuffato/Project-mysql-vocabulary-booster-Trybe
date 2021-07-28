e25d922674385f4072bed168a6e63cd166d5ab02
hheloisa <hhackenhaar@gmail.com>
2021-07-28 17:10:46 -0300

    Req. 8 - Altera fonte do nome dos clientes

--- desafio8.sql
+++ desafio8.sql
@@ -1,5 +1,5 @@
 SELECT
-cc.CustomerName AS `Nome de contato`,
+cc.concatName AS `Nome de contato`,
 cs.ShipperName AS `Empresa que fez o envio`,
 co.OrderDate AS `Data do pedido`
 FROM w3schools.customers AS cc

8651b808be97f7c09ec2642547113fba5ce454fc
w-cypriano <profwellington17@gmail.com>
2021-07-28 17:21:58 -0300

    Inciando projeto- desafio1

---
+++ desafio1.sql
@@ -0,0 +1,6 @@
+SELECT C.COUNTRY_NAME AS País, 
+(SELECT IF(R.REGION_NAME = 'Europe', 'incluído', 'não incluído')) AS "Status Inclusão"
+FROM hr.countries AS C
+INNER JOIN hr.regions AS R
+ON C.REGION_ID = R.REGION_ID
+ORDER BY País;

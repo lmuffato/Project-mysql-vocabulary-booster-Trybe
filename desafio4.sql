107a42a478f909a8cb16b00f248f8eb80a321e1f
Orlando Flores <orlando_flores@id.uff.br>
2021-07-28 11:48:29 -0300

    fix: desafio 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,4 +1,4 @@
-SELECT t1.JOB_TITLE,
+SELECT t1.JOB_TITLE AS Cargo,
 ROUND(AVG(t2.SALARY), 2) AS `Média salarial`,
 CASE
 WHEN ROUND(AVG(t2.SALARY), 2) <= 5800 THEN 'Júnior'
@@ -9,4 +9,5 @@ END AS 'Senioridade'
 FROM hr.employees AS t2
 INNER JOIN hr.jobs AS t1 ON t1.JOB_ID=t2.JOB_ID
 GROUP BY t1.JOB_TITLE
-ORDER BY `Média salarial`;
+ORDER BY `Média salarial`, Cargo;
+

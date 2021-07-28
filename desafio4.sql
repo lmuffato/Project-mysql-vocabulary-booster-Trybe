68ee0ddb5917f5d6ffa5be414bb37a604b8d04a8
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2021-07-28 16:41:31 -0300

    Fix: error Code Climate

--- desafio4.sql
+++ desafio4.sql
@@ -2,16 +2,13 @@
 
 SELECT job.JOB_TITLE `Cargo`, ROUND(AVG(employee.SALARY), 2) `Média salarial`,
 CASE
-    WHEN ROUND(AVG(employee.SALARY), 2) >= 2000 AND ROUND(AVG(employee.SALARY), 2) <= 5800 THEN 'Júnior'
+  WHEN ROUND(AVG(employee.SALARY), 2) >= 2000 AND ROUND(AVG(employee.SALARY), 2) <= 5800 THEN 'Júnior'
 	WHEN ROUND(AVG(employee.SALARY), 2) >= 5801 AND ROUND(AVG(employee.SALARY), 2) <= 7500 THEN 'Pleno'
 	WHEN ROUND(AVG(employee.SALARY), 2) >= 7501 AND ROUND(AVG(employee.SALARY), 2) <= 10500 THEN 'Sênior'
-  ELSE 'CEO'
+ELSE 'CEO'
 END AS `Senioridade`
 FROM hr.jobs AS job
 INNER JOIN hr.employees AS employee
 ON job.JOB_ID = employee.JOB_ID
 GROUP BY `Cargo`
 ORDER BY `Média salarial` ASC, `Cargo` ASC;
-
-
-

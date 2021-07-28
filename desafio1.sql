a7dec1d319f35bd37e9b97648adb5d31746bf961
Rodrigo Queiroz <rodrigoandradequeiroz@gmail.com>
2021-07-28 10:39:17 -0300

    code climate!

--- desafio1.sql
+++ desafio1.sql
@@ -1,3 +1,2 @@
-SELECT country_name AS 'País',
-	IF (region_id = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
+SELECT country_name AS 'País', IF (region_id = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
 FROM hr.countries ORDER BY country_name;

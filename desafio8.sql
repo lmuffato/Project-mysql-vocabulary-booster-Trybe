954904cdbcdd52697f2be8b34cc32856ad7065ea
luanlsr <luanfswd@gmail.com>
2021-07-28 18:42:53 -0300

    requisito 8 finalizado

--- desafio8.sql
+++ desafio8.sql
@@ -7,4 +7,5 @@ INNER JOIN w3schools.orders AS o
 ON o.CustomerID = c.CustomerID
 INNER JOIN w3schools.shippers AS s
 ON s.ShipperID = o.ShipperID
+WHERE ShipperName = 'Speedy Express' OR ShipperName = 'United Package'
 ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;

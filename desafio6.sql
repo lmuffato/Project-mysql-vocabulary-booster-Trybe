0b81bb45ac0d159dd076aaa4f03a93a98de661b8
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-07-27 21:46:49 -0300

    reenvio do 6 com alteração não salva

--- desafio6.sql
+++ desafio6.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT_WS(FIRST_NAME, " ", LAST_NAME) AS "Nome completo", 
+SELECT CONCAT_WS(" ", FIRST_NAME, LAST_NAME) AS "Nome completo", 
 jobs.JOB_TITLE AS Cargo,
 DATE(START_DATE) AS "Data de início do cargo",
 department.DEPARTMENT_NAME AS "Departamento"
@@ -6,4 +6,4 @@ FROM hr.job_history AS `history`
 INNER JOIN hr.jobs AS jobs ON `history`.JOB_ID = jobs.JOB_ID
 INNER JOIN hr.employees AS employees ON employees.EMPLOYEE_ID = `history`.EMPLOYEE_ID
 INNER JOIN hr.departments AS department ON `history`.DEPARTMENT_ID = department.DEPARTMENT_ID
-ORDER BY CONCAT_WS(FIRST_NAME, " ", LAST_NAME) DESC, Cargo ASC;
+ORDER BY CONCAT_WS(" ", FIRST_NAME, LAST_NAME) DESC, Cargo ASC;

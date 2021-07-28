6a9ad416f9460ec19dcc7ffac575283ab88bed5a
João Nasc <oi@joaonasc.dev>
2021-07-28 20:44:29 -0300

    feat: challenge 10

---
+++ desafio10.sql
@@ -0,0 +1,15 @@
+SELECT
+  `products`.ProductName AS `Produto`,
+  MIN(`order_details`.Quantity) AS `Mínima`,
+  MAX(`order_details`.Quantity) AS `Máxima`,
+  ROUND(AVG(`order_details`.Quantity), 2) AS `Média`
+FROM
+  `w3schools`.products AS `products`
+  INNER JOIN `w3schools`.order_details AS `order_details` ON `order_details`.ProductID = `products`.ProductID
+GROUP BY
+  `products`.ProductID
+HAVING
+  ROUND(AVG(`order_details`.Quantity), 2) > 20
+ORDER BY
+  `Média` ASC,
+  `Produto` ASC;

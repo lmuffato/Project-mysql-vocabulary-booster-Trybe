4bfad09b0fb4e78fea94004a75b56b5c205893d8
Marina Alane <marina.alane@outlook.com>
2021-07-28 18:56:43 -0300

    fix req4 SCR

--- desafio4.sql
+++ desafio4.sql
@@ -1,14 +1,14 @@
-SELECT 
-    j.JOB_TITLE AS Cargo, ROUND(AVG(e.SALARY), 2) AS `Média Salarial`,
+SELECT j.JOB_TITLE AS Cargo,
+    ROUND(AVG(e.SALARY), 2) AS `Média salarial`,
     CASE
         WHEN AVG(e.SALARY) BETWEEN 2000 AND 5800 THEN 'Júnior'
         WHEN AVG(e.SALARY) BETWEEN 5801 AND 7500 THEN 'Pleno'
         WHEN AVG(e.SALARY) BETWEEN 7501 AND 10500 THEN 'Sênior'
         ELSE 'CEO'
-    END AS 'Senioridade'
+    END AS Senioridade
 FROM
-    hr.jobs AS j
+    hr.jobs j
         INNER JOIN
-    hr.employees AS e ON j.JOB_ID = e.JOB_ID
+    hr.employees e ON j.JOB_ID = e.JOB_ID
 GROUP BY e.JOB_ID
-ORDER BY `Média Salarial`, j.JOB_TITLE;
+ORDER BY `Média salarial` , Cargo ASC;

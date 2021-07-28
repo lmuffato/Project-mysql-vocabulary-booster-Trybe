ac88cc71e16b5302840f87e6d01f2b7438a9bf28
gabrieldeori <deori.pessoal@gmail.com>
2021-07-28 18:44:40 -0300

    desafio7 strange tabbing rule

--- desafio7.sql
+++ desafio7.sql
@@ -1,5 +1,4 @@
-SELECT 
-	UPPER(CONCAT(FIRST_NAME, ' ', LAST_NAME)) AS 'Nome completo',
+SELECT UPPER(CONCAT(FIRST_NAME, ' ', LAST_NAME)) AS 'Nome completo',
     START_DATE AS 'Data de início',
     SALARY AS 'Salário'
 FROM
@@ -7,4 +6,4 @@ FROM
         INNER JOIN
     hr.employees AS em ON jh.EMPLOYEE_ID = em.EMPLOYEE_ID
 HAVING MONTH(`Data de início`) IN (01 , 02, 03)
-ORDER BY `Nome completo` ASC , `Data de início` ASC;
+ORDER BY `Nome completo` ASC, `Data de início` ASC

778de3a6ec93aaf8cd200271ab1b900e57938de4
lucas-chave <lcchavesr150@gmail.com>
2021-07-28 16:02:44 -0300

    correcting challenge 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,3 +1,3 @@
 -- challenge 1;
 
-SELECT country_name AS 'País', IF (region_id = 1, 'incluído', 'não incluído') AS 'Status Inclusão' FROM hr.countries;
+SELECT country_name AS 'País', IF (region_id = 1, 'incluído', 'não incluído') AS 'Status Inclusão' FROM hr.countries ORDER BY country_name;

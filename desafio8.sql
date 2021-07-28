96b9c9d81681615d0c9c91adbe6a4f023319ef02
hheloisa <hhackenhaar@gmail.com>
2021-07-28 16:53:12 -0300

    Requisito 8 - Exibe todas as pessoas consumidoras cujos pedidos já foram enviados pelas empresas Speedy Express ou United Package

---
+++ desafio8.sql
@@ -0,0 +1,11 @@
+SELECT
+cc.CustomerName AS `Nome de contato`,
+cs.ShipperName AS `Empresa que fez o envio`,
+co.OrderDate AS `Data do pedido`
+FROM w3schools.customers AS cc
+INNER JOIN w3schools.shippers AS cs
+INNER JOIN w3schools.orders AS co
+ON ((cc.CustomerID = co.CustomerID)
+AND (co.ShipperID = cs.ShipperID))
+WHERE co.shipperID IN (1, 2)
+ORDER BY 1, 2, 3;

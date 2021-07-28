dd11098e37840dba72fc1394a16278b13a0b9254
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-07-28 12:53:25 -0400

    desafio 10 done

---
+++ desafio10.sql
@@ -0,0 +1,16 @@
+-- Exiba todos os produtos que já foram pedidos e que possuem uma média de quantidade nos pedidos registrados acima de 20.00.
+-- Use w3schools como referência
+-- Monte uma query que exiba 04 colunas:
+-- A primeira deve possuir o alias "Produto" e exibir o nome do produto.
+-- A segunda deve possuir o alias "Mínima" e exibir a quantidade mínima que já foi pedida desse produto.
+-- A terceira deve possuir o alias "Máxima" e exibir a quantidade máxima que já foi pedida desse produto.
+-- A quarta deve deve possuir o alias "Média" e exibir a média de quantidade nos pedidos deste produto, arredondada para duas casas decimais.
+-- Os resultados devem estar ordenados pela média de quantidade nos pedidos em ordem crescente.
+-- Em caso de empate na média, os resultados devem ser ordenados pelo nome do produto em ordem alfabética.
+
+SELECT p.ProductName AS 'Produto', MIN(od.Quantity) AS 'Mínima', MAX(od.Quantity) AS 'Máxima', ROUND(AVG(od.Quantity), 2) AS 'Média'
+FROM w3schools.products AS p
+INNER JOIN w3schools.order_details AS od
+ON p.ProductID = od.ProductID
+GROUP BY p.ProductName
+ORDER BY ROUND(AVG(od.Quantity), 2), p.ProductName;

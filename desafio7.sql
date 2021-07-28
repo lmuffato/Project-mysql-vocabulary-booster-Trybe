a77211b54305bcc7cb830bfd6ddb8a8bcba7bc4c
Marcus C. H. Filho <mchusemann@gmail.com>
2021-07-28 18:44:07 -0300

    feat: challenge 7

---
+++ desafio7.sql
@@ -0,0 +1,12 @@
+SELECT
+  UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
+  jh.START_DATE AS 'Data de início',
+  e.SALARY AS 'Salário'
+FROM
+  hr.employees e
+  INNER JOIN hr.job_history jh ON e.JOB_ID = jh.JOB_ID
+WHERE
+  MONTH(jh.START_DATE) IN (1, 2, 3)
+ORDER BY
+  `Nome completo`,
+  `Data de início`;

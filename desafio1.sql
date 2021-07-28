5f7d0897e36216407b751a8f198a1df4d95aa00e
Orlando Flores <orlando_flores@id.uff.br>
2021-07-28 11:13:53 -0300

    fix: desafio 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,4 +1,4 @@
-SELECT t1.COUNTRY_NAME AS País, IF(t2.REGION_NAME = 'EUROPE', 'incluído', 'não incluido') AS  'Status Inclusão'
+SELECT t1.COUNTRY_NAME AS País, IF(t2.REGION_NAME = 'Europe', 'incluído', 'não incluído') AS  'Status Inclusão'
 FROM hr.countries AS t1
 INNER JOIN	hr.regions AS t2
 ON t1.REGION_ID=t2.REGION_ID

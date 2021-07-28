3a1dcaa371ecc167bfb8c094a4fe2f2271bfaa75
João Nasc <oi@joaonasc.dev>
2021-07-28 20:27:15 -0300

    feat: challenge 9

---
+++ desafio9.sql
@@ -0,0 +1,10 @@
+SELECT
+  CONCAT(`employees`.FirstName, ' ', `employees`.LastName) AS `Nome completo`,
+  COUNT(`orders`.`EmployeeID`) AS `Total de pedidos`
+FROM
+  `w3schools`.employees AS `employees`
+  INNER JOIN `w3schools`.orders AS `orders` ON `orders`.EmployeeID = `employees`.EmployeeID
+GROUP BY
+  `employees`.EmployeeID
+ORDER BY
+  `Total de pedidos` ASC;

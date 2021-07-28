2dca27c58859757f42a6eab25dd98e8c41f7aa2b
Mateus <mtauws@gmail.com>
2021-07-28 14:53:33 -0300

    chore: requirement 8

---
+++ desafio8.sql
@@ -0,0 +1,15 @@
+SELECT
+  c.ContactName AS 'Nome de contato',
+  s.ShipperName AS 'Empresa que fez o envio',
+  o.OrderDate AS 'Data do pedido'
+FROM
+  w3schools.orders AS o
+  INNER JOIN w3schools.customers AS c ON o.CustomerID = c.CustomerID
+  INNER JOIN w3schools.shippers AS s ON o.ShipperID = s.ShipperID
+WHERE
+  o.ShipperID = 1
+  OR o.ShipperID = 2
+ORDER BY
+  c.ContactName ASC,
+  s.ShipperName ASC,
+  o.OrderDate ASC;

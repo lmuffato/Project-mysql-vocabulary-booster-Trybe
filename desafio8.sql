0a5e99442fe16a4b92192c2350ab132901dbf1a4
Maria Baeta <m.martinsdg@gmail.com>
2021-07-28 20:39:52 -0300

    Lint adequacy

--- desafio8.sql
+++ desafio8.sql
@@ -1,11 +1,14 @@
-SELECT c.ContactName AS 'Nome de contato' ,
-CASE 
-	WHEN od.ShipperID = 1 THEN 'Speedy Express'
-    WHEN od.ShipperID = 2 THEN 'United Package'
-END AS 'Empresa que fez o envio',
-od.OrderDate AS 'Data do pedido'
-FROM w3schools.customers AS c
-INNER JOIN w3schools.orders AS od
-ON c.CustomerID = od.CustomerID
-WHERE od.ShipperID = 1 OR od.ShipperID = 2
-ORDER BY 1, 2, 3;
+SELECT 
+    c.ContactName AS 'Nome de contato',
+    CASE
+        WHEN od.ShipperID = 1 THEN 'Speedy Express'
+        WHEN od.ShipperID = 2 THEN 'United Package'
+    END AS 'Empresa que fez o envio',
+    od.OrderDate AS 'Data do pedido'
+FROM
+    w3schools.customers AS c
+        INNER JOIN
+    w3schools.orders AS od ON c.CustomerID = od.CustomerID
+WHERE
+    od.ShipperID = 1 OR od.ShipperID = 2
+ORDER BY 1 , 2 , 3;

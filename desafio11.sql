831dd205da3a2f67568ff30e78a36d9f8c6a2892
Hernanne <hernannedomiciano@gmail.com>
2021-07-28 15:44:52 -0300

    fix: req11

--- desafio11.sql
+++ desafio11.sql
@@ -1,6 +1,6 @@
 SELECT T1.contactName AS `Nome`,
 T1.country AS `País`,
-COUNT(T1.customerID) AS `Número de compatriotas`
+COUNT(T1.contactName) AS `Número de compatriotas`
 FROM w3schools.customers AS T1, w3schools.customers AS T2
 WHERE T1.country = T2.country
 GROUP BY T1.customerID

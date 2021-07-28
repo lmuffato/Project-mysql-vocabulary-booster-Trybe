f084e855cdf7e6efb8d44d1ac43f2e79486da978
hheloisa <hhackenhaar@gmail.com>
2021-07-28 15:49:29 -0300

    Corrige Media Salarial para Media salarial

--- desafio4.sql
+++ desafio4.sql
@@ -1,5 +1,5 @@
 SELECT cj.job_title AS `Cargo`,
-ROUND(AVG(ce.salary), 2) AS `Média Salarial`,
+ROUND(AVG(ce.salary), 2) AS `Média salarial`,
 CASE
 WHEN ROUND(AVG(ce.salary), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
 WHEN ROUND(AVG(ce.salary), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'

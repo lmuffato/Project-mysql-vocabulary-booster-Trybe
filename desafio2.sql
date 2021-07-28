68aa4544ccfe1804ce39e1394b15931c15fe8d5b
Ana Ventura <anaventura1811@gmail.com>
2021-07-27 21:06:12 -0300

    fix: correções codeclimate

--- desafio2.sql
+++ desafio2.sql
@@ -1,10 +1,10 @@
 -- 2. Exiba os cargos com seu nível de renumeração associado, com base no salário máximo do cargo
 SELECT job_title AS 'Cargo',
 CASE
-	WHEN max_salary>=5000 AND max_salary<=10000 THEN 'Baixo'
-    WHEN max_salary>=10001 AND max_salary<=20000 THEN 'Médio'
-    WHEN max_salary>=20001 AND max_salary<=30000 THEN 'Alto'
-    ELSE 'Altíssimo'
+WHEN max_salary>=5000 AND max_salary<=10000 THEN 'Baixo'
+WHEN max_salary>=10001 AND max_salary<=20000 THEN 'Médio'
+WHEN max_salary>=20001 AND max_salary<=30000 THEN 'Alto'
+ELSE 'Altíssimo'
 END AS 'Nível'
 FROM hr.jobs
 ORDER BY job_title ASC;
--- desafio20.sql
+++ desafio20.sql
@@ -6,21 +6,21 @@ DELIMITER $$
 
 CREATE PROCEDURE exibir_historico_completo_por_funcionario(email varchar(25))
 BEGIN
-	SELECT CONCAT(e.first_name, " ", e.last_name)
-    AS "Nome completo",
-    d.department_name
-    AS "Departamento",
-    j.job_title
-    AS "Cargo"
-    FROM job_history AS jh
-    INNER JOIN departments AS d
-    INNER JOIN jobs AS j
-    INNER JOIN employees AS e
+SELECT CONCAT(e.first_name, " ", e.last_name)
+AS "Nome completo",
+d.department_name
+AS "Departamento",
+j.job_title
+AS "Cargo"
+FROM job_history AS jh
+INNER JOIN departments AS d
+INNER JOIN jobs AS j
+INNER JOIN employees AS e
 ON (j.job_id = jh.job_id
-    AND d.department_ID = jh.department_ID
-    AND e.employee_ID = jh.employee_ID
-    AND e.email LIKE CONCAT('%', email, '%')
-    )
-    ORDER BY jh.department_ID, j.job_title;
+  AND d.department_ID = jh.department_ID
+  AND e.employee_ID = jh.employee_ID
+  AND e.email LIKE CONCAT('%', email, '%')
+)
+ORDER BY jh.department_ID, j.job_title;
 END $$
 DELIMITER ;

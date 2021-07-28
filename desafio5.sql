7693b3832c0fe69ef7db90b82586ea192a54ce7d
lucas-paz <lucas.hpaz@outlook.com>
2021-07-28 16:30:45 -0300

    fixes

--- desafio5.sql
+++ desafio5.sql
@@ -1,5 +1,5 @@
 SELECT
-	J.JOB_TITLE AS Cargo,
+	  J.JOB_TITLE AS Cargo,
     (J.MAX_SALARY - J.MIN_SALARY) AS 'Variação Salarial',
     ROUND((J.MIN_SALARY / 12), 2) AS 'Média mínima mensal',
     ROUND((J.MAX_SALARY / 12), 2) AS 'Média máxima mensal'

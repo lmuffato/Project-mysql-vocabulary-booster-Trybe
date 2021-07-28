c6f51f173c5c587875b4cdbc5c003f1264eeed1e
Eduardo Seije <eduardoseije@gmail.com>
2021-07-28 17:39:09 -0300

    Desafio 06 - Refatorado - utilizando o job_history na condição

--- desafio6.sql
+++ desafio6.sql
@@ -1,13 +1,14 @@
-SELECT CONCAT(e.first_name," ", e.last_name)
-AS 'Nome completo',
+SELECT CONCAT(e.first_name," ", e.last_name) AS 'Nome completo',
 j.job_title AS 'Cargo',
 jh.start_date AS 'Data de início do cargo',
 d.department_name AS 'Departamento'
 FROM hr.employees AS e
-INNER JOIN hr.jobs AS j
-ON j.job_id = e.job_id
-INNER JOIN hr.departments AS d
-ON d.department_id = e.department_id
-INNER JOIN hr.job_history as jh
-ON jh.employee_id = e.employee_id
+INNER JOIN 
+hr.jobs AS j
+INNER JOIN 
+hr.departments AS d
+INNER JOIN 
+hr.job_history AS jh
+ON ((d.department_id = jh.department_id)
+AND (j.JOB_ID = jh.JOB_ID) AND (jh.employee_id = e.employee_id))
 ORDER BY CONCAT(e.first_name," ", e.last_name) DESC, j.job_title ASC;

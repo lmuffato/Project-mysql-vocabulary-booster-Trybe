da304151f80edadf2726e6fcf094f93662b0b815
maricegalla <maricegalla@gmail.com>
2021-07-28 12:43:40 -0300

    Desafio 5

--- desafio1.sql
+++ desafio1.sql
@@ -1,4 +1,4 @@
-SELECT COUNTRY_NAME AS "País",
-IF (REGION_ID = 1, "incluído", "não incluído") AS "Status Inclusão" 
+SELECT COUNTRY_NAME AS 'País',
+IF (REGION_ID = 1, 'incluído', 'não incluído') AS 'Status Inclusão' 
 FROM hr.countries
 ORDER BY COUNTRY_NAME ASC;

858e04b89f5020b6b81239f21add7415e037a17b
lucas-paz <lucas.hpaz@outlook.com>
2021-07-28 16:27:58 -0300

    req 1 ao 5

--- desafio1.sql
+++ desafio1.sql
@@ -1,8 +1,10 @@
 SELECT 
     C.COUNTRY_NAME AS País,
-    IF(C.REGION_ID = 1,
+    IF(R.REGION_NAME = 'Europe',
         'incluído',
-        'não incluído') AS Status Inclusão
+        'não incluído') AS `Status Inclusão`
 FROM
     hr.countries AS C
+    INNER JOIN hr.regions AS R
+    ON C.REGION_ID = R.REGION_ID
 ORDER BY C.COUNTRY_NAME;
---
+++ desafio5.sql
@@ -0,0 +1,7 @@
+SELECT
+	J.JOB_TITLE AS Cargo,
+    (J.MAX_SALARY - J.MIN_SALARY) AS 'Variação Salarial',
+    ROUND((J.MIN_SALARY / 12), 2) AS 'Média mínima mensal',
+    ROUND((J.MAX_SALARY / 12), 2) AS 'Média máxima mensal'
+FROM hr.jobs AS J
+ORDER BY `Variação Salarial`, Cargo;

741585361bb0f5893121d2a7d054dbec4b56104b
João Nasc <oi@joaonasc.dev>
2021-07-28 19:44:23 -0300

    feat: challenge 6

---
+++ desafio6.sql
@@ -0,0 +1,17 @@
+SELECT
+  CONCAT(
+    `employees`.FIRST_NAME,
+    ' ',
+    `employees`.LAST_NAME
+  ) AS `Nome completo`,
+  `jobs`.JOB_TITLE AS `Cargo`,
+  `job_history`.START_DATE AS `Data de início do cargo`,
+  `departments`.DEPARTMENT_NAME AS `Departamento`
+FROM
+  `hr`.employees AS `employees`
+  INNER JOIN `hr`.jobs AS `jobs` ON `jobs`.JOB_ID = `employees`.JOB_ID
+  INNER JOIN `hr`.job_history AS `job_history` ON `job_history`.EMPLOYEE_ID = `employees`.EMPLOYEE_ID
+  INNER JOIN `hr`.departments AS `departments` ON `departments`.DEPARTMENT_ID = `employees`.DEPARTMENT_ID
+ORDER BY
+  `Nome completo` DESC,
+  `Cargo` ASC;

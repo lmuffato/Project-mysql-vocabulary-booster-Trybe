018949552b1a4e6961d1c10db4731053ed9be043
hheloisa <hhackenhaar@gmail.com>
2021-07-28 15:29:21 -0300

    Requisito 4 - Exiba a média salarial e o nível de senioridade de todas as pessoas empregadas, agrupadas pelo cargo

---
+++ desafio4.sql
@@ -0,0 +1,13 @@
+SELECT cj.job_title AS `Cargo`,
+ROUND(AVG(ce.salary), 2) AS `Média Salarial`,
+CASE
+WHEN ROUND(AVG(ce.salary), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
+WHEN ROUND(AVG(ce.salary), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
+WHEN ROUND(AVG(ce.salary), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
+WHEN ROUND(AVG(ce.salary), 2) > 10500 THEN 'CEO'
+END AS `Senioridade`
+FROM hr.jobs AS cj
+INNER JOIN hr.employees AS ce
+ON cj.job_id = ce.job_id
+GROUP BY ce.job_id
+ORDER BY `Média Salarial` ASC, cj.job_title ASC;

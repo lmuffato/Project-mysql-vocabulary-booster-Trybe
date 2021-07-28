eb1c43a26c6f7b4b495a1057a4699986d8bdd0aa
guilhermemd <guilherme.md@gmail.com>
2021-07-28 13:55:02 -0300

    fix: :bug: req 04

--- desafio4.sql
+++ desafio4.sql
@@ -7,6 +7,6 @@ WHEN AVG(e.SALARY) BETWEEN 7501 AND 10500 THEN 'Sênior'
 WHEN AVG(e.SALARY) > 10500 THEN 'CEO'
 END AS 'Senioridade'
 FROM hr.jobs AS j
-	INNER JOIN hr.employees AS e ON j.JOB_ID = e.JOB_ID
+INNER JOIN hr.employees AS e ON j.JOB_ID = e.JOB_ID
 GROUP BY Cargo
 ORDER BY Senioridade;

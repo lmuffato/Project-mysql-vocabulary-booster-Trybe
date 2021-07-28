5fa5faa46316f4fe8fab0820e0b45f9a70ea9eb6
Pedro Ramos <pedro.ramos@betrybe.com>
2021-07-28 10:44:19 -0300

    FIX: Fix code climate

--- desafio5.sql
+++ desafio5.sql
@@ -1,10 +1,10 @@
 SELECT
-	j.JOB_TITLE AS `Cargo`,
+    j.JOB_TITLE AS `Cargo`,
     ROUND((j.MAX_SALARY) - (j.MIN_SALARY), 2) AS `Variação Salarial`,
     ROUND((j.MIN_SALARY/12), 2) AS `Média mínima mensal`,
     ROUND((j.MAX_SALARY/12), 2) AS `Média máxima mensal`
 FROM
-	jobs AS j
+    jobs AS j
 ORDER BY
-	`Variação Salarial` ASC,
+    `Variação Salarial` ASC,
     j.JOB_TITLE ASC;

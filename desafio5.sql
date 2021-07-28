e5574eb9b3153742573ca9570c123445ec988c29
Alexandre Oliveira <alexandremolive@gmail.com>
2021-07-28 16:25:35 -0300

    fix req 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,10 +1,8 @@
 SELECT  
-J.JOB_TITLE  AS `Cargo`,
-J.MAX_SALARY - J.MIN_SALARY AS `Variação Salarial`,
-ROUND(AVG(J.MIN_SALARY DIV 12), 2 ) AS `Média mínima mensal`,
-ROUND(AVG(J.MAX_SALARY DIV 12), 2 ) AS `Média máxima mensal`
-FROM hr.employees AS A
-INNER JOIN hr.jobs AS J
-ON A.JOB_ID = J.JOB_ID
+JOB_TITLE  AS `Cargo`,
+MAX_SALARY - MIN_SALARY AS `Variação Salarial`,
+ROUND(AVG(MIN_SALARY DIV 12), 2 ) AS `Média mínima mensal`,
+ROUND(AVG(MAX_SALARY DIV 12), 2 ) AS `Média máxima mensal`
+FROM hr.jobs
 GROUP BY `Cargo`
 ORDER BY `Variação Salarial` ASC, `Cargo` ASC;

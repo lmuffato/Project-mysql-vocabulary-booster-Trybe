ba2c9c661203bf8797c322549aa329587d37c555
Orlando Flores <orlando_flores@id.uff.br>
2021-07-28 15:20:53 -0300

    feat: desafio 10

---
+++ desafio10.sql
@@ -0,0 +1,9 @@
+SELECT p.ProductName AS Produto,
+MIN(od.Quantity) AS Mínima,
+MAX(od.Quantity) AS Máxima,
+ROUND(AVG(od.Quantity), 2) AS Média
+FROM w3schools.order_details AS od
+INNER JOIN w3schools.products AS p ON p.ProductID=od.ProductID
+GROUP BY Produto
+HAVING Média > 20
+ORDER BY Média, Produto;

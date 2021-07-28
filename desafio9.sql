0dd65dbaaef6b40ff9b4cbc8f332c8a5d6da65e6
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-07-28 06:10:02 +0000

    feat: desafio 9

---
+++ desafio9.sql
@@ -0,0 +1,9 @@
+# 9 - Exibe todos as pessoas funcionárias que já realizaram algum pedido, mostrando também seu total de pedidos feitos
+SELECT
+    CONCAT(e.FirstName, ' ', e.LastName) AS `Nome completo`,
+    COUNT(*) AS `Total de pedidos`
+FROM
+    w3schools.orders o
+        INNER JOIN
+    w3schools.employees e ON e.EmployeeID = o.EmployeeID
+GROUP BY o.EmployeeID;

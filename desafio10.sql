2504c9d77b2e68ee05b3936bcf69d0fa5f3be3fd
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-07-28 06:32:25 +0000

    feat: desafio 10

---
+++ desafio10.sql
@@ -0,0 +1,13 @@
+# 10 - Exibe todos os produtos que já foram pedidos, que possuem uma média de quantidade nos pedidos registrados acima de 20.00
+SELECT
+    p.ProductName AS Produto,
+    MIN(od.Quantity) AS Mínima,
+    MAX(od.Quantity) AS Máxima,
+    ROUND(AVG(od.Quantity), 2) AS Média
+FROM
+    w3schools.products p
+        INNER JOIN
+    w3schools.order_details od ON p.ProductID = od.ProductID
+GROUP BY p.ProductName
+HAVING Média > 20
+ORDER BY Média , Produto;

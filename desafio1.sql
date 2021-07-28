e8836fe5a5764b05a9a175228e96bdf5c66fbf11
Mateus Santiago <mateussantiago.victor@gmail.com>
2021-07-28 14:03:58 -0300

    Rodando o lint req1

--- desafio1.sql
+++ desafio1.sql
@@ -1,6 +1,4 @@
 SELECT COUNTRY_NAME AS 'País',
-    IF(REGION_ID > 1,
-		'não incluído',
-		'incluído') AS 'Status Inclusão'
+    IF(REGION_ID > 1, 'não incluído', 'incluído') AS 'Status Inclusão'
 FROM hr.countries
 ORDER BY COUNTRY_NAME;

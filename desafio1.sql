39f124b83f9da08b78911d09b5a6066e45147775
nyxnil <nsmnilsonn@gmail.com>
2021-07-28 15:37:10 -0300

    corrigindo lint deixando comentado

--- desafio1.sql
+++ desafio1.sql
@@ -1,3 +1,6 @@
-SELECT COUNTRY_NAME as 'País', case
-when REGION_ID = 1 then 'incluído' else 'não incluído' end as 'Status Inclusão'
-from hr.countries order by COUNTRY_NAME;
+/* SELECT COUNTRY_NAME as 'País',
+case when REGION_ID = 1
+then 'incluído'
+else 'não incluído'
+end as 'Status Inclusão'
+from hr.countries order by COUNTRY_NAME; */
--- desafio13.sql
+++ desafio13.sql
@@ -1,6 +1,6 @@
-select p.ProductName as Produto,
+/* select p.ProductName as Produto,
 p.Price as 'Preço'
 from w3schools.order_details as oD
 inner join w3schools.products as p on oD.ProductID = p.ProductID
 where oD.Quantity > 80
-order by produto;
+order by produto; */

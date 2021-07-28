becbb6d9af73aa1bcbc423f1dbd9f500d998ad33
Rodrigo Rech <rodrigorech92@gmail.com>
2021-07-28 18:16:39 -0300

    Feat: Report all employes who have done and order showing total of orders

--- desafio9.sql
+++ desafio9.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT(employees.FirstName, ' ',employees.LastName) AS `Nome Completo`,
+SELECT CONCAT(employees.FirstName, ' ',employees.LastName) AS `Nome completo`,
 COUNT(orders.OrderID) AS `Total de pedidos`
 FROM w3schools.orders AS orders
 INNER JOIN w3schools.employees AS employees ON employees.EmployeeID = orders.EmployeeId

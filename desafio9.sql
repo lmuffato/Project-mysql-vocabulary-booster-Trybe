75b5ee007852433dcccdb723886724e405900738
Flavio Franco <flavio.win@gmail.com>
2021-07-28 17:55:55 -0300

    requisito 9 - nao passa no avaliador

--- desafio9.sql
+++ desafio9.sql
@@ -1,9 +1,9 @@
 SELECT
   CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome completo',
-  COUNT(e.FirstName) AS 'Total de pedidos'
+  COUNT(o.EmployeeID) AS 'Total de pedidos'
 FROM
   w3schools.employees e
 INNER JOIN
   w3schools.orders o ON e.EmployeeID = o.EmployeeID
-GROUP BY e.FirstName
+GROUP BY 1
 ORDER BY 2;

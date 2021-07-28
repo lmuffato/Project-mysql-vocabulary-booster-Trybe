10e4fd197734a1d9c1fbe242c88add400fa468cd
Tiago Santos <tiago881@gmail.com>
2021-07-28 11:29:54 -0300

    to fix

--- desafio3.sql
+++ desafio3.sql
@@ -1,6 +1,3 @@
-SELECT
-    job_title AS Cargo,
-	max_salary - min_salary AS 'Diferença entre salários máximo e mínimo'
-    FROM jobs
-    ORDER BY max_salary - min_salary ASC, job_title ASC;
-    
+SELECT job_title AS Cargo, max_salary - min_salary AS 'Diferença entre salários máximo e mínimo'
+FROM jobs
+ORDER BY max_salary - min_salary ASC, job_title ASC;
--- desafio4.sql
+++ desafio4.sql
@@ -1,14 +1,14 @@
-SELECT
-     j.job_title AS Cargo, ROUND(AVG(e.salary), 2) AS 'Média Salarial',
-	 CASE
-        WHEN ROUND(AVG(e.salary), 2) >= 2000 AND ROUND(AVG(e.salary), 2) <= 5800 THEN 'Júnior'
-        WHEN ROUND(AVG(e.salary), 2) >= 5801 AND ROUND(AVG(e.salary), 2) <= 7500 THEN 'Pleno'
-		WHEN ROUND(AVG(e.salary), 2) >= 7501 AND ROUND(AVG(e.salary), 2) <= 10500 THEN 'Sênior'
-		WHEN ROUND(AVG(e.salary), 2) > 10500 THEN 'CEO'
-        ELSE ''
-    END AS Senioridade
-FROM jobs AS j
-INNER JOIN employees AS e
-ON e.job_id = j.job_id
+SELECT j.job_title AS Cargo, ROUND(AVG(e.salary), 2) AS 'Média Salarial',
+CASE
+  WHEN ROUND(AVG(e.salary), 2) >= 2000
+  AND ROUND(AVG(e.salary), 2) <= 5800 THEN 'Júnior'
+  WHEN ROUND(AVG(e.salary), 2) >= 5801
+  AND ROUND(AVG(e.salary), 2) <= 7500 THEN 'Pleno'
+  WHEN ROUND(AVG(e.salary), 2) >= 7501
+  AND ROUND(AVG(e.salary), 2) <= 10500 THEN 'Sênior'
+  WHEN ROUND(AVG(e.salary), 2) > 10500 THEN 'CEO'
+  ELSE ''
+END AS Senioridade
+FROM jobs AS j INNER JOIN employees AS e ON e.job_id = j.job_id
 GROUP BY j.job_title
 ORDER BY ROUND(AVG(e.salary), 2) ASC, j.job_title ASC;

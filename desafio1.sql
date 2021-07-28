8c1d0c016cbddf379a9d5fed401ba1eac1ea58eb
AndreBarroso <andre_barroso17@hotmail.com>
2021-07-28 18:03:36 -0300

    feat: desafio4

--- desafio1.sql
+++ desafio1.sql
@@ -2,8 +2,8 @@ USE hr;
 
 SELECT COUNTRY_NAME AS 'País',
 CASE 
-	WHEN REGION_ID = 1 THEN 'incluído'
-    ELSE 'não classificado'
+WHEN REGION_ID = 1 THEN 'incluído'
+ELSE 'não classificado'
 END AS 'Status Inclusão'
 FROM countries
 ORDER BY COUNTRY_NAME;
---
+++ desafio4.sql
@@ -0,0 +1,13 @@
+USE hr;
+SELECT j.JOB_TITLE AS 'Cargo', ROUND(AVG(e.SALARY), 2) AS 'Média salarial',
+CASE 
+WHEN 'Média salarial' BETWEEN 2000 AND 5800 THEN 'Júnior'
+WHEN 'Média salarial' BETWEEN 5801 AND 7500 THEN 'Pleno'
+WHEN 'Média salarial' BETWEEN 7501 AND 10500 THEN 'Sênior'
+WHEN 'Média salarial' > 10500 THEN 'CEO'
+END AS 'Senioridade'
+FROM employees AS e
+INNER JOIN jobs AS j
+ON j.JOB_ID = e.JOB_ID
+GROUP BY j.JOB_TITLE
+ORDER BY `Média salarial` ASC;

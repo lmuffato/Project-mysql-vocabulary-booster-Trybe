53ecd6b40c0edbdc2d84d725ec41bf59258246b7
Antônio Schappo <antonioschappo@gmail.com>
2021-07-28 14:04:25 -0300

    Second try challenge 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,7 +1,7 @@
-SELECT t1.JOB_TITLE AS Cargo,
-ROUND((t1.MAX_SALARY - t1.MIN_SALARY), 2) AS 'Variação Salarial',
-ROUND((t1.MIN_SALARY / 12), 2) AS 'Média mínima mensal',
-ROUND((t1.MAX_SALARY / 12), 2) AS 'Média máxima mensal'
-FROM hr.jobs AS t1
-ORDER BY ROUND((t1.MAX_SALARY - t1.MIN_SALARY), 2),
+SELECT JOB_TITLE AS Cargo,
+(MAX_SALARY - MIN_SALARY) AS 'Variação Salarial',
+ROUND((MIN_SALARY / 12), 2) AS 'Média mínima mensal',
+ROUND((MAX_SALARY / 12), 2) AS 'Média máxima mensal'
+FROM hr.jobs
+ORDER BY (MAX_SALARY - MIN_SALARY) ASC,
 Cargo;

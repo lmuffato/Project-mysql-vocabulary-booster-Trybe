cb534e9e3f40f87df9519eb6c2cd7b5b24362c8e
gabrieldeori <deori.pessoal@gmail.com>
2021-07-28 19:14:31 -0300

    Pre-Correcting possible codeclimate errors

--- desafio9.sql
+++ desafio9.sql
@@ -1,13 +1,9 @@
-SELECT
-	concat(emp.FirstName, ' ', emp.LastName) AS 'Nome completo',
-	count(ods.OrderID) AS 'Total de pedidos'
+SELECT 
+    CONCAT(emp.FirstName, ' ', emp.LastName) AS 'Nome completo',
+    COUNT(ods.OrderID) AS 'Total de pedidos'
 FROM
-	w3schools.employees AS emp
-INNER JOIN
-	w3schools.orders AS ods
-ON
-	emp.EmployeeID = ods.EmployeeID
-GROUP BY
-	emp.EmployeeID
-ORDER BY
-	`Total de pedidos`;
+    w3schools.employees AS emp
+        INNER JOIN
+    w3schools.orders AS ods ON emp.EmployeeID = ods.EmployeeID
+GROUP BY emp.EmployeeID
+ORDER BY `Total de pedidos`;

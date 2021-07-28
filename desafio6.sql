1f7c42a8978831cee8e2f6bf9657b8d05abb7108
Ana Ventura <anaventura1811@gmail.com>
2021-07-27 21:24:27 -0300

    fix: corrige novamente req. 6

--- desafio6.sql
+++ desafio6.sql
@@ -8,16 +8,10 @@ jh.start_date
 AS 'Data de início do cargo',
 d.department_name
 AS 'Departamento'
-FROM
-hr.employees
-AS e
-INNER JOIN hr.jobs
-AS j
-ON j.job_id = jh.job_id
-INNER JOIN hr.departments
-AS d
-ON d.department_id = jh.department_id
-INNER JOIN hr.job_history
-as jh
-ON jh.employee_id = e.employee_id
+FROM hr.employees AS e
+INNER JOIN hr.jobs AS j
+INNER JOIN hr.departments AS d
+INNER JOIN hr.job_history AS jh
+ON ((d.department_id = jh.department_id)
+AND (j.JOB_ID = jh.JOB_ID) AND (jh.employee_id = e.employee_id))
 ORDER BY CONCAT(e.first_name," ", e.last_name) DESC, j.job_title ASC;

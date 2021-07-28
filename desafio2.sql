82813a85b9c30d26f5f7829ee1ce60139525d808
Marina Alane <marina.alane@outlook.com>
2021-07-28 13:43:39 -0300

    desafio 1 e 2

---
+++ desafio2.sql
@@ -0,0 +1,9 @@
+SELECT job_title AS 'Cargo',
+CASE
+WHEN MAX_SALARY <= 10000 THEN 'Baixo'
+WHEN MAX_SALARY <= 20000 THEN 'Médio'
+WHEN MAX_SALARY <= 30000 THEN 'Alto'
+ELSE 'Altíssimo'
+END AS Nível
+FROM hr.jobs
+ORDER BY 'Cargo';

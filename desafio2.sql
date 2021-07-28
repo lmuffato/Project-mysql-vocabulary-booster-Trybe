49ad9ed43298dada224ae1f74e45534683b725e1
rogerio-lambert <rogerio.cl@gmail.com>
2021-07-28 17:24:52 -0300

    correção nas tabulações

--- desafio2.sql
+++ desafio2.sql
@@ -1,11 +1,11 @@
 SELECT 
-	JOB_TITLE AS 'Cargo',
-	CASE
-        WHEN MAX_SALARY <= 10000 THEN 'Baixo'
-        WHEN MAX_SALARY <= 20000 THEN 'Médio'
-		WHEN MAX_SALARY <= 30000 THEN 'Alto'
-		WHEN MAX_SALARY > 30000 THEN 'Altíssimo'
-        ELSE 'erro de leitura'
-    END AS 'Nível'
+  JOB_TITLE AS 'Cargo',
+  CASE
+    WHEN MAX_SALARY <= 10000 THEN 'Baixo'
+    WHEN MAX_SALARY <= 20000 THEN 'Médio'
+    WHEN MAX_SALARY <= 30000 THEN 'Alto'
+    WHEN MAX_SALARY > 30000 THEN 'Altíssimo'
+    ELSE 'erro de leitura'
+  END AS 'Nível'
 FROM hr.jobs
 ORDER BY JOB_TITLE ASC;

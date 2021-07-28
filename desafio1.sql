a5bf3dd69bbc5732a0b0855d2418365e41d5a700
hheloisa <hhackenhaar@gmail.com>
2021-07-27 22:27:44 -0300

    teste no req. 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,5 +1,4 @@
-SELECT * FROM hr.countries;
 SELECT country_name AS `País`,
 IF (region_id = 1, 'incluído', 'não incluído') AS `Status Inclusão`
-FROM hr.countries
-ORDER BY country_name ASC;
+FROM countries
+ORDER BY country_name;

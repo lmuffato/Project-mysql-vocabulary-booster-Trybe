9abe5133f501e5577f4c8bf5f76606cd006d5f29
hheloisa <hhackenhaar@gmail.com>
2021-07-28 15:47:47 -0300

    Requisito 5 - Exiba os cargos com sua variaçao salarial e suas medias maxima e minima mensal, considerando salarios maximo e minimo como anuais

---
+++ desafio5.sql
@@ -0,0 +1,6 @@
+SELECT job_title AS `Cargo`,
+(max_salary - min_salary) AS `Variação Salarial`,
+ROUND((min_salary/12), 2) AS `Média mínima mensal`,
+ROUND((max_salary/12), 2) AS `Média maxima mensal`
+FROM hr.jobs
+ORDER BY (max_salary - min_salary) ASC, job_title ASC;

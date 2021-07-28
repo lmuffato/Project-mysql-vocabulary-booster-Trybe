c7d40e3525aee215d51fc26bf2909d5b9c99a12c
Debora Passos <deborinhapassos@gmail.com>
2021-07-28 11:24:25 -0300

    codeclimate

--- desafio4.sql
+++ desafio4.sql
@@ -1,10 +1,10 @@
 SELECT
     j.job_title AS Cargo, ROUND(AVG(e.salary), 2) AS 'Média salarial',
-	CASE
-    WHEN ROUND(AVG(e.salary), 2) >= 2000 AND ROUND(AVG(e.salary), 2) <= 5800 THEN 'Júnior'
-    WHEN ROUND(AVG(e.salary), 2) >= 5801 AND ROUND(AVG(e.salary), 2) <= 7500 THEN 'Pleno'
-		WHEN ROUND(AVG(e.salary), 2) >= 7501 AND ROUND(AVG(e.salary), 2) <= 10500 THEN 'Sênior'
-		WHEN ROUND(AVG(e.salary), 2) > 10500 THEN 'CEO'
+CASE
+  WHEN ROUND(AVG(e.salary), 2) >= 2000 AND ROUND(AVG(e.salary), 2) <= 5800 THEN 'Júnior'
+  WHEN ROUND(AVG(e.salary), 2) >= 5801 AND ROUND(AVG(e.salary), 2) <= 7500 THEN 'Pleno'
+  WHEN ROUND(AVG(e.salary), 2) >= 7501 AND ROUND(AVG(e.salary), 2) <= 10500 THEN 'Sênior'
+  WHEN ROUND(AVG(e.salary), 2) > 10500 THEN 'CEO'
     ELSE ''
     END AS Senioridade
 FROM jobs AS j

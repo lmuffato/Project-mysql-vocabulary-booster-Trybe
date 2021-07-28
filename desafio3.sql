c670e062885aa5e1909a8aaf3ae0638b9d297164
lucas-chave <lcchavesr150@gmail.com>
2021-07-28 16:41:46 -0300

    challenge 3

---
+++ desafio3.sql
@@ -0,0 +1,5 @@
+-- challenge 3;
+
+SELECT job_title AS 'Cargo', max_salary - min_salary AS 'Diferença entre salários máximo e mínimo'
+FROM hr.jobs
+ORDER BY 'Diferença entre salários máximo e mínimo' OR job_title;

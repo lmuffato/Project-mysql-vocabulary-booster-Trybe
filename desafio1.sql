e073084f126a35f86e185c3545c3b0837bf30310
João Nasc <oi@joaonasc.dev>
2021-07-28 19:20:55 -0300

    refactor: challenge 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,12 +1,12 @@
 SELECT
-  countries.COUNTRY_NAME AS `País`,
+  `COUNTRY_NAME` AS `País`,
   IF (
     regions.REGION_ID = 1,
     'incluído',
     'não incluído'
   ) AS `Status Inclusão`
 FROM
-  countries
-  INNER JOIN regions ON countries.REGION_ID = regions.REGION_ID
+  `hr`.countries AS `countries`
+  INNER JOIN `hr`.regions AS `regions` ON `countries`.REGION_ID = `regions`.REGION_ID
 ORDER BY
-  countries.COUNTRY_NAME ASC;
+  `País` ASC;

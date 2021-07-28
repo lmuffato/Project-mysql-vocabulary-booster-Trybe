09a7b69af5904a00ba54bb6a257f9426ff9b0702
Orlando Flores <orlando_flores@id.uff.br>
2021-07-28 12:10:58 -0300

    fix: desafio 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,7 +1,6 @@
 SELECT JOB_TITLE AS Cargo,
 MAX_SALARY - MIN_SALARY AS `Variação Salarial`,
-ROUND(AVG(MIN_SALARY)/12, 2) AS `Média mínima mensal`,
-ROUND(AVG(MAX_SALARY)/12, 2) AS `Média máxima mensal`
+ROUND(MIN_SALARY/12, 2) AS `Média mínima mensal`,
+ROUND(MAX_SALARY/12, 2) AS `Média máxima mensal`
 FROM hr.jobs
-GROUP BY Cargo
 ORDER BY `Variação Salarial`, Cargo;

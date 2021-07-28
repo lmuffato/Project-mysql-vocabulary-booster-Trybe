5ce50706f3b30c606ec1ae1c25076da6ffd177d0
luanlsr <luanfswd@gmail.com>
2021-07-28 16:21:16 -0300

    requisito 6 finalizado

--- desafio5.sql
+++ desafio5.sql
@@ -6,7 +6,7 @@ j.MAX_SALARY - j.MIN_SALARY AS 'Variação Salarial',
 ROUND(AVG(j.MIN_SALARY)/12, 2) AS 'Média mínima mensal',
 ROUND(AVG(j.MAX_SALARY)/12, 2) AS 'Média máxima mensal'
 FROM hr.jobs AS j
-	INNER JOIN hr.employees AS e
+INNER JOIN hr.employees AS e
 ON e.JOB_ID = j.JOB_ID
 GROUP BY `Cargo`, `Variação Salarial` 
 ORDER BY `Variação Salarial`, `Cargo`;

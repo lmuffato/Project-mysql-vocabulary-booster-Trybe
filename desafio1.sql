244da72ebd8c2288f13dc11291ce8b10957f650f
Renata Andrade <renata@MacBook-Air-de-Renata.local>
2021-07-28 18:26:37 -0300

    feat: exibe pais e indica se esta ou nao incluso na Europa

---
+++ desafio1.sql
@@ -0,0 +1,6 @@
+SELECT País.COUNTRY_NAME AS País, 
+IF(Regioes.REGION_NAME = 'Europe', 'íncluído', 'não incluído') AS `Status Inclusão`
+FROM hr.countries AS País
+INNER JOIN hr.regions AS Regioes
+ON País.REGION_ID = Regioes.REGION_ID
+ORDER BY País ASC;

cc696c387d0bb6d5d25c326b80ecbd22be3a4959
Mateus <mtauws@gmail.com>
2021-07-28 14:15:51 -0300

    fix: 1, 2 and 4

--- desafio1.sql
+++ desafio1.sql
@@ -1,10 +1,11 @@
 SELECT
-  c.COUNTRY_NAME AS "País",
+  COUNTRY_NAME AS 'País',
   IF(
-    r.REGION_NAME LIKE "Europe",
-    "incluído",
-    "não incluído"
-  ) AS "Status Inclusão"
+    REGION_ID = 1,
+    'incluído',
+    'não incluído'
+  ) AS 'Status Inclusão'
 FROM
-  hr.countries c
-  INNER JOIN hr.regions r ON c.REGION_ID = r.REGION_ID;
+  hr.countries
+ORDER BY
+  COUNTRY_NAME ASC;
--- desafio4.sql
+++ desafio4.sql
@@ -1,19 +1,20 @@
 SELECT
-  j.JOB_TITLE 'Cargo',
-  ROUND(AVG(e.SALARY), 2) 'Média salarial',
+  j.JOB_TITLE AS Cargo,
+  ROUND(AVG(e.SALARY), 2) AS 'Média salarial',
   CASE
-    WHEN AVG(e.SALARY) BETWEEN 2000
+    WHEN ROUND(AVG(e.SALARY), 2) BETWEEN 2000
     AND 5800 THEN 'Júnior'
-    WHEN AVG(e.SALARY) BETWEEN 5801
+    WHEN ROUND(AVG(e.SALARY), 2) BETWEEN 5801
     AND 7500 THEN 'Pleno'
-    WHEN AVG(e.SALARY) BETWEEN 7501
+    WHEN ROUND(AVG(e.SALARY), 2) BETWEEN 7501
     AND 10500 THEN 'Sênior'
-    WHEN AVG(e.SALARY) > 10500 THEN 'CEO'
-  END AS 'Senioridade'
+    ELSE 'CEO'
+  END AS Senioridade
 FROM
-  hr.jobs j
-  INNER JOIN hr.employees e ON j.JOB_ID = e.JOB_ID
+  hr.jobs AS j
+  INNER JOIN hr.employees AS e ON j.JOB_ID = e.JOB_ID
 GROUP BY
-  `Cargo`
+  JOB_TITLE
 ORDER BY
-  `Média Salarial`;
+  ROUND(AVG(e.SALARY), 2),
+  JOB_TITLE ASC;

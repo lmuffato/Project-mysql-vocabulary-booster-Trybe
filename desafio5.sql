012e50e19fca892ef2b7b69a2961e0332decf9af
Mateus Santiago <mateussantiago.victor@gmail.com>
2021-07-28 16:29:09 -0300

    Fix lint req5

--- desafio5.sql
+++ desafio5.sql
@@ -1,9 +1,7 @@
 SELECT jobs.JOB_TITLE AS Cargo,
-  (MAX(jobs.MAX_SALARY) - MIN(jobs.MIN_SALARY) AS 'Variação Salarial',
+  (MAX(jobs.MAX_SALARY) - MIN(jobs.MIN_SALARY)) AS 'Variação Salarial',
   ROUND(AVG(jobs.MIN_SALARY) / 12, 2) AS 'Média mínima mensal',
-  ROUND(AVG(jobs.MAX_SALARY) / 12, 2) AS 'Média máxima mensal',
-FROM hr.jobs AS jobs
-INNER JOIN
-	 hr.employees AS employees ON jobs.JOB_ID = employees.JOB_ID
+  ROUND(AVG(jobs.MAX_SALARY) / 12, 2) AS 'Média máxima mensal'
+FROM hr.jobs AS jobs INNER JOIN hr.employees AS employees ON jobs.JOB_ID = employees.JOB_ID
 GROUP BY Cargo
 ORDER BY (MAX(jobs.MAX_SALARY) - MIN(jobs.MIN_SALARY)), jobs.JOB_TITLE;

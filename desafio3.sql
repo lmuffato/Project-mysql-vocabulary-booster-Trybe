50997d4ab984327498f6172b4a0ba59c56b032cb
lucas-paz <lucas.hpaz@outlook.com>
2021-07-28 16:36:23 -0300

    fixes

--- desafio3.sql
+++ desafio3.sql
@@ -1,5 +1,8 @@
 SELECT
-	  JOB_TITLE AS Cargo,
-    (MAX_SALARY - MIN_SALARY) AS `Diferença entre salários máximo e mínimo`
-FROM hr.jobs
-ORDER BY `Diferença entre salários máximo e mínimo`, Cargo;
+  JOB_TITLE AS Cargo,
+  (MAX_SALARY - MIN_SALARY) AS `Diferença entre salários máximo e mínimo`
+FROM
+  hr.jobs
+ORDER BY
+  `Diferença entre salários máximo e mínimo`,
+  Cargo;
--- desafio5.sql
+++ desafio5.sql
@@ -1,7 +1,10 @@
 SELECT
-	  J.JOB_TITLE AS Cargo,
-    (J.MAX_SALARY - J.MIN_SALARY) AS 'Variação Salarial',
-    ROUND((J.MIN_SALARY / 12), 2) AS 'Média mínima mensal',
-    ROUND((J.MAX_SALARY / 12), 2) AS 'Média máxima mensal'
-FROM hr.jobs AS J
-ORDER BY `Variação Salarial`, Cargo;
+  J.JOB_TITLE AS Cargo,
+  (J.MAX_SALARY - J.MIN_SALARY) AS 'Variação Salarial',
+  ROUND((J.MIN_SALARY / 12), 2) AS 'Média mínima mensal',
+  ROUND((J.MAX_SALARY / 12), 2) AS 'Média máxima mensal'
+FROM
+  hr.jobs AS J
+ORDER BY
+  `Variação Salarial`,
+  Cargo;

f0f0eef8abd3be9685aee480302c41155121e39f
Sergio Martins <sergiomos2010@gmail.com>
2021-07-28 15:39:02 -0300

    Desafio 1

---
+++ desafio1.sql
@@ -0,0 +1,5 @@
+SELECT 
+  C.COUNTRY_NAME AS 'País', 
+  IF((SELECT REGION_NAME FROM hr.regions AS R
+    WHERE R.REGION_ID = C.REGION_ID) = 'Europe', 'incluso', 'não incluso') AS 'Status inclusão'
+FROM hr.countries AS C;

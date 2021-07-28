6a975d44d9411cf38f99f11f0c5153f9e8bc5944
guilhermemd <guilherme.md@gmail.com>
2021-07-27 21:22:52 -0300

    fix: :bug: req 01

--- desafio1.sql
+++ desafio1.sql
@@ -1,7 +1,10 @@
-SELECT c.COUNTRY_NAME AS 'País',
-  IF(R.REGION_NAME LIKE 'Europe',
+SELECT 
+	c.COUNTRY_NAME AS 'País',
+  IF(r.REGION_NAME LIKE 'Europe',
     'incluído', 'não incluído') 'Status Inclusão'
-FROM hr.countries AS c
+FROM 
+	hr.countries AS c
 	INNER JOIN
-hr.regions AS r ON r.REGION_ID = c.REGION_ID
-ORDER BY 'Pais';
+hr.regions AS r
+	ON c.REGION_ID = r.REGION_ID
+ORDER BY País;

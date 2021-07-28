0a81ed4dcf891fc944c7d3a720104a84f6120ebc
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-07-28 05:13:14 +0000

    fix: desafio 4 | typo

--- desafio4.sql
+++ desafio4.sql
@@ -1,7 +1,7 @@
 # 4 - Exiba a média salarial e o nível de senioridade de todas as pessoas empregadas, agrupadas pelo cargo
 SELECT
     j.JOB_TITLE AS Cargo,
-    ROUND(AVG(e.SALARY), 2) AS `Média Salarial`,
+    ROUND(AVG(e.SALARY), 2) AS `Média salarial`,
     CASE
         WHEN AVG(e.SALARY) BETWEEN 2000 AND 5800 THEN 'Júnior'
         WHEN AVG(e.SALARY) BETWEEN 5801 AND 7500 THEN 'Pleno'
@@ -13,4 +13,4 @@ FROM
         INNER JOIN
     hr.employees e ON j.JOB_ID = e.JOB_ID
 GROUP BY e.JOB_ID
-ORDER BY `Média Salarial` , Cargo ASC;
+ORDER BY `Média salarial` , Cargo ASC;

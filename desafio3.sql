c0f7be47a200596239c4401c87ffc4f9ea9aed06
Robertson <robertson.nunes@gmail.com>
2021-07-28 18:14:08 -0300

    Requisito 3

---
+++ desafio3.sql
@@ -0,0 +1,7 @@
+-- 3 - Exiba os cargos com a diferença entre seus salários máximo e mínimo
+SELECT
+	JOB_TITLE Cargo,
+    (MAX_SALARY - MIN_SALARY) 'Diferença entre salários máximo e mínimo'
+FROM
+	hr.jobs
+ORDER BY `Diferença entre salários máximo e mínimo`, Cargo;

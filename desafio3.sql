197b167311a2fcf62c338c586dc9916a9cde0294
Debora Passos <deborinhapassos@gmail.com>
2021-07-28 08:32:09 -0300

    correçao codeclimate

--- desafio3.sql
+++ desafio3.sql
@@ -1,5 +1,5 @@
 SELECT
     job_title AS Cargo,
-	max_salary - min_salary AS 'Diferença entre salários máximo e mínimo'
+	  max_salary - min_salary AS 'Diferença entre salários máximo e mínimo'
     FROM jobs
     ORDER BY max_salary - min_salary ASC, job_title ASC;

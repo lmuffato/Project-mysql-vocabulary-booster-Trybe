65f84dccc8fac68a905808c20749f91da214d290
samuelmmjr <samuelmmjunior@gmail.com>
2021-07-28 17:14:36 -0300

    Fix: alteraçao para passar no test

--- desafio2.sql
+++ desafio2.sql
@@ -3,6 +3,6 @@ CASE
 WHEN max_salary BETWEEN 5000 AND 10000 THEN 'Baixo'
 WHEN max_salary BETWEEN 10001 AND 20000 THEN 'Médio'
 WHEN max_salary BETWEEN 20001 AND 30000 THEN 'Alto'
-WHEN max_salary > 30000 THEN 'Altíssimo'
+ELSE 'Altíssimo'
 END AS 'Nível' FROM hr.jobs
 ORDER BY 'Cargo';

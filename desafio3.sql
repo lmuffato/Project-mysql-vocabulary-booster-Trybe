4d631a2d5c3249056856911ff39762d2758f515b
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2021-07-28 15:18:34 -0300

    Feat: desafio3

---
+++ desafio3.sql
@@ -0,0 +1,6 @@
+-- Exiba os cargos com a diferença entre seus salários máximo e mínimo
+
+SELECT JOB_TITLE `Cargo`,
+MAX_SALARY - MIN_SALARY `Diferença entre salários máximo e mínimo`
+FROM hr.jobs
+ORDER BY `Diferença entre salários máximo e mínimo` ASC, `Cargo`;

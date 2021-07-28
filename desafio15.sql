27e3d4f945fac7586d775fc42c58b1a835e484df
Bruno Mendes <mendessbruno@gmail.com>
2021-07-28 16:49:54 -0300

    tentativa de resolve problema de lint

--- desafio15.sql
+++ desafio15.sql
@@ -4,12 +4,12 @@ DELIMITER $$
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(100))
 BEGIN
     SELECT
-		ROUND(AVG(e.SALARY), 2) AS `Média salarial`
-		FROM hr.employees AS e
-		INNER JOIN hr.jobs AS j
-		ON e.JOB_ID = j.JOB_ID
-		WHERE j.JOB_TITLE = cargo
-		GROUP BY j.JOB_TITLE;
+	ROUND(AVG(e.SALARY), 2) AS `Média salarial`
+	FROM hr.employees AS e
+	INNER JOIN hr.jobs AS j
+	ON e.JOB_ID = j.JOB_ID
+	WHERE j.JOB_TITLE = cargo
+	GROUP BY j.JOB_TITLE;
 END $$
 
 DELIMITER ;

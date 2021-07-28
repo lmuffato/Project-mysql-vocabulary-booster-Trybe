2e485a153c13dec167055ce9c83a7ab7f153dcd8
Orlando Flores <orlando_flores@id.uff.br>
2021-07-28 12:49:15 -0300

    fix: desafios 6 e 7

--- desafio6.sql
+++ desafio6.sql
@@ -3,7 +3,7 @@ j.JOB_TITLE AS Cargo,
 h.START_DATE AS `Data de início do cargo`,
 d.DEPARTMENT_NAME AS Departamento
 FROM hr.job_history AS h
-INNER JOIN hr.employees AS e ON e.EMPLOYEE_ID=h.EMPLOYEE_ID
-INNER JOIN hr.jobs AS j ON h.JOB_ID=j.JOB_ID
-INNER JOIN hr.departments AS d ON h.DEPARTMENT_ID=d.DEPARTMENT_ID
+JOIN hr.employees AS e ON e.EMPLOYEE_ID=h.EMPLOYEE_ID
+JOIN hr.jobs AS j ON h.JOB_ID=j.JOB_ID
+JOIN hr.departments AS d ON h.DEPARTMENT_ID=d.DEPARTMENT_ID
 ORDER BY `Nome Completo` DESC, Cargo;
--- desafio7.sql
+++ desafio7.sql
@@ -2,6 +2,6 @@ SELECT UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS `Nome Completo`,
 h.START_DATE AS `Data de início`,
 e.SALARY AS Salário
 FROM hr.job_history AS h
-INNER JOIN hr.employees AS e ON e.EMPLOYEE_ID=h.EMPLOYEE_ID
+JOIN hr.employees AS e ON e.EMPLOYEE_ID=h.EMPLOYEE_ID
 WHERE MONTH(h.START_DATE ) IN (1,2,3)
 ORDER BY `Nome Completo`, `Data de início`;

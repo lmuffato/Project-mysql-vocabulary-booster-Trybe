d64044b419c0a37acdd68d9d53512e29530e1682
Mateus Santiago <mateussantiago.victor@gmail.com>
2021-07-28 14:00:22 -0300

    Resolvendo codeclimate de novo req1

--- desafio1.sql
+++ desafio1.sql
@@ -1,8 +1,6 @@
-SELECT
-	COUNTRY_NAME AS 'País',
-    IF(REGION_ID > 1, 
-    'não incluído', 
-    'incluído') AS 'Status Inclusão'
-FROM
-	hr.countries
+SELECT COUNTRY_NAME AS 'País',
+    IF(REGION_ID > 1,
+		'não incluído',
+		'incluído') AS 'Status Inclusão'
+FROM hr.countries
 ORDER BY COUNTRY_NAME;

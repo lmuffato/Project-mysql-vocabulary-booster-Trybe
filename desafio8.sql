cd6f96d5f833d7895dc8d3776128324ca68f9106
Gustavo Sousa <gustavohl98@gmail.com>
2021-07-28 18:25:57 -0300

    Req 8 - relatorios de consumidores conforme empresa de envios

---
+++ desafio8.sql
@@ -0,0 +1,13 @@
+SELECT C.ContactName AS `Nome de contato`,
+S.ShipperName AS `Empresa que fez o envio`,
+O.OrderDate AS `Data do pedido`
+FROM w3schools.orders AS O
+INNER JOIN w3schools.customers AS C
+ON O.CustomerID=C.CustomerID
+INNER JOIN w3schools.shippers AS S
+ON O.ShipperID=S.ShipperID
+WHERE O.ShipperID IN (
+	SELECT ShipperID FROM w3schools.shippers
+    WHERE ShipperName IN ('Speedy Express', 'United Package')
+)
+ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;

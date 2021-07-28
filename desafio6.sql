751a765f6d4bf13494d08e6d7d2df3f63ca0a283
Patrick Morais <ppternunes@gmail.com>
2021-07-28 17:09:55 -0300

    requisito 6

---
+++ desafio6.sql
@@ -0,0 +1,13 @@
+SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo', 
+  j.JOB_TITLE AS 'Cargo',
+  jh.START_DATE AS 'Data de início do cargo',
+  d.DEPARTMENT_NAME AS 'Departamento'
+FROM 
+  hr.job_history AS jh
+    INNER JOIN hr.jobs AS j
+      ON jh.JOB_ID = j.JOB_ID
+    INNER JOIN hr.employees AS e
+      ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+    INNER JOIN hr.departments AS d
+      ON d.DEPARTMENT_ID = jh.DEPARTMENT_ID
+ORDER BY `Nome completo` DESC, `Cargo`;

49e92b94456046f4b74faa0b5402befda8fb1086
AndreBarroso <andre_barroso17@hotmail.com>
2021-07-29 20:45:40 -0300

    fix: desafios 3, 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,5 +1,5 @@
-SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo', jh.START_DATE AS 'Data de início do cargo', e.SALARY
-FROM employees AS e
+SELECT UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo', jh.START_DATE AS 'Data de início do cargo', e.SALARY
+FROM hr.employees AS e
 INNER JOIN hr.job_history AS jh
 ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
 INNER JOIN hr.jobs AS j 

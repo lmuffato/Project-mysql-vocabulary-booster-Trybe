8ab71033408c52ecfbadaeb6252316414fe327ed
Rodrigo Rech <rodrigorech92@gmail.com>
2021-07-29 17:45:45 -0300

    Feat: Report customers name  with country and how many people are from the same country as the customer

--- desafio11.sql
+++ desafio11.sql
@@ -1,3 +1,3 @@
-SELECT t1.CustomerName AS `Nome`, t1.Country AS `País`, (
+SELECT t1.ContactName AS `Nome`, t1.Country AS `País`, (
 SELECT (COUNT(1) -1) FROM w3schools.customers WHERE t1.Country = Country) AS `Número de compatriotas`
 FROM w3schools.customers AS t1 ORDER BY `Nome`;

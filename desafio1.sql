5dcab6a05905c007593bf6890f602530d7e674ae
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-07-29 18:30:30 -0300

    correção requsitos 1 ao 3

--- desafio1.sql
+++ desafio1.sql
@@ -1,5 +1,5 @@
 SELECT 
-	COUNTRY_NAME AS 'País',
-	IF (REGION_ID = 1, 'incluído', 'não incluído') AS 'Status Inclusão' 
+COUNTRY_NAME AS 'País',
+IF (REGION_ID = 1, 'incluído', 'não incluído') AS 'Status Inclusão' 
 FROM hr.countries
 ORDER BY COUNTRY_NAME ASC;
--- desafio3.sql
+++ desafio3.sql
@@ -1,5 +1,5 @@
 SELECT 
-	JOB_TITLE AS 'Cargo',
-	(MAX_SALARY- MIN_SALARY) AS 'Diferença entre salários máximo e mínimo'
+JOB_TITLE AS 'Cargo',
+(MAX_SALARY- MIN_SALARY) AS 'Diferença entre salários máximo e mínimo'
 FROM hr.jobs
 ORDER BY (MAX_SALARY- MIN_SALARY) ASC, JOB_TITLE ASC;

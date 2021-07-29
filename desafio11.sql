9d2b9578ecd732d107326d5a48b35bc537e63005
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-07-29 11:57:56 -0300

    feat: requirement11

--- desafio11.sql
+++ desafio11.sql
@@ -1,1 +1,12 @@
 -- 11 - Exibe todas as pessoas clientes que possuem compatriotas, mostrando a quantidade de compatriotas para cada pessoa cliente
+SELECT
+  ContactName AS `Nome`,
+  Country AS `País`,
+  (
+    SELECT 
+      COUNT(*) - 1
+    FROM w3schools.customers
+    WHERE c.Country = Country
+  ) AS `Número de compatriotas`
+FROM w3schools.customers AS c
+ORDER BY ContactName ASC;

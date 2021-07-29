0389699d2b274d44f855a1708da7691ed446589b
Mateus Santiago <mateussantiago.victor@gmail.com>
2021-07-29 19:22:54 -0300

    Requisito 11

---
+++ desafio11.sql
@@ -0,0 +1,6 @@
+SELECT customers.ContactName AS 'Nome', customers.Country AS 'País', COUNT(*) AS 'Número de compatriotas'
+FROM w3schools.customers AS customers, w3schools.customers AS customers2
+WHERE customers.Country = customers2.Country
+AND customers.ContactName <> customers2.ContactName
+GROUP BY `Nome`, `País`
+ORDER BY `Nome` ASC;

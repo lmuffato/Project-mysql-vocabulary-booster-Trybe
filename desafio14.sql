09ed3ee75e450d6258c81083d1eac6d9a84dcb9f
Elisa <elisafranca.carvalho@gmail.com>
2021-07-29 19:21:09 -0300

    fix: codeclimate

--- desafio14.sql
+++ desafio14.sql
@@ -1,5 +1,5 @@
 -- Considerando o conjunto formado pelas pessoas consumidoras e empresas fornecedoras de produtos,
- -- queremos saber quais são os cinco primeiros países distintos, em ordem alfabética, presentes nesse conjunto
+-- queremos saber quais são os cinco primeiros países distintos, em ordem alfabética, presentes nesse conjunto
 SELECT Country AS `País` FROM w3schools.customers
 UNION
 SELECT Country AS `País` FROM w3schools.suppliers

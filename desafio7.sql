bff1cdea01770b3bd0b2005edc57c0d185ae74eb
Renata Andrade <renata@MacBook-Air-de-Renata.local>
2021-07-29 15:59:57 -0300

    fix: corrige alias Data de inicio arquivo desafio7

--- desafio7.sql
+++ desafio7.sql
@@ -1,8 +1,8 @@
 SELECT UCASE(CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME)) AS `Nome completo`, 
-JH.START_DATE AS `Data de início do cargo`,
+JH.START_DATE AS `Data de início`,
 E.SALARY AS `Salário`
 FROM hr.employees AS E
 INNER JOIN hr.job_history AS JH
 ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
 WHERE MONTH(JH.START_DATE) <= 3
-ORDER BY `Nome completo` ASC, `Data de início do cargo` ASC;
+ORDER BY `Nome completo` ASC, `Data de início` ASC;

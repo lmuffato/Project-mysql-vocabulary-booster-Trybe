f6eaf89f1a1b8a06b0478da9ca2dc9d73373ab51
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-07-29 13:24:45 -0300

    feat: requisito 10

---
+++ desafio10.sql
@@ -0,0 +1,7 @@
+SELECT ProductName AS 'Produto', MIN(o.Quantity) AS 'Mínima', MAX(o.Quantity) AS 'Máxima', ROUND(AVG(o.Quantity), 2) AS 'Média'
+FROM w3schools.products AS p
+INNER JOIN w3schools.order_details AS o
+ON p.ProductID = o.ProductID
+GROUP BY `Produto`
+HAVING `Média` > 20
+ORDER BY `Média`, `Produto`;

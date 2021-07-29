d7480f62456a053ffb90e22db22bac0f449684d6
Lucas André <lucas.cancado89@gmail.com>
2021-07-28 21:04:28 -0300

    desafio18-change date format do brazilian type

--- desafio18.sql
+++ desafio18.sql
@@ -1,6 +1,6 @@
 SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
-jh.START_DATE AS 'Data de início',
-jh.END_DATE AS 'Data de rescisão',
+CONCAT(DAY(jh.START_DATE),'/',MONTH(jh.START_DATE),'/',YEAR(jh.START_DATE)) AS 'Data de início',
+CONCAT(DAY(jh.END_DATE),'/',MONTH(jh.END_DATE),'/',YEAR(jh.END_DATE)) AS 'Data de rescisão',
 ROUND(DATEDIFF(jh.END_DATE, jh.START_DATE)/365, 2) AS 'Anos trabalhados'
 FROM hr.employees AS e
 RIGHT JOIN hr.job_history AS jh

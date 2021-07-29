7864206ab345fcbadfcb419d686a4b2ef0c5aa8e
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-07-29 19:05:12 -0300

    DEsafio 8: filtrando pessoas consumidoras atraves das empresas q fizeram envio

---
+++ desafio8.sql
@@ -0,0 +1,9 @@
+SELECT C.ContactName AS 'Nome de contato', S.ShipperName AS 'Empresa que fez o envio',
+O.OrderDate AS 'Data do pedido'
+FROM w3schools.orders O
+INNER JOIN w3schools.customers C
+ON O.CustomerID = C.CustomerID
+INNER JOIN w3schools.shippers S
+ON O.ShipperID = S.ShipperID
+WHERE S.ShipperName IN ('Speedy Express','United Package')
+ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;

057067f79d779dddd2ae487e45d7cbaae0f65d49
Marcelo Maurício <mmauricio.design@gmail.com>
2021-07-29 11:08:28 -0300

    fix: another code climate fixes

--- desafio8.sql
+++ desafio8.sql
@@ -1,9 +1,1 @@
-SELECT cust.ContactName AS `Nome de contato`,
-ship.ShipperName AS `Empresa que fez o envio`,
-orde.OrderDate AS `Data do pedido`
-FROM w3schools.customers AS cust
-INNER JOIN w3schools.orders AS orde
-INNER JOIN w3schools.shippers AS ship
-ON orde.CustomerID = cust.CustomerID AND ship.ShipperID = orde.ShipperID
-WHERE ship.ShipperName = 'Speedy Express' OR ship.ShipperName = 'United Package'
-ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;
+SELECT cust.ContactName AS `Nome de contato`, ship.ShipperName AS `Empresa que fez o envio`, orde.OrderDate AS `Data do pedido` FROM w3schools.customers AS cust INNER JOIN w3schools.orders AS orde INNER JOIN w3schools.shippers AS ship ON orde.CustomerID = cust.CustomerID AND ship.ShipperID = orde.ShipperID WHERE ship.ShipperName = 'Speedy Express' OR ship.ShipperName = 'United Package' ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;

3183db78df8bf1efb390dac4e5a885d654aedbcd
mallmann02 <leomallmann020302@gmail.com>
2021-07-29 09:29:10 -0300

    code-climate: fix style issues

--- desafio6.sql
+++ desafio6.sql
@@ -1,17 +1,17 @@
 SELECT
 (
-	SELECT CONCAT(FIRST_NAME, ' ', LAST_NAME)
+SELECT CONCAT(FIRST_NAME, ' ', LAST_NAME)
   FROM hr.employees
   WHERE hr.employees.EMPLOYEE_ID = hr.job_history.EMPLOYEE_ID
 ) AS `Nome completo`,
 (
-	SELECT JOB_TITLE
+SELECT JOB_TITLE
   FROM hr.jobs
   WHERE hr.jobs.JOB_ID = hr.job_history.JOB_ID
 ) AS `Cargo`,
 START_DATE AS `Data de início do cargo`,
 (
-	SELECT DEPARTMENT_NAME
+SELECT DEPARTMENT_NAME
   FROM hr.departments
   WHERE hr.departments.DEPARTMENT_ID = hr.job_history.DEPARTMENT_ID
 ) AS `Departamento`

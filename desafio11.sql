86bf62eb50967be3598ff38b6bc570a783bf514f
Gustavo Sousa <gustavohl98@gmail.com>
2021-07-29 13:55:48 -0300

    fix:bug no Req 11

--- desafio11.sql
+++ desafio11.sql
@@ -3,5 +3,5 @@ T1.Country AS `País`,
 COUNT(*) AS `Número de compatriotas`
 FROM w3schools.customers AS T1, w3schools.customers AS T2
 WHERE T1.Country=T2.Country AND T1.ContactName<>T2.ContactName
-GROUP BY T1.ContactName
+GROUP BY T1.ContactName, `País`
 ORDER BY Nome;

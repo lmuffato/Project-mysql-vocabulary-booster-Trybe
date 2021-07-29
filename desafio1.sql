c3f5d156e354632b7cbe1a9fbf281a8d127246d5
Sergio Martins <sergiomos2010@gmail.com>
2021-07-29 17:24:26 -0300

    Fix: desafio 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,5 +1,5 @@
 SELECT 
   C.COUNTRY_NAME AS 'País', 
   IF((SELECT REGION_NAME FROM hr.regions AS R
-    WHERE R.REGION_ID = C.REGION_ID) = 'Europe', 'incluso', 'não incluso') AS 'Status Inclusão'
+    WHERE R.REGION_ID = C.REGION_ID) = 'Europe', 'incluído', 'não incluído') AS 'Status Inclusão'
 FROM hr.countries AS C;

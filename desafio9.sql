1c7810f35a8ffe8f8a148dbcd9351edc52e56e2c
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-07-28 23:48:14 -0300

    feat: requirement9

--- desafio9.sql
+++ desafio9.sql
@@ -1,1 +1,9 @@
 -- 9 - Exibe todos as pessoas funcionárias que já realizaram algum pedido, mostrando também seu total de pedidos feitos
+SELECT 
+  CONCAT(e.FirstName, ' ', e.LastName) AS `Nome completo`,
+  COUNT(*) AS `Total de pedidos`
+FROM w3schools.employees AS e
+INNER JOIN w3schools.orders AS o
+ON e.EmployeeID = o.EmployeeID
+GROUP BY e.EmployeeID
+ORDER BY `Total de pedidos` ASC;

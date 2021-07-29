a0d41cd3268c7cbfa5b37d21cb837a2ac5e53737
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-07-29 16:28:30 -0300

    feat: requirement16

--- desafio16.sql
+++ desafio16.sql
@@ -1,1 +1,19 @@
 -- 16 - Crie uma função chamada buscar_quantidade_de_empregos_por_funcionario no banco de dados hr que, ao receber o email de uma pessoa funcionária, retorne a quantidade de empregos presentes em seu histórico
+USE hr;
+DELIMITER $$
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(15))
+RETURNS INT READS SQL DATA
+BEGIN
+  DECLARE total_empregos INT;
+  SELECT
+    COUNT(*)
+  FROM hr.employees AS e
+  INNER JOIN hr.job_history AS j
+  WHERE e.EMPLOYEE_ID = j.EMPLOYEE_ID
+  GROUP BY j.EMPLOYEE_ID, e.Email
+  HAVING e.EMAIL = email INTO total_empregos;
+  RETURN total_empregos;
+END $$
+
+DELIMITER ;

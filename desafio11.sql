5de0f448f4dc5b96cb9b94ff3124b2fd29e46b8a
mallmann02 <leomallmann020302@gmail.com>
2021-07-29 16:38:17 -0300

    code-climate: fix style issues

--- desafio11.sql
+++ desafio11.sql
@@ -1,7 +1,7 @@
 SELECT
-	t1.CustomerName AS `Nome`,
-	t1.Country AS `País`,
-  (count(t1.Country = t2.Country) - 1) AS `Número de compatriotas`
+t1.CustomerName AS `Nome`,
+t1.Country AS `País`,
+(count(t1.Country = t2.Country) - 1) AS `Número de compatriotas`
 FROM w3schools.customers AS t1, w3schools.customers AS t2
 WHERE t1.Country = t2.Country
 GROUP BY t1.CustomerID

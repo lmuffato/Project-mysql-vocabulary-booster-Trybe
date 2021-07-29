4560bcece75719c9f7877b2906f78c0611b1797f
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-07-28 22:24:48 -0300

    feat: requirement6

--- desafio6.sql
+++ desafio6.sql
@@ -1,1 +1,24 @@
 -- 6 - Faça um relatório que mostra o histórico de cargos das pessoas empregadas
+SELECT
+  (
+  SELECT 
+      CONCAT(FIRST_NAME, ' ', LAST_NAME)
+    FROM hr.employees
+    WHERE hr.employees.EMPLOYEE_ID = hr.job_history.EMPLOYEE_ID
+  ) AS `Nome completo`,
+  (
+    SELECT
+      JOB_TITLE
+    FROM hr.jobs
+    WHERE hr.jobs.JOB_ID = hr.job_history.JOB_ID
+  ) AS `Cargo`,
+  START_DATE AS `Data de início do cargo`,
+  (
+    SELECT
+      DEPARTMENT_NAME
+    FROM hr.departments
+    WHERE hr.departments.DEPARTMENT_ID = hr.job_history.DEPARTMENT_ID
+  ) AS `Departamento`
+FROM hr.job_history
+WHERE JOB_ID IS NULL OR JOB_ID<>''
+ORDER BY `Nome completo` DESC, `Cargo` ASC;

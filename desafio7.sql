d8741f0f5fe6c5c87139c1b59e370817d463c8eb
Gabriel <g.pereira.a.m@gmail.com>
2021-07-28 22:16:57 -0300

    fix(desafio7) - :sparkle: Corrigido

--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,6 @@
 SELECT UCASE(CONCAT(e.first_name,' ',e.last_name)) AS 'Nome completo',
-	h.start_date AS 'Data de início',
-    e.salary AS 'Salário'
+h.start_date AS 'Data de início',
+e.salary AS 'Salário'
 FROM hr.job_history AS h
 INNER JOIN hr.employees AS e
 ON e.employee_id = h.employee_id

8cd703514cf8cb9222f12cecf2ba3a391ef718ec
luanlsr <luanfswd@gmail.com>
2021-07-29 00:59:30 -0300

    requisito 10 finalizado

---
+++ desafio10.sql
@@ -0,0 +1,12 @@
+-- 10 - Exibe todos os produtos que já foram pedidos, que possuem uma média de quantidade nos pedidos registrados acima de 20.00
+SELECT ProductName AS `Produto`,
+COUNT(MIN(Quantity)) AS `Mínima`,
+COUNT(MAX(Quantity)) AS `Máxima`,
+ROUND(AVG(Quantity), 2) AS `Média`
+FROM w3schools.products AS p
+INNER JOIN w3schools.order_details AS od
+ON od.ProductID = p.ProductID
+GROUP BY `Produto`
+HAVING ROUND(AVG(Quantity), 2) >= 20.00
+ORDER BY `Média`, `Produto`;
+

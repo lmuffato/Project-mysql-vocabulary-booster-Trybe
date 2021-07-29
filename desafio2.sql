80240489cfb34c72386b36dd4b25d8e636d3b084
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-07-28 22:40:11 -0300

    Implementaçao do 2

---
+++ desafio2.sql
@@ -0,0 +1,10 @@
+SELECT
+job_title AS 'Cargo',
+CASE
+  WHEN max_salary <= 10000 THEN 'Baixo'
+  WHEN max_salary <= 20000 THEN 'Médio'
+  WHEN max_salary <= 30000 THEN 'Alto'
+  ELSE 'Altíssimo'
+END AS 'Nível'
+FROM hr.jobs
+ORDER BY job_title;

3fa8ec426e14026a650ad5e22c5bc57109cf185c
Sergio Martins <sergiomos2010@gmail.com>
2021-07-29 19:25:55 -0300

    Fix: desafio 1

--- desafio1.sql
+++ desafio1.sql
@@ -2,4 +2,5 @@ SELECT
   C.COUNTRY_NAME AS 'País', 
   IF((SELECT REGION_NAME FROM hr.regions AS R
     WHERE R.REGION_ID = C.REGION_ID) = 'Europe', 'incluído', 'não incluído') AS 'Status Inclusão'
-FROM hr.countries AS C;
+FROM hr.countries AS C
+ORDER BY País;

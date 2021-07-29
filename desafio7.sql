1117ffeeeb00c51cbb7706f15cd5439348651db5
Edrei <edreimateus@gmail.com>
2021-07-29 01:04:02 -0300

    fix: 7 requiriment

--- desafio7.sql
+++ desafio7.sql
@@ -1,4 +1,4 @@
-SELECT UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS `Nome Completo`,
+SELECT UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS `Nome completo`,
 s.START_DATE AS `Data de início`,
 e.SALARY AS Salário
 FROM hr.employees AS e

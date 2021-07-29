770e4669bea2e253b92fcd6c2c6f01baebbe7eb0
Gabriel <g.pereira.a.m@gmail.com>
2021-07-28 22:00:30 -0300

    feat(Desafio9) - :sparkle: Feito

---
+++ desafio9.sql
@@ -0,0 +1,7 @@
+SELECT CONCAT(e.firstName,' ',e.lastName) AS 'Nome completo',
+	COUNT(orderID) AS 'Total de pedidos'
+FROM w3schools.orders AS o
+INNER JOIN w3schools.employees AS e
+ON e.employeeid = o.employeeid
+GROUP BY `Nome completo`
+ORDER BY `Total de pedidos`;

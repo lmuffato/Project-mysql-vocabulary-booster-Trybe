69b238cda808511a2d75e0558b2287ca309cef34
Felipe Muller <felipeavmuller@gmail.com>
2021-07-28 23:47:24 -0300

    Requisito 11 - self join SQL

---
+++ desafio11.sql
@@ -0,0 +1,5 @@
+SELECT t1.ContactName AS 'Nome', t1.Country AS 'País', COUNT(t2.Country) - 1 AS 'Número de compatriotas'
+FROM w3schools.customers t1, w3schools.customers t2
+WHERE t1.Country = t2.Country
+GROUP BY t1.ContactName
+ORDER BY `Nome`;

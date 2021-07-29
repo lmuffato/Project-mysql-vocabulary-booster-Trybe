594cb113eff81f8e514a2fa21741bab2eda1421e
mallmann02 <leomallmann020302@gmail.com>
2021-07-29 17:06:38 -0300

    fix: select correct column

--- desafio11.sql
+++ desafio11.sql
@@ -1,5 +1,5 @@
 SELECT
-t1.CustomerName AS `Nome`,
+t1.ContactName AS `Nome`,
 t1.Country AS `País`,
 (count(t1.Country = t2.Country) - 1) AS `Número de compatriotas`
 FROM w3schools.customers AS t1, w3schools.customers AS t2

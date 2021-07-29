037e88bccdf9e85416fe324be59e99f37178d793
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-07-29 19:55:48 -0300

    requisito 4

--- desafio4.sql
+++ desafio4.sql
@@ -2,10 +2,10 @@ SELECT
 j.JOB_TITLE AS 'Cargo',
 round(AVG(e.SALARY),2) AS 'Média salarial',
 CASE
-	WHEN round(AVG(e.SALARY),2)>=2000 AND round(AVG(e.SALARY),2)<=5800 THEN 'Júnior'
-	WHEN round(AVG(e.SALARY),2)>=5801 AND round(AVG(e.SALARY),2)<=7500 THEN 'Pleno'
-	WHEN round(AVG(e.SALARY),2)>=7501 AND round(AVG(e.SALARY),2)<=10500 THEN 'Sênior'
-	WHEN round(AVG(e.SALARY),2)>10500 THEN 'CEO'
+WHEN round(AVG(e.SALARY),2)>=2000 AND round(AVG(e.SALARY),2)<=5800 THEN 'Júnior'
+WHEN round(AVG(e.SALARY),2)>=5801 AND round(AVG(e.SALARY),2)<=7500 THEN 'Pleno'
+WHEN round(AVG(e.SALARY),2)>=7501 AND round(AVG(e.SALARY),2)<=10500 THEN 'Sênior'
+WHEN round(AVG(e.SALARY),2)>10500 THEN 'CEO'
 END AS 'Senioridade'
 FROM hr.jobs AS j
 INNER JOIN hr.employees AS e

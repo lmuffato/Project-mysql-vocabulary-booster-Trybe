8ce01d99f818ef03004c0738e47f3eeb626ef266
Gabriel <g.pereira.a.m@gmail.com>
2021-07-28 22:16:16 -0300

    fix(desafio5) - :sparkle: Corrigido

--- desafio5.sql
+++ desafio5.sql
@@ -1,5 +1,5 @@
 SELECT job_title AS 'Cargo', max_salary - min_salary AS 'Variação Salarial',
-	ROUND(min_salary / 12, 2) AS 'Média mínima mensal',
-    ROUND(max_salary / 12, 2) AS 'Média máxima mensal'
+ROUND(min_salary / 12, 2) AS 'Média mínima mensal',
+ROUND(max_salary / 12, 2) AS 'Média máxima mensal'
 FROM hr.jobs
 ORDER BY `Variação Salarial`, `Cargo`;

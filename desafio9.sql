e3e390c967f640ca074a5434a69dcaa582600fb8
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-07-29 13:02:51 -0300

    feat: requisito 9

---
+++ desafio9.sql
@@ -0,0 +1,6 @@
+SELECT CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome completo', COUNT(*) AS 'Total de pedidos'
+FROM w3schools.employees AS e
+INNER JOIN w3schools.orders AS o
+ON o.EmployeeID = e.EmployeeID
+GROUP BY `Nome completo`
+ORDER BY `Total de pedidos`;

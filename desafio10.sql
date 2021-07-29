da5ad4253d1267a10a232d2c62b0f5fa89901b9a
Gustavo Sousa <gustavohl98@gmail.com>
2021-07-29 13:22:10 -0300

    Req 10 -todos os produtos ja pedidos com media de qtde nos pedidoss>20

---
+++ desafio10.sql
@@ -0,0 +1,10 @@
+SELECT P.ProductName AS Produto,
+MIN(OD.Quantity) AS `Mínima`,
+MAX(OD.Quantity) AS `Máxima`,
+ROUND(AVG(OD.Quantity), 2) AS `Média`
+FROM w3schools.order_details AS OD
+INNER JOIN w3schools.products AS P
+ON P.ProductID=OD.ProductID
+GROUP BY OD.ProductID
+HAVING `Média`>20.0
+ORDER BY `Média`, Produto;

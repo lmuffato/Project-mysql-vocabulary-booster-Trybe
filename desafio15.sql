63c928a4782e3ad55d7e391d5ab9201d88fbf6d0
João Nasc <oi@joaonasc.dev>
2021-07-28 22:13:03 -0300

    fix: unexpected tabs

--- desafio15.sql
+++ desafio15.sql
@@ -2,12 +2,12 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN `job_title` VARCHAR(50))
 BEGIN
-	SELECT
-      ROUND(AVG(`employees`.SALARY), 2) AS `Média salarial`
-    FROM
-      `hr`.employees AS `employees`
-      INNER JOIN `hr`.jobs AS `jobs` ON `employees`.JOB_ID = `jobs`.JOB_ID
-    WHERE `jobs`.JOB_TITLE = `job_title`;
+  SELECT
+    ROUND(AVG(`employees`.SALARY), 2) AS `Média salarial`
+  FROM
+    `hr`.employees AS `employees`
+    INNER JOIN `hr`.jobs AS `jobs` ON `employees`.JOB_ID = `jobs`.JOB_ID
+  WHERE `jobs`.JOB_TITLE = `job_title`;
 END $$
 
 DELIMITER ;

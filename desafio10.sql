19244f1ecfed9a0e425d1f32834013d847b15db9
Felipe Muller <felipeavmuller@gmail.com>
2021-07-28 23:33:22 -0300

    Requisito 10 - SQL com GROUP BY, HAVING e INNER JOIN

---
+++ desafio10.sql
@@ -0,0 +1,7 @@
+SELECT p.ProductName AS 'Produto', MIN(od.Quantity) AS 'Mínima', MAX(od.Quantity) AS 'Máxima',
+ROUND(AVG(od.Quantity), 2) AS 'Média'
+FROM w3schools.products p
+INNER JOIN w3schools.order_details od ON p.ProductID = od.ProductID
+GROUP BY p.ProductName
+HAVING `Média` > 20
+ORDER BY `Média`, `Produto`;

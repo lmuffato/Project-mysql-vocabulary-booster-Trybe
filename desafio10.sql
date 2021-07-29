b8a15a4da7103eefacfcd66f431eb23ad75c2063
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-07-29 01:19:15 -0300

    feat: requirement10

--- desafio10.sql
+++ desafio10.sql
@@ -1,1 +1,15 @@
 -- 10 - Exibe todos os produtos que já foram pedidos, que possuem uma média de quantidade nos pedidos registrados acima de 20.00
+SELECT 
+  (
+  SELECT
+    ProductName
+  FROM w3schools.products
+  WHERE o.ProductID = ProductID
+  ) AS `Produto`,
+  MIN(Quantity) AS `Mínima`,
+  MAX(Quantity) AS `Máxima`,
+  ROUND(AVG(Quantity), 2) AS `Média`
+FROM w3schools.order_details as o
+GROUP BY o.ProductID
+HAVING `Média` > 20.00
+ORDER BY `Média` ASC, `Produto` ASC;

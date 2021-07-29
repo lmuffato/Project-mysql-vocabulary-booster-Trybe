b737e8997d6667b4e8b4606a71eb50cb765b97ba
mallmann02 <leomallmann020302@gmail.com>
2021-07-29 17:31:25 -0300

    code-climate: fix style issues

--- desafio15.sql
+++ desafio15.sql
@@ -1,6 +1,6 @@
 CREATE PROCEDURE buscar_media_por_cargo(IN nome_do_cargo VARCHAR(50))
 BEGIN
-	SELECT ROUND(AVG(SALARY),2) AS `Média salarial`
+SELECT ROUND(AVG(SALARY),2) AS `Média salarial`
   FROM hr.employees
   WHERE JOB_ID = (SELECT JOB_ID FROM hr.jobs WHERE JOB_TITLE = nome_do_cargo);
 END

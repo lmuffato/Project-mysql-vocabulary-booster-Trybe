8c5d13058ce87e751df797ff6ce5150ffc4fa41d
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-07-29 00:48:01 -0300

    Desafio 5: cargos com variaçao mensal, media minima e media maxima mensal

---
+++ desafio5.sql
@@ -0,0 +1,9 @@
+SELECT * FROM hr.jobs;
+SELECT * FROM hr.employees;
+
+SELECT JOB_TITLE AS 'Cargo', (MAX_SALARY - MIN_SALARY) AS 'Variação Salarial',
+ROUND(AVG(MIN_SALARY/12),2) AS 'Média mínima mensal', ROUND(AVG(MAX_SALARY/12),2) AS 'Média máxima mensal'
+FROM hr.jobs
+GROUP BY JOB_TITLE
+ORDER BY `Variação Salarial`, `Cargo`;
+

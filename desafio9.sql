8ac25ae2c93baed2b566367a3d71977354f2539f
Mateus Santiago <mateussantiago.victor@gmail.com>
2021-07-29 18:54:56 -0300

    Requisito 9

--- desafio9.sql
+++ desafio9.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT(employees.FirstName, ' ', employees.LastName) AS 'Nome Completo', COUNT(*) AS 'Total de pedidos'
+SELECT CONCAT(employees.FirstName, ' ', employees.LastName) AS 'Nome completo', COUNT(*) AS 'Total de pedidos'
 FROM w3schools.employees AS employees INNER JOIN w3schools.orders AS orders ON orders.EmployeeID = employees.EmployeeID
 GROUP BY `Nome completo`
 ORDER BY `Total de pedidos` ASC;

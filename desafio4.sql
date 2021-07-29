f8da3da5a527b97a0f7eb69a2f18f496bf14fe18
IzeldaValeriano <zel.valeriano@gmail.com>
2021-07-29 11:10:32 -0300

    Requisitos 1 a 5

---
+++ desafio4.sql
@@ -0,0 +1,13 @@
+SELECT job.JOB_TITLE AS "Cargo",
+ROUND(AVG(eploy.SALARY) ,2) AS "Média salarial",
+CASE
+WHEN ROUND(AVG(eploy.SALARY), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
+WHEN ROUND(AVG(eploy.SALARY), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
+WHEN ROUND(AVG(eploy.SALARY), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
+ELSE 'CEO'
+END AS "Senioridade"
+FROM hr.jobs AS job
+INNER JOIN hr.employees AS eploy
+ON job.JOB_ID = eploy.JOB_ID
+GROUP BY `Cargo`
+ORDER BY `Média salarial`, `Cargo`;

bd93694ca6745940cc13ce2f33368322bee9e51f
AndreBarroso <andre_barroso17@hotmail.com>
2021-07-29 13:28:29 -0300

    test: remove use

--- desafio1.sql
+++ desafio1.sql
@@ -1,7 +1,7 @@
 SELECT COUNTRY_NAME AS 'País',
 CASE 
 WHEN REGION_ID = 1 THEN 'incluído'
-ELSE 'não classificado'
+ELSE 'não incluído'
 END AS 'Status Inclusão'
 FROM hr.countries AS c
 ORDER BY c.COUNTRY_NAME;
--- desafio3.sql
+++ desafio3.sql
@@ -1,5 +1,3 @@
-USE hr;
-
 SELECT JOB_TITLE AS 'Cargo', MAX_SALARY - MIN_SALARY AS `Diferença entre salários máximo e mínimo`
-FROM jobs
+FROM hr.jobs
 ORDER BY `Diferença entre salários máximo e mínimo`;

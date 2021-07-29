c84f71c761c586fcbe0eb4e02818ec3f712ca0f5
mallmann02 <leomallmann020302@gmail.com>
2021-07-29 09:21:21 -0300

    Challenges[6-10]: c10

---
+++ desafio10.sql
@@ -0,0 +1,10 @@
+SELECT
+  t2.ProductName AS `Produto`,
+  MIN(t1.Quantity) AS `Mínima`,
+  MAX(t1.Quantity) AS `Máxima`,
+  ROUND(AVG(t1.Quantity),2) AS `Média`
+FROM w3schools.order_details AS t1
+INNER JOIN w3schools.products AS t2
+ON t1.ProductID = t2.ProductID
+GROUP BY t2.ProductID
+ORDER BY ROUND(AVG(t1.Quantity),2), t2.ProductName;

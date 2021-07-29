9cf75decd3075b41363eaf7708a3bb1a48ee99e4
mallmann02 <leomallmann020302@gmail.com>
2021-07-28 23:28:36 -0300

    code-climate: fix style issues

--- desafio2.sql
+++ desafio2.sql
@@ -1,10 +1,10 @@
 SELECT
- JOB_TITLE AS `Cargo`,
-    CASE
-		  WHEN MAX_SALARY > 30000 THEN 'Altíssimo'
-      WHEN MAX_SALARY > 20001 THEN 'Alto'
-      WHEN MAX_SALARY > 10001 THEN 'Médio'
-      WHEN MAX_SALARY > 5000 THEN 'Baixo'
-      ELSE 'Baixo'
-    END AS `Nível`
+  JOB_TITLE AS `Cargo`,
+  CASE
+		WHEN MAX_SALARY > 30000 THEN 'Altíssimo'
+    WHEN MAX_SALARY > 20001 THEN 'Alto'
+    WHEN MAX_SALARY > 10001 THEN 'Médio'
+    WHEN MAX_SALARY > 5000 THEN 'Baixo'
+    ELSE 'Baixo'
+  END AS `Nível`
 FROM hr.jobs ORDER BY JOB_TITLE;

8768ed0daeffbf933fa591e25a23c75f8a5232dd
Luiz <luizfvaccarif@gmail.com>
2021-07-29 19:43:28 -0300

    fix: req6

--- desafio6.sql
+++ desafio6.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT(e.FIRST_NAME, '', e.LAST_NAME) AS `Nome completo`,
+SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome completo`,
 j.JOB_TITLE AS 'Cargo',
 jh.START_DATE AS 'Data de início do cargo',
 d.DEPARTMENT_NAME AS `Departamento`

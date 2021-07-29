66e2c8b981f5213be220021e912704629f760c58
Debora Passos <deborinhapassos@gmail.com>
2021-07-29 18:47:32 -0300

    req 15

---
+++ desafio15.sql
@@ -0,0 +1,14 @@
+DELIMITER $$
+
+CREATE PROCEDURE buscar_media_por_cargo(IN job_title VARCHAR(100))
+BEGIN
+    SELECT ROUND(AVG(e.salary), 2) AS 'Média salarial'
+    FROM employees AS e
+    INNER JOIN jobs AS j
+	ON j.job_id = e.job_id
+    WHERE j.job_title = job_title;
+END $$
+
+DELIMITER ;
+
+CALL buscar_media_por_cargo('Programmer');

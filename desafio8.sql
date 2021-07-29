990aa324b7b272d3a8a1e16201f04077e823e03d
Gabriel <g.pereira.a.m@gmail.com>
2021-07-28 22:17:18 -0300

    fix(desafio8) - :sparkle: Corrigido

--- desafio8.sql
+++ desafio8.sql
@@ -1,6 +1,6 @@
 SELECT c.contactName AS 'Nome de contato',
-	s.shipperName AS 'Empresa que fez o envio',
-    o.orderDate AS 'Data do pedido'
+s.shipperName AS 'Empresa que fez o envio',
+o.orderDate AS 'Data do pedido'
 FROM w3schools.orders AS o
 INNER JOIN w3schools.customers AS c
 ON c.customerID = o.customerID

2e4f0d1090535340f82ba2ab55ab91073fca1fd2
WillianMendes <willianmendesbr@gmail.com>
2021-07-29 19:53:26 -0300

    feat: requisito 8

---
+++ desafio8.sql
@@ -0,0 +1,14 @@
+SELECT
+  c.ContactName AS 'Nome de contato',
+  s.ShipperName AS 'Empresa que fez o envio',
+  o.OrderDate AS 'Data do pedido'
+FROM
+  orders AS o
+INNER JOIN
+  customers AS c ON o.CustomerID = c.CustomerID
+INNER JOIN
+  shippers s on s.ShipperID = o.ShipperID
+WHERE 
+  o.ShipperID = 1 OR o.ShipperID = 2
+ORDER BY
+  `Nome de contato`, `Empresa que fez o envio`;

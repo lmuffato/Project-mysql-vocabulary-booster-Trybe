fdee701d3dbab5f36942ad8a250556960748f8de
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-07-28 22:09:21 -0300

    Challenge 11: Fix

--- desafio11.sql
+++ desafio11.sql
@@ -1,6 +1,6 @@
 SELECT 
-    t1.ContactName AS Nome,
-    t1.Country AS País,
+    t1.ContactName AS 'Nome',
+    t1.Country AS 'País',
     COUNT(t2.Country) - 1 AS 'Número de compatriotas'
 FROM
     w3schools.customers AS t1,
@@ -8,4 +8,5 @@ FROM
 WHERE
     t1.Country = t2.Country
 GROUP BY t1.ContactName , t1.Country
-ORDER BY Nome;
+HAVING `Número de compatriotas` != 0
+ORDER BY `Nome`;

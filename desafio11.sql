8ec0355f9fd44aad254908f03af2c4e5453fb942
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-07-29 20:42:13 -0300

    Desafio 11 OK

---
+++ desafio11.sql
@@ -0,0 +1,8 @@
+SELECT 
+	t1.ContactName AS 'Nome',
+    t1.Country AS 'País', 
+    COUNT(t2.Country) AS 'Número de compatriotas'
+FROM customers AS t1, customers AS t2
+WHERE t1.Country = t2.Country AND t1.ContactName NOT IN (t2.ContactName)
+GROUP BY t1.CustomerID
+ORDER BY t1.ContactName;

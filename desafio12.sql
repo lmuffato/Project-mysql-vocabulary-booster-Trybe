b254439cd9619a3ca039d6558d92c6acef83a83c
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-07-29 14:02:36 -0300

    feat: requirement13

--- desafio12.sql
+++ desafio12.sql
@@ -1,1 +1,22 @@
 -- 12 - Faça um relatório que lista todas as pessoas funcionárias que possuem o mesmo cargo
+SELECT * FROM hr.employees;
+
+-- SELECT
+--   ContactName AS `Nome`,
+--   Country AS `País`,
+--   (
+--     SELECT 
+--       COUNT(*) - 1
+--     FROM hr.employees
+--     WHERE c.Country = Country
+--   ) AS `Número de compatriotas`
+-- FROM hr.employees AS c
+-- HAVING `Número de compatriotas` >= 1
+-- ORDER BY `Nome` ASC;
+
+-- SELECT
+--   COUNT(JOB_ID),
+--   JOB_ID
+-- FROM hr.employees
+-- GROUP BY JOB_ID
+-- HAVING COUNT(JOB_ID) > 1;
--- desafio13.sql
+++ desafio13.sql
@@ -1,2 +1,9 @@
-
 -- 13 - Exibe todos produtos que já tiveram um pedido associado requerindo uma quantidade desse produto maior que 80
+SELECT
+  p.ProductName AS `Produto`,
+  p.Price AS `Preço`
+FROM w3schools.order_details AS d
+INNER JOIN w3schools.products AS p
+ON d.ProductID = p.ProductID
+WHERE d.Quantity > 80
+ORDER BY `Produto` ASC;

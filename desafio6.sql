55dc6fd8ea9d54d29713740022fb87730e954474
Marcus C. H. Filho <mchusemann@gmail.com>
2021-07-29 16:36:56 -0300

    fix(challenge6): Change inner join e.DEPARTMENT_ID to jh.DEPARTMENT_ID

--- desafio6.sql
+++ desafio6.sql
@@ -8,7 +8,7 @@ FROM
   hr.employees e
   INNER JOIN hr.job_history jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
   INNER JOIN hr.jobs j ON jh.JOB_ID = j.JOB_ID
-  INNER JOIN hr.departments d ON e.DEPARTMENT_ID = d.DEPARTMENT_ID
+  INNER JOIN hr.departments d ON jh.DEPARTMENT_ID = d.DEPARTMENT_ID
 ORDER BY
   `Nome completo` DESC,
   `Cargo`;

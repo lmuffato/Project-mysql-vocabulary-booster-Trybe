11cfa1dd228d879c4796f4650d6aceeb34999951
Guilherme de Prais <guilherme.prais@gmail.com>
2021-07-29 11:30:12 -0300

    req 5

--- desafio2.sql
+++ desafio2.sql
@@ -1,5 +1,5 @@
 SELECT
-  job_title AS 'Cargo',
+  job_title AS `Cargo`,
   CASE
     WHEN max_salary BETWEEN 5000
     AND 10000 THEN 'Baixo'
@@ -8,8 +8,8 @@ SELECT
     WHEN max_salary BETWEEN 20001
     AND 30000 THEN 'Alto'
     ELSE 'Altíssimo'
-  END AS 'Nível'
+  END AS `Nível`
 FROM
   hr.jobs
 ORDER BY
-  Cargo ASC;
+  `Cargo` ASC;

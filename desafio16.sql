644348cd68ab5c06d5f9bf7384670ad4e9cb3832
luanlsr <luanfswd@gmail.com>
2021-07-29 15:11:16 -0300

    requisito 16 finalizado

--- desafio16.sql
+++ desafio16.sql
@@ -6,13 +6,13 @@ DELIMITER $$
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(30))
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE total_empregos INT;
-    SELECT COUNT(jh.JOB_ID)
-    FROM hr.job_history AS jh
-    INNER JOIN hr.employees AS e
-    ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
-    WHERE e.EMAIL = 'NKOCHHAR' INTO total_empregos;
-    RETURN total_empregos;
+DECLARE total_empregos INT;
+SELECT COUNT(jh.JOB_ID)
+FROM hr.job_history AS jh
+INNER JOIN hr.employees AS e
+ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+WHERE e.EMAIL = 'NKOCHHAR' INTO total_empregos;
+RETURN total_empregos;
 END $$
 
 DELIMITER ;

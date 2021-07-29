795ef7bc220034afad8c5111e538dfe06972e838
AndreBarroso <andre_barroso17@hotmail.com>
2021-07-29 13:07:15 -0300

    fix: desafio1

--- desafio1.sql
+++ desafio1.sql
@@ -1,9 +1,7 @@
-USE hr;
-
 SELECT COUNTRY_NAME AS 'País',
 CASE 
 WHEN REGION_ID = 1 THEN 'incluído'
 ELSE 'não classificado'
 END AS 'Status Inclusão'
-FROM countries
-ORDER BY COUNTRY_NAME;
+FROM hr.countries AS c
+ORDER BY c.COUNTRY_NAME;

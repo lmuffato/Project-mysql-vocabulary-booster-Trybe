203a297bfe3322cb4b898420455c36354646239e
jlfagundes <jlfagundes@gmail.com>
2021-07-29 20:26:31 -0300

    feat: challenge 5 done

---
+++ desafio5.sql
@@ -0,0 +1,7 @@
+SELECT j.JOB_TITLE AS Cargo,
+  j.MAX_SALARY - j.MIN_SALARY AS `Variação Salarial`,
+  ROUND(j.MIN_SALARY / 12, 2) AS `Média mínima mensal`,
+  ROUND(j.MAX_SALARY / 12, 2) AS `Média máxima mensal`
+FROM hr.jobs AS j
+ORDER BY `Variação Salarial` ASC,
+  Cargo ASC;

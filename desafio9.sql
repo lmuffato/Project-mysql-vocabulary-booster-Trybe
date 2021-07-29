afacc2263b039a57d0741c2c7e5572877c83c952
Gabriel <g.pereira.a.m@gmail.com>
2021-07-28 22:17:38 -0300

    fix(desafio9) - :sparkle: Corrigido

--- desafio9.sql
+++ desafio9.sql
@@ -1,5 +1,5 @@
 SELECT CONCAT(e.firstName,' ',e.lastName) AS 'Nome completo',
-	COUNT(orderID) AS 'Total de pedidos'
+COUNT(orderID) AS 'Total de pedidos'
 FROM w3schools.orders AS o
 INNER JOIN w3schools.employees AS e
 ON e.employeeid = o.employeeid

bb129fc8b56fa08ef31ff12d24aba34e9f98079f
luanlsr <luanfswd@gmail.com>
2021-07-29 02:10:04 -0300

    requisito 12 finalizado

--- desafio13.sql
+++ desafio13.sql
@@ -1,1 +1,8 @@
 -- 13 - Exibe todos produtos que já tiveram um pedido associado requerindo uma quantidade desse produto maior que 80
+SELECT ProductName AS `Produto`,
+Price AS `Preço`
+FROM w3schools.products AS p
+INNER JOIN w3schools.order_details AS od
+ON od.ProductID = p.ProductID
+WHERE Quantity > 80
+ORDER BY `Produto`;

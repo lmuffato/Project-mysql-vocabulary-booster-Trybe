b7ff2a2e66e2f09631edad952fa6fb06808f91da
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2021-07-29 12:01:01 -0300

    Fix: desafio6

--- desafio6.sql
+++ desafio6.sql
@@ -5,10 +5,10 @@ j.JOB_TITLE `Cargo`,
 h.START_DATE `Data de início do cargo`,
 d.DEPARTMENT_NAME `Departamento`
 FROM hr.employees AS e
-INNER JOIN hr.jobs AS j
-ON e.JOB_ID = j.JOB_ID
-INNER JOIN hr.departments AS d
-ON e.DEPARTMENT_ID = d.DEPARTMENT_ID
 INNER JOIN hr.job_history AS h
 ON h.EMPLOYEE_ID = e.EMPLOYEE_ID
+INNER JOIN hr.departments AS d
+ON h.DEPARTMENT_ID = d.DEPARTMENT_ID
+INNER JOIN hr.jobs AS j
+ON h.JOB_ID = j.JOB_ID
 ORDER BY `Nome completo` DESC, `Cargo` ASC;

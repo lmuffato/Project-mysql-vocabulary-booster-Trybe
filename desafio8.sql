eb075e90f1df1a6d0b3eb64cbeaf0d904490e600
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-07-28 23:35:34 -0300

    feat: requirement8

--- desafio8.sql
+++ desafio8.sql
@@ -1,1 +1,21 @@
 -- 8 - Exibe todas as pessoas consumidoras cujos pedidos já foram enviados pelas empresas "Speedy Express" ou "United Package"
+SELECT
+  (
+    SELECT
+      ContactName
+    FROM w3schools.customers
+    WHERE o.CustomerID = CustomerID
+  ) AS `Nome de contato`,
+  (
+    SELECT
+      ShipperName
+    FROM w3schools.shippers
+    WHERE o.ShipperID = ShipperID
+  ) AS `Empresa que fez o envio`,
+  OrderDate AS `Data do pedido`
+FROM w3schools.orders AS o
+WHERE ShipperID in (1, 2)
+ORDER BY 
+  `Nome de contato` ASC, 
+  `Empresa que fez o envio` ASC,
+  `Data do pedido` ASC;

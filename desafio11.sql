99eeeca07a4b4f72d681f8ccad2b2cd6e9ad0ad4
Felipe Muller <felipeavmuller@gmail.com>
2021-07-29 00:01:29 -0300

    FIX requisito 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,5 +1,6 @@
 SELECT t1.ContactName AS 'Nome', t1.Country AS 'País', COUNT(t2.Country) - 1 AS 'Número de compatriotas'
 FROM w3schools.customers t1, w3schools.customers t2
 WHERE t1.Country = t2.Country
-GROUP BY t1.ContactName
+GROUP BY `Nome`
+HAVING `Número de compatriotas` > 0
 ORDER BY `Nome`;

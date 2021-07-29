b3100795c34b917aeeb37c4577d88bc48528f0e5
hheloisa <hhackenhaar@gmail.com>
2021-07-28 23:02:11 -0300

    Req. 15 - remove tabs

--- desafio15.sql
+++ desafio15.sql
@@ -2,10 +2,10 @@ USE hr;
 DELIMITER $$
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(20))
 BEGIN
-	SELECT round(AVG(te.salary), 2) AS `Média salarial`
-    FROM jobs AS tj
-	INNER JOIN employees AS te
-	WHERE tj.job_id = te.job_id AND cargo = tj.job_title;
+SELECT round(AVG(te.salary), 2) AS `Média salarial`
+FROM jobs AS tj
+INNER JOIN employees AS te
+WHERE tj.job_id = te.job_id AND cargo = tj.job_title;
 END $$
 
 DELIMITER ;

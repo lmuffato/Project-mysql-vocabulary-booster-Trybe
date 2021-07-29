cb10233184ad998e031ee978a62d3d926e86f98a
biazidioti <bzferreira.bio@gmail.com>
2021-07-28 23:44:15 -0300

    Challenge 8 - mtfcker dessas colunas

---
+++ desafio8.sql
@@ -0,0 +1,14 @@
+-- 8 - Exibe todas as pessoas consumidoras cujos pedidos já foram enviados pelas empresas "Speedy Express" ou "United Package"
+
+SELECT c.ContactName AS 'Nome de contato',
+  s.ShipperName AS 'Empresa que fez o envio',
+  o.OrderDate AS 'Data do pedido'
+FROM 
+  w3schools.customers c
+    INNER JOIN 
+  w3schools.orders o ON o.CustomerID = c.CustomerID
+    INNER JOIN 
+  w3schools.shippers s ON s.ShipperID = o.ShipperID
+WHERE 
+  s.ShipperName IN ('Speedy Express', 'United Package')
+ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;

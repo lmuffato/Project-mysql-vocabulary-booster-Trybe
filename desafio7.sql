a01a9fe20586d17d73f88c88fa87ce53fced9e3d
Marina Alane <marina.alane@outlook.com>
2021-07-29 14:07:43 -0300

    fix req7 - AGORA VAI

--- desafio7.sql
+++ desafio7.sql
@@ -1,8 +1,11 @@
-SELECT UPPER(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome Completo',
-s.START_DATE AS 'Data de início',
-e.SALARY AS 'Salário'
-FROM hr.employees AS e
-INNER JOIN hr.job_history AS s
-ON e.EMPLOYEE_ID = s.EMPLOYEE_ID
-WHERE MONTH(s.START_DATE) IN (1, 2, 3)
+SELECT
+    UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS `Nome completo`,
+    jh.START_DATE AS `Data de início`,
+    e.SALARY AS Salário
+FROM
+    hr.employees e
+        INNER JOIN
+    hr.job_history jh ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+WHERE
+    MONTH(jh.START_DATE) BETWEEN 1 AND 3
 ORDER BY `Nome completo`, `Data de início`;

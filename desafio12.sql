f04c094df39ff0a98ac8736f8d35fb0d90c4e9f3
Felipe Muller <felipeavmuller@gmail.com>
2021-07-29 00:46:24 -0300

    FIX requisito 12

--- desafio12.sql
+++ desafio12.sql
@@ -4,5 +4,5 @@ CONCAT(t2.FIRST_NAME, ' ', t2.LAST_NAME) AS 'Nome completo funcionário 2',
 t2.SALARY AS 'Salário funcionário 2', t2.PHONE_NUMBER AS 'Telefone funcionário 2'
 FROM hr.employees t1, hr.employees t2
 WHERE t1.JOB_ID = t2.JOB_ID AND CONCAT(t1.FIRST_NAME, ' ', t1.LAST_NAME) <> CONCAT(t2.FIRST_NAME, ' ', t2.LAST_NAME)
-GROUP BY `Nome completo funcionário 1`, `Nome completo funcionário 2`
+GROUP BY CONCAT(t1.FIRST_NAME, ' ', t1.LAST_NAME), t1.SALARY, t1.PHONE_NUMBER
 ORDER BY `Nome completo funcionário 1`, `Nome completo funcionário 2`;

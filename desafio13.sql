6602ba2ec8957b1f77f87352bcce73886abfe76c
maricegalla <maricegalla@gmail.com>
2021-07-28 21:22:01 -0300

    Desafio 13

---
+++ desafio13.sql
@@ -0,0 +1,8 @@
+SELECT
+p.ProductName AS 'Produto',
+p.Price AS 'Preço'
+FROM w3schools.products AS p
+INNER JOIN w3schools.order_details AS o
+ON p.ProductID = o.ProductID
+WHERE o.Quantity > 80
+ORDER BY p.ProductName ASC;

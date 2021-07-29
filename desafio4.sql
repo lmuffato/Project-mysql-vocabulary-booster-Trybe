f8c8ea7d86566fa11dd3e1f0ef6d9b7a06d8343e
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-07-29 06:08:43 -0300

    requisito 4 feito

---
+++ desafio4.sql
@@ -0,0 +1,14 @@
+SELECT
+  j.job_title AS `Cargo`,
+  ROUND(AVG(e.salary), 2) AS `Média salarial`,
+CASE
+  WHEN AVG(e.salary) <= 5800 THEN 'Júnior'
+  WHEN AVG(e.salary) <= 7500 THEN 'Pleno'
+  WHEN AVG(e.salary) <= 10500 THEN 'Sênior'
+  ELSE 'CEO'
+END AS `Senioridade`
+FROM hr.employees AS e
+INNER JOIN hr.jobs AS j
+ON e.job_id = j.job_id
+GROUP BY e.job_id
+ORDER BY `Média salarial`, `Cargo`;

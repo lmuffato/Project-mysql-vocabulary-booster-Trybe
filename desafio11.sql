a67af1ff7a7a222831527205a51fddf8eae912f9
Marcus C. H. Filho <mchusemann@gmail.com>
2021-07-29 16:12:43 -0300

    fix(challenge11): add a selfjoin and a having

--- desafio11.sql
+++ desafio11.sql
@@ -1,12 +1,11 @@
+-- Carlos Gabriel Campos auxiliou no Having
 SELECT 
   c1.ContactName AS 'Nome',
   c1.Country AS 'País',
-  (SELECT 
-    COUNT(*)-1 
-  FROM 
-    w3schools.customers c2 
-  WHERE 
-    c2.Country LIKE c1.Country)
+  COUNT(c1.Country)-1
 AS 'Número de compatriotas'
-FROM w3schools.customers c1
+FROM w3schools.customers c1, w3schools.customers c2
+WHERE c1.Country = c2.Country
+GROUP BY `País`, `Nome`
+HAVING `Número de compatriotas` > 0
 ORDER BY `Nome`;

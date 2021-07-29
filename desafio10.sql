c6b64a6c2ecd49c8b1d3ed2cb9b335228439fda3
Gabriel <g.pereira.a.m@gmail.com>
2021-07-28 22:15:05 -0300

    fix(desafio10) - :sparkle: Corrigido

--- desafio10.sql
+++ desafio10.sql
@@ -1,6 +1,6 @@
 SELECT p.productName AS 'Produto', MIN(o.quantity) AS 'Mínima',
-	MAX(o.quantity) AS 'Máxima',
-    ROUND(AVG(o.quantity), 2) AS 'Média'
+MAX(o.quantity) AS 'Máxima',
+ROUND(AVG(o.quantity), 2) AS 'Média'
 FROM w3schools.order_details AS o
 INNER JOIN w3schools.products AS p
 ON p.productID = o.productID

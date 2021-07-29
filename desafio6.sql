00db5e0b8ff8232d73c31ec346f05875918a74d3
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-07-29 02:06:18 -0300

    Desafio 6: Faltou colocar espaço dentro das aspas simples concatenadas

--- desafio6.sql
+++ desafio6.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT(E.FIRST_NAME,'',E.LAST_NAME) AS 'Nome completo', J.JOB_TITLE AS 'Cargo',
+SELECT CONCAT(E.FIRST_NAME,' ',E.LAST_NAME) AS 'Nome completo', J.JOB_TITLE AS 'Cargo',
 JH.START_DATE AS 'Data de início do cargo', D.DEPARTMENT_NAME AS 'Departamento'
 FROM hr.job_history JH
 INNER JOIN hr.jobs J

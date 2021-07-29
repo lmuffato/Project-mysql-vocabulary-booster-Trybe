17aa962b065c231c2cd504231bddd9d1078905af
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-07-29 01:00:49 -0300

    Desafio 5: Correçao - group by retirado

--- desafio5.sql
+++ desafio5.sql
@@ -1,5 +1,4 @@
 SELECT JOB_TITLE AS 'Cargo', (MAX_SALARY - MIN_SALARY) AS 'Variação Salarial',
-ROUND(AVG(MIN_SALARY/12),2) AS 'Média mínima mensal', ROUND(AVG(MAX_SALARY/12),2) AS 'Média máxima mensal'
+ROUND(MIN_SALARY/12,2) AS 'Média mínima mensal', ROUND(MAX_SALARY/12,2) AS 'Média máxima mensal'
 FROM hr.jobs
-GROUP BY JOB_TITLE
 ORDER BY `Variação Salarial`, `Cargo`;

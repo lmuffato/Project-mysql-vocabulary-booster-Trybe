a70590968121f6dd05596ebbc45afd4352a3fb14
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-07-28 21:42:23 -0300

    Challenge 11: Fix

--- desafio11.sql
+++ desafio11.sql
@@ -1,9 +1,11 @@
-SELECT t1.ContactName AS Nome,
-       t1.Country AS País,
-       Count(t2.Country) -1 AS 'Número de compatriotas'
- FROM w3schools.customers AS t1,
- w3schools.customers AS t2
- WHERE t1.Country = t2.Country
- GROUP BY t1.ContactName, t1.Country
- ORDER BY `Nome`;
- 
+SELECT 
+    t1.ContactName AS Nome,
+    t1.Country AS País,
+    COUNT(t2.Country) - 1 AS 'Número de compatriotas'
+FROM
+    w3schools.customers AS t1,
+    w3schools.customers AS t2
+WHERE
+    t1.Country = t2.Country
+GROUP BY t1.ContactName , t1.Country
+ORDER BY Nome;

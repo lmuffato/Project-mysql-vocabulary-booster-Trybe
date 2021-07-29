285f92b5731d4b8c2052d0fa6acaee0d615be629
Perycles reis <perycles_reis@hotmail.com>
2021-07-29 13:16:57 -0300

    desafio 10

--- desafio10.sql
+++ desafio10.sql
@@ -11,7 +11,12 @@
 
 SELECT
   ProductName AS `Produto`,
-
+  MIN(Quantity) AS `Mínima`,
+  MAX(Quantity) AS `Máxima`,
+  ROUND(AVG(Quantity), 2) AS `Média`
 FROM w3schools.products P
 INNER JOIN w3schools.order_details OD
-ON OD.ProductID = P.ProductID;
+ON OD.ProductID = P.ProductID
+GROUP BY `Produto`
+HAVING `Média` > 20
+ORDER BY `Média`, `Produto`;

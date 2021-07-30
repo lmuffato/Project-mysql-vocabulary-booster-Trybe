0a63209e9f15c7747e2de9128eed841dc0a78af3
Rodrigo Queiroz <rodrigoandradequeiroz@gmail.com>
2021-07-29 23:21:52 -0300

    feat: req14

--- desafio8.sql
+++ desafio8.sql
@@ -5,4 +5,4 @@ ON o.CustomerID =  c.CustomerID
 INNER JOIN w3schools.shippers as s
 ON s.ShipperID = o.ShipperID
 WHERE s.ShipperName IN ('Speedy Express', 'United Package')
-ORDER BY `Nome de contato`, `Empresa que fez o envio`;
+ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;

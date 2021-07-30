373a47de807e1bcbf031fcddd9af22a0ba8317b8
Luiz <luizfvaccarif@gmail.com>
2021-07-30 19:19:10 -0300

    fix: req11

--- desafio11.sql
+++ desafio11.sql
@@ -3,6 +3,6 @@ c1.Country AS `País`,
 Count("País")-1 AS `Número de compatriotas`
 FROM w3schools.customers AS c1, w3schools.customers AS c2
 WHERE c1.Country = c2.Country
-GROUP BY `Nome`
+GROUP BY `Nome`, `País`
 HAVING `Número de compatriotas` <> 0
 ORDER BY `Nome`;

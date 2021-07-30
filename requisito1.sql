33d96b92c7558ce1fbc0dc221c427abb58235733
Carlos Campo <carlosgabrielcampo@gmail.com>
2021-07-30 13:39:10 -0300

    requirement 1 order and linter fixed

--- requisito1.sql
+++ requisito1.sql
@@ -1,1 +1,2 @@
-SELECT c.COUNTRY_NAME, IF(c.REGION_ID = 1, "incluído", "não incluído") AS "Status Inclusão" FROM countries c;
+SELECT c.COUNTRY_NAME, IF(c.REGION_ID = 1, "incluído", "não incluído") AS "Status Inclusão" FROM countries c
+ORDER BY c.COUNTRY_NAME ASC;

81f5f04133aae52354a20f7db2ac24b051fc55ea
Carlos Campo <carlosgabrielcampo@gmail.com>
2021-07-30 17:13:11 -0300

    requirement 4 completed

---
+++ desafio4.sql
@@ -0,0 +1,11 @@
+SELECT j.JOB_TITLE AS 'Cargo', AVG(e.SALARY) AS "Média salarial", 
+CASE
+        WHEN AVG(e.SALARY) <= 5800 THEN 'Júnior'
+        WHEN AVG(e.SALARY) <= 7500 THEN 'Pleno'
+        WHEN AVG(e.SALARY) <= 10500 THEN 'Sênior'
+        ELSE 'CEO' 
+        END AS 'Senioridade' FROM employees e
+INNER JOIN jobs j
+ON e.JOB_ID = j.JOB_ID
+GROUP BY e.JOB_ID
+ORDER BY 2 ASC;

0fcd36dafe8b25dc789bbfd35c8ae6ee3dcafbaf
Carlos Campo <carlosgabrielcampo@gmail.com>
2021-07-30 18:17:03 -0300

    requirement 4 and 5 fixed

--- desafio5.sql
+++ desafio5.sql
@@ -1,4 +1,3 @@
-SELECT j.JOB_TITLE AS 'Cargo', (-j.MIN_SALARY + j.MAX_SALARY) AS "Variação Salarial", ROUND(MIN(j.MIN_SALARY )/12,2) AS "Média mínima mensal", 
-ROUND(MAX(j.MAX_SALARY)/12, 2) AS "Média máxima mensal" FROM jobs j
-GROUP BY j.JOB_TITLE
+SELECT JOB_TITLE AS "Cargo", (j.MAX_SALARY - j.MIN_SALARY) AS "Variação Salarial" , ROUND(MIN_SALARY/12, 2) AS "Média mínima mensal", 
+ROUND(MAX_SALARY/12, 2) AS "Média máxima mensal" FROM jobs j
 ORDER BY 2 ASC, 1 ASC;

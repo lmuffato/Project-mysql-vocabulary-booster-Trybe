47be2e5507981cbb40675350f65a2b9310b2c0a1
Maria Baeta <m.martinsdg@gmail.com>
2021-07-29 21:12:26 -0300

    Adequação do lint

--- desafio15.sql
+++ desafio15.sql
@@ -1,17 +1,13 @@
 USE hr;
 DELIMITER $$
-CREATE PROCEDURE buscar_media_por_cargo(
-	IN cargo VARCHAR(200)
-)
+CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(200))
 BEGIN
-	SELECT 
-    ROUND(AVG(e.SALARY), 2) AS 'Média salarial'
-    FROM hr.employees AS e
-    INNER JOIN hr.jobs AS jb
-    ON e.JOB_ID = jb.JOB_ID
-    WHERE jb.JOB_TITLE = cargo
-    GROUP BY jb.JOB_TITLE;
+SELECT
+ROUND(AVG(e.SALARY), 2) AS 'Média salarial'
+FROM hr.employees AS e
+INNER JOIN hr.jobs AS jb
+ON e.JOB_ID = jb.JOB_ID
+WHERE jb.JOB_TITLE = cargo
+GROUP BY jb.JOB_TITLE;
 END $$
 DELIMITER ;
-
-CALL buscar_media_por_cargo('Programmer')

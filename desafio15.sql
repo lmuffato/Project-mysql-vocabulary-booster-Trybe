efa8e4bdfa804dec0c49f505321fc8ca7657a36b
Sergio Martins <sergiomos2010@gmail.com>
2021-07-30 10:49:34 -0300

    fix: desafio 15

--- desafio15.sql
+++ desafio15.sql
@@ -1,11 +1,12 @@
-USE hr;
+DROP PROCEDURE IF EXISTS `buscar_media_por_cargo`;
 
+USE hr;
 DELIMITER $$;
-CREATE PROCEDURE buscar_media_por_cargo(IN jobTitle VARCHAR(50), OUT salaryAverage DOUBLE)
+
+CREATE PROCEDURE buscar_media_por_cargo(IN jobTitle VARCHAR(50))
 BEGIN
   SELECT ROUND(AVG(SALARY), 2) AS `Média salarial` FROM employees AS E
-  WHERE E.JOB_ID = (SELECT J.JOB_ID FROM jobs AS J WHERE J.JOB_TITLE = jobTitle)
-  INTO salaryAverage;
+  WHERE E.JOB_ID = (SELECT J.JOB_ID FROM jobs AS J WHERE J.JOB_TITLE = jobTitle);
 END $$
 
 DELIMITER ;

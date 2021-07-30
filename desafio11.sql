8aac09e02130798f796cacfb885afb144b028e10
Sergio Martins <sergiomos2010@gmail.com>
2021-07-30 10:11:41 -0300

    fix: desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -3,4 +3,5 @@ SELECT
   c.Country AS 'País',
   (SELECT COUNT(cu.Country) FROM w3schools.customers AS cu WHERE cu.Country = c.Country) - 1 AS 'Número de compatriotas'
 FROM w3schools.customers AS c
+WHERE (SELECT COUNT(cu.Country) FROM w3schools.customers AS cu WHERE cu.Country = c.Country) > 1
 ORDER BY Nome;

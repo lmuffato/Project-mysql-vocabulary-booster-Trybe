ab6d08da2ae9aeb0a315853d4e64fb807cc8b953
Luiz <luizfvaccarif@gmail.com>
2021-07-30 15:41:02 -0300

    fix: req5(4)

--- desafio5.sql
+++ desafio5.sql
@@ -1,7 +1,7 @@
 SELECT JOB_TITLE AS `Cargo`,
 MAX_SALARY - MIN_SALARY AS `Variação Salarial`,
-ROUND((MIN_SALARY / 12), 2) AS `Média mínima mensal`,
-ROUND((MAX_SALARY / 12), 2) AS `Média máxima mensal`
+ROUND(MIN_SALARY / 12, 2) AS `Média mínima mensal`,
+ROUND(MAX_SALARY / 12, 2) AS `Média máxima mensal`
 FROM hr.jobs
 GROUP BY `Cargo`
 ORDER BY `Variação Salarial`, `Cargo`;

1f1f2ab4dcc5154894ea58f4260abec2f4876fd1
WillianMendes <willianmendesbr@gmail.com>
2021-07-30 16:20:51 -0300

    fix: requisito 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,10 +1,14 @@
 SELECT
   c1.ContactName AS Nome,
   c1.Country AS País,
-  COUNT(c1.Country) AS 'Número de compatriotas'
+  COUNT(c1.Country) - 1 AS 'Número de compatriotas'
 FROM
   customers AS c1
 INNER JOIN
   customers AS c2 ON c1.Country = c2.Country
 GROUP BY
-  Nome, País;
+  Nome, País
+HAVING
+  `Número de compatriotas` >= 1
+ORDER BY
+  Nome;

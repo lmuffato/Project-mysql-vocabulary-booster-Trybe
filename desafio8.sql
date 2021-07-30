94d8bc9585f51048c8811d1b98478f50b3ed9913
Luiz <luizfvaccarif@gmail.com>
2021-07-30 14:57:36 -0300

    fix: req8

--- desafio8.sql
+++ desafio8.sql
@@ -1,7 +1,7 @@
 USE w3schools;
 SELECT c.ContactName AS `Nome de contato`,
 s.ShipperName AS `Empresa que fez o envio`,
- o.OrderDate AS `Data do pedido`
+o.OrderDate AS `Data do pedido`
 FROM customers AS c
 INNER JOIN orders AS o ON o.CustomerID = c.CustomerID
 INNER JOIN shippers AS s ON s.ShipperID = o.ShipperID

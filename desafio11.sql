dda7f4cea8bb0d325951bfb829b90a7ed358a339
Vinicius Rodrigues <v.rodriguesjk@gmail.com>
2021-07-30 14:25:59 -0300

    Fix: Requisitos 4 e 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,11 +1,13 @@
 SELECT 
   C1.ContactName AS `Nome`,
   C1.Country AS `País`,
-  COUNT(C1.Country) - 1 AS `Número de compatriotas`
+  COUNT(C1.Country) AS `Número de compatriotas`
 FROM 
   w3schools.customers AS C1,
   w3schools.customers AS C2
 WHERE
   C1.Country = C2.Country
+AND
+  C1.ContactName <> C2.ContactName
 GROUP BY `Nome`, `País`
 ORDER BY C1.ContactName;
--- desafio4.sql
+++ desafio4.sql
@@ -1,4 +1,4 @@
-SELECT JOB_TITLE AS `Cargo`,
+SELECT J.JOB_TITLE AS `Cargo`,
 ROUND(AVG(E.SALARY), 2) AS `Média salarial`,
 CASE
   WHEN AVG(E.SALARY) BETWEEN 2000 AND 5800 THEN 'Júnior'
@@ -6,8 +6,9 @@ CASE
   WHEN AVG(E.SALARY) BETWEEN 7501 AND 10500 THEN 'Sênior'
   ELSE 'CEO'
 END AS `Senioridade`
-FROM hr.jobs AS J
-INNER JOIN hr.employees AS E
-ON J.JOB_ID = E.JOB_ID
+FROM
+  hr.jobs AS J
+INNER JOIN
+  hr.employees AS E ON J.JOB_ID = E.JOB_ID
 GROUP BY Cargo
-ORDER BY `Média salarial` ASC;
+ORDER BY `Média salarial`, `Cargo`;

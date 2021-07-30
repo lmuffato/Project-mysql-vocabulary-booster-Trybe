330a98f8624dd66556e00a1bb1543e94eeb3055d
AndreBarroso <andre_barroso17@hotmail.com>
2021-07-29 21:24:31 -0300

    fix: desafios 7 e  8

--- desafio7.sql
+++ desafio7.sql
@@ -1,4 +1,4 @@
-SELECT UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo', jh.START_DATE AS 'Data de início', e.SALARY
+SELECT UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo', jh.START_DATE AS 'Data de início', e.SALARY AS 'Salário'
 FROM hr.employees AS e
 INNER JOIN hr.job_history AS jh
 ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
--- desafio8.sql
+++ desafio8.sql
@@ -1,1 +1,6 @@
-SELECT CONTACTNAME AS 'Nome de contato' FROM w3schools.customers
+SELECT cus.CONTACTNAME AS 'Nome de contato', sh.ShipperName AS 'Empresa que fez o envio',od.OrderDate AS 'Data do pedido' FROM w3schools.customers AS cus
+INNER JOIN w3schools.orders AS od
+INNER JOIN w3schools.shippers AS sh
+ON cus.CustomerID = od.CustomerID AND od.ShipperID = sh.ShipperID
+WHERE sh.ShipperName IN ('Speedy Express', 'United Package')
+ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;

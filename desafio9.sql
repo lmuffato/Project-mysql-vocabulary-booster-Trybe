e67708d2e6288d7302d897bb6bccc4949cbe8c64
Luiz <luizfvaccarif@gmail.com>
2021-07-30 16:43:11 -0300

    fix: req9

--- desafio9.sql
+++ desafio9.sql
@@ -1,7 +1,6 @@
-USE w3schools;
 SELECT CONCAT(e.FirstName, ' ', e.LastName) AS `Nome completo`,
 Count(*) AS `Total de pedidos`
-FROM employees AS e
-INNER JOIN orders AS o ON o.EmployeeID = e.EmployeeID
+FROM w3schools.employees AS e
+INNER JOIN w3schools.orders AS o ON o.EmployeeID = e.EmployeeID
 GROUP BY `Nome completo`
 ORDER BY `Total de pedidos`;

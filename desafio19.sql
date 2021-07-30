4cd781240c68e4e4f1e5a33d8cb77f3d727ee974
gabrieldeori <deori.pessoal@gmail.com>
2021-07-30 11:14:18 -0300

    Correcting CC issues

--- desafio19.sql
+++ desafio19.sql
@@ -3,13 +3,13 @@ CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano (mnth INT, y
 RETURNS INT READS SQL DATA
 BEGIN
 	DECLARE retornado INT;
-    SELECT 
-    count(EMPLOYEE_ID)
+SELECT 
+    COUNT(EMPLOYEE_ID)
 FROM
     hr.employees
 WHERE
-	MONTH(HIRE_DATE) = mnth AND
-	YEAR(HIRE_DATE) = yr INTO retornado;
+    MONTH(HIRE_DATE) = mnth
+        AND YEAR(HIRE_DATE) = yr INTO retornado;
     RETURN retornado;
 END $$
 DELIMITER ;

ed1a785db4d86de13d138abcfaa6a4cee227ed8d
gabrieldeori <deori.pessoal@gmail.com>
2021-07-30 14:59:17 -0300

    desafio5 BUGFIX for Online Evaluator, removing ROUND from 'Variacao salarial'

--- desafio5.sql
+++ desafio5.sql
@@ -1,6 +1,6 @@
 SELECT 
     jo.JOB_TITLE AS 'Cargo',
-    ROUND(jo.MAX_SALARY - jo.MIN_SALARY, 2) AS 'Variação Salarial',
+    jo.MAX_SALARY - jo.MIN_SALARY AS 'Variação Salarial',
     ROUND(jo.MIN_SALARY / 12, 2) AS 'Média mínima mensal',
     ROUND(jo.MAX_SALARY / 12, 2) AS 'Média máxima mensal'
 FROM

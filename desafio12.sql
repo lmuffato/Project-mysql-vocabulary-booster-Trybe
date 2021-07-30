c28c93520279e79e7a14253dc3e2d8e59d2cf14d
Tiago Santos <tiago881@gmail.com>
2021-07-30 19:18:16 -0300

    to fix 12

--- desafio12.sql
+++ desafio12.sql
@@ -1,10 +1,4 @@
-SELECT  CONCAT(e1.first_name, ' ', e1.last_name) AS 'Nome completo funcionário 1',
-e1.salary AS 'Salário funcionário 1',
-e1.phone_number AS 'Telefone funcionário 1',
-CONCAT(e2.first_name, ' ', e2.last_name) AS 'Nome completo funcionário 2',
-e2.salary AS 'Salário funcionário 2',
-e2.phone_number AS 'Telefone funcionário 2'
-FROM employees AS e1, employees  AS e2
-WHERE e1.job_id = e2.job_id
-AND CONCAT(e1.first_name, ' ', e1.last_name) <> CONCAT(e2.first_name, ' ', e2.last_name)
+SELECT  CONCAT(e1.first_name, ' ', e1.last_name) AS 'Nome completo funcionário 1', e1.salary AS 'Salário funcionário 1', e1.phone_number AS 'Telefone funcionário 1',
+CONCAT(e2.first_name, ' ', e2.last_name) AS 'Nome completo funcionário 2', e2.salary AS 'Salário funcionário 2', e2.phone_number AS 'Telefone funcionário 2' 
+FROM employees AS e1, employees  AS e2 WHERE e1.job_id = e2.job_id AND CONCAT(e1.first_name, ' ', e1.last_name) <> CONCAT(e2.first_name, ' ', e2.last_name)
 ORDER BY `Nome completo funcionário 1` ASC, `Nome completo funcionário 2` ASC;

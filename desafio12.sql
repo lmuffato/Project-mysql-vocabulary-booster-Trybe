d40073d6037fb6207964e544db07a6958f8905d0
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-07-29 21:49:40 -0300

    feat: requirement12

--- desafio12.sql
+++ desafio12.sql
@@ -20,3 +20,18 @@ SELECT * FROM hr.employees;
 -- FROM hr.employees
 -- GROUP BY JOB_ID
 -- HAVING COUNT(JOB_ID) > 1;
+
+
+SELECT
+  CONCAT(e1.FIRST_NAME, " ", e1.LAST_NAME) AS `Nome completo funcionário 1`,
+  e1.SALARY AS `Salário funcionário 1`,
+  e1.PHONE_NUMBER AS `Telefone funcionário 1`,
+  CONCAT(e2.FIRST_NAME, " ", e2.LAST_NAME) AS `Nome completo funcionário 2`,
+  e2.SALARY AS `Salário funcionário 2`,
+  e2.PHONE_NUMBER AS `Telefone funcionário 2`
+FROM hr.employees AS e1
+CROSS JOIN hr.employees AS e2
+ON e1.JOB_ID = e2.JOB_ID
+WHERE e1.JOB_ID = e2.JOB_ID AND e1.PHONE_NUMBER <> e2.PHONE_NUMBER
+ORDER BY `Nome completo funcionário 1` ASC, `Nome completo funcionário 2` ASC;
+

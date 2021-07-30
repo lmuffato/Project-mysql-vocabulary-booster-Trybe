1fd4bb5a6ae905621a0b6c1787cd38ab797f94a6
Sergio Martins <sergiomos2010@gmail.com>
2021-07-30 15:44:03 -0300

    fix: desafio 17

--- desafio17.sql
+++ desafio17.sql
@@ -1,12 +1,13 @@
 DROP TRIGGER IF EXISTS `setOrderDate`;
 
+USE w3schools;
 DELIMITER $$
 
 CREATE TRIGGER setOrderDate 
-  BEFORE INSERT ON w3schools.orders
+  BEFORE INSERT ON orders
   FOR EACH ROW
 BEGIN
-  SET NEW.OrderDate = DATE(NOW())
+  SET NEW.OrderDate = DATE(NOW());
 END $$
 
 DELIMITER ;

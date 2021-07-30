6070292d9b5b614543925ad0dde64ef78dc402e5
rogerio-lambert <rogerio.cl@gmail.com>
2021-07-30 00:40:09 -0300

    Correção NO desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -4,4 +4,8 @@ SELECT
   (SELECT COUNT(C2.Country) FROM w3schools.customers AS C2
     WHERE C2.Country = C1.Country AND C1.CustomerID <> C2.CustomerID) AS `Número de compatriotas`
 FROM w3schools.customers AS C1
+WHERE EXISTS (
+  SELECT C2.Country FROM w3schools.customers AS C2
+  WHERE C2.Country = C1.Country AND C1.CustomerID <> C2.CustomerID
+)
 ORDER BY `Nome`;

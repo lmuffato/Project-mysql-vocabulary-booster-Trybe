c6c85b3c5c93c77af264c43389f99f980a984e8e
rogerio-lambert <rogerio.cl@gmail.com>
2021-07-29 23:33:56 -0300

    implementação do desafio 12

--- desafio11.sql
+++ desafio11.sql
@@ -2,6 +2,6 @@ SELECT
   ContactName AS `Nome`,
   Country AS `País`,
   (SELECT COUNT(C2.Country) FROM w3schools.customers AS C2
-    WHERE C2.Country = C1.Country) AS `Número de compatriotas`
+    WHERE C2.Country = C1.Country AND C1.CustomerID <> C2.CustomerID) AS `Número de compatriotas`
 FROM w3schools.customers AS C1
 ORDER BY `Nome`;
---
+++ desafio12.sql
@@ -0,0 +1,10 @@
+SELECT
+  CONCAT(E1.FIRST_NAME, ' ', E1.LAST_NAME) AS  `Nome completo funcionário 1`,
+  E1.SALARY AS `Salário funcionário 1`,
+  E1.PHONE_NUMBER AS `Telefone funcionário 1`,
+  CONCAT(E2.FIRST_NAME, ' ', E2.LAST_NAME) AS  `Nome completo funcionário 2`,
+  E2.SALARY AS `Salário funcionário 2`,
+  E2.PHONE_NUMBER AS `Telefone funcionário 2`
+FROM hr.employees AS E1, hr.employees AS E2
+WHERE E1.JOB_ID = E2.JOB_ID AND E1.EMPLOYEE_ID <> E2.EMPLOYEE_ID
+ORDER BY `Salário funcionário 1` ASC, `Salário funcionário 2` ASC;

a92923f4df5c9f985ccfa814aeb2eb0879d47b1a
Felipe Muller <felipeavmuller@gmail.com>
2021-07-30 00:53:43 -0300

    FIX code climate

--- desafio16.sql
+++ desafio16.sql
@@ -1,17 +1,15 @@
-  USE hr;
-  DELIMITER $$
+USE hr;
+DELIMITER $$
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(50))
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE numero_empregos INT;
+SELECT COUNT(j.EMPLOYEE_ID) FROM hr.job_history j
+INNER JOIN hr.employees e ON j.EMPLOYEE_ID = e.EMPLOYEE_ID
+WHERE email = e.EMAIL
+INTO numero_empregos;
+RETURN numero_empregos;
+END $$
   
-  CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(50))
-  RETURNS INT READS SQL DATA
-  BEGIN
-  DECLARE numero_empregos INT;
-  SELECT COUNT(j.EMPLOYEE_ID) FROM hr.job_history j
-  INNER JOIN hr.employees e ON j.EMPLOYEE_ID = e.EMPLOYEE_ID
-  WHERE email = e.EMAIL
-  INTO numero_empregos;
-  RETURN numero_empregos;
-  END $$
-  
-  DELIMITER ;
-  
-  
+DELIMITER ;

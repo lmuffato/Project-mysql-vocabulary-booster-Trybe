64f1e51cc57d0c0bd163a7a05b81ac1b18c38c12
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-07-30 13:26:01 -0300

    requisito 9

--- desafio9.sql
+++ desafio9.sql
@@ -1,6 +1,6 @@
 SELECT 
 CONCAT(e.FirstName,' ',e.LastName) AS 'Nome completo',
-COUNT(o.OrderID) AS 'Total de Pedidos'
+COUNT(o.OrderID) AS 'Total de pedidos'
 FROM w3schools.employees AS e
 INNER JOIN w3schools.orders AS o
 ON e.EmployeeID = o.EmployeeID

db59b5053805a6bff130b75d174559fbc353f5ce
Fernando Castello Branco <fernandocastellob@gmail.com>
2021-07-30 13:53:14 -0300

    fix:req06 e 07

--- desafio6.sql
+++ desafio6.sql
@@ -1,10 +1,12 @@
 SELECT CONCAT(FIRST_NAME, ' ', LAST_NAME) AS 'Nome completo',
 job_title AS 'Cargo',
-hire_date AS 'Data de início do cargo',
+start_date AS 'Data de início do cargo',
 department_name AS 'Departamento'
 FROM hr.employees as t1
-INNER JOIN hr.jobs as t2
+INNER JOIN hr.job_history as t2
 ON t1.JOB_ID = t2.JOB_ID
+INNER JOIN hr.jobs as t4
+ON t4.JOB_ID = t1.JOB_ID
 INNER JOIN hr.departments as t3
 ON t1.DEPARTMENT_ID = t3.DEPARTMENT_ID
-ORDER BY CONCAT(FIRST_NAME, ' ', LAST_NAME), Cargo;
+ORDER BY CONCAT(FIRST_NAME, ' ', LAST_NAME) DESC, Cargo;
--- desafio7.sql
+++ desafio7.sql
@@ -1,5 +1,8 @@
 SELECT UCASE(CONCAT(FIRST_NAME, ' ', LAST_NAME)) AS 'Nome completo',
-hire_date AS 'Data de início',
+start_date AS 'Data de início',
 salary AS 'Salário'
-FROM hr.employees
+FROM hr.employees as t1
+INNER JOIN hr.job_history as t2
+ON t1.EMPLOYEE_ID = t2.EMPLOYEE_ID
+WHERE MONTH(start_date) BETWEEN 1 AND 3
 ORDER BY CONCAT(FIRST_NAME, ' ', LAST_NAME),hire_date;

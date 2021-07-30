e56eaaa21d31093c56fae5788be040d6bce78794
Tiago Santos <tiago881@gmail.com>
2021-07-30 19:07:28 -0300

    requirement: 13

---
+++ desafio13.sql
@@ -0,0 +1,6 @@
+SELECT p.ProductName AS Produto, p.price AS Preço
+FROM products AS p
+INNER JOIN order_details AS od
+ON od.ProductID = p.ProductID
+WHERE od.Quantity > 80.00
+ORDER BY p.ProductName ASC;

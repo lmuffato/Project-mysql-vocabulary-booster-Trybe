3626dc4cb1b3f1fe9a9a173a7543c59ab56bf127
rogerio-lambert <rogerio.cl@gmail.com>
2021-07-29 21:56:52 -0300

    correção no filtro de tipos de empresas de envio do desafio 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,4 +1,4 @@
-/*SELECT 
+SELECT 
   (SELECT CustomerName 
     FROM w3schools.customers AS C
     WHERE C.CustomerID = O.CustomerID) AS `Nome de Contato`,
@@ -8,9 +8,9 @@
   OrderDate AS `Data do pedido`    
 FROM w3schools.orders AS O
 WHERE ShipperID IN (1, 3)
-ORDER BY `Nome de Contato` ASC, `Empresa que fez o envio` ASC, `Data do pedido` ASC;*/
+ORDER BY `Nome de Contato` ASC, `Empresa que fez o envio` ASC, `Data do pedido` ASC;
 
-SELECT 
+/*SELECT 
   C.CustomerName AS `Nome de Contato`,
   S.ShipperName AS `Empresa que fez o envio`,
   OrderDate AS `Data do pedido`    
@@ -18,4 +18,4 @@ FROM w3schools.orders AS O
 INNER JOIN w3schools.customers AS C
 INNER JOIN w3schools.shippers AS S
 ON S.ShipperName IN ('Speedy Express', 'United Package') AND C.CustomerID = O.CustomerID AND S.ShipperID = O.ShipperID
-ORDER BY `Nome de Contato` ASC, `Empresa que fez o envio` ASC, `Data do pedido` ASC;
+ORDER BY `Nome de Contato` ASC, `Empresa que fez o envio` ASC, `Data do pedido` ASC;*/

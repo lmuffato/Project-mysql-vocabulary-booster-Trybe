1bcc13e916ecad41cc0ee19df9abf4fe91474097
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-07-30 08:03:46 -0300

    req 6 done

---
+++ desafio6.sql
@@ -0,0 +1,13 @@
+SELECT 
+  CONCAT(t1.FIRST_NAME,' ',t1.LAST_NAME) AS 'Nome completo',
+  t3.JOB_TITLE AS 'Cargo',
+  t2.START_DATE AS 'Data de início do cargo',
+  t4.DEPARTMENT_NAME AS 'Departamento'
+  FROM hr.employees AS t1
+  INNER JOIN hr.job_history AS t2
+  ON t1.EMPLOYEE_ID = t2.EMPLOYEE_ID
+  INNER JOIN hr.jobs AS t3
+  ON t2.JOB_ID = t3.JOB_ID
+  INNER JOIN hr.departments AS t4
+  ON t4.DEPARTMENT_ID = t2.DEPARTMENT_ID
+  ORDER BY CONCAT(t1.FIRST_NAME,' ',t1.LAST_NAME) DESC, t3.JOB_TITLE;

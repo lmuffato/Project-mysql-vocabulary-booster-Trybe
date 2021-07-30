3b76afc9134d6049a5a368256d3239c713be0c75
AndreBarroso <andre_barroso17@hotmail.com>
2021-07-30 11:30:52 -0300

    fix: desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,6 +1,6 @@
-SELECT t1.CustomerName AS 'Nome', t1.Country AS 'País', COUNT(t1.CustomerName) -1 AS 'Número de compatriotas'
+SELECT t1.ContactName AS 'Nome', t1.Country AS 'País', COUNT(t2.ContactName) -1 AS 'Número de compatriotas'
 FROM w3schools.customers AS t1, w3schools.customers AS t2
 WHERE t1.Country = t2.Country 
-GROUP BY t1.CustomerName, t1.Country
+GROUP BY t1.ContactName, t1.Country
 HAVING `Número de compatriotas` > 0
-ORDER BY `Nome`;
+ORDER BY t1.ContactName;

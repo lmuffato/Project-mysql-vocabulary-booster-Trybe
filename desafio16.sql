6372ef04a91bcbeb39e310453b30dcd10d69c0b5
gabrieldeori <deori.pessoal@gmail.com>
2021-07-30 10:49:22 -0300

    desafio16 Bugfix: Transforming procedure into function

--- desafio16.sql
+++ desafio16.sql
@@ -1,12 +1,15 @@
 DELIMITER $$
-CREATE PROCEDURE buscar_quantidade_de_empregos_por_funcionario(IN emEmail VARCHAR(50))
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(emEmail VARCHAR(50))
+RETURNS INT READS SQL DATA
 BEGIN
+DECLARE retornado INT;
 SELECT
     count(jh.JOB_ID) AS 'total_empregos'
 FROM
     hr.employees AS em
 INNER JOIN hr.job_history AS jh ON em.EMPLOYEE_ID = jh.EMPLOYEE_ID
 WHERE em.EMAIL = emEmail
-GROUP BY jh.EMPLOYEE_ID;
+GROUP BY jh.EMPLOYEE_ID INTO retornado;
+RETURN retornado;
 END $$
 DELIMITER ;

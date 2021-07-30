9773dd26971f825727fbf5a18675231f7988a0e7
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-07-30 11:13:25 -0300

    requisito 9

---
+++ desafio9.sql
@@ -0,0 +1,8 @@
+SELECT 
+CONCAT(e.FirstName,' ',e.LastName) AS 'Nome completo', 
+COUNT(o.OrderID) AS 'Total de Pedidos'
+FROM w3schools.employees AS e
+INNER JOIN w3schools.orders AS o
+ON e.EmployeeID = o.EmployeeID
+GROUP BY e.EmployeeID
+ORDER BY COUNT(o.OrderID) ASC;

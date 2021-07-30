3fc630fb176b8efd14755dbda8fc92b0ea3efa71
rogerio-lambert <rogerio.cl@gmail.com>
2021-07-30 01:03:12 -0300

    Correção na formaçaõ da saida do desafio 15

--- desafio15.sql
+++ desafio15.sql
@@ -3,7 +3,7 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo( IN cargo VARCHAR(100) )
 BEGIN 
-  SELECT AVG(SALARY) 
+  SELECT ROUND(AVG(SALARY), 2) AS `Média salarial`
   FROM hr.employees AS E
   INNER JOIN hr.jobs AS J
   WHERE E.JOB_ID = J.JOB_ID AND J.JOB_TITLE = cargo;

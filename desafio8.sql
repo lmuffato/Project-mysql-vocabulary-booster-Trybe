a9f7f38bf6b50a4d9df108a6a01158a6e8824f95
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-07-30 13:01:26 -0300

    requisito 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,8 +1,8 @@
-SELECT c.CustomerName AS 'Nome de contato', s.ShipperName AS 'Empresa que fez o envio', o.OrderDate AS 'Data do pedido'
+SELECT c.ContactName AS 'Nome de contato', s.ShipperName AS 'Empresa que fez o envio', o.OrderDate AS 'Data do pedido'
 FROM w3schools.orders AS o
 INNER JOIN w3schools.customers AS c
 ON o.CustomerID = c.CustomerID
 INNER JOIN w3schools.shippers AS s
 ON o.ShipperID = s.ShipperID
 WHERE s.ShipperName IN ('Speedy Express','United Package')
-ORDER BY c.CustomerName ASC, s.ShipperName ASC, o.OrderDate ASC;
+ORDER BY c.ContactName ASC, s.ShipperName ASC, o.OrderDate ASC;

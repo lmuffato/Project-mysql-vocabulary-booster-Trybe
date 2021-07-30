81e131edbec6b3454667dd95194990e1a9287144
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-07-30 20:02:05 -0300

    desafio 10

---
+++ desafio10.sql
@@ -0,0 +1,11 @@
+SELECT
+  p.ProductName AS 'Produto',
+  MIN(od.Quantity) AS 'Mínima',
+  MAX(od.Quantity) AS 'Máxima',
+  ROUND(AVG(od.Quantity), 2) AS 'Média'
+FROM w3schools.products AS p
+LEFT JOIN w3schools.order_details AS od
+ON p.ProductID = od.ProductID
+GROUP BY `Produto`
+HAVING `Média` > 20
+ORDER BY `Média`, `Produto`;

9baa2ae36fc132ae24c533861685c7897a47e6c4
biazidioti <bzferreira.bio@gmail.com>
2021-07-30 19:21:59 -0300

    Challenge 11 - esse foi dificil

---
+++ desafio11.sql
@@ -0,0 +1,13 @@
+-- 11 - Exibe todas as pessoas clientes que possuem compatriotas, mostrando a quantidade de compatriotas para cada pessoa cliente
+
+SELECT c1.ContactName AS `Nome`,
+c1.Country AS `País`,
+COUNT(c1.Country) AS `Número de compatriotas`
+FROM
+  w3schools.customers AS c1,
+  w3schools.customers AS c2
+WHERE c1.Country = c2.Country AND c1.ContactName <> c2.ContactName
+GROUP BY `Nome`, `País`
+ORDER BY `Nome`;
+
+-- ref: https://github.com/tryber/sd-010-a-mysql-vocabulary-booster/pull/85/files

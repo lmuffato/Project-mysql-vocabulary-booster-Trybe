665efc0e675f80166d135d2b169ebb9106d94d8b
Sergio Martins <sergiomos2010@gmail.com>
2021-07-30 08:47:14 -0300

    Desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,6 @@
+SELECT
+  ContactName AS 'Nome',
+  c.Country AS 'País',
+  (SELECT COUNT(cu.Country) FROM w3schools.customers AS cu WHERE cu.Country = c.Country) - 1 AS 'Número de compatriotas'
+FROM w3schools.customers AS c
+ORDER BY Nome;

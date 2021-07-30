ff3925876fab5fda8828f508d923260a40417e25
Luiz <luizfvaccarif@gmail.com>
2021-07-30 18:52:59 -0300

    feat: req11

---
+++ desafio11.sql
@@ -0,0 +1,8 @@
+SELECT c1.ContactName AS `Nome`,
+c1.Country AS `País`,
+Count("País")-1 AS `Número de compatriotas`
+FROM w3schools.customers AS c1, w3schools.customers AS c2
+WHERE c1.Country = c2.Country
+GROUP BY `Nome`
+HAVING `Número de compatriotas` <> 0
+ORDER BY `Nome`;

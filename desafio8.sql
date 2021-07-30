20f7639be071eb3e95aa61ad978110fae189c8a2
rogerio-lambert <rogerio.cl@gmail.com>
2021-07-29 22:38:23 -0300

    implementação do desafio 10

--- desafio8.sql
+++ desafio8.sql
@@ -1,15 +1,3 @@
-/*SELECT 
-  (SELECT ContactName 
-    FROM w3schools.customers AS C
-    WHERE C.CustomerID = O.CustomerID) AS `Nome de Contato`,
-  (SELECT ShipperName 
-    FROM w3schools.shippers AS S
-    WHERE S.ShipperID = O.ShipperID) AS `Empresa que fez o envio`,
-  OrderDate AS `Data do pedido`    
-FROM w3schools.orders AS O
-WHERE ShipperID IN (1, 2)
-ORDER BY `Nome de Contato` ASC, `Empresa que fez o envio` ASC, `Data do pedido` ASC;*/
-
 SELECT 
   C.ContactName AS `Nome de Contato`,
   S.ShipperName AS `Empresa que fez o envio`,

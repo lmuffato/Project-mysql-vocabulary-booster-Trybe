14b12dbf261ccd58e0e04f86a9d4fe8fff383d5c
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-07-30 18:02:42 -0300

    requisito 6 e 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,7 +1,7 @@
 SELECT 
 p.ProductName AS 'Produto',
 MIN(od.Quantity) AS 'Mínima',
-MAX(od.Quantity) AS 'Maxíma',
+MAX(od.Quantity) AS 'Máxima',
 round(AVG(od.Quantity) ,2) AS 'Média'
 FROM w3schools.products AS p
 INNER JOIN w3schools.order_details AS od
--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
 SELECT
 CONCAT(e.FIRST_NAME,' ',e.LAST_NAME) AS 'Nome completo',
 j.JOB_TITLE AS 'Cargo',
-jh.START_DATE AS 'Data de início',
+jh.START_DATE AS 'Data de início do cargo',
 d.DEPARTMENT_NAME AS 'Departamento'
 FROM hr.employees AS e
 INNER JOIN hr.jobs AS j

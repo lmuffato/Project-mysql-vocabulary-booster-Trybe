39dac545aa8672884721cd89305f0c7bc3d72eb8
Felipe Muller <felipeavmuller@gmail.com>
2021-07-30 00:49:17 -0300

    Requisito 16 - stored function SQL

---
+++ desafio16.sql
@@ -0,0 +1,16 @@
+  USE hr;
+  DELIMITER $$
+  
+  CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(50))
+  RETURNS INT READS SQL DATA
+  BEGIN
+  DECLARE numero_empregos INT;
+  SELECT COUNT(j.EMPLOYEE_ID) FROM hr.job_history j
+  INNER JOIN hr.employees e ON j.EMPLOYEE_ID = e.EMPLOYEE_ID
+  WHERE email = e.EMAIL
+  INTO numero_empregos;
+  RETURN numero_empregos;
+  END $$
+  
+  DELIMITER ;
+  

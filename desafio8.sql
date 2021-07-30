84a5ba8623408e2569c09e888b1f7c13a3caedd3
Marina Alane <marina.alane@outlook.com>
2021-07-30 19:04:15 -0300

    identado, tão bonitinho

--- desafio8.sql
+++ desafio8.sql
@@ -1,8 +1,13 @@
-SELECT c.CustomerName AS 'Nome de contato',
-s.ShipperName AS 'Empresa que fez o envio',
-d.OrderDate AS 'Data do pedido'
-FROM w3schools.orders AS d
-INNER JOIN w3schools.customers AS c ON d.CustomerID = c.CustomerID
-INNER JOIN w3schools.shippers AS s 
-ON s.ShipperID = d.ShipperID AND ShipperName in ("Speedy Express", "United Package" )
-ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;
+SELECTSELECT
+    c.ContactName AS `Nome de contato`,
+    s.ShipperName AS `Empresa que fez o envio`,
+    o.OrderDate AS `Data do pedido`
+FROM
+    w3schools.customers c
+        INNER JOIN
+    w3schools.orders o ON c.CustomerID = o.CustomerID
+        INNER JOIN
+    w3schools.shippers s ON o.ShipperID = s.ShipperID
+WHERE
+    s.ShipperName IN ('Speedy Express' , 'United Package')
+ORDER BY `Nome de contato` , `Empresa que fez o envio`, `Data do pedido`;

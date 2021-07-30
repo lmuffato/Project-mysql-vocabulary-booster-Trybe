02e87f0c4173d9a116648d0d603e71833d522a4a
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-07-29 21:07:44 -0300

    Feat: (req-3) Display positions with the difference between their maximum and minimum wages

---
+++ desafio3.sql
@@ -0,0 +1,6 @@
+-- SELECT * FROM hr.jobs;
+
+SELECT JOB_TITLE AS `Cargo`,
+(MAX_SALARY - MIN_SALARY) AS `Diferença entre salários máximo e mínimo`
+FROM hr.jobs
+ORDER BY `Diferença entre salários máximo e mínimo` ASC, Cargo ASC ;

0937fdaa8639bc4b04a20e809cd539914bff4c10
Edrei <edreimateus@gmail.com>
2021-07-29 21:54:30 -0300

    feat: 9 requiriment

---
+++ desafio9.sql
@@ -0,0 +1,6 @@
+SELECT CONCAT(e.FirstName, ' ', e.LastName) AS `Nome Completo`,
+COUNT(*) AS `Total de pedidos`
+FROM w3schools.employees AS e
+INNER JOIN w3schools.orders AS o ON e.EmployeeID = o.EmployeeID
+GROUP BY e.EmployeeID
+ORDER BY `Total de pedidos` ASC;

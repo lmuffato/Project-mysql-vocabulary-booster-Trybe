17f2c29204da0d75231ca79c28bd9582d07c6842
gabrieldeori <deori.pessoal@gmail.com>
2021-07-30 10:46:51 -0300

    Add desafio19.sql not tested

---
+++ desafio19.sql
@@ -0,0 +1,15 @@
+DELIMITER $$
+CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano (mnth INT, yr INT)
+RETURNS INT READS SQL DATA
+BEGIN
+	DECLARE retornado INT
+    SELECT 
+    count(EMPLOYEE_ID)
+FROM
+    hr.employees
+WHERE
+	MONTH(HIRE_DATE) = mnth AND
+	YEAR(HIRE_DATE) = yr INTO retornado;
+    RETURN retornado;
+END $$
+DELIMITER ;

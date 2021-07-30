3dc73afdf27d8c266a1d547f2ddd2c6ea8dac7ff
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-07-30 18:11:44 -0300

    requisito 6

--- desafio6.sql
+++ desafio6.sql
@@ -4,10 +4,10 @@ j.JOB_TITLE AS 'Cargo',
 jh.START_DATE AS 'Data de início do cargo',
 d.DEPARTMENT_NAME AS 'Departamento'
 FROM hr.employees AS e
-INNER JOIN hr.jobs AS j
-ON j.JOB_ID = e.JOB_ID
 INNER JOIN hr.job_history AS jh
 ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+INNER JOIN hr.jobs AS j
+ON j.JOB_ID = jh.JOB_ID
 INNER JOIN hr.departments AS d
 ON d.DEPARTMENT_ID = jh.DEPARTMENT_ID
 ORDER BY CONCAT(e.FIRST_NAME,' ',e.LAST_NAME) DESC, j.JOB_TITLE ASC;

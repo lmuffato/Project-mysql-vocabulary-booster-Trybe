ce24426377ada7cd3fd92fea7f27363c93b55172
biazidioti <bzferreira.bio@gmail.com>
2021-07-30 18:18:21 -0300

    Challenge 9

---
+++ desafio9.sql
@@ -0,0 +1,11 @@
+-- 9 - Exibe todos as pessoas funcionárias que já realizaram algum pedido, mostrando também seu total de pedidos feitos
+
+SELECT CONCAT (e.FirstName, ' ', e.LastName) AS 'Nome completo',
+COUNT(o.OrderID) AS 'Total de pedidos'
+FROM w3schools.employees e
+  INNER JOIN 
+    w3schools.orders o
+  ON e.EmployeeID = o.EmployeeID
+GROUP BY e.EmployeeID
+ORDER BY COUNT(o.OrderID) ASC;
+  

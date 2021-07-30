9bb029a9e96f7da40f2c578bf4f710685a96386c
AndreBarroso <andre_barroso17@hotmail.com>
2021-07-29 21:50:49 -0300

    feat: desafio 9

---
+++ desafio9.sql
@@ -0,0 +1,6 @@
+SELECT CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome completo' , COUNT(o.orderID) AS 'Total de pedidos'
+FROM w3schools.employees AS e
+INNER JOIN w3schools.orders AS o
+ON e.EmployeeID = o.EmployeeID
+GROUP BY `Nome completo`
+ORDER BY `Total de pedidos`;

972321e4236fba405a4ddc9f35e7def41a8bf71d
Marina Alane <marina.alane@outlook.com>
2021-07-30 19:10:18 -0300

    mds

--- desafio8.sql
+++ desafio8.sql
@@ -1,4 +1,4 @@
-SELECTSELECT
+SELECT
     c.ContactName AS `Nome de contato`,
     s.ShipperName AS `Empresa que fez o envio`,
     o.OrderDate AS `Data do pedido`
@@ -10,4 +10,4 @@ FROM
     w3schools.shippers s ON o.ShipperID = s.ShipperID
 WHERE
     s.ShipperName IN ('Speedy Express' , 'United Package')
-ORDER BY `Nome de contato` , `Empresa que fez o envio`, `Data do pedido`;
+ORDER BY `Nome de contato` , s.ShipperName , o.OrderDate;

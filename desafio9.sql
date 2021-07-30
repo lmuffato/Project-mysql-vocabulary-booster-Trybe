641a2bc66e0b6a369ae5167142ebf2c67e87bd42
phlopes <phlopes93@gmail.com>
2021-07-30 11:14:46 -0300

    challenge 9 completed

---
+++ desafio9.sql
@@ -0,0 +1,7 @@
+SELECT CONCAT(e.FirstName, ' ', e.LastName) AS `Nome completo`,
+COUNT(o.EmployeeID) AS `Total de pedidos`
+FROM w3schools.employees AS e
+INNER JOIN w3schools.orders AS o
+ON e.EmployeeID = o.EmployeeID
+GROUP BY `Nome completo`
+ORDER BY `Total de pedidos`;

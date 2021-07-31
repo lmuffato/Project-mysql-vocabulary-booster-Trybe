76255dacc96e23021df215c599b9d2912f398410
AndreBarroso <andre_barroso17@hotmail.com>
2021-07-31 17:36:44 -0300

    feat: desafio 17

---
+++ desafio17.sql
@@ -0,0 +1,11 @@
+USE w3schools;
+DELIMITER $$
+CREATE TRIGGER insertDate
+AFTER INSERT ON orders 
+FOR EACH ROW 
+BEGIN 
+INSERT INTO orders(OrderDate)
+VALUES (CURRENT_DATE());
+END $$
+
+DELIMITER :

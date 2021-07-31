3386c2dff6572ce1ca22d90da850f47602b3c3b6
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-07-31 19:16:51 -0300

    Testando retirar acento

--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,6 @@
 SELECT UPPER(CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME)) AS 'Nome Completo',
-	h.START_DATE AS 'Data de início',
-	emp.SALARY AS 'Salário'
+	h.START_DATE AS 'Data de inicio',
+	emp.SALARY AS 'Salario'
 FROM hr.employees AS emp, hr.job_history AS h
 WHERE h.JOB_ID = emp.JOB_ID AND MONTH(h.START_DATE) IN (1, 2, 3)
 ORDER BY CONCAT(FIRST_NAME, ' ', LAST_NAME), h.START_DATE;

974b56e407bccf88784cdd7eca36a4f62f5e1f8d
Marcelo Maurício <mmauricio.design@gmail.com>
2021-07-31 12:22:28 -0300

    fix: clode climate fixes

--- desafio15.sql
+++ desafio15.sql
@@ -1,5 +1,6 @@
 USE hr;
 DELIMITER $$
+
 CREATE PROCEDURE buscar_media_por_cargo(IN job VARCHAR(255))
 BEGIN
 SELECT CAST(AVG(empl.SALARY) AS DECIMAL(10, 2)) AS `Média salarial` 
@@ -8,4 +9,5 @@ INNER JOIN jobs AS jo
 ON empl.JOB_ID = jo.JOB_ID
 WHERE jo.JOB_TITLE = job;
 END $$
+
 DELIMITER ;

851255db8143cbb2b1fc9b8149ae7d85d2f756ac
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-07-31 08:59:43 -0300

    req 7 done

---
+++ desafio7.sql
@@ -0,0 +1,9 @@
+SELECT 
+  UCASE(CONCAT(t1.FIRST_NAME,' ',t1.LAST_NAME)) AS 'Nome completo',
+  t2.START_DATE AS 'Data de início',
+  t1.SALARY AS 'Salário'
+  FROM hr.employees AS t1
+  INNER JOIN hr.job_history AS t2
+  ON t1.EMPLOYEE_ID = t2.EMPLOYEE_ID
+  WHERE MONTH(t2.START_DATE) IN (1, 2, 3)
+  ORDER BY CONCAT(t1.FIRST_NAME,' ',t1.LAST_NAME), DATE(t2.START_DATE);

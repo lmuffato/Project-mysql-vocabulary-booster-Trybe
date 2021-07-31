48451c59d87e2b0365d2c3223783b628d3c6b2c2
Marcelo Maurício <mmauricio.design@gmail.com>
2021-07-31 12:15:31 -0300

    feat: create a procedure where put a job title and bring a salary average - req 15

---
+++ desafio15.sql
@@ -0,0 +1,12 @@
+USE hr;
+DELIMITER $$
+CREATE PROCEDURE buscar_media_por_cargo(IN job VARCHAR(255))
+BEGIN
+SELECT CAST(AVG(empl.SALARY) AS DECIMAL(10, 2)) AS `Média salarial` 
+FROM employees AS empl
+INNER JOIN jobs AS jo
+ON empl.JOB_ID = jo.JOB_ID
+WHERE jo.JOB_TITLE = job;
+END $$
+
+DELIMITER ;

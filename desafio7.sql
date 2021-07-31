487efa74c2434c06c5fb548e468be773650632ee
Felipe Flores <felipefloresvendas@gmail.com>
2021-07-31 20:33:47 -0300

    fix: Code Climate

--- desafio7.sql
+++ desafio7.sql
@@ -6,6 +6,5 @@ FROM
   hr.employees AS e
 INNER JOIN hr.job_history AS jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
 WHERE
- MONTH(jh.START_DATE) IN('1', '2', '3')
+  MONTH(jh.START_DATE) IN('1', '2', '3')
 ORDER BY 1, 2;
-

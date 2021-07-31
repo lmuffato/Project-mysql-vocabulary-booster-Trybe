4a1b7d22de85f66677ada07607854e368dbcce5f
phlopes <phlopes93@gmail.com>
2021-07-30 21:23:38 -0300

    challenge 13 completed

---
+++ desafio13.sql
@@ -0,0 +1,7 @@
+SELECT p.ProductName AS Produto,
+p.Price AS Preço
+FROM w3schools.products AS p
+INNER JOIN w3schools.order_details AS od
+ON p.ProductID = od.ProductID
+WHERE od.Quantity > 80
+ORDER BY Produto;

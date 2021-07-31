e092339df8a4735455dd276795da34ef8b43c7a7
biazidioti <bzferreira.bio@gmail.com>
2021-07-31 10:48:48 -0300

    Challenge 14

---
+++ desafio14.sql
@@ -0,0 +1,9 @@
+-- 14 - Considerando o conjunto formado pelas pessoas consumidoras e empresas fornecedoras de produtos, queremos saber quais são os cinco primeiros países distintos, em ordem alfabética, presentes nesse conjunto.
+
+SELECT
+  Country AS 'País' FROM customers
+UNION
+SELECT 
+  Country AS 'País' FROM suppliers
+ORDER BY 1
+Limit 5;

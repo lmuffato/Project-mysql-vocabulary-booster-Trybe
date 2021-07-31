43ba895917313c95733be2aa894dde9e51ba44a2
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-07-31 19:24:59 -0300

    desafio 13

---
+++ desafio13.sql
@@ -0,0 +1,10 @@
+SELECT
+  p.ProductName AS Produto,
+  p.Price AS Preço
+FROM w3schools.products AS p
+WHERE EXISTS (
+  SELECT *
+  FROM w3schools.order_details AS od
+  WHERE (p.ProductID = od.ProductID) AND (od.Quantity > 80)
+)
+ORDER BY `Produto`;

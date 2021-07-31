ec24ce31500e3cdce13b21c71464499a7054eb9b
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-07-31 19:10:58 -0300

    Desafio 15 OK

---
+++ desafio15.sql
@@ -0,0 +1,9 @@
+DELIMITER $$;
+
+CREATE PROCEDURE buscar_media_por_cargo(IN nome_cargo VARCHAR(50))
+BEGIN
+	SELECT
+	ROUND(AVG(emp.SALARY), 2) AS 'Média salarial'
+FROM hr.employees AS emp, hr.departments AS dep
+WHERE dep.DEPARTMENT_ID = emp.DEPARTMENT_ID AND dep.DEPARTMENT_NAME = nome_cargo;
+END $$;

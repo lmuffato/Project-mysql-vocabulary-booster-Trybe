ec1b39e3d1ad9173dc1823d13e92225aa056c784
Felipe Flores <felipefloresvendas@gmail.com>
2021-07-31 20:21:14 -0300

    feat: requirement 13

---
+++ desafio13.sql
@@ -0,0 +1,11 @@
+SELECT
+  p.ProductName AS 'Produto',
+  p.Price AS 'Preço'
+FROM
+  w3schools.products AS p
+WHERE EXISTS (
+  SELECT * FROM order_details AS od
+  WHERE od.productId = p.productId 
+  AND od.quantity > 80
+)
+ORDER BY 1;

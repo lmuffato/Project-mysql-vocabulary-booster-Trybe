35bfded6a4fee9051949d827b3ad998400286160
Renata Andrade <renata@MacBook-Air-de-Renata.local>
2021-07-31 08:08:27 -0300

    feat: exibe nome cliente e data pedido de transportadoras especificas

---
+++ desafio8.sql
@@ -0,0 +1,10 @@
+SELECT C.ContactName AS `Nome de contato`,
+S.ShipperName AS `Empresa que fez o envio`,
+O.OrderDate AS `Data do pedido`
+FROM w3schools.customers AS C
+INNER JOIN w3schools.orders AS O
+ON C.CustomerID = O.CustomerID
+INNER JOIN w3schools.shippers AS S
+ON O.ShipperID = S.ShipperID
+WHERE S.ShipperName = 'Speedy Express' OR S.ShipperName = 'United Package'
+ORDER BY `Nome de contato` ASC, `Empresa que fez o envio` ASC, `Data do pedido` ASC;

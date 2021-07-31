e001feafc3fd7be11951ea9fecc7843ff7054fea
rogerio-lambert <rogerio.cl@gmail.com>
2021-07-30 22:08:26 -0300

    implementação do desafio 18

---
+++ desafio18.sql
@@ -0,0 +1,10 @@
+SELECT
+  CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS `Nome completo`,
+  DATE_FORMAT(JH.START_DATE, '%d/%m/%y') AS `Data de início`,
+  DATE_FORMAT(JH.END_DATE, '%d/%m/%y') AS `Data de rescisão`,
+  ROUND(DATEDIFF(JH.END_DATE, JH.START_DATE)/365, 2) AS `Anos trabalhados`
+FROM hr.job_history AS JH
+INNER JOIN hr.employees AS E
+ON JH.EMPLOYEE_ID = E.EMPLOYEE_ID
+ORDER BY `Nome completo` ASC, `Anos trabalhados` ASC;
+-- Error Code: 1054. Unknown column 'E.FIRS_NAME' in 'field list'

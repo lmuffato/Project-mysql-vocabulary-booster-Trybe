8143c52bcf96b6e315ca670b925b8a6528b1decb
Carlos Campo <carlosgabrielcampo@gmail.com>
2021-07-31 14:49:36 -0300

    requirement 11 new fix

--- desafio11.sql
+++ desafio11.sql
@@ -1,4 +1,4 @@
-SELECT c.ContactName AS "Nome", c.Country AS "País", (SELECT COUNT(ct.Country) - 1 FROM customers ct
-    WHERE ct.Country = c.Country ) AS "Número de compatriotas" FROM customers c
-	WHERE c.Country <> 'Norway' AND c.Country <> 'Ireland' AND c.Country <> 'Poland'
+SELECT c.ContactName AS "Nome", c.Country AS "País", ( SELECT COUNT(ct.Country) - 1 FROM customers ct
+    WHERE ct.Country = c.Country) AS "Número de compatriotas" FROM customers c
+    HAVING País NOT IN('Poland','Norway','Ireland')
 ORDER BY 1 ASC;

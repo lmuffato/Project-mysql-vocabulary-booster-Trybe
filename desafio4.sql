125eec3d6e457cb8bbb03c4f2ea8019df4b9bf35
Lucas Ribeiro <martins7ribeiro@gmail.com>
2021-07-31 19:35:39 -0300

    desafio 4: fino coletivo - dragão

---
+++ desafio4.sql
@@ -0,0 +1,13 @@
+SELECT job.JOB_TITLE AS `Cargo`,
+ROUND(AVG(emp.SALARY), 2) AS `Média salarial`,
+CASE
+	WHEN ROUND(AVG(emp.SALARY), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
+    WHEN ROUND(AVG(emp.SALARY), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
+    WHEN ROUND(AVG(emp.SALARY), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
+    WHEN ROUND(AVG(emp.SALARY), 2) > 10500 THEN 'CEO'
+END AS `Senioridade`
+FROM hr.jobs AS job
+INNER JOIN hr.employees AS emp
+ON job.JOB_ID = emp.JOB_ID
+GROUP BY `Cargo`
+ORDER BY `Média salarial` ASC, `Cargo` ASC;

e4b1eed3bb91dffb51e3f4bd812d7ca01ddd706c
AndreBarroso <andre_barroso17@hotmail.com>
2021-07-31 18:54:19 -0300

    fix: desafio 19

--- desafio18.sql
+++ desafio18.sql
@@ -1,6 +1,6 @@
 SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
-CONCAT(DAY(j.START_DATE), '/', MONTH(j.START_DATE), '/', YEAR(j.START_DATE)) AS 'Data de início',
-CONCAT(DAY(j.END_DATE), '/', MONTH(j.END_DATE), '/', YEAR(j.END_DATE)) AS 'Data de rescisão',
+DATE_FORMAT(j.START_DATE, '%d/%m/%Y') AS 'Data de início',
+DATE_FORMAT(j.END_DATE, '%d/%m/%Y') AS 'Data de rescisão',
 ROUND(DATEDIFF(j.END_DATE, j.START_DATE)/365, 2) AS 'Anos trabalhados'
 FROM hr.job_history AS j
 INNER JOIN hr.employees AS e
---
+++ desafio19.sql
@@ -0,0 +1,12 @@
+DELIMITER $$
+
+CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(mes INT, ano INT)
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE quantidade INT;
+SELECT COUNT(*) FROM hr.employees
+WHERE MONTH(HIRE_DATE) = mes AND YEAR(HIRE_DATE) = ano 
+INTO quantidade;
+RETURN quantidade;
+END $$
+DELIMITER ;

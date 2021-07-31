b2853362aa38f275ac611710e4face614e72cb43
biazidioti <bzferreira.bio@gmail.com>
2021-07-31 09:56:58 -0300

    Challenge 12

---
+++ desafio12.sql
@@ -0,0 +1,18 @@
+-- 12 - Faça um relatório que lista todas as pessoas funcionárias que possuem o mesmo cargo
+-- ref: https://github.com/tryber/sd-010-a-mysql-vocabulary-booster/pull/30/files
+
+SELECT
+CONCAT(e1.FIRST_NAME, ' ', e1.LAST_NAME) AS 'Nome completo funcionário 1',
+e1.SALARY AS 'Salário funcionário 1',
+e1.PHONE_NUMBER AS 'Telefone funcionário 1',
+CONCAT(e2.FIRST_NAME, ' ', e2.LAST_NAME) AS 'Nome completo funcionário 2',
+e2.SALARY AS 'Salário funcionário 2',
+e2.PHONE_NUMBER AS 'Telefone funcionário 2'
+FROM
+  hr.employees e1, hr.employees e2
+WHERE
+  e1.EMPLOYEE_ID <> e2.EMPLOYEE_ID
+AND
+  e1.JOB_ID = e2.JOB_ID
+  ORDER BY 1, 4;
+  

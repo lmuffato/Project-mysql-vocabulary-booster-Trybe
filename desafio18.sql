820741fbe5b45ddbab7fd6a9a44b0f09be03a764
rogerio-lambert <rogerio.cl@gmail.com>
2021-07-30 22:24:16 -0300

    correçaõ no desafio 18 e implementação do desafio 19

--- desafio18.sql
+++ desafio18.sql
@@ -1,7 +1,7 @@
 SELECT
   CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS `Nome completo`,
-  DATE_FORMAT(JH.START_DATE, '%d/%m/%y') AS `Data de início`,
-  DATE_FORMAT(JH.END_DATE, '%d/%m/%y') AS `Data de rescisão`,
+  DATE_FORMAT(JH.START_DATE, '%d/%m/%Y') AS `Data de início`,
+  DATE_FORMAT(JH.END_DATE, '%d/%m/%Y') AS `Data de rescisão`,
   ROUND(DATEDIFF(JH.END_DATE, JH.START_DATE)/365, 2) AS `Anos trabalhados`
 FROM hr.job_history AS JH
 INNER JOIN hr.employees AS E
---
+++ desafio19.sql
@@ -0,0 +1,14 @@
+USE hr;
+DELIMITER $$
+
+CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(mes int, ano int)
+RETURNS INT READS SQL DATA
+BEGIN
+    DECLARE quant_pessoas INT;
+    SELECT COUNT(*)
+    FROM hr.employees AS E
+    WHERE MONTH(E.HIRE_DATE) = mes AND YEAR(E.HIRE_DATE) = ano  INTO movie_total;
+    RETURN movie_total;
+END $$
+
+DELIMITER ;

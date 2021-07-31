19740effe25e1bf9d1e46344c5592abc8312533f
guilhermemd <guilherme.md@gmail.com>
2021-07-30 22:41:41 -0300

    feat: :sparkles: req 15 implementation

---
+++ desafio15.sql
@@ -0,0 +1,16 @@
+USE hr;
+DELIMITER $$
+
+CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(100))
+BEGIN
+SELECT 
+ROUND(AVG(employees.salary), 2) AS `Média salarial`
+FROM hr.employees AS employees
+INNER JOIN
+hr.jobs AS jobs
+ON employees.job_id = jobs.job_id
+GROUP BY job_title
+HAVING job_title = cargo;
+END $$
+
+DELIMITER ;

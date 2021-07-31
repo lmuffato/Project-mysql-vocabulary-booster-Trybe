614bf233137fb13056dc2229a1d959033bcb9387
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2021-07-31 11:25:02 -0300

    Feat: desafio15

---
+++ desafio15.sql
@@ -0,0 +1,16 @@
+-- Crie uma procedure chamada buscar_media_por_cargo que recebe como parâmetro o nome de um cargo e em retorno deve mostrar a média salarial de todas as pessoas que possuem esse cargo
+USE hr;
+DELIMITER $$
+
+CREATE PROCEDURE buscar_media_por_cargo(IN job VARCHAR(100))
+BEGIN 
+SELECT ROUND(AVG(e.SALARY), 2) `Média salarial`
+FROM hr.employees AS e
+INNER JOIN hr.jobs AS j
+ON e.JOB_ID = j.JOB_ID
+WHERE job = j.JOB_TITLE;
+END $$
+
+DELIMITER ;
+
+CALL buscar_media_por_cargo('Programmer');

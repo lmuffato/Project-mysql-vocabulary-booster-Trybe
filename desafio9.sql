035a1474c5c2fd2e5bf0af4acc18e3e5e07e279e
Renata Andrade <renata@MacBook-Air-de-Renata.local>
2021-07-31 07:46:20 -0300

    feat: exibe nome e pedidos feitos pelo funcionario

---
+++ desafio9.sql
@@ -0,0 +1,7 @@
+SELECT CONCAT(E.FirstName, ' ', E.LastName) AS `Nome completo`,
+COUNT(O.EmployeeID) AS `Total de pedidos`
+FROM w3schools.employees AS E
+INNER JOIN w3schools.orders AS O
+ON E.EmployeeID = O.EmployeeID
+GROUP BY O.EmployeeID
+ORDER BY `Total de pedidos` ASC;

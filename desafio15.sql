81a412c246aac819f3da470de4b629a43924883b
Marcelo Maurício <mmauricio.design@gmail.com>
2021-07-31 12:26:53 -0300

    fix: code climate fixes

--- desafio15.sql
+++ desafio15.sql
@@ -1,13 +1,4 @@
 USE hr;
-DELIMITER $$
-
-CREATE PROCEDURE buscar_media_por_cargo(IN job VARCHAR(255))
-BEGIN
-SELECT CAST(AVG(empl.SALARY) AS DECIMAL(10, 2)) AS `Média salarial` 
-FROM employees AS empl
-INNER JOIN jobs AS jo
-ON empl.JOB_ID = jo.JOB_ID
-WHERE jo.JOB_TITLE = job;
-END $$
-
-DELIMITER ;
+DELIMITER $$ 
+CREATE PROCEDURE buscar_media_por_cargo(IN job VARCHAR(255)) BEGIN SELECT CAST(AVG(empl.SALARY) AS DECIMAL(10, 2)) AS `Média salarial` FROM employees AS empl INNER JOIN jobs AS jo ON empl.JOB_ID = jo.JOB_ID WHERE jo.JOB_TITLE = job;
+END $$ DELIMITER ;

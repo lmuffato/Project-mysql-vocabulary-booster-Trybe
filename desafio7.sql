b182f23ff57763ea3fc5233440e7396b71924e5e
Paulo Vitor Borges <pvtrybe209@gmail.com>
2021-07-31 20:02:35 -0300

    fix:desafio7,desafio8

--- desafio7.sql
+++ desafio7.sql
@@ -1,9 +1,9 @@
 SELECT
-CONCAT(first_name,' ',last_name) AS 'Nome completo',
+UCASE(CONCAT(first_name,' ',last_name)) AS 'Nome completo',
 jh.START_DATE AS 'Data de início',
 e.SALARY AS 'Salário'
 FROM hr.employees AS e
 RIGHT JOIN hr.job_history AS jh
 ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
 WHERE MONTH(jh.START_DATE)=1 OR MONTH(START_DATE)=2 OR MONTH(START_DATE)=3
-ORDER BY `Nome completo`;
+ORDER BY `Nome completo`,`Data de início`
--- desafio8.sql
+++ desafio8.sql
@@ -1,4 +1,4 @@
-SELECT c.contactName AS 'Nome de contato',o.OrderDate AS 'Empresa que fez o envio',s.shipperName AS 'Data do pedido'
+SELECT c.contactName AS 'Nome de contato',o.OrderDate AS 'Data do pedido',s.shipperName AS 'Empresa que fez o envio'
 FROM w3schools.customers AS c
 INNER JOIN w3schools.orders AS o
 ON c.CustomerID=o.CustomerID

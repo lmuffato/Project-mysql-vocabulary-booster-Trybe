e0ba217e9ab9b4c53e29f77bb07d9a7f68d9fb89
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-07-30 23:14:20 -0300

    requisito 16

---
+++ desafio16.sql
@@ -0,0 +1,23 @@
+USE hr;
+
+DELIMITER $$
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(100))
+RETURNS INT READS SQL DATA
+BEGIN
+    DECLARE quantidade_emprego INT;
+    
+    SELECT COUNT(jh.EMPLOYEE_ID)
+    FROM hr.employees AS e
+    INNER JOIN hr.job_history AS jh
+    ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+    WHERE e.EMPLOYEE_ID = email;
+    
+    RETURN quantidade_emprego;
+END $$
+
+DELIMITER ;
+
+
+-- uso INT pq retorno um numero inteiro e READS SQL DATA pq estou lendo dados do mysql
+-- uso o inner join para comparar as tabelas e contar quantas vezes o jh.EMPLOYEE_ID se repete

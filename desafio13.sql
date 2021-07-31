1ffd98ada720230510eaa2fbac7614ed7b56b4df
Marcelo Maurício <mmauricio.design@gmail.com>
2021-07-31 11:28:51 -0300

    feat: create a query where select some products wich has a order with quantity bigger than 80 - req 13

---
+++ desafio13.sql
@@ -0,0 +1,1 @@
+SELECT prod.ProductName AS `Produto`, prod.Price AS `Preço` FROM w3schools.products AS prod INNER JOIN w3schools.order_details AS orde ON prod.ProductID = orde.ProductID WHERE orde.Quantity > 80 ORDER BY `Produto`;

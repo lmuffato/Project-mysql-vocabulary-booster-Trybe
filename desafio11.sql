921ae50128c6e2bb7d8220b79beba4993f6eaead
Felipe Flores <felipefloresvendas@gmail.com>
2021-07-31 14:35:18 -0300

    feat: requirement 11

---
+++ desafio11.sql
@@ -0,0 +1,15 @@
+SELECT
+  c1.ContactName AS `Nome`,
+  c1.Country AS 'País',
+  COUNT(c1.Country) - 1 AS 'Número de compatriotas'
+FROM
+  w3schools.customers as c1,
+  w3schools.customers as c2
+WHERE
+  c1.Country = c2.Country
+GROUP BY
+  1,
+  2
+HAVING
+  3 != 0
+ORDER BY 1;

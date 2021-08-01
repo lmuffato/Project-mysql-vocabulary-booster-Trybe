826892f0652e30926d61fc574ef53f42f7d11942
Erick-Marinho <erickgaves@gmail.com>
2021-08-01 19:36:47 -0300

    Cumprindo desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,19 @@
+SELECT
+    j.job_title Cargo,
+CASE
+    WHEN salary BETWEEN 5000 AND 10000 THEN 'Baixo'
+    WHEN salary BETWEEN 10001 AND 20000 THEN 'Médio'
+    WHEN salary BETWEEN 20001 AND 30000 THEN 'Alto'
+    ELSE 'Altíssimo'
+END 
+    Nível
+FROM
+    hr.jobs j
+INNER JOIN
+    hr.employees e
+USING
+    (job_id)
+GROUP BY
+    1
+ORDER BY
+    1;

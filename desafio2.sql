c0fb4fe5601d42d828bf9635222e2666475e5f3f
andy silva <andssilva.23@gmail.com>
2021-08-01 18:06:55 -0400

    feat(req 2): :rocket: challenge 2

---
+++ desafio2.sql
@@ -0,0 +1,25 @@
+/* 2 - Exiba os cargos com seu nível de renumeração associado, com base no salário máximo do cargo
+Categorize o nível de remuneração de cada cargo.
+
+Use o banco hr como referência
+
+Seu relatório deve possuir as seguintes colunas:
+
+  A) A primeira deve possuir o alias "Cargo" e exibir o nome do cargo.
+  B) A segunda deve possuir o alias "Nível" e exibir o nível de remuneração do cargo com base no seu salário máximo, categorizando os níveis de remuneração da seguinte forma:
+    -> Salário máximo entre 5000 e 10000 -> Baixo
+    -> Salário máximo entre 10001 e 20000 -> Médio
+    -> Salário máximo entre 20001 e 30000 -> Alto
+    -> Salário máximo acima de 30000 -> Altíssimo
+
+Os resultados devem estar ordenados pelo nome do cargo em ordem alfabética. */
+SELECT
+JOB_TITLE AS 'Cargo', 
+  CASE
+    WHEN MAX_SALARY BETWEEN 5000 AND 10000 THEN 'Baixo'
+    WHEN MAX_SALARY BETWEEN 10001 AND 20000 THEN 'Médio'
+    WHEN MAX_SALARY BETWEEN 20001 AND 30000 THEN 'Alto'
+    ELSE 'Altíssimo'
+  END AS Nível
+FROM hr.jobs
+ORDER BY JOB_TITLE;

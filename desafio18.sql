d2256168883cd363b7c9f15a5ad718a183f8d353
Felipe Muller <felipeavmuller@gmail.com>
2021-08-01 16:54:37 -0300

    AJuste no ANO - requisito 18

--- desafio18.sql
+++ desafio18.sql
@@ -1,6 +1,6 @@
 SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
-DATE_FORMAT(h.START_DATE, '%d/%m/%y') AS 'Data de início',
-DATE_FORMAT(h.END_DATE, '%d/%m/%y') AS 'Data de rescisão',
+DATE_FORMAT(h.START_DATE, '%d/%m/%Y') AS 'Data de início',
+DATE_FORMAT(h.END_DATE, '%d/%m/%Y') AS 'Data de rescisão',
 ROUND(DATEDIFF(h.END_DATE, h.START_DATE)/365, 2) AS 'Anos trabalhados'
 FROM hr.employees e
 INNER JOIN hr.job_history h ON h.EMPLOYEE_ID = e.EMPLOYEE_ID

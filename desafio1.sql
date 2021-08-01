1beaf7ceceb9fd23af8ee9814684ca5ff4a1fd9d
andy silva <andssilva.23@gmail.com>
2021-08-01 17:17:32 -0400

    feat(req 1): :rocket: challenge 1

---
+++ desafio1.sql
@@ -0,0 +1,21 @@
+/* 1 - Exiba os países e indicando se cada um deles se encontra ou não na região formada pela Europa
+Crie uma query que mostre se o país pertence ou não à região "Europe".
+
+Use o banco hr como referência
+
+Seu relatório deve apresentar 02 colunas:
+
+  A) A primeira deve possuir o alias "País" e exibir o nome do país.
+  B) A segunda deve possuir o alias "Status Inclusão" e deve exibir "incluído" se o país em questão está incluso em "Europe", ou "não incluído", caso contrário.
+
+Os resultados devem estar ordenados pelo nome do país em ordem alfabética. */
+SELECT
+c.COUNTRY_NAME AS 'País',
+  CASE
+    WHEN r.REGION_NAME = 'Europe' THEN 'incluído'
+    ELSE 'não incluído'
+  END AS 'Status Inclusão'
+FROM hr.countries c
+INNER JOIN hr.regions r
+ON c.REGION_ID = r.REGION_ID
+ORDER BY c.COUNTRY_NAME;

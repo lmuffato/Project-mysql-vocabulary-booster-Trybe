2c3f8bee710e51add7b8a73a888342c38a8774fa
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-08-01 14:10:11 -0300

    Corrigindo

--- desafio8.sql
+++ desafio8.sql
@@ -2,7 +2,8 @@ SELECT
   cust.ContactName AS 'Nome de contato', 
   ship.ShipperName AS 'Empresa que fez o envio',
   ord.OrderDate AS 'Data do pedido'
-FROM w3schools.orders AS ord
+FROM 
+	w3schools.orders AS ord
 	INNER JOIN w3schools.customers AS cust 
     ON ord.CustomerID = cust.CustomerID
 	INNER JOIN w3schools.shippers AS ship 

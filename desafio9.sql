d9e3fadf2c54ab9180ccb8a2f673eecab76e7ae6
ScriptCamilo <script.camilo@gmail.com>
2021-08-01 14:43:16 -0300

    fix: ninth requirement done

--- desafio9.sql
+++ desafio9.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT(FirstName, ' ', LastName),
+SELECT CONCAT(FirstName, ' ', LastName) AS 'Nome completo',
 COUNT(o.EmployeeID) AS 'Total de pedidos'
 FROM w3schools.employees AS e
 INNER JOIN w3schools.orders AS o

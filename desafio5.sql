5389457b824aa1a3375c9fd1d2a24680a6a1cb39
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-08-01 20:25:33 -0300

    Desafio 5 correção nas informações das strings

--- desafio5.sql
+++ desafio5.sql
@@ -1,8 +1,8 @@
 SELECT 
     JOB_TITLE AS 'Cargo',
     MAX_SALARY - MIN_SALARY AS 'Variação Salarial',
-    ROUND((MIN_SALARY / 12), 2) AS 'Variação Mínima Salarial',
-    ROUND((MAX_SALARY / 12), 2) AS 'Variação Máxima Salarial'
+    ROUND((MIN_SALARY / 12), 2) AS 'Média Mínima Salarial',
+    ROUND((MAX_SALARY / 12), 2) AS 'Média Máxima Salarial'
 FROM
     hr.jobs
 ORDER BY `Variação Salarial` , JOB_TITLE;

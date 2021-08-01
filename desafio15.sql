eda35c3a913e5f739063241d2b32d770e178b883
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-08-01 13:49:31 -0300

    Corrigindo

--- desafio15.sql
+++ desafio15.sql
@@ -3,9 +3,13 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN nome_cargo VARCHAR(50))
 BEGIN
-	SELECT ROUND(AVG(salary), 2) AS 'Média salarial'
-	FROM hr.employees
-	WHERE job_id IN (SELECT job_id FROM jobs WHERE job_title = nome_cargo);
+SELECT ROUND(AVG(salary), 2) AS `Média salarial`
+FROM hr.employees
+WHERE job_id IN (
+  SELECT job_id
+  FROM jobs
+  WHERE job_title = nome_cargo
+);
 END $$
 
 DELIMITER ;

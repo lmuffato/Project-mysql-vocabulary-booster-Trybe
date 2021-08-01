c429d8f7d8cd7530a6b4067a6d6a02bb1dfeea53
Nathalia Zebral <natzebral@gmail.com>
2021-08-01 17:11:55 -0300

    challenge 12 Done

---
+++ desafio12.sql
@@ -0,0 +1,14 @@
+SELECT 
+    CONCAT(func1.FIRST_NAME, ' ', func1.LAST_NAME) AS 'Nome completo funcinário 1',
+    func1.SALARY AS 'Salário Funcionário 1',
+    func1.PHONE_NUMBER AS 'Telefone funcionario 1',
+    CONCAT(func2.FIRST_NAME, ' ', func2.LAST_NAME) AS 'Nome completo funcinário 2',
+    func2.SALARY AS 'Salário Funcionário 2',
+    func2.PHONE_NUMBER AS 'Telefone funcionario 2'
+FROM
+    hr.employees AS func1,
+    hr.employees AS func2
+WHERE
+    func1.EMPLOYEE_ID <> func2.EMPLOYEE_ID
+        AND func1.JOB_ID = func2.JOB_ID
+ORDER BY `Nome completo funcinário 1` , `Nome completo funcinário 2`;

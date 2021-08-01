9834f238f8e724b32bb60cba3c4f52b94f982184
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-08-01 19:33:04 -0300

    :sparkles: Requirement 16
    16 - Crie uma função chamada buscar_quantidade_de_empregos_por_funcionario no banco de dados hr que, ao receber o email de uma pessoa funcionária, retorne a quantidade de empregos presentes em seu histórico
            Crie uma função chamada buscar_quantidade_de_empregos_por_funcionario que, ao receber o email de uma pessoa funcionária, retorne a quantidade de empregos presentes em seu histórico.
            Use o banco hr como referência
            Confirme que sua função retorna o valor 2 ao ser chamada passando uma pessoa funcionária cujo email é "NKOCHHAR"

---
+++ desafio16.sql
@@ -0,0 +1,21 @@
+-- 16 - Crie uma função chamada buscar_quantidade_de_empregos_por_funcionario no banco de dados hr que, ao receber o email de uma pessoa funcionária, retorne a quantidade de empregos presentes em seu histórico
+--        Crie uma função chamada buscar_quantidade_de_empregos_por_funcionario que, ao receber o email de uma pessoa funcionária, retorne a quantidade de empregos presentes em seu histórico.
+--        Use o banco hr como referência
+--        Confirme que sua função retorna o valor 2 ao ser chamada passando uma pessoa funcionária cujo email é "NKOCHHAR"
+USE hr;
+DELIMITER $$
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(emailValue VARCHAR(30))
+RETURNS NUMERIC READS SQL DATA
+BEGIN
+  DECLARE qtd NUMERIC;
+  SELECT
+    COUNT(*)
+  FROM hr.employees AS e
+  INNER JOIN hr.job_history AS jh ON e.EMPLOYEE_ID=jh.EMPLOYEE_ID
+  WHERE
+	e.EMAIL=emailValue
+  INTO qtd;
+  RETURN qtd;
+END $$
+
+DELIMITER ;

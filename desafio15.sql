346969dedfc04e2582a537013b2f9d5d84c6a6c3
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-08-01 19:05:51 -0300

    :sparkles: Requirement 15
    15 - Crie uma procedure chamada buscar_media_por_cargo que recebe como parâmetro o nome de um cargo e em retorno deve mostrar a média salarial de todas as pessoas que possuem esse cargo
            Crie uma procedure chamada buscar_media_por_cargo que recebe como parâmetro o nome de um cargo e em retorno deve mostrar a média salarial de todas as pessoas que possuem esse cargo.
            Sua procedure deve retornar somente 01 coluna com o alias "Média salarial", que mostra a média salarial arredondada para duas casas decimais.
            Use o banco hr como referência
            Confirme a execução correta da procedure chamando-a e passando o nome de cargo igual a "Programmer":
            CALL buscar_media_por_cargo('Programmer');
            Chamando-a dessa forma, sua procedure deve retornar 5760.00 como média salarial para pessoas que ocupam o cargo "Programmer"

---
+++ desafio15.sql
@@ -0,0 +1,17 @@
+-- 15 - Crie uma procedure chamada buscar_media_por_cargo que recebe como parâmetro o nome de um cargo e em retorno deve mostrar a média salarial de todas as pessoas que possuem esse cargo
+--        Crie uma procedure chamada buscar_media_por_cargo que recebe como parâmetro o nome de um cargo e em retorno deve mostrar a média salarial de todas as pessoas que possuem esse cargo.
+--        Sua procedure deve retornar somente 01 coluna com o alias "Média salarial", que mostra a média salarial arredondada para duas casas decimais.
+--        Use o banco hr como referência
+--        Confirme a execução correta da procedure chamando-a e passando o nome de cargo igual a "Programmer":
+--        CALL buscar_media_por_cargo('Programmer');
+--        Chamando-a dessa forma, sua procedure deve retornar 5760.00 como média salarial para pessoas que ocupam o cargo "Programmer"
+USE hr;
+DELIMITER $$
+CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(30))
+BEGIN
+  SELECT ROUND(AVG(e.SALARY),2) AS 'Média salarial' FROM hr.employees AS e
+  INNER JOIN hr.jobs AS j ON e.JOB_ID=j.JOB_ID
+  WHERE j.JOB_TITLE=cargo;
+END $$
+
+DELIMITER ;

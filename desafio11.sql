6e528622309c649b406b58b1ec3e62c4c8933e92
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-08-01 12:52:04 -0300

    Corrigindo

--- desafio11.sql
+++ desafio11.sql
@@ -1,8 +1,9 @@
 SELECT 
 	t1.ContactName AS 'Nome',
     t1.Country AS 'País', 
-    COUNT(t2.Country) AS 'Número de compatriotas'
-FROM w3schools.customers AS t1, w3schools.customers AS t2
-WHERE t1.Country = t2.Country AND t1.ContactName NOT IN (t2.ContactName)
-GROUP BY t1.CustomerID
+    COUNT(t1.Country) AS 'Número de compatriotas'
+FROM w3schools.customers AS t1
+JOIN w3schools.customers AS t2
+ON t1.Country = t2.Country AND t1.ContactName NOT IN (t2.ContactName)
+GROUP BY t1.ContactName, t2.Country
 ORDER BY t1.ContactName;
--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,8 @@
-SELECT UPPER(CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME)) AS 'Nome Completo',
+SELECT 
+	UCASE(CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME)) AS 'Nome Completo',
 	h.START_DATE AS 'Data de início',
 	emp.SALARY AS 'Salário'
 FROM hr.employees AS emp
 INNER JOIN hr.job_history AS h ON emp.EMPLOYEE_ID = h.EMPLOYEE_ID
-WHERE MONTH(h.START_DATE) IN ('1', '2', '3')
-ORDER BY 1, 2;
+WHERE MONTH(h.START_DATE) IN('1', '2', '3')
+ORDER BY UCASE(CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME)), h.START_DATE;

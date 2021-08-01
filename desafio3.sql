ef725a95738bfee8670b199942d338e2b255b882
igson <igson660@gmail.com>
2021-08-01 10:34:37 -0500

    fix desafio3

--- desafio3.sql
+++ desafio3.sql
@@ -1,6 +1,6 @@
 SELECT
   JOB_TITLE AS 'Cargo',
-  MAX_SALARY - MIN_SALARY AS 'Diferença entre salários máximo e mínimo'
+  MAX_SALARY - MIN_SALARY AS `Diferença entre salários máximo e mínimo`
 FROM hr.jobs
 ORDER BY
-  'Diferença entre salários máximo e mínimo';
+  `Diferença entre salários máximo e mínimo` ASC;

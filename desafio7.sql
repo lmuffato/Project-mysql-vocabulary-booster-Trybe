1bd0f5ce6ebc8e53b91470883e274918aac36f6d
AdelinoJnr <adelinojunior96@gmail.com>
2021-08-01 10:42:16 -0300

    [fix: desafio 7]

--- desafio7.sql
+++ desafio7.sql
@@ -1,5 +1,5 @@
 SELECT
-  CONCAT(UCASE(e.FIRST_NAME), ' ', UCASE(e.LAST_NAME)) AS `Nome completo`,
+  UPPER(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS `Nome completo`,
   jh.START_DATE AS `Data de início do cargo`,
   e.SALARY AS `Salário`
 FROM hr.job_history AS jh

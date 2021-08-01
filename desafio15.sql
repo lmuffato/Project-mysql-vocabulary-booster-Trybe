63a12242be09d8b1085857eb295abbe857902d5a
Lucas Ribeiro <martins7ribeiro@gmail.com>
2021-07-31 21:22:31 -0300

    codeclimate suikoden

--- desafio15.sql
+++ desafio15.sql
@@ -3,10 +3,10 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(50))
 BEGIN
-	SELECT ROUND(AVG(emp.SALARY), 2) AS `Média salarial`
-    FROM employees AS emp
-    INNER JOIN jobs AS job ON emp.JOB_ID = job.JOB_ID
-    WHERE JOB_TITLE = cargo;
+SELECT ROUND(AVG(emp.SALARY), 2) AS `Média salarial`
+FROM employees AS emp
+INNER JOIN jobs AS job ON emp.JOB_ID = job.JOB_ID
+WHERE JOB_TITLE = cargo;
 END $$
 
 DELIMITER ;

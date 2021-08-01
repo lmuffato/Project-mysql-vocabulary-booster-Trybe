655a84762773e9a7d93e9b0641fc54b4d049d943
igson <igson660@gmail.com>
2021-08-01 17:05:02 -0500

    desafio 07

---
+++ desafio7.sql
@@ -0,0 +1,7 @@
+LECT UCASE(CONCAT(t1.FIRST_NAME, ' ', t1.LAST_NAME)) AS `Nome completo`,
+t2.START_DATE AS `Data de início`,
+t1.SALARY AS `Salário`
+FROM hr.employees AS t1
+INNER JOIN hr.job_history AS t2 ON t1.EMPLOYEE_ID = t2.EMPLOYEE_ID
+WHERE MONTH(t2.START_DATE) IN(1, 2, 3)
+ORDER BY `Nome completo` ASC, `Data de início` ASC;

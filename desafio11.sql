ae439313e3a22675fe956c3cd77583ef6319db45
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-08-01 16:29:05 -0300

    fix: requisito 11,refat

--- desafio11.sql
+++ desafio11.sql
@@ -1,5 +1,5 @@
 SELECT c.ContactName AS 'Nome', c.Country AS 'País', COUNT(c.Country) AS 'Número de compatriotas'
 FROM w3schools.customers AS c, w3schools.customers AS cust
-WHERE c.Country = cust.Country AND c.ContactName <> cust.ContactName
-GROUP BY c.ContactName, cust.Country
-ORDER BY c.ContactName;
+WHERE c.Country = cust.Country AND  c.ContactName <> cust.ContactName
+GROUP BY `Nome`, `País`
+ORDER BY `Nome`;

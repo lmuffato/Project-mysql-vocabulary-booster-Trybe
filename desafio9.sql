c7bb2a26c72ee5be53aa267920e97f206fbab9be
w-cypriano <profwellington17@gmail.com>
2021-07-31 23:59:22 -0300

    desafio9

---
+++ desafio9.sql
@@ -0,0 +1,8 @@
+SELECT 
+    CONCAT(E.FirstName, ' ', E.LastName) AS 'Nome completo',
+    COUNT(O.OrderID) AS 'Total de pedidos'
+FROM w3schools.employees AS E
+INNER JOIN w3schools.orders AS O
+ON E.EmployeeID = O.EmployeeID
+GROUP BY E.EmployeeID
+ORDER BY COUNT(O.OrderID) ASC;

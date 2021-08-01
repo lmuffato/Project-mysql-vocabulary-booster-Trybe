271d68e49365c75f73d2ad25d86dcdc94c3f35c5
igson <igson660@gmail.com>
2021-08-01 12:14:17 -0500

    fix  desafio6

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
-SELECT CONCAT(t1.FIRST_NAME, ' ', t1.LAST_NAME) AS 'Nome completo',
-t3.JOB_TITLE AS 'Cargo',
-t2.START_DATE AS 'Data de inicio do cargo',
-t4.DEPARTMENT_NAME AS 'Departamento'
+SELECT CONCAT(t1.FIRST_NAME, ' ', t1.LAST_NAME) AS `Nome completo`,
+t3.JOB_TITLE AS `Cargo`,
+t2.START_DATE AS `Data de inicio do cargo`,
+t4.DEPARTMENT_NAME AS `Departamento`
 FROM hr.employees AS t1
 INNER JOIN hr.job_history AS t2 ON t1.EMPLOYEE_ID = t2.EMPLOYEE_ID
 INNER JOIN hr.jobs AS t3 ON t2.JOB_ID = t3.JOB_ID

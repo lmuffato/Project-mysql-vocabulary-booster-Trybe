acfacf69db84a7d3b65e1b681d2878c943342c6a
Erick-Marinho <erickgaves@gmail.com>
2021-08-01 19:36:16 -0300

    Refatorando codigo - usando subsecao USING

--- desafio1.sql
+++ desafio1.sql
@@ -3,12 +3,13 @@ SELECT
 CASE
     WHEN r.region_name = 'Europe' THEN 'incluído'
     ELSE 'não incluído'
-END 'Status Inclusão'
+END 
+    'Status Inclusão'
 FROM
     hr.countries c
 INNER JOIN
     hr.regions r
-ON
-    c.region_id = r.region_id
+USING
+    (region_id)
 ORDER BY
     1;

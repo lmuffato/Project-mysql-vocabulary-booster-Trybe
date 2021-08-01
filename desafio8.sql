d33b64d2662b78d0c09e137c2b3f8b4b7bcd9d86
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-08-01 14:25:24 -0300

    Corrigindo

--- desafio8.sql
+++ desafio8.sql
@@ -3,10 +3,10 @@ SELECT
   ship.ShipperName AS 'Empresa que fez o envio',
   ord.OrderDate AS 'Data do pedido'
 FROM 
-	w3schools.orders AS ord
-	INNER JOIN w3schools.customers AS cust 
-    ON ord.CustomerID = cust.CustomerID
-	INNER JOIN w3schools.shippers AS ship 
-    ON ord.ShipperID = ship.ShipperID
+w3schools.orders AS ord
+INNER JOIN w3schools.customers AS cust 
+   ON ord.CustomerID = cust.CustomerID
+INNER JOIN w3schools.shippers AS ship 
+   ON ord.ShipperID = ship.ShipperID
 WHERE ship.ShipperName = 'Speedy Express' or ship.ShipperName = 'United Package'
 ORDER BY cust.ContactName, ship.ShipperName, ord.OrderDate;

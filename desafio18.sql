93956cf2a6f3d484f078eda9f3dd96c986f608dd
Felipe Flores <felipefloresvendas@gmail.com>
2021-08-01 18:03:18 -0300

    fix: requirement 18(3)

--- desafio18.sql
+++ desafio18.sql
@@ -1,10 +1,9 @@
---CONSULTADO DATEFORMAT EM: https://www.w3schools.com/mysql/func_mysql_date_format.asp;
-SELECT
-  CONCAT(e.first_name, ' ', e.last_name) AS `Nome completo`,
-  DATE_FORMAT(jh.start_date, '%d/%m/%Y') AS `Data de início`,
-  DATE_FORMAT(jh.end_date, '%d/%m/%Y') AS `Data de rescisão`,
-  ROUND(DATEDIFF(jh.end_date, jh.start_date) / 365, 2) AS `Anos trabalhados`
-FROM hr.job_history AS jh
-JOIN hr.employees AS e
-ON e.employee_id = jh.employee_id
-ORDER BY `Nome completo`, `Anos trabalhados`;
+SELECT 
+  CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo' ,
+  DATE_FORMAT(jh.START_DATE, '%d/%m/%Y') AS 'Data de início',
+  DATE_FORMAT(jh.END_DATE, '%d/%m/%Y') AS 'Data de rescisão',
+  ROUND(DATEDIFF(jh.END_DATE, jh.START_DATE)/365, 2) AS 'Anos trabalhados'
+  FROM hr.employees e
+  INNER JOIN hr.job_history jh
+  ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+  ORDER BY `Nome completo`, `Anos trabalhados`;

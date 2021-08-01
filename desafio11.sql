3621c18b59f2c588c2fa93e2f3d048d774d7bc77
Nathalia Zebral <natzebral@gmail.com>
2021-08-01 16:29:10 -0300

    challenge 11 Done

---
+++ desafio11.sql
@@ -0,0 +1,10 @@
+SELECT nm.ContactName AS 'Nome',
+nm.Country AS 'País',
+COUNT(nm.Country) AS 'Número de Compatriotas'
+FROM w3schools.customers AS nm,
+w3schools.customers AS comp
+WHERE nm.ContactName <> comp.ContactName
+AND
+nm.Country = comp.Country
+GROUP BY `Nome`, `País`
+ORDER BY `Nome`;

7a443e8c76370758a604ff50e466987e99e78950
Felipe Muller <felipeavmuller@gmail.com>
2021-08-01 18:13:35 -0300

    FIX requisito 20 - PROCEDURE SQL

--- desafio20.sql
+++ desafio20.sql
@@ -7,8 +7,8 @@ SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
 d.DEPARTMENT_NAME AS 'Departamento',
 j.JOB_TITLE AS 'Cargo'
 FROM employees e
-INNER JOIN departments d ON d.DEPARTMENT_ID = e.DEPARTMENT_ID
-INNER JOIN job_history h ON h.EMPLOYEE_ID = e.EMPLOYEE_ID
+INNER JOIN job_history h ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
+INNER JOIN departments d ON h.DEPARTMENT_ID = d.DEPARTMENT_ID
 INNER JOIN jobs j ON j.JOB_ID = h.JOB_ID
 WHERE e.EMAIL = email
 ORDER BY `Departamento`, `Cargo`;

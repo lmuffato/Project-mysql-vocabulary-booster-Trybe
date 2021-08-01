84656b260a2d924b7d658a4d1c2df542594c2e3f
igson <igson660@gmail.com>
2021-08-01 17:28:27 -0500

    fix desafio 07 and 08

--- desafio7.sql
+++ desafio7.sql
@@ -1,4 +1,4 @@
-LECT UCASE(CONCAT(t1.FIRST_NAME, ' ', t1.LAST_NAME)) AS `Nome completo`,
+SELECT UCASE(CONCAT(t1.FIRST_NAME, ' ', t1.LAST_NAME)) AS `Nome completo`,
 t2.START_DATE AS `Data de início`,
 t1.SALARY AS `Salário`
 FROM hr.employees AS t1
--- desafio8.sql
+++ desafio8.sql
@@ -1,10 +1,9 @@
-SELECT 
-t1.contactName AS `Nome de contato`,
-t3.shipperName AS `Empresa que fez o envio`,
-t2.orderDate AS `Data do pedido`
-FROM w3schools.customers AS t1
-INNER JOIN w3schools.orders AS t2
-INNER JOIN w3schools.shippers AS t3
-ON t1.customerID = t2.customerId 
-AND ((t2.shipperId = 1 AND t3.shipperId = t3.shipperId) OR (t3.shipperId = 2 AND t3.shipperId = t2.shipperId))
+SELECT
+t1.ContactName AS `Nome de contato`,
+t3.ShipperName AS `Empresa que fez o envio`,
+t2.OrderDate AS `Data do pedido`
+FROM w3schools.customers AS t1 
+INNER JOIN w3schools.orders AS t2 ON t1.CustomerID = t2.CustomerID
+INNER JOIN w3schools.shippers AS t3 ON t2.ShipperID = t3.ShipperID
+WHERE t3.ShipperID IN (1, 2)
 ORDER BY `Nome de contato` ASC, `Empresa que fez o envio` ASC, `Data do pedido` ASC;

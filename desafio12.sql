a989e07adbfa798454c724a6f0097f058225c7d4
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-08-01 08:52:44 -0300

    Feat: (req-12) Make a report that lists all employees who hold the same position

---
+++ desafio12.sql
@@ -0,0 +1,13 @@
+-- SELECT * FROM hr.employees;
+
+SELECT 
+CONCAT(T1.FIRST_NAME, ' ', T1.LAST_NAME) AS `Nome completo funcionário 1`,
+T1.SALARY AS `Salário funcionário 1`,
+T1.PHONE_NUMBER AS `Telefone funcionário 1`,
+CONCAT(T2.FIRST_NAME, ' ', T2.LAST_NAME) AS `Nome completo funcionário 2`,
+T2.SALARY AS `Salário funcionário 2`,
+T2.PHONE_NUMBER AS `Telefone funcionário 2`
+FROM hr.employees AS T1, hr.employees AS T2
+WHERE T1.JOB_ID = T2.JOB_ID
+AND CONCAT(T1.FIRST_NAME, ' ', T1.LAST_NAME) <> CONCAT(T2.FIRST_NAME, ' ', T2.LAST_NAME)
+ORDER BY `Nome completo funcionário 1`, `Nome completo funcionário 2`;

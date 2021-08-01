cb7c73dabfed7de22b20f051282849d8809dfbcb
Fernando Castello Branco <fernandocastellob@gmail.com>
2021-08-01 11:55:39 -0300

    fix:req06 e 07,agora vai

--- desafio7.sql
+++ desafio7.sql
@@ -5,4 +5,4 @@ FROM hr.employees as t1
 INNER JOIN hr.job_history as t2
 ON t1.EMPLOYEE_ID = t2.EMPLOYEE_ID
 WHERE MONTH(start_date) BETWEEN 1 AND 3
-ORDER BY CONCAT(FIRST_NAME, ' ', LAST_NAME),hire_date;
+ORDER BY CONCAT(FIRST_NAME, ' ', LAST_NAME),start_date;

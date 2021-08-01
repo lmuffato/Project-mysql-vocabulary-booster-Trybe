85641888d7fa0487c1606a2144cf6b6c4f6479ba
igson <igson660@gmail.com>
2021-08-01 18:01:45 -0500

    desafio 13

---
+++ desafio13.sql
@@ -0,0 +1,7 @@
+SELECT 
+ProductName AS 'Produto',
+Price AS 'Preço'
+FROM w3schools.products AS t1
+INNER JOIN w3schools.order_details AS t2 ON t1.ProductID = t2.ProductID
+WHERE t2.Quantity > 80
+ORDER BY `Produto` ASC;

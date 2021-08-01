aae88cc7203094db829544ae5affe8eddf75d787
Felipe Flores <felipefloresvendas@gmail.com>
2021-07-31 21:12:07 -0300

    fix: requirement 10(2)

--- desafio10.sql
+++ desafio10.sql
@@ -1,16 +1,13 @@
 SELECT
-  p.ProductName AS 'PRODUTO',
-  MIN(o.Quantity) AS 'Mínima',
-  MAX(o.Quantity) AS 'Máxima',
-  ROUND(AVG(o.Quantity), 2) AS `Média`
+  products.ProductName AS Produto,
+  MIN(order_details.Quantity) 'Mínima',
+  MAX(order_details.Quantity) AS 'Máxima',
+  ROUND(AVG(order_details.Quantity), 2) AS 'Média'
 FROM
-  w3schools.products AS p
-INNER JOIN
-  w3schools.order_details AS o
-ON
-  o.ProductID = p.ProductID
+  w3schools.order_details AS order_details
+  INNER JOIN w3schools.products AS products ON order_details.ProductID = products.ProductID
 GROUP BY
-  o.ProductID
+  order_details.ProductID
 HAVING
-  `Média` > 20
-ORDER BY 4, 1;
+  `Média` > 20.00
+ORDER BY `Média`, Produto;

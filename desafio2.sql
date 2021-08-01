333ad2bd4b39d193c3aac6f430b2c4b3ad917917
Erick-Marinho <erickgaves@gmail.com>
2021-08-01 19:42:21 -0300

    refatorando - modificando faixa de valores

--- desafio2.sql
+++ desafio2.sql
@@ -1,7 +1,7 @@
 SELECT
     j.job_title Cargo,
 CASE
-    WHEN salary BETWEEN 5000 AND 10000 THEN 'Baixo'
+    WHEN salary BETWEEN 4999 AND 10000 THEN 'Baixo'
     WHEN salary BETWEEN 10001 AND 20000 THEN 'Médio'
     WHEN salary BETWEEN 20001 AND 30000 THEN 'Alto'
     ELSE 'Altíssimo'

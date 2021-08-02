ae4ca864b83490b1cd95cd76f05e7c113aea3d39
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-08-01 21:52:37 -0300

    Challenge 16: fix

--- desafio16.sql
+++ desafio16.sql
@@ -2,9 +2,9 @@ USE hr;
 DELIMITER $$
 
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(50))
-RETURNS VARCHAR(50) READS SQL DATA
+RETURNS INT READS SQL DATA
 BEGIN
-DECLARE EMAIL VARCHAR(50);
+DECLARE num INT;
 SELECT 
     COUNT(e.employee_id)
 FROM
@@ -13,9 +13,10 @@ FROM
     hr.employees AS e
 WHERE
     job_history.employee_ID = e.employee_ID
-        && e.email = 'NKOCHHAR' INTO EMAIL;
-RETURN EMAIL;
+        && e.email = email INTO num;
+RETURN num;
 END $$
 
 
-SELECT BUSCAR_QUANTIDADE_DE_EMPREGOS_POR_FUNCIONARIO('NKOCHHAR') DELIMITER
+SELECT BUSCAR_QUANTIDADE_DE_EMPREGOS_POR_FUNCIONARIO('NKOCHHAR'); 
+DELIMITER;

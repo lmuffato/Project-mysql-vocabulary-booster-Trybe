490b61b40db08b44b482ccde7c67cc3610b350d7
Erick-Marinho <erickgaves@gmail.com>
2021-08-01 21:19:49 -0300

    Refatorando codigo-Modificando coluna de consulta

--- desafio2.sql
+++ desafio2.sql
@@ -1,20 +1,13 @@
 SELECT
-    j.job_title Cargo,
+    job_title Cargo,
 CASE
-    WHEN salary BETWEEN 5000 AND 10000 THEN 'Baixo'
-    WHEN salary BETWEEN 10001 AND 20000 THEN 'Médio'
-    WHEN salary BETWEEN 20001 AND 30000 THEN 'Alto'
-    WHEN salary > 30000 THEN 'Altíssimo'
-    ELSE 'Baixo'
+    WHEN max_salary BETWEEN 4999 AND 10000 THEN 'Baixo'
+    WHEN max_salary BETWEEN 10001 AND 20000 THEN 'Médio'
+    WHEN max_salary BETWEEN 20001 AND 30000 THEN 'Alto'
+    ELSE 'Altíssimo'
 END 
     Nível
 FROM
-    hr.jobs j
-INNER JOIN
-    hr.employees e
-USING
-    (job_id)
-GROUP BY
-    1
+    hr.jobs
 ORDER BY
     1;

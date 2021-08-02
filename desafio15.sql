2afedc4d188e2cd3638cb1ac6f652218dcdfc928
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-08-02 12:11:41 -0300

    Desafio 15: Corrigindo codeclimate

--- desafio15.sql
+++ desafio15.sql
@@ -2,13 +2,11 @@ USE hr;
 DELIMITER $$
 CREATE PROCEDURE buscar_media_por_cargo(IN TITLE_JOB VARCHAR(20))
 BEGIN
-	SELECT ROUND(AVG(E.SALARY),2) AS 'Média salarial'
+	SELECT ROUND(AVG(E.SALARY), 2) AS 'Média salarial'
     FROM employees E
     INNER JOIN jobs J
     ON E.JOB_ID = J.JOB_ID
     WHERE J.JOB_TITLE = TITLE_JOB;
 END $$
-DELIMITER ;
 
-CALL buscar_media_por_cargo('Programmer');
-	
+DELIMITER ;	

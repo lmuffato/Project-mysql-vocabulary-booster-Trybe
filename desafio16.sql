03b34dbad70edc59a541665acd010a19eec29b6b
Alexandre Oliveira <alexandremolive@gmail.com>
2021-08-02 11:59:35 -0300

    fix req 16 function

--- desafio16.sql
+++ desafio16.sql
@@ -1,11 +1,14 @@
 DELIMITER $$
-CREATE PROCEDURE buscar_quantidade_de_empregos_por_funcionario(IN email VARCHAR(100), OUT total_empregos NUMERIC)
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(100))
+RETURNS NUMERIC READS SQL DATA 
 BEGIN
+DECLARE total_empregos NUMERIC;
 SELECT
 COUNT(J.EMPLOYEE_ID) INTO total_empregos
 FROM hr.job_history AS J
 INNER JOIN hr.employees AS E
 ON J.EMPLOYEE_ID = E.EMPLOYEE_ID 
 WHERE E.EMAIL = email;
+RETURN total_empregos;
 END $$
 DELIMITER ;

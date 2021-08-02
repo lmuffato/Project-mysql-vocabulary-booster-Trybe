9ede02dab3abe94d61c0aa50d5537e2c14cb38fe
Erick-Marinho <erickgaves@gmail.com>
2021-08-01 23:38:24 -0300

    Refatorando codigo - excluindo funcao AVG

--- desafio5.sql
+++ desafio5.sql
@@ -1,8 +1,8 @@
 SELECT
     job_title Cargo,
     max_salary - min_salary 'Variação Salarial',
-    ROUND(AVG(min_salary / 12), 2) 'Média mínima mensal',
-    ROUND(AVG(max_salary / 12), 2) 'Média máxima mensal'
+    ROUND(min_salary / 12, 2) 'Média mínima mensal',
+    ROUND(max_salary / 12, 2) 'Média máxima mensal'
 FROM
     hr.jobs
 GROUP BY

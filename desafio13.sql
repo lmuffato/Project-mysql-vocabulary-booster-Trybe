6b99fac44a6e312d37dadc508c31f33d7f6bb684
Alexandre Oliveira <alexandremolive@gmail.com>
2021-08-02 10:15:25 -0300

    feat req 13

---
+++ desafio13.sql
@@ -0,0 +1,8 @@
+SELECT
+P.ProductName AS `Produto`,
+P.Price AS `Preço`
+FROM w3schools.products AS P
+INNER JOIN w3schools.order_details AS O
+ON P.ProductID = O.ProductID 
+WHERE O.Quantity > 80
+ORDER BY `Produto` ASC;

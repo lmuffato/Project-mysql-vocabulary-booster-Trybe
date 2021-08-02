7720e53338701034c35a14bf7a74fb166816e412
Antônio Schappo <antonioschappo@gmail.com>
2021-08-02 13:37:20 -0300

    second try req. 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,7 +1,14 @@
-SELECT t1.ContactName AS Nome,
-t1.Country AS País,
-(SELECT COUNT(ContactName) FROM w3schools.customers
-WHERE Country = País AND ContactName <> Nome) AS 'Número de compatriotas'
-FROM w3schools.customers AS t1
-WHERE 'Número de compatriotas' > 0
-ORDER BY Nome;
+SELECT 
+t1.ContactName AS `Nome`,
+t1.Country AS `País`,
+(
+SELECT COUNT(c.ContactName)
+FROM w3schools.customers AS c
+WHERE c.Country = `País` AND c.ContactName <> `Nome`
+) AS `Número de compatriotas`
+FROM
+w3schools.customers AS t1
+HAVING
+`Número de compatriotas` > 0
+ORDER BY
+Nome;

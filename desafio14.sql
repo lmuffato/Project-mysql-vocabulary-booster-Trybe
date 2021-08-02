a028cc65ce97a6fa0025f0ad79fcff335dadadfd
Robertson <robertson.nunes@gmail.com>
2021-08-02 19:32:16 -0300

    Requisito 14

---
+++ desafio14.sql
@@ -0,0 +1,11 @@
+-- 14 - Considerando o conjunto formado pelas pessoas consumidoras e empresas fornecedoras de produtos, queremos saber quais são os cinco primeiros países distintos, em ordem alfabética, presentes nesse conjunto
+SELECT 
+    Country País
+FROM
+    w3schools.customers 
+UNION SELECT 
+    Country
+FROM
+    w3schools.suppliers
+ORDER BY País
+LIMIT 5;

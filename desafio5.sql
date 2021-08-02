7e17d3faf1b56cd226ceeb458ee909828f5fc41c
Erick-Marinho <erickgaves@gmail.com>
2021-08-01 23:30:48 -0300

    Cumprindo desafio 5

---
+++ desafio5.sql
@@ -0,0 +1,11 @@
+SELECT
+    job_title Cargo,
+    max_salary - min_salary 'Variação Salarial',
+    ROUND(AVG(min_salary / 12), 2) 'Média mínima mensal',
+    ROUND(MAX(max_salary / 12), 2) 'Média máxima mensal'
+FROM
+    hr.jobs
+GROUP BY
+    1
+ORDER BY
+    2, 1;

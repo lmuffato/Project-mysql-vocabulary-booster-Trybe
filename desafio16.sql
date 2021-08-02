29c872c190ca6ccd7e0d83678659c4efbcdedbcb
Rodrigo Queiroz <rodrigoandradequeiroz@gmail.com>
2021-08-02 11:27:10 -0300

    feat: req16

---
+++ desafio16.sql
@@ -0,0 +1,14 @@
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(50))
+RETURNS INT READS SQL DATA
+BEGIN 
+	DECLARE qnt INT;
+	SELECT COUNT(*) AS 'Total de empregos'
+    FROM hr.employees as e
+    INNER JOIN hr.job_history as j
+    ON e.employee_id = j.employee_id
+    WHERE email = e.email
+    INTO qnt;
+    RETURN qnt;
+END $$
+
+DELIMITER ;

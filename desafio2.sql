fa72fec7154859c7c86cbe1f93142a2854e3a7c4
Erick-Marinho <erickgaves@gmail.com>
2021-08-01 21:20:31 -0300

    Refatorando codigo - alterando faixa de valor

--- desafio2.sql
+++ desafio2.sql
@@ -1,7 +1,7 @@
 SELECT
     job_title Cargo,
 CASE
-    WHEN max_salary BETWEEN 4999 AND 10000 THEN 'Baixo'
+    WHEN max_salary BETWEEN 5000 AND 10000 THEN 'Baixo'
     WHEN max_salary BETWEEN 10001 AND 20000 THEN 'Médio'
     WHEN max_salary BETWEEN 20001 AND 30000 THEN 'Alto'
     ELSE 'Altíssimo'

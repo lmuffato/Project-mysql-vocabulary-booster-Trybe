bd7cfd4a294fb312d1a40cba3c1ab2b791f8051b
Mateus Martins - Turma 10 - Tribo A <mateusmartinsmck@gmail.com>
2021-08-02 12:08:58 -0300

    feat: desafio 10

---
+++ desafio10.sql
@@ -0,0 +1,12 @@
+SELECT p.ProductName AS `Produto`,
+MIN(od.Quantity) AS `Mínima`,
+MAX(od.Quantity) AS `Máxima`,
+ROUND(AVG(od.Quantity), 2) AS `Média`
+FROM w3schools.orders AS o
+INNER JOIN w3schools.order_details AS od
+ON o.OrderID = od.OrderID
+INNER JOIN w3schools.products AS p
+ON od.ProductID = p.ProductID
+GROUP BY `Produto`
+HAVING `Média` > 20
+ORDER BY `Média`, `Produto`;

c19385e7fd2de208802da7665a7c66dab76fba27
ScriptCamilo <script.camilo@gmail.com>
2021-08-02 08:23:41 -0300

    feat: eleventh requirement done

---
+++ desafio11.sql
@@ -0,0 +1,8 @@
+SELECT c1.ContactName,
+c1.Country AS 'País',
+COUNT(c2.Country) - 1 AS 'Números de compatriotas'
+FROM w3schools.customers AS c1, w3schools.customers AS c2
+WHERE c1.Country = c2.Country
+GROUP BY c1.CustomerID
+HAVING COUNT(c2.Country) - 1 > 0
+ORDER BY c1.ContactName ASC;

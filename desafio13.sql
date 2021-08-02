74968f07f053c355c737849412e6d407b32c99ea
PollyanaOliveira <pollyana.deoliveir@gmail.com>
2021-08-02 11:04:57 -0400

    feat: :sparkles: Requirement 13

---
+++ desafio13.sql
@@ -0,0 +1,10 @@
+SELECT
+productName AS `Produto`,
+price AS `Preço`
+FROM 
+w3schools.products AS products
+INNER JOIN 
+w3schools.order_details AS order_details
+ON products.ProductID = order_details.ProductID
+WHERE order_details.Quantity > 80
+ORDER BY `Produto`;

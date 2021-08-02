8f6c738051c3796bf3c2faad1d1bfbce2e024a14
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-08-01 22:30:54 -0300

    Desafio 12: Correçao - Remoçao do comando GROUP BY

--- desafio12.sql
+++ desafio12.sql
@@ -4,5 +4,4 @@ CONCAT(E2.FIRST_NAME,' ',E2.LAST_NAME) AS 'Nome completo funcionário 2',
 E2.Salary AS 'Salário funcionário 2', E2.PHONE_NUMBER AS 'Telefone funcionário 2'
 FROM hr.employees E1, hr.employees E2
 WHERE E1.JOB_ID = E2.JOB_ID AND CONCAT(E1.FIRST_NAME,' ',E1.LAST_NAME) != CONCAT(E2.FIRST_NAME,' ',E2.LAST_NAME)
-GROUP BY CONCAT(E1.FIRST_NAME,' ',E1.LAST_NAME),CONCAT(E2.FIRST_NAME,' ',E2.LAST_NAME)
 ORDER BY `Nome completo funcionário 1`, `Nome completo funcionário 2`;

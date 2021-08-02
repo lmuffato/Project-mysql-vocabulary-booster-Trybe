d4823a2699381d4ae1f1a4da5d337d29d2fedf4e
ScriptCamilo <script.camilo@gmail.com>
2021-08-02 09:09:00 -0300

    fix: eleventh requirement done

--- desafio11.sql
+++ desafio11.sql
@@ -1,6 +1,6 @@
-SELECT c1.ContactName,
+SELECT c1.ContactName AS 'Nome',
 c1.Country AS 'País',
-COUNT(c2.Country) - 1 AS 'Números de compatriotas'
+COUNT(c2.Country) - 1 AS 'Número de compatriotas'
 FROM w3schools.customers AS c1, w3schools.customers AS c2
 WHERE c1.Country = c2.Country
 GROUP BY c1.CustomerID

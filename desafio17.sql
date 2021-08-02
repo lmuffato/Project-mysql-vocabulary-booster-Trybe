10dd9f82bb65af377c6eb5afc4ad2ef4eedc74e9
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-08-01 22:10:30 -0300

    challenge 17: fix

--- desafio17.sql
+++ desafio17.sql
@@ -2,9 +2,7 @@ USE w3schools;
 DELIMITER $ $ 
 
 CREATE TRIGGER insert_orderDate
- BEFORE 
- INSERT
-  ON orders FOR EACH ROW BEGIN
+ BEFORE INSERT ON orders FOR EACH ROW BEGIN
 SET
   NEW.OrderDate = CURRENT_DATE();
 END $ $ 

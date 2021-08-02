880e7772e322bd639ad0b1d6e6f77c61f323b82a
Luiz <luizfvaccarif@gmail.com>
2021-08-02 14:08:27 -0300

    feat: req13

---
+++ desafio13.sql
@@ -0,0 +1,5 @@
+SELECT p.ProductName AS `Produto`, p.Price AS `Preço` 
+FROM w3schools.products AS p
+INNER JOIN w3schools.order_details AS od ON od.ProductID = p.ProductID
+WHERE od.Quantity > 80
+ORDER BY p.ProductName;

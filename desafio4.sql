617ee2567568b4cab2e0b5affd7eb607802d8c4f
Ederson S <edersonrodrigo31@gmail.com>
2021-08-02 16:33:22 -0300

    fix: requisito 4 lint

--- desafio4.sql
+++ desafio4.sql
@@ -1,14 +1,14 @@
 select  jobs.JOB_TITLE as Cargo , 
-	ROUND(avg(employees.salary),2) as 'Média salarial',
-	case
-		when ROUND(avg(employees.salary),2) 
-    between '2000' and '5800' then 'Júnior'
-		when ROUND(avg(employees.salary),2) 
-    between '5801' and '7500' then 'Pleno'
-		when ROUND(avg(employees.salary),2) 
-    between '7501' and '10500' then 'Sênior'
-		else 'CEO'
-	end as 'Senioridade'
+	  ROUND(avg(employees.salary),2) as 'Média salarial',
+	  case
+		  when ROUND(avg(employees.salary),2) 
+      between '2000' and '5800' then 'Júnior'
+		  when ROUND(avg(employees.salary),2) 
+      between '5801' and '7500' then 'Pleno'
+		  when ROUND(avg(employees.salary),2) 
+      between '7501' and '10500' then 'Sênior'
+		  else 'CEO'
+	  end as 'Senioridade'
 from hr.jobs as jobs
 inner join hr.employees as employees
 	on jobs.JOB_ID = employees.JOB_ID

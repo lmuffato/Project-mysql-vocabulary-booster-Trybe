1dde9a3a1ed9964d81a00daa05f9548ce85a0b11
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-08-02 18:56:54 -0300

    requisito 16

--- desafio16.sql
+++ desafio16.sql
@@ -8,16 +8,20 @@ BEGIN
     DECLARE quantidade_emprego INT;
     
     SELECT COUNT(jh.EMPLOYEE_ID)
-    FROM hr.employees AS e
-    INNER JOIN hr.job_history AS jh
+    FROM  hr.job_history  AS jh
+    INNER JOIN hr.employees AS e
     ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
-    WHERE e.EMPLOYEE_ID = email;
+    WHERE e.EMAIL LIKE email 
+    
+    INTO quantidade_emprego;
     
     RETURN quantidade_emprego;
 END $$
 
 DELIMITER ;
 
+SELECT buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR');
 
 -- uso INT pq retorno um numero inteiro e READS SQL DATA pq estou lendo dados do mysql
 -- uso o inner join para comparar as tabelas e contar quantas vezes o jh.EMPLOYEE_ID se repete
+-- uso o LIKE porque funcinoa melhor para VARCHAR('string') do que o =

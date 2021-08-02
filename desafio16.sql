91cace552bea00ceff2994dd80f3f01af9dbc4b5
Alexandre Oliveira <alexandremolive@gmail.com>
2021-08-02 12:51:29 -0300

    fix req 16

--- desafio16.sql
+++ desafio16.sql
@@ -3,13 +3,13 @@ DELIMITER $$
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(100))
 RETURNS NUMERIC READS SQL DATA 
 BEGIN
-DECLARE total_empregos TINYINT;
+DECLARE total_empregos INT;
 SELECT
-COUNT(J.EMPLOYEE_ID) INTO total_empregos
+COUNT(J.EMPLOYEE_ID) 
 FROM hr.job_history AS J
 INNER JOIN hr.employees AS E
 ON J.EMPLOYEE_ID = E.EMPLOYEE_ID 
-WHERE E.EMAIL = email;
+WHERE E.EMAIL = email INTO total_empregos;
 RETURN total_empregos;
 END $$
 DELIMITER ;

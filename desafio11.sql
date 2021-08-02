4a395f5cb5c75046d8e5d9142a4019a32f9efd0f
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-08-01 21:34:26 -0300

    Desafio 11 - Corrigindo palavra

--- desafio11.sql
+++ desafio11.sql
@@ -1,4 +1,4 @@
-SELECT C1.ContactName AS 'Nome', C1.Country AS 'País', COUNT(C2.Country)-1 AS 'Numero de compatriotas'
+SELECT C1.ContactName AS 'Nome', C1.Country AS 'País', COUNT(C2.Country)-1 AS 'Número de compatriotas'
 FROM w3schools.customers C1, w3schools.customers C2
 WHERE C1.Country = C2.Country
 GROUP BY C1.ContactName, C1.Country

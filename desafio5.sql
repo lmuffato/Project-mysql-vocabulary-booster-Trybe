c33f5ad922108809fea671f8bab81db4dc58a1a5
Erick-Marinho <erickgaves@gmail.com>
2021-08-01 23:32:48 -0300

    Refatorando codigo - Alterando função de agregação

--- desafio5.sql
+++ desafio5.sql
@@ -2,7 +2,7 @@ SELECT
     job_title Cargo,
     max_salary - min_salary 'Variação Salarial',
     ROUND(AVG(min_salary / 12), 2) 'Média mínima mensal',
-    ROUND(MAX(max_salary / 12), 2) 'Média máxima mensal'
+    ROUND(AVG(max_salary / 12), 2) 'Média máxima mensal'
 FROM
     hr.jobs
 GROUP BY

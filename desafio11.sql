4a6d286889dd02abb7b9fc7e4eaaae677de421ef
w-cypriano <profwellington17@gmail.com>
2021-08-01 22:15:02 -0300

    desafio12

--- desafio11.sql
+++ desafio11.sql
@@ -1,11 +1,8 @@
-SELECT 
-    c.ContactName AS Nome, 
-    c.Country AS País, 
-    (Sub.NP-1) AS 'Número de compatriotas'
+SELECT c.ContactName AS Nome, 
+c.Country AS País, 
+(Sub.NP-1) AS 'Número de compatriotas'
 FROM(
-SELECT 
-    Country, 
-    COUNT(*) AS NP
+SELECT Country, COUNT(*) AS NP
 FROM w3schools.customers 
 GROUP BY (Country)) AS Sub
 INNER JOIN w3schools.customers AS c

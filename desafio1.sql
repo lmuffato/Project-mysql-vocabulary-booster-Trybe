a3a82c55bae7a5ab011dcdb2d77836c7151bbe72
vinicios <soicinivt@gmail.com>
2021-08-02 16:01:13 -0300

    FIX - Order by pais.

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,2 @@
-SELECT country_name AS País, IF(region_id = 1, 'incluído', 'não incluído') AS 'Status Inclusão' FROM hr.countries;
+SELECT country_name AS País, IF(region_id = 1, 'incluído', 'não incluído') AS 'Status Inclusão' FROM hr.countries
+ORDER BY País;

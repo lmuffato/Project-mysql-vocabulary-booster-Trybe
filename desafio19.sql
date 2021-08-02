077afec6e3e38c5033b5cb73b6b1d1fee04d37b1
mallmann02 <leomallmann020302@gmail.com>
2021-08-02 14:29:25 -0300

    Bonus challenges: c19

---
+++ desafio19.sql
@@ -0,0 +1,10 @@
+CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(mes INT, ano INT)
+RETURNS INT READS SQL DATA
+BEGIN
+	DECLARE quantidade_total_de_pessoas_contratadas INT;
+  SELECT COUNT(*)
+  FROM hr.employees AS t1
+  WHERE YEAR(t1.HIRE_DATE) = ano AND MONTH(t1.HIRE_DATE) = mes
+  INTO quantidade_total_de_pessoas_contratadas;
+  RETURN quantidade_total_de_pessoas_contratadas;
+END

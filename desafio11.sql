8a8428356624eda86c504e9639d5df41e9f9bdcc
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-08-01 21:29:08 -0300

    Desafio 11: Filtrando clientes por nacionalidade - contando o numero de compatriotas

---
+++ desafio11.sql
@@ -0,0 +1,6 @@
+SELECT C1.ContactName AS 'Nome', C1.Country AS 'País', COUNT(C2.Country)-1 AS 'Numero de compatriotas'
+FROM w3schools.customers C1, w3schools.customers C2
+WHERE C1.Country = C2.Country
+GROUP BY C1.ContactName, C1.Country
+HAVING COUNT(C2.Country)-1 > 0
+ORDER BY C1.ContactName;

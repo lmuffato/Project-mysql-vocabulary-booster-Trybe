ed80e9e216a7b74459ee030d083370e812c6cb04
Britosl1 <lucasbrito.carneiro@gmail.com>
2021-08-02 20:54:08 -0300

    Desafios 6,7,8

---
+++ desafio7.sql
@@ -0,0 +1,14 @@
+SELECT
+  UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
+  h.START_DATE AS 'Data de início',
+  e.SALARY AS 'Salário'
+FROM
+  hr.job_history AS h
+  INNER JOIN hr.employees AS e ON h.EMPLOYEE_ID = e.EMPLOYEE_ID
+WHERE
+  MONTH(h.START_DATE) BETWEEN 1
+  AND 3
+ORDER BY
+  `Nome completo`,
+  `Data de início`;
+  

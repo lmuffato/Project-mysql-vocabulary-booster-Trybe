fbe81693325c6fc00ee2f0ec37d89d7729b1bec6
jlfagundes <jlfagundes@gmail.com>
2021-08-02 13:07:22 -0300

    fix: filter employees active

--- desafio6.sql
+++ desafio6.sql
@@ -2,11 +2,11 @@ SELECT CONCAT(e.FIRST_NAME, " ",e.LAST_NAME) AS `Nome completo`,
   j.JOB_TITLE AS 'Cargo',
   jh.START_DATE AS 'Data de início do cargo',
   d.DEPARTMENT_NAME AS 'Departamento'
-FROM hr.employees AS e
-  INNER JOIN hr.jobs AS j
-    ON j.JOB_ID = e.JOB_ID
-  INNER JOIN hr.job_history AS jh
+FROM hr.job_history AS jh
+  INNER JOIN hr.employees AS e
     ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+  INNER JOIN hr.jobs AS j
+    ON jh.JOB_ID = j.JOB_ID
   INNER JOIN hr.departments AS d
-    ON d.DEPARTMENT_ID = e.DEPARTMENT_ID
+    ON jh.DEPARTMENT_ID = d.DEPARTMENT_ID
 ORDER BY `Nome completo` DESC, Cargo ASC;

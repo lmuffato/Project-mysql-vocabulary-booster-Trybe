ed26bac3fb59e9eb22c23eed163ded5e9904e3c4
Antônio Schappo <antonioschappo@gmail.com>
2021-08-02 13:11:50 -0300

    second try challenge 8

--- desafio11.sql
+++ desafio11.sql
@@ -3,4 +3,5 @@ t1.Country AS País,
 (SELECT COUNT(ContactName) FROM w3schools.customers
 WHERE Country = País AND ContactName <> Nome) AS 'Número de compatriotas'
 FROM w3schools.customers AS t1
+WHERE 'Número de compatriotas' > 0
 ORDER BY Nome;

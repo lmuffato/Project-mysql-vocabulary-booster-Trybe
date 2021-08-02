1479dff76a1a2fb13a616cd0ffdb225dc616d8db
vinicios <soicinivt@gmail.com>
2021-08-02 17:46:55 -0300

    FIX - Nivel written wrong in desafio 2 and wrong order in desafio 3.

--- desafio3.sql
+++ desafio3.sql
@@ -1,3 +1,4 @@
-SELECT job_title AS Cargo, max_salary - min_salary AS 'Diferença entre salários máximo e mínimo'
+SELECT job_title AS Cargo, (max_salary - min_salary) AS 'Diferença entre salários máximo e mínimo'
 FROM hr.jobs
-ORDER BY Cargo;
+ORDER BY `Diferença entre salários máximo e mínimo`, Cargo;
+SELECT * FROM hr.jobs;

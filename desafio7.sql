14270b8e6e3765b05ddd29476ef9289564354971
vinicios <soicinivt@gmail.com>
2021-08-03 13:45:24 -0300

    Desafio 7 - Columns with complete name on upper case, start and salary.

--- desafio7.sql
+++ desafio7.sql
@@ -1,3 +1,5 @@
-SELECT CONCAT(UCASE(first_name), ' ', UCASE(last_name)) AS 'Nome completo', hire_date AS 'Data de início do cargo', salary AS Salário FROM hr.employees
-WHERE hire_date BETWEEN '1987-01-01' AND '1987-03-31'
+SELECT CONCAT(UCASE(first_name), ' ', UCASE(last_name)) AS 'Nome completo', `start_date` AS 'Data de início do cargo', salary AS Salário FROM hr.employees e
+INNER JOIN hr.job_history j_h
+ON j_h.employee_id = e.employee_id
+WHERE MONTH(`start_date`) BETWEEN '01' AND '03'
 ORDER BY `Nome completo`, `Data de início do cargo`;

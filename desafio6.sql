5049d7f1c56542995902d2eee7b1bd035ba6ab5f
andy silva <andssilva.23@gmail.com>
2021-08-02 20:09:24 -0400

    fix(req 6): :bug: challenge 6 v.2

--- desafio6.sql
+++ desafio6.sql
@@ -16,13 +16,13 @@ Em caso de empate no nome completo, ordene os resultados pelo nome do cargo em o
 SELECT
 CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome completo`,
 j.JOB_TITLE AS `Cargo`,
-e.HIRE_DATE AS `Data de início do cargo`,
+h.START_DATE AS `Data de início do cargo`,
 d.DEPARTMENT_NAME AS `Departamento`
 FROM hr.employees e
-INNER JOIN hr.jobs j
-ON e.JOB_ID = j.JOB_ID
-INNER JOIN hr.departments d
-ON e.DEPARTMENT_ID = d.DEPARTMENT_ID
 INNER JOIN hr.job_history h
 ON h.EMPLOYEE_ID = e.EMPLOYEE_ID
+INNER JOIN hr.jobs j
+ON h.JOB_ID = j.JOB_ID
+INNER JOIN hr.departments d
+ON h.DEPARTMENT_ID = d.DEPARTMENT_ID
 ORDER BY `Nome completo` DESC, `Cargo`;

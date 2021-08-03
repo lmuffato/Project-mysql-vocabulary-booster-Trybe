e76e599dc05438b3243ccfb6380545847f88ff1d
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-08-02 21:22:35 -0300

    Desafio 5 - Corrigindo

--- desafio5.sql
+++ desafio5.sql
@@ -1,8 +1,8 @@
 SELECT 
-	JOB_TITLE AS 'Cargo',
-    (MAX_SALARY - MIN_SALARY) AS 'Variação Salarial',
-    ROUND(MIN(MIN_SALARY DIV 12), 2) AS 'Média Mínima Mensal',
-    ROUND(MAX(MIN_SALARY DIV 12), 2) AS 'Média Mínima Mensal'
+	JOB_TITLE AS `Cargo`,
+    (MAX_SALARY - MIN_SALARY) AS `Variação Salarial`,
+    ROUND(MIN(MIN_SALARY DIV 12), 2) AS `Média Mínima Mensal`,
+    ROUND(MAX(MIN_SALARY DIV 12), 2) AS `Média Mínima Mensal`
 FROM hr.jobs
 GROUP BY JOB_ID
 ORDER BY 2, 1;

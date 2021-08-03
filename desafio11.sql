1964685f553ba8bcaaaa66f9c7e9c622bd664a00
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-08-03 17:41:02 -0300

    Desafio 11 test2

--- desafio11.sql
+++ desafio11.sql
@@ -4,4 +4,5 @@ COUNT(c2.Country) -1 as 'Número de compatriotas'
 FROM w3schools.customers as c1, w3schools.customers as c2
 WHERE c1.Country = c2.Country
 GROUP BY c1.ContactName, c1.Country
+HAVING `Número de compatriotas` <> 0
 ORDER BY c1.ContactName;

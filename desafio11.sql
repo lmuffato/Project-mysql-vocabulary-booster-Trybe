9d1e9eeb20857aca9346e5dd651e8e7abc76de4b
Renata Andrade <renata@MacBook-Air-de-Renata.local>
2021-08-02 23:09:11 -0300

    fix: corrige teste

--- desafio11.sql
+++ desafio11.sql
@@ -4,7 +4,7 @@ FROM (
   SELECT C1.ContactName AS `Nome`,
   C1.Country AS `País`
   FROM w3schools.customers AS C1, w3schools.customers AS C2
-  WHERE C1.Country = C2.Country AND C1.ContactName <> C2.ContactName
+WHERE C1.Country = C2.Country AND C1.ContactName <> C2.ContactName
 ) AS `A`
-GROUP BY `Nome`
+GROUP BY `Nome`, `País`
 ORDER BY `Nome`;

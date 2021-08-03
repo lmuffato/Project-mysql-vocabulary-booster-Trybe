bcab675516ba13ea98451628f7745bf9c16e7598
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-08-03 17:04:38 -0300

    Desafio 11 test

---
+++ desafio11.sql
@@ -0,0 +1,7 @@
+SELECT c1.ContactName as 'Nome',
+c1.Country as 'País',
+COUNT(c2.Country) -1 as 'Número de compatriotas'
+FROM w3schools.customers as c1, w3schools.customers as c2
+WHERE c1.Country = c2.Country
+GROUP BY c1.ContactName, c1.Country
+ORDER BY c1.ContactName;

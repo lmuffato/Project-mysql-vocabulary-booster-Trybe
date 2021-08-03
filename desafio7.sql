b40e7e0c696f2f99d657d6d96050ee0a6193ce54
andy silva <andssilva.23@gmail.com>
2021-08-02 21:12:23 -0400

    feat(req 7): :rocket: challenge 7

---
+++ desafio7.sql
@@ -0,0 +1,23 @@
+/* 7 - Faça um relatório que mostra o histórico de cargos das pessoas empregadas que iniciaram seus cargos nos meses de janeiro, fevereiro ou março
+Faça um relatório que mostre o histórico de cargos das pessoas empregadas que iniciaram seus cargos nos meses de janeiro, fevereiro ou março.
+
+Use o banco hr como referência
+
+Monte uma query que exiba 03 colunas:
+
+  A) A primeira deve possuir o alias "Nome completo" e exibir o nome completo da pessoa empregada em CAIXA ALTA (não se esqueça do espaço entre o nome e o sobrenome).
+  B) A segunda deve possuir o alias "Data de início" e exibir a data que a pessoa iniciou o cargo.
+  C) A terceira deve possuir o alias "Salário" e exibir o salário da pessoa.
+
+Os resultados devem estar ordenados pelo nome completo das pessoas empregadas em ordem alfabética.
+
+Em caso de empate no nome completo, ordene os resultados pela data de início que a pessoa iniciou seu cargo, em ordem crescente. */
+SELECT
+UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS `Nome completo`,
+h.START_DATE AS `Data de início`,
+e.SALARY AS `Salário`
+FROM hr.employees e
+INNER JOIN hr.job_history h
+ON h.EMPLOYEE_ID = e.EMPLOYEE_ID
+WHERE MONTH(h.START_DATE) IN (1, 2, 3)
+ORDER BY `Nome completo`, `Data de início`;

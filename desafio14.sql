f1c5aca3e0d6896e604bead201b029f885d2ff2f
Gabriel Ferreira <glucas709@gmail.com>
2021-08-03 14:17:32 -0300

    Challenge 14

---
+++ desafio14.sql
@@ -0,0 +1,17 @@
+(
+  SELECT
+    Country AS 'País'
+  FROM
+    w3schools.suppliers
+)
+UNION
+  (
+    SELECT
+      Country AS 'País'
+    FROM
+      w3schools.customers
+  )
+ORDER BY
+  `País`
+LIMIT
+  5

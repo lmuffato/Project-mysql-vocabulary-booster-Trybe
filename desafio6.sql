810126559cfbf04d343b196d142b0c2b65dfa3f0
lucas-chave <lcchavesr150@gmail.com>
2021-08-03 17:24:10 -0300

    fix challenge 6

--- desafio6.sql
+++ desafio6.sql
@@ -2,11 +2,12 @@
 
 SELECT CONCAT(first_name, ' ', last_name) AS `Nome completo`,
 job_title AS Cargo,
-hire_date AS `Data de início do cargo`,
+start_date AS `Data de início do cargo`,
 department_name AS Departamento
-FROM hr.employees AS employees
+FROM hr.job_history AS history_JOB
+INNER JOIN hr.employees AS employees
+ON employees.employee_id=history_JOB.employee_id
 INNER JOIN hr.jobs AS jobs
-ON jobs.job_id=employees.job_id
+ON history_JOB.job_id=jobs.job_id
 INNER JOIN hr.departments AS departments
-ON departments.department_id=employees.department_id
-ORDER BY `Nome completo` DESC, Cargo;
+ON departments.department_id=history_JOB.department_id;

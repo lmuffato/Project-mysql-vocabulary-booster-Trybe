19a73c3277c7423e0c9a37503750afa8f91920b6
carlos25xrm <carlos25xrm@gmail.com>
2021-08-03 05:45:16 -0300

    fix: requisito 10 e teste com mudança no valor da média

--- desafio10.sql
+++ desafio10.sql
@@ -1,9 +1,12 @@
 SELECT products.ProductName AS Produto,
-MIN(order_details.ProductID * order_details.Quantity) AS Mínima,
-MAX(order_details.ProductID * order_details.Quantity) AS Máxima,
-ROUND(AVG(order_details.ProductID * order_details.Quantity), 2) AS Média
+MIN(order_details.Quantity) AS Mínima,
+MAX(order_details.Quantity) AS Máxima,
+ROUND(AVG(order_details.Quantity), 2) AS Média
 FROM w3schools.products AS products
 INNER JOIN order_details AS order_details
 ON order_details.ProductID = products.ProductID
-GROUP BY Produto
+GROUP BY Produto HAVING Média >= 20.01
 ORDER BY Média, Produto;
+
+-- Testando colocar Média >= 20.01 para analisar
+-- se isso influencia na aprovação pelo avaliador

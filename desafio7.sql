e4f8df11ce49f154a9a0063a2e6fddd5a5efafe1
Ederson S <edersonrodrigo31@gmail.com>
2021-08-03 15:59:07 -0300

    feat: requisito 7

---
+++ desafio7.sql
@@ -0,0 +1,21 @@
+SELECT
+    CONCAT(employees.FIRST_NAME,
+    ' ',
+    employees.LAST_NAME) AS 'Nome completo',
+    job_history.START_DATE AS 'Data de início do cargo',
+    employees.SALARY AS 'Salário' 
+FROM
+    hr.job_history as job_history 
+INNER JOIN
+    hr.employees AS employees          
+        ON employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID  
+WHERE
+    MONTH(job_history.START_DATE) IN (
+        '1','2','3'
+    ) 
+ORDER BY
+    CONCAT(employees.FIRST_NAME,
+    ' ',
+    employees.LAST_NAME),
+    job_history.START_DATE;
+    

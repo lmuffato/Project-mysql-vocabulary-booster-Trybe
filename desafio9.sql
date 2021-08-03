8ea8c4a755a2d1cc9b5c54d8aa061754fa328b99
Guilherme de Prais <guilherme.prais@gmail.com>
2021-08-03 10:41:23 -0300

    req 09

---
+++ desafio9.sql
@@ -0,0 +1,8 @@
+SELECT
+  CONCAT(E.FirstName, ' ', E.LastName) AS 'Nome completo',
+  COUNT(*) AS 'Total de pedidos'
+FROM
+  w3schools.employees AS E
+  INNER JOIN w3schools.orders AS O ON O.EmployeeID = E.EmployeeID
+GROUP BY `Nome completo`
+ORDER BY `Total de pedidos`;

88add25639d1c5e73d05682e765d6a77893c30da
vinicios <soicinivt@gmail.com>
2021-08-03 02:50:52 -0300

    FIX - Desafio 4, using correct table now.

--- desafio4.sql
+++ desafio4.sql
@@ -1,9 +1,11 @@
-SELECT j.job_title AS Cargo, ROUND(AVG(j.max_salary + j.min_salary), 2) AS 'Média salarial', CASE
-    WHEN AVG(j.max_salary + j.min_salary) BETWEEN 2000 AND 5800 THEN "Júnior"
-    WHEN AVG(j.max_salary + j.min_salary) BETWEEN 5801 AND 7500 THEN "Pleno"
-    WHEN AVG(j.max_salary + j.min_salary) BETWEEN 7501 AND 10500 THEN "Sênior"
-    WHEN AVG(j.max_salary + j.min_salary) > 10500 THEN "CEO"
+SELECT j.job_title AS Cargo, ROUND(AVG((e.salary)), 2) AS 'Média salarial', CASE
+    WHEN AVG(e.salary) BETWEEN 2000 AND 5800 THEN "Júnior"
+    WHEN AVG(e.salary) BETWEEN 5801 AND 7500 THEN "Pleno"
+    WHEN AVG(e.salary) BETWEEN 7501 AND 10500 THEN "Sênior"
+    WHEN AVG(e.salary) > 10500 THEN "CEO"
     END AS Nível
 FROM hr.jobs j
+INNER JOIN hr.employees e
+ON e.job_id = j.job_id
 GROUP BY Cargo
 ORDER BY `Média salarial`, Cargo;

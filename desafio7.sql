30319afa98b5fa11e75f202acbd8b1ffd556f877
lucas-chave <lcchavesr150@gmail.com>
2021-08-03 17:47:38 -0300

    challenge 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,9 +1,7 @@
 -- challenge 7;
 
-SELECT UCASE(CONCAT(first_name, ' ', last_name)) AS `Nome completo`,
-start_date AS `Data de início`,
-salary AS Salário
 FROM hr.job_history AS history_JOB
 INNER JOIN hr.employees AS employees
 ON employees.employee_id=history_JOB.employee_id
+WHERE MONTH(start_date)=01 OR MONTH(start_date)=02 OR MONTH(start_date)=03
 ORDER BY `Nome completo` ASC, `Data de início` ASC;

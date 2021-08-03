9b779f9684b641ee4c0c7dacf7960784f976b7ce
jonathan <jonathanfernandesjf98@gmail.com>
2021-08-03 16:48:20 -0300

    desafio 15

---
+++ desafio15.sql
@@ -0,0 +1,22 @@
+USE hr;
+ 
+DROP PROCEDURE IF EXISTS buscar_media_por_cargo;
+
+DELIMITER $$
+
+CREATE PROCEDURE `buscar_media_por_cargo`(IN cargo VARCHAR(250))
+BEGIN
+  SELECT ROUND(AVG(e.salary),2) AS 'Média salarial' FROM employees AS e
+  INNER JOIN jobs AS j
+  WHERE e.JOB_ID=j.JOB_ID
+  AND j.JOB_TITLE=cargo;
+END $$
+
+DELIMITER ;
+
+CALL buscar_media_por_cargo('Programmer');
+ 
+
+  
+  
+  

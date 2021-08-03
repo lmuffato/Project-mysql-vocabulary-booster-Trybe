0b49f0a1c718c5d202daeb31e49a3baed7d23059
Guilherme de Prais <guilherme.prais@gmail.com>
2021-08-03 11:21:36 -0300

    req 11

---
+++ desafio11.sql
@@ -0,0 +1,15 @@
+SELECT
+  C1.ContactName AS 'Nome',
+  C1.Country AS 'País',
+  COUNT(*) AS 'Número de compatriotas'
+FROM
+  w3schools.customers AS C1,
+  w3schools.customers AS C2
+WHERE
+  C1.Country = C2.Country
+  AND C1.ContactName <> C2.ContactName
+GROUP BY
+  C1.ContactName,
+  C1.Country
+ORDER BY
+  C1.ContactName;

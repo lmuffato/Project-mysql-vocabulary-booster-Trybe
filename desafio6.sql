eb1bb318a30670b54cda323ae4553e21b83a6487
carlos25xrm <carlos25xrm@gmail.com>
2021-08-03 02:11:39 -0300

    fix(2): requisito 6

--- desafio6.sql
+++ desafio6.sql
@@ -2,16 +2,14 @@ SELECT CONCAT(employees.first_name, ' ', employees.last_name) AS 'Nome completo'
 jobs.job_title AS Cargo,
 job_history.start_date AS 'Data de início do cargo',
 departments.department_name AS Departamento
-FROM hr.employees AS employees
+FROM hr.job_history AS job_history
+INNER JOIN employees AS employees
+ON employees.employee_id = job_history.employee_id
 INNER JOIN jobs AS jobs
-ON jobs.job_id = employees.job_id
-INNER JOIN job_history AS job_history
-ON job_history.employee_id = employees.employee_id
+ON jobs.job_id = job_history.job_id
 INNER JOIN departments AS departments
-ON departments.department_id = employees.department_id
+ON departments.department_id = job_history.department_id
 ORDER BY `Nome Completo` DESC, Cargo;
 
--- OBS: Todos os INNER JOINs tem que estar ligados
--- com a tabela informada no FROM. Consequentemente
--- a tabela do FROM deve conter todas as ligações
--- necessárias para ser ligada aos INNER JOINs.
+-- OBS: Identificar a tabela principal que faz as ligações
+-- com todas as outras tabelas informadas nos INNER JOINs e ONs

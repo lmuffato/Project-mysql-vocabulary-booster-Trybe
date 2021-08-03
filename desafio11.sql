74df48feb8f3562186eb7d26b7f638883a858fc7
Erick-Marinho <erickgaves@gmail.com>
2021-08-03 15:05:30 -0300

    Inserindo desigualdade para otimizacao da query

--- desafio11.sql
+++ desafio11.sql
@@ -1,13 +1,15 @@
 SELECT
     c1.contactName Nome,
     c1.country País,
-    COUNT(c2.country) - 1 'Número de compatriotas'
+    COUNT(c2.country) 'Número de compatriotas'
 FROM
     w3schools.customers c1
 INNER JOIN
     w3schools.customers c2
 ON
     c1.country = c2.country
+AND
+    c1.customerID <> c2.customerId
 GROUP BY
     1
 ORDER BY

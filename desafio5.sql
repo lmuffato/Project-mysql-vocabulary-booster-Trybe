4658ff5ae8640701f144b1165af437a96cdaff71
ScriptCamilo <script.camilo@gmail.com>
2021-08-03 14:26:18 -0300

    fix: fifth requirement done

--- desafio5.sql
+++ desafio5.sql
@@ -1,5 +1,5 @@
 SELECT j.JOB_TITLE AS Cargo,
-(MAX_SALARY - MIN_SALARY) AS 'Variação salarial',
+(MAX_SALARY - MIN_SALARY) AS 'Variação Salarial',
 ROUND((AVG(MIN_SALARY) / 12), 2) AS 'Média mínima mensal',
 ROUND((AVG(MAX_SALARY) / 12), 2) AS 'Média máxima mensal'
 FROM hr.jobs AS j

7c0f2af53227b46c30e69b55c93588aa760c3929
Gabriel Ferreira <glucas709@gmail.com>
2021-08-03 20:41:46 -0300

    Challenge 20

---
+++ desafio20.sql
@@ -0,0 +1,19 @@
+USE hr;
+DELIMITER $$
+
+CREATE PROCEDURE exibir_historico_completo_por_funcionario(IN email VARCHAR(100))
+BEGIN
+  SELECT
+    CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
+    d.DEPARTMENT_NAME AS 'Departamento',
+    j.JOB_TITLE AS 'Cargo'
+  FROM hr.job_history jh
+  INNER JOIN hr.employees e ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+  INNER JOIN hr.jobs j ON j.JOB_ID = jh.JOB_ID
+  INNER JOIN hr.departments d ON d.DEPARTMENT_ID = jh.DEPARTMENT_ID
+  WHERE e.EMAIL = email;
+END $$
+
+DELIMITER ;
+
+CALL exibir_historico_completo_por_funcionario('NKOCHHAR');

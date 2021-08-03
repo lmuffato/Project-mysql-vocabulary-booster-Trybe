609acc558476cfef0aa986939922545872e6c45a
ScriptCamilo <script.camilo@gmail.com>
2021-08-03 14:27:36 -0300

    fix: fifteenth requirement done

--- desafio15.sql
+++ desafio15.sql
@@ -2,7 +2,7 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(90))
 BEGIN
-  SELECT ROUND(AVG(SALARY), 2) AS 'Média Salarial'
+  SELECT ROUND(AVG(SALARY), 2) AS 'Média salarial'
   FROM hr.employees AS e, hr.jobs AS j
   WHERE e.JOB_ID = j.JOB_ID AND j.JOB_TITLE = cargo;
 END $$

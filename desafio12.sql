10c3b6b824fafb18abfb2319b6d44516740a91cf
Walvesb65 <willian.alvesb65@gmail.com>
2021-08-04 06:19:39 -0300

    req 12

---
+++ desafio12.sql
@@ -0,0 +1,10 @@
+SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo funcionário 1',
+e.SALARY AS 'Salário funcionário 1',
+e.PHONE_NUMBER AS 'Telefone funcionário 1',
+CONCAT(e2.FIRST_NAME, ' ', e2.LAST_NAME) AS 'Nome completo funcionário 2',
+e2.SALARY AS 'Salário funcionário 2',
+e2.PHONE_NUMBER AS 'Telefone funcionário 2'
+FROM hr.employees AS e, hr.employees AS r2
+WHERE e.employee_id <> e2.employee_id
+AND e.JOB_ID = e2.JOB_ID
+ORDER BY 1, 4;

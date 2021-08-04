59e1d53e6d7ca8ff5a2ce4832a17a83570e5674a
Walvesb65 <willian.alvesb65@gmail.com>
2021-08-04 05:42:30 -0300

    req 9

--- desafio9.sql
+++ desafio9.sql
@@ -1,8 +1,6 @@
-SELECT
-CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome completo',
-COUNT(*) AS 'Total de pedidos'
-FROM w3schools.employees e
-INNER JOIN w3schools.orders o
+SELECT CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome completo', COUNT(*) AS 'Total de pedidos'
+FROM w3schools.employees AS e
+INNER JOIN w3schools.orders AS o
 ON e.EmployeeID = o.EmployeeID
 GROUP BY CONCAT(e.FirstName, ' ', e.LastName)
 ORDER BY COUNT(*);

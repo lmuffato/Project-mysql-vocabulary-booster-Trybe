be30cf42485415f8aecf9a80b0d3808c19fcc289
jepaur5 <jepaur5@gmail.com>
2021-08-04 12:45:23 -0300

    requisito7-corrigindo1

--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,6 @@
 SELECT 
     UPPER(CONCAT(e.first_name, ' ', e.last_name)) AS `Nome completo`,
-    h.start_date `AS Data de início do cargo`,
+    h.start_date `Data de início`,
     e.salary AS `Salário`
 FROM
     hr.job_history AS h
@@ -8,4 +8,4 @@ FROM
     hr.employees AS e ON h.employee_id = e.employee_id
 WHERE
     MONTH(h.start_date) IN (1 , 2, 3)
-ORDER BY `Nome completo` ASC, `AS Data de início do cargo` ASC;
+ORDER BY `Nome completo` ASC, `Data de início` ASC;

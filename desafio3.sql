871a5b25f498d5de058dc1b0a9bdf23b42b2853d
Roberto Natividade <robertonnv@gmail.com>
2021-08-04 09:39:25 -0300

    Desafio 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,2 +1,2 @@
 select job_title as Cargo, max_salary - MIN_SALARY as 'Diferença entre máximo e mínimo'        
-from hr.jobs order by cargo asc;
+from hr.jobs order by max_salary - MIN_SALARY, cargo asc;

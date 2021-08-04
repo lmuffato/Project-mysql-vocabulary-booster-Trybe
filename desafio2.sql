0046e87abf563c95135986501139943bff5f489b
Roberto Natividade <robertonnv@gmail.com>
2021-08-04 02:09:22 -0300

    Desafio 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,9 +1,7 @@
-select
-	job_title as Cargo,
-	case
-		when max_salary between 5000 and 10000 then 'Baixo'
-		when max_salary between 10001 and 20000 then 'Médio'
-		when max_salary between 20001 and 30000 then 'Alto'
-		when max_salary > 30000 then 'Altíssimo' end as 'Nível'        
-	from hr.jobs
-    order by cargo asc; 
+select job_title as Cargo,
+case when max_salary between 5000 and 10000 then 'Baixo'
+when max_salary between 10001 and 20000 then 'Médio'
+when max_salary between 20001 and 30000 then 'Alto'
+when max_salary > 30000 then 'Altíssimo' end as 'Nível'        
+from hr.jobs order by cargo asc;
+ 

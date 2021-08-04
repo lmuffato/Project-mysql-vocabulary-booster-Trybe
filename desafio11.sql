39806fa7ea28fa55e1a50148e02b578cdeea9102
vinicios <soicinivt@gmail.com>
2021-08-04 03:27:01 -0300

    FIX Desafio 11 - HAVING`Número de compatriotas > 0.

--- desafio11.sql
+++ desafio11.sql
@@ -1,4 +1,5 @@
 SELECT t1.ContactName AS 'Nome', t2.Country AS 'País', COUNT(t2.Country) - 1 AS 'Número de compatriotas' FROM w3schools.customers t1, w3schools.customers t2
 WHERE t1.Country = t2.Country
 GROUP BY `País`, `Nome`
+HAVING `Número de compatriotas` > 0
 ORDER BY `Nome`;

152fd6df36ef9bbec84658de9fbdcebdcceb08c1
vinicios <soicinivt@gmail.com>
2021-08-04 04:32:38 -0300

    FIX Desafio 14. Use UNION.

--- desafio14.sql
+++ desafio14.sql
@@ -1,5 +1,5 @@
-SELECT DISTINCT c.Country AS 'País' FROM w3schools.customers AS c
-INNER JOIN w3schools.suppliers AS s
-ON s.Country = c.Country
+SELECT Country AS 'País' FROM w3schools.customers
+UNION 
+SELECT Country from w3schools.suppliers
 ORDER BY País
 LIMIT 5;

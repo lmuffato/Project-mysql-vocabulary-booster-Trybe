022a6f2a375662bd8103b5059bd0891606d9eed8
hheloisa <hhackenhaar@gmail.com>
2021-08-04 22:16:53 -0300

    Requisito 18 - Corrige style

--- desafio18.sql
+++ desafio18.sql
@@ -1,16 +1,16 @@
 SELECT
-	CONCAT(te.first_name, " ", te.last_name) AS `Nome completo`,
-    CONCAT(
-		DAY(tjh.start_date),
-        "/", MONTH(tjh.start_date),
-        "/", YEAR(tjh.start_date)
-        )AS `Data de início do cargo`,
-	CONCAT(
-		DAY(tjh.end_date),
-        "/", MONTH(tjh.end_date),
-        "/", YEAR(tjh.end_date)
-        )AS `Data de rescisão`,
-	ROUND(DATEDIFF(tjh.end_date, tjh.start_date)/365, 2) AS `Anos trabalhados`
+CONCAT(te.first_name, " ", te.last_name) AS `Nome completo`,
+CONCAT(
+DAY(tjh.start_date),
+"/", MONTH(tjh.start_date),
+"/", YEAR(tjh.start_date)
+)AS `Data de início do cargo`,
+CONCAT(
+DAY(tjh.end_date),
+"/", MONTH(tjh.end_date),
+"/", YEAR(tjh.end_date)
+)AS `Data de rescisão`,
+ROUND(DATEDIFF(tjh.end_date, tjh.start_date)/365, 2) AS `Anos trabalhados`
 FROM hr.employees AS te
 INNER JOIN hr.job_history AS tjh
 ON te.employee_id = tjh.employee_id

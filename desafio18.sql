345b1c919d950481f1997831bbe0dc11fc8de266
andy silva <andssilva.23@gmail.com>
2021-08-06 06:09:58 -0400

    fix(req 18): :bug: fix challenge 18

--- desafio18.sql
+++ desafio18.sql
@@ -9,10 +9,17 @@ Faça um relatório que mostra o histórico de cargos das pessoas empregadas, mo
   D) A quarta coluna deve possuir o alias "Anos trabalhados" e exibir, usando como base as datas de saída e entrada, os anos que a pessoa ficou no cargo em questão, arredondados para duas casas decimais.
 
 Os resultados devem estar ordenados pelo nome completo das pessoas empregadas em ordem alfabética. Em caso de empate no nome completo, ordene os resultados pelos anos trabalhados por ela no cargo em questão, em ordem crescente. */
+
+-- Ideia para criação dos anos no formato esperado no teste, obtido através do gabarito dos exercícios 14 e 15 do bloco 21.1
+-- link: https://app.betrybe.com/course/back-end/sql/common-functions/solutions#exercicios
 SELECT
 CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome completo`,
-CONCAT(DAY(jh.START_DATE), '/', MONTH(jh.START_DATE), '/', YEAR(jh.START_DATE)) AS `Data de início`,
-CONCAT(DAY(jh.END_DATE), '/', MONTH(jh.END_DATE), '/', YEAR(jh.END_DATE)) AS `Data de rescisão`,
+CONCAT(
+  RIGHT(jh.START_DATE, 2), '/', SUBSTRING(jh.START_DATE, 6, 2), '/', YEAR(jh.START_DATE)
+) AS `Data de início`,
+CONCAT(
+  RIGHT(jh.END_DATE, 2), '/', SUBSTRING(jh.END_DATE, 6, 2), '/', YEAR(jh.END_DATE)
+) AS `Data de rescisão`,
 ROUND(DATEDIFF(jh.END_DATE, jh.START_DATE) / 365, 2) AS `Anos trabalhados`
 FROM hr.employees e
 INNER JOIN hr.job_history jh

dbd7f96eb34756532917d58d008d6b10ffc6c2d3
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-08-08 21:57:51 -0300

    Conserta desafio 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,2 +1,2 @@
-SELECT  as "País", IF(region_id = 1, 'incluído', 'não incluído') AS "Status Inclusão"
+SELECT country_name AS "País", IF(region_id = 1, 'incluído', 'não incluído') AS "Status Inclusão"
 FROM hr.countries;

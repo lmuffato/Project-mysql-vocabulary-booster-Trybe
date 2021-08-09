3bf8d1d5a12768ef5b2c35d3ee8b11cbf137b024
Rodrigo Rech <rodrigorech92@gmail.com>
2021-08-09 10:14:52 -0300

    Fix: CodeClimate desafio17

--- desafio17.sql
+++ desafio17.sql
@@ -1,9 +1,9 @@
 DELIMITER $$
 CREATE TRIGGER w3schools.trigger_update_date
-	BEFORE INSERT ON w3schools.orders
-    FOR EACH ROW
+BEFORE INSERT ON w3schools.orders
+FOR EACH ROW
 BEGIN 
-	SET NEW.OrderDate = NOW();
+SET NEW.OrderDate = NOW();
 END $$
 
 DELIMITER ;

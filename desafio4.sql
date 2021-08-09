b9f86c176094f04f1016c05611540e9dc96a7a60
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-08-08 22:32:59 -0300

    Desafio 4 e 5

---
+++ desafio4.sql
@@ -0,0 +1,13 @@
+SELECT j.job_title AS Cargo,
+ROUND(AVG(SALARY), 2) AS 'Média salarial',
+CASE 
+WHEN ROUND(AVG(SALARY), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
+WHEN ROUND(AVG(SALARY), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
+WHEN ROUND(AVG(SALARY), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
+WHEN ROUND(AVG(SALARY), 2) > 10500 THEN 'CEO'
+END AS Senioridade
+FROM hr.jobs AS j
+INNER JOIN hr.employees AS e
+ON j.job_id = e.job_id
+GROUP BY Cargo
+ORDER BY `Média salarial`, Cargo;

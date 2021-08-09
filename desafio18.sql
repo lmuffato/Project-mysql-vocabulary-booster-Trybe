4403ec9542ea04c79c070857a999d5fba820c61b
Rodrigo Rech <rodrigorech92@gmail.com>
2021-08-09 10:58:34 -0300

    Desafio18 fix

--- desafio18.sql
+++ desafio18.sql
@@ -4,4 +4,4 @@ DATE_FORMAT(END_DATE, '%d/%m/%Y') AS 'Data de rescisão',
 ROUND((YEAR(END_DATE) + MONTH(END_DATE)/12 + DAY(END_DATE)/365 )- (YEAR(START_DATE) + MONTH(START_DATE)/12 + DAY(START_DATE)/365 ),2) AS 'Anos trabalhados'
 FROM hr.job_history AS h
 INNER JOIN hr.employees AS e ON h.EMPLOYEE_ID = e.EMPLOYEE_ID
-ORDER BY 'Nome completo', 'Anos trabalhados';
+ORDER BY `Nome completo`, `Anos trabalhados`;

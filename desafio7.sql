fecb07b51ef5e183de1cfd3201a48b5338c351c8
Villefor <franklincrius@gmail.com>
2021-08-08 22:19:17 -0300

    requisitos do 5 ao 10

---
+++ desafio7.sql
@@ -0,0 +1,8 @@
+SELECT UCASE(CONCAT(emp.FIRST_NAME, ' ',emp.LAST_NAME)) AS `Nome completo`,
+hist.START_DATE AS `Data de início`,
+emp.SALARY AS `Salário`
+FROM hr.employees AS emp
+INNER JOIN hr.job_history AS hist
+ON emp.EMPLOYEE_ID = hist.EMPLOYEE_ID
+WHERE MONTH(hist.START_DATE) BETWEEN 01 AND 03
+ORDER BY `Nome completo`, `Data de início`;

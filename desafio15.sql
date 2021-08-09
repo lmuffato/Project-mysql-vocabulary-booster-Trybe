9e8c7f07571d5ed9e2a94af6b0c469b667756444
Villefor <franklincrius@gmail.com>
2021-08-08 23:22:44 -0300

    requisitos 14, 15 e 16

---
+++ desafio15.sql
@@ -0,0 +1,14 @@
+DELIMITER $$
+
+CREATE PROCEDURE buscar_media_por_cargo(IN jobSelected VARCHAR(50))
+BEGIN
+SELECT ROUND(AVG(emp.SALARY), 2) AS `Média salarial`
+FROM hr.employees AS `emp`
+INNER JOIN hr.jobs AS `job`
+ON emp.JOB_ID = job.JOB_ID
+WHERE job.JOB_TITLE LIKE jobSelected;
+END $$
+
+DELIMITER ;
+
+CALL buscar_media_por_cargo('Programmer');

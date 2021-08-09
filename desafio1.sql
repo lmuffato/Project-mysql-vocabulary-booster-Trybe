9f6a63ae92f3a2fe9b342df50c924a55585fdf1a
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-08-08 22:03:15 -0300

    Desafio 1 em nome de jesus

--- desafio1.sql
+++ desafio1.sql
@@ -1,2 +1,3 @@
-SELECT country_name AS "País", IF(region_id = 1, 'incluído', 'não incluído') AS "Status Inclusão"
-FROM hr.countries;
+SELECT country_name AS 'País', IF(region_id = 1, 'incluído', 'não incluído') AS "Status Inclusão"
+FROM hr.countries
+ORDER BY País;

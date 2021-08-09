f5d04f77fe78a42a1e94aa8ea5dc36d816f7cd05
Rodrigo Rech <rodrigorech92@gmail.com>
2021-08-09 10:18:44 -0300

    Fix: desafio16.sql

--- desafio16.sql
+++ desafio16.sql
@@ -7,7 +7,7 @@ DECLARE jobs_total INT;
 SELECT COUNT(1)
 FROM hr.job_history AS j
 INNER JOIN hr.employees AS e ON e.EMPLOYEE_ID = j.EMPLOYEE_ID
-WHERE e.EMAIL LIKE email INTO jobs_total;
+WHERE e.EMAIL = email INTO jobs_total;
 RETURN jobs_total;
 END $$
 

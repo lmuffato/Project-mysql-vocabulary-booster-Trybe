5a755c9aeea673878e8e49afdf742b62de091d72
Rodrigo Rech <rodrigorech92@gmail.com>
2021-08-09 11:43:33 -0300

    Fix: Desafio16

--- desafio16.sql
+++ desafio16.sql
@@ -1,16 +1,15 @@
 DELIMITER $$
 
-CREATE FUNCTION hr.buscar_quatidade_de_empregos_por_funcionario(email VARCHAR(50))
+CREATE FUNCTION buscar_quatidade_de_empregos_por_funcionario(email VARCHAR(50))
 RETURNS INT READS SQL DATA
 BEGIN 
 DECLARE jobs_total INT;
 SELECT COUNT(1)
 FROM hr.job_history AS j
 INNER JOIN hr.employees AS e ON e.EMPLOYEE_ID = j.EMPLOYEE_ID
-WHERE e.EMAIL = email INTO jobs_total;
+WHERE e.EMAIL = email
+INTO jobs_total;
 RETURN jobs_total;
 END $$
 
 DELIMITER ;
-
-SELECT hr.buscar_quatidade_de_empregos_por_funcionario("NKOCHHAR");

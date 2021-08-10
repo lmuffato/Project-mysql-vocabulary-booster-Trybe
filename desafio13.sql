4e26da2c80a5bde685d144b41f7af9fa45fc948e
lucas-chave <lcchavesr150@gmail.com>
2021-08-10 12:37:24 -0300

    challenge 13

---
+++ desafio13.sql
@@ -0,0 +1,9 @@
+-- challenge 13;
+
+SELECT ProductName AS Produto,
+Price AS Preço
+FROM w3schools.products AS products
+INNER JOIN w3schools.order_details AS details_order
+ON products.ProductID=details_order.ProductID
+WHERE Quantity > 80
+ORDER BY Produto;

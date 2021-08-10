0939ec6ac4d49abbab7355ff9dc105f0285366b6
Roberto Natividade <robertonnv@gmail.com>
2021-08-09 22:22:58 -0300

    Desafio 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,6 +1,6 @@
 select
 job_title as Cargo, 
-max_salary - MIN_SALARY as 'Variação Salarial',        
+y.max_salary - y.MIN_SALARY as 'Variação Salarial',        
 round(y.min_salary/12,2) as 'Média mínima mensal',
 round(y.max_salary/12,2) as 'Média máxima mensal'
 from hr.jobs as y

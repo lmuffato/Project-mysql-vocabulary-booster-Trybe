a3228517f3f65a102cfb841f8ad03d06dcbe6eb7
Roberto Natividade <robertonnv@gmail.com>
2021-08-10 11:43:20 -0300

    Desafio 6

--- deasfio6.sql
+++ deasfio6.sql
@@ -1,12 +1,12 @@
 SELECT CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) AS 'Nome completo',
-j.JOB_TITLE AS 'Cargo',
+j.JOB_TITLE AS Cargo,
 histo.START_DATE AS 'Data de início do cargo',
-depart.DEPARTMENT_NAME AS 'Departamento'
+depart.DEPARTMENT_NAME AS Departamento
 FROM hr.employees AS emp
 INNER JOIN hr.job_history AS histo
 ON histo.EMPLOYEE_ID = emp.EMPLOYEE_ID
 INNER JOIN hr.jobs AS j
-ON histo.JOB_ID = j.JOB_ID
+ON j.JOB_ID = histo.JOB_ID
 INNER JOIN hr.departments AS depart
-ON histo.DEPARTMENT_ID = depart.DEPARTMENT_ID
-ORDER BY CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) DESC, j.JOB_TITLE;
+ON depart.DEPARTMENT_ID = histo.DEPARTMENT_ID
+ORDER BY `Nome completo` DESC, Cargo;

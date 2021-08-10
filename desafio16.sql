33db5989079b3fc82bcacce638361ef444f9acb5
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-08-09 23:07:09 -0300

    Desafio 16

---
+++ desafio16.sql
@@ -0,0 +1,16 @@
+USE hr;
+DELIMITER $$
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(50))
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE num INT;
+SELECT
+    COUNT(e.employee_id)
+FROM
+    hr.employees AS e
+WHERE
+    job_history.employee_ID = e.employee_ID
+        && e.email = email INTO num;
+RETURN num;
+END $$
+SELECT BUSCAR_QUANTIDADE_DE_EMPREGOS_POR_FUNCIONARIO('NKOCHHAR');

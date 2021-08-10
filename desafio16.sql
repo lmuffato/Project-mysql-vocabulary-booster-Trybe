9a557eb03589da6a88c8e4c0948a775f7f429058
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-08-09 23:13:49 -0300

    Desafio 16 conserta

--- desafio16.sql
+++ desafio16.sql
@@ -1,4 +1,4 @@
-USE hr;
+USE hr
 DELIMITER $$
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(50))
 RETURNS INT READS SQL DATA
@@ -7,6 +7,8 @@ DECLARE num INT;
 SELECT
     COUNT(e.employee_id)
 FROM
+    hr.job_history
+        INNER JOIN
     hr.employees AS e
 WHERE
     job_history.employee_ID = e.employee_ID
@@ -14,3 +16,4 @@ WHERE
 RETURN num;
 END $$
 SELECT BUSCAR_QUANTIDADE_DE_EMPREGOS_POR_FUNCIONARIO('NKOCHHAR');
+DELIMITER;

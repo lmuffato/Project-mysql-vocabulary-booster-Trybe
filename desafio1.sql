ebd0d36bbe46e16388f36e0134b8680f24f76005
carol-bitencourt <carolmedeirosbitencourt@gmail.com>
2021-08-11 20:34:49 -0300

    Req 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,3 +1,3 @@
 SELECT country_name AS 'País',
-IF (region_id = 1, 'incluido', 'não incluído') AS 'Status Inclusão'
+IF(region_id = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
 FROM hr.countries ORDER BY País;

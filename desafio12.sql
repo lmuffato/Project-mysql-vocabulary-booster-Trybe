08816b160a117e66f416e1fdfe6304288955c5ec
lucas-chave <lcchavesr150@gmail.com>
2021-08-11 17:02:06 -0300

    fix challenge 12

--- desafio12.sql
+++ desafio12.sql
@@ -1,4 +1,5 @@
 -- challenge 12;
+-- https://github.com/tryber/sd-010-a-mysql-vocabulary-booster/pull/85/commits/8f586a6f4111f872f9b9f66a3485160be08d8ef9
 
 SELECT CONCAT(employee.FIRST_NAME, ' ', employee.LAST_NAME) AS `Nome completo funcionário 1`,
 employee.SALARY AS `Salário funcionário 1`,
@@ -7,5 +8,5 @@ CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME) AS `Nome completo funcion
 employees.SALARY `Salário funcionário 2`,
 employees.PHONE_NUMBER AS `Telefone funcionário 2`
 FROM hr.employees AS employee, hr.employees AS employees
-WHERE employee.JOB_ID=employees.JOB_ID 
+WHERE employee.JOB_ID=employees.JOB_ID AND employee.EMPLOYEE_ID<>employees.EMPLOYEE_ID
 ORDER BY `Nome completo funcionário 1`, `Nome completo funcionário 2`

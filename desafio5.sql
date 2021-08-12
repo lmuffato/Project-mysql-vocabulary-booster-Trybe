9f751b9db8032c755410531026fde40c09de3c09
carol-bitencourt <carolmedeirosbitencourt@gmail.com>
2021-08-12 00:52:08 -0300

    Req 5, dnv

--- desafio5.sql
+++ desafio5.sql
@@ -1,7 +1,14 @@
 SELECT job_title AS 'Cargo',
 max_salary - min_salary AS 'Variação Salarial',
-ROUND(AVG(min_salary / 12), 2) AS 'Média mínima mensal',
-ROUND(AVG(max_salary / 12), 2) AS 'Média máxima mensal'
+ROUND((min_salary / 12), 2) AS 'Média mínima mensal',
+ROUND((max_salary / 12), 2) AS 'Média máxima mensal'
 FROM hr.jobs
-GROUP BY job_title
 ORDER BY max_salary - min_salary, job_title;
+-- Primeira versão
+-- SELECT job_title AS 'Cargo',
+-- max_salary - min_salary AS 'Variação Salarial',
+-- ROUND(AVG(min_salary / 12), 2) AS 'Média mínima mensal',
+-- ROUND(AVG(max_salary / 12), 2) AS 'Média máxima mensal'
+-- FROM hr.jobs
+-- GROUP BY job_title
+-- ORDER BY max_salary - min_salary, job_title;

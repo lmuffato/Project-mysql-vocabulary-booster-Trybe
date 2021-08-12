72df9e4dce26737b2f1f3144b16974042c92f98c
carol-bitencourt <carolmedeirosbitencourt@gmail.com>
2021-08-12 20:26:44 -0300

    req 7, dnv, era um ALIAS

--- desafio7.sql
+++ desafio7.sql
@@ -1,5 +1,5 @@
 SELECT UCASE(concat(E.FIRST_NAME, ' ', E.LAST_NAME)) AS 'Nome completo',
-JH.START_DATE AS 'Data de início do cargo',
+JH.START_DATE AS 'Data de início',
 E.SALARY AS 'Salário'
 FROM hr.employees AS E
 INNER JOIN hr.job_history AS JH

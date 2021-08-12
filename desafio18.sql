4ae1deedae049dacee5f95641849cf529945e056
lucas-chave <lcchavesr150@gmail.com>
2021-08-12 13:45:03 -0300

    fix challenge 18

--- desafio18.sql
+++ desafio18.sql
@@ -5,7 +5,7 @@
 SELECT CONCAT(FIRST_NAME, ' ', LAST_NAME) AS `Nome completo`,
 DATE_FORMAT(job_history.START_DATE, '%d/%m/%y') AS `Data de início`,
 DATE_FORMAT(job_history.END_DATE, '%d/%m/%y') AS `Data de rescisão`,
-ROUND(DATEDIFF(END_DATE, START_DATE) / 365.25, 2)  AS `Anos trabalhados`
+ROUND(DATEDIFF(END_DATE, START_DATE) / 365, 2)  AS `Anos trabalhados`
 FROM hr.employees AS employees INNER JOIN hr.job_history AS job_history
 ON employees.EMPLOYEE_ID=job_history.EMPLOYEE_ID
 ORDER BY `Nome completo`, `Anos trabalhados`;

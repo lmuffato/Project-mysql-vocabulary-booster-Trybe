5fc3032ba366247784867ca270348430ca376daf
Roberto Natividade <robertonnv@gmail.com>
2021-08-12 11:44:03 -0300

    Desafio 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,9 +1,10 @@
-SELECT ucase(CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME))
+SELECT
+ucase(CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME))
 AS 'Nome completo',
 histo.start_date as 'Data de início',
 emp.SALARY as Salário
-from hr.employees as emp
-INNER JOIN hr.job_history AS histo
+from hr.employees emp
+INNER JOIN hr.job_history histo
 on histo.employee_id = emp.employee_id
 where month(histo.start_date) in (1,2,3)
-ORDER BY 'Nome completo', 'Data de início'; 
+order by 'Nome Completo', 'Data de início';

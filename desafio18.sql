140abc21948dd848e5834c6bd35a294185d06cdd
lucas-chave <lcchavesr150@gmail.com>
2021-08-12 13:55:32 -0300

    fix challenge 18

--- desafio18.sql
+++ desafio18.sql
@@ -3,8 +3,8 @@
 -- https://www.dirceuresende.com/blog/sql-server-como-calcular-a-diferenca-em-anos-idade-entre-duas-datas-utilizando-tsql-ou-clr/
 
 SELECT CONCAT(FIRST_NAME, ' ', LAST_NAME) AS `Nome completo`,
-DATE_FORMAT(job_history.START_DATE, '%d/%m/%y') AS `Data de início`,
-DATE_FORMAT(job_history.END_DATE, '%d/%m/%y') AS `Data de rescisão`,
+DATE_FORMAT(job_history.START_DATE, '%d/%m/%Y') AS `Data de início`,
+DATE_FORMAT(job_history.END_DATE, '%d/%m/%Y') AS `Data de rescisão`,
 ROUND(DATEDIFF(job_history.END_DATE, job_history.START_DATE) / 365, 2)  AS `Anos trabalhados`
 FROM hr.employees AS employees INNER JOIN hr.job_history AS job_history
 ON employees.EMPLOYEE_ID=job_history.EMPLOYEE_ID

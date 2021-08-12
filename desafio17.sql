f972bfb196c36dcf2700728b610238754012b5ce
lucas-chave <lcchavesr150@gmail.com>
2021-08-12 14:37:13 -0300

    adding requiremnt reference 17

--- desafio17.sql
+++ desafio17.sql
@@ -1,5 +1,6 @@
 -- challenge 17;
 -- https://blog.betrybe.com/tecnologia/o-que-e-como-usar/#:~:text=Trigger%20significa%20gatilho%2C%20em%20portugu%C3%AAs,ou%20altera%C3%A7%C3%A3o%20de%20alguma%20linha.
+-- https://github.com/tryber/sd-010-a-mysql-vocabulary-booster/pull/77/commits/15f02ce08d48977010e475b9ed30f64ff4854066
 
 USE w3schools;
 

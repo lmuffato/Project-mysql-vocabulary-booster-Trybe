a25b8d46992fb980df79a028ee97a50479a9201e
lucas-chave <lcchavesr150@gmail.com>
2021-08-12 14:30:52 -0300

    adding requiremnt reference 17

--- desafio17.sql
+++ desafio17.sql
@@ -1,4 +1,6 @@
 -- challenge 17;
+-- https://blog.betrybe.com/tecnologia/o-que-e-como-usar/#:~:text=Trigger%20significa%20gatilho%2C%20em%20portugu%C3%AAs,ou%20altera%C3%A7%C3%A3o%20de%20alguma%20linha.
+
 USE w3schools;
 
 DELIMITER $$

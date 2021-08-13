c7bdbd3a8e8455aaa3313ea0a4c4e3d76250e339
Roberto Natividade <robertonnv@gmail.com>
2021-08-13 12:08:53 -0300

    Desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,14 @@
+select
+wcustomers.contactname 'Nome de contato',
+wshippers.ShipperName 'Empresa que fez o envio',
+wOrders.OrderDate 'Data do pedido'
+from 
+w3schools.customers wcustomers
+inner join w3schools.orders wOrders
+
+on wcustomers.CustomerID = wOrders.CustomerID
+
+inner join w3schools.shippers wshippers
+on wOrders.ShipperID = wshippers.ShipperID
+and wshippers.ShipperID in (1,2)
+order by contactname asc, wshippers.ShipperName, wOrders.OrderDate asc;

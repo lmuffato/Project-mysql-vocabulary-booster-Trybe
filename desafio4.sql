9165e7abc57e2b1e8d87dbe4a529c10b877db57b
Alan Albuquerque Ferreira Lopes <alan.alb.flopes@gmail.com>
2021-08-13 14:48:29 -0400

    fix: add missing endline

---
+++ desafio4.sql
@@ -0,0 +1,15 @@
+SELECT jobs.JOB_TITLE AS 'Cargo', ROUND(AVG(employees.SALARY), 2) AS 'Média salarial',
+CASE 
+    WHEN ROUND(AVG(employees.SALARY))>=2000 AND ROUND(AVG(employees.SALARY))<=5800
+    THEN 'Júnior'
+    WHEN ROUND(AVG(employees.SALARY))>5800 AND ROUND(AVG(employees.SALARY))<=7500
+    THEN 'Pleno'
+    WHEN ROUND(AVG(employees.SALARY))>7500 AND ROUND(AVG(employees.SALARY))<=10500
+    THEN 'Senior'
+    WHEN ROUND(AVG(employees.SALARY))>10500 THEN 'CEO'
+    END AS 'Senioridade'
+FROM hr.jobs AS jobs
+INNER JOIN hr.employees AS employees
+ON jobs.JOB_ID = employees.JOB_ID
+GROUP BY Cargo
+ORDER BY AVG(employees.SALARY), Cargo;

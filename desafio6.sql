980331d9c331eb8f8c97a13edfc76e85f917e99b
Alan Albuquerque Ferreira Lopes <alan.alb.flopes@gmail.com>
2021-08-16 15:17:35 -0400

    feat: query that select full name, job, intial date in job and departament from 3 diff tables

---
+++ desafio6.sql
@@ -0,0 +1,12 @@
+SELECT CONCAT(empregados.FIRST_NAME,' ', empregados.LAST_NAME) AS `Nome Completo`,
+cargos.JOB_TITLE AS `Cargo`, historico_cargos.START_DATE AS `Data de início do cargo`,
+DEPARTMENT_NAME AS `Departamento`
+FROM hr.employees AS empregados
+INNER JOIN hr.jobs AS cargos 
+  ON empregados.JOB_ID = cargos.JOB_ID
+INNER JOIN hr.job_history AS historico_cargos
+  ON empregados.EMPLOYEE_ID = historico_cargos.EMPLOYEE_ID
+INNER JOIN hr.departments AS departamento
+  ON empregados.DEPARTMENT_ID = departamento.DEPARTMENT_ID
+GROUP BY `Nome Completo`
+ORDER BY `Nome Completo` DESC, Cargo;

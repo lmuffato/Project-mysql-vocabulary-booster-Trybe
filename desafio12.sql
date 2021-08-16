c3dc9e75984658ef9c69efca8da6128dbb6e8b32
Roberto Natividade <robertonnv@gmail.com>
2021-08-16 10:41:38 -0300

    Desafio 12

--- desafio12.sql
+++ desafio12.sql
@@ -1,13 +1,12 @@
 select
-CONCAT(emp.first_name, ' ',emp.last_Name) 'Nome completo funcionário 1',
+CONCAT(emp.first_name, ' ',emp.last_Name) `Nome completo funcionário 1`,
 emp.SALARY 'Salário funcionário 1',
 emp.PHONE_NUMBER 'Telefone funcionário 1',
-CONCAT(emp1.first_name, ' ',emp1.last_Name) 'Nome completo funcinário 2',
+CONCAT(emp1.first_name, ' ',emp1.last_Name) `Nome completo funcinário 2`,
 emp1.SALARY 'Salário funcionário 2',
 emp1.PHONE_NUMBER 'Telefone funcionário 2'
-from hr.employees emp, hr.employees emp1
-where
-emp.job_id = emp1.job_id
+from hr.employees emp
+inner join hr.employees emp1
+on emp.job_id = emp1.job_id
 and emp.employee_id <> emp1.employee_id
-order by CONCAT(emp.first_name, ' ',emp.last_Name) asc,
-CONCAT(emp1.first_name, ' ',emp1.last_Name)asc;
+order by `Salário funcionário 1`,`Telefone funcionário 2` ;

45c99ef9ddaa0f4ac2fb3fc9ae51a6a35f5602a0
Roberto Natividade <robertonnv@gmail.com>
2021-08-16 12:36:57 -0300

    Desafio 12.1

--- desafio12.sql
+++ desafio12.sql
@@ -5,11 +5,8 @@ t1.PHONE_NUMBER  'Telefone funcionário 1',
 CONCAT(t2.FIRST_NAME, ' ', t2.LAST_NAME)  'Nome completo funcionário 2',
 t2.SALARY  'Salário funcionário 2',
 t2.PHONE_NUMBER  'Telefone funcionário 2'
-FROM
-hr.employees  t1
+FROM hr.employees  t1
 INNER JOIN hr.employees  t2 ON t1.JOB_ID = t2.JOB_ID
 AND t1.EMPLOYEE_ID <> t2.EMPLOYEE_ID
-ORDER BY
-`Nome completo funcionário 1`,
-`Nome completo funcionário 2`;
+ORDER BY `Nome completo funcionário 1`,`Nome completo funcionário 2`;
   

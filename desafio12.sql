fe13ea3899efe7f4fce6c0693b5d684be3c17a1a
Roberto Natividade <robertonnv@gmail.com>
2021-08-16 10:19:10 -0300

    Desafio 12

---
+++ desafio12.sql
@@ -0,0 +1,11 @@
+select
+CONCAT(emp.first_name, ' ',emp.last_Name) 'Nome completo  funcionário 1',
+emp.SALARY 'Salário funcionário 1',
+emp.PHONE_NUMBER 'Telefone funcionário 1',
+CONCAT(emp1.first_name, ' ',emp1.last_Name) 'Nome completo funcinário 2',
+emp1.SALARY 'Salário funcionário 2',
+emp1.PHONE_NUMBER 'Telefone funcionário 2'
+from hr.employees emp, hr.employees emp1
+
+order by CONCAT(emp.first_name, ' ',emp.last_Name) asc,
+CONCAT(emp1.first_name, ' ',emp1.last_Name);

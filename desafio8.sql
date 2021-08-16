09b4ff46faa13e5d3cd085da4e42f65a92d696c8
Alan Albuquerque Ferreira Lopes <alan.alb.flopes@gmail.com>
2021-08-16 16:03:41 -0400

    feat: query that select contact name, shippers and order date

---
+++ desafio8.sql
@@ -0,0 +1,14 @@
+SELECT * FROM w3schools.orders;
+SELECT * FROM w3schools.customers;
+SELECT * FROM w3schools.shippers;
+
+SELECT customers.ContactName AS `Nome de contato`,
+shippers.ShipperName AS `Empresa que fez o envio`,
+orders.OrderDate AS `Data do Pedido`
+FROM w3schools.customers customers
+INNER JOIN w3schools.orders orders
+    ON orders.CustomerID = customers.CustomerID
+INNER JOIN w3schools.shippers shippers
+    ON shippers.ShipperID = orders.ShipperID
+WHERE shippers.ShipperName IN ('Speedy Express', 'United Package')
+ORDER BY customers.ContactName, shippers.ShipperName, orders.OrderDate;

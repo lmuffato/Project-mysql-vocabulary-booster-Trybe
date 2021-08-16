5f1bc75b73fc368fa1873aeb79fca1a73950d93a
Alan Albuquerque Ferreira Lopes <alan.alb.flopes@gmail.com>
2021-08-16 17:34:59 -0400

    feat: query that select products and the min, max and average of quantity

--- desafio10.sql
+++ desafio10.sql
@@ -1,7 +1,12 @@
 SELECT * FROM products;
 
 SELECT * FROM order_details;
-SELECT products.ProductName
+SELECT products.ProductName AS `Produto`,
+MIN(order_details.Quantity) AS `Mínima`,
+MAX(order_details.Quantity) AS `Máxima`,
+ROUND(AVG(order_details.Quantity), 2) AS `Média`
 FROM w3schools.products products
 INNER JOIN w3schools.order_details as order_details
-    ON order_details.ProductID = products.ProductID;
+    ON order_details.ProductID = products.ProductID
+GROUP BY products.ProductName
+ORDER BY `Média`, `Produto`;

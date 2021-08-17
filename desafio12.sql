fe53f8e06782ec219a9c738fc47abc23b75ce7ca
Roberto Natividade <robertonnv@gmail.com>
2021-08-16 23:27:55 -0300

    Desafio 12.3

--- desafio12.sql
+++ desafio12.sql
@@ -1,14 +1,12 @@
-SELECT
-CONCAT(t1.FIRST_NAME, ' ', t1.LAST_NAME) 'Nome completo funcionário 1',
-t1.SALARY 'Salário funcionário 1',
-t1.PHONE_NUMBER 'Telefone funcionário 1',
-CONCAT(t2.FIRST_NAME, ' ', t2.LAST_NAME)  'Nome completo funcionário 2',
-t2.SALARY  'Salário funcionário 2',
-t2.PHONE_NUMBER  'Telefone funcionário 2'
-FROM hr.employees t1
-INNER JOIN hr.employees t2 
-ON t1.JOB_ID = t2.JOB_ID
-AND t1.EMPLOYEE_ID <> t2.EMPLOYEE_ID
-ORDER BY CONCAT(t1.FIRST_NAME, ' ', t1.LAST_NAME),
-CONCAT(t2.FIRST_NAME, ' ', t2.LAST_NAME);
-  
+select
+concat(func1.first_name, '', func1.last_name) 'Nome completo funcionário 1',
+func1.salary 'Salário funcionário 1',
+func1.phone_number 'Telefone funcionário 1',
+concat(func2.first_name, '', func2.last_name) 'Nome completo funcionário 2',
+func2.salary 'Salário funcionário 2',
+func2.phone_number 'Telefone funcionário 2'
+from hr.employees func1
+inner join hr.employees func2
+on func1.job_id = func2.job_id
+and func1.employee_id <> func2.employee_id
+order by `Nome completo funcionário 1`, `Nome completo funcionário 2`;

a1ef808f6bea93b81874b9ecf60a0ad7a7a7926b
nyxnil <nsmnilsonn@gmail.com>
2021-08-17 11:52:29 -0300

    requisitos refatorados e novos do 1 ao 15

--- desafio11.sql
+++ desafio11.sql
@@ -1,5 +1,5 @@
 select c.ContactName as Nome,
-	c.Country as País, count(qnt.Country) -1 as "Número de compatriotas"
+c.Country as País, count(qnt.Country) -1 as "Número de compatriotas"
 from w3schools.customers as c, w3schools.customers as qnt
 where c.Country = qnt.Country
 group by c.ContactName, c.Country having count(qnt.Country) -1
--- desafio12.sql
+++ desafio12.sql
@@ -1,10 +1,10 @@
 select concat(F1.FIRST_NAME, ' ', F1.LAST_NAME) 'Nome completo funcionario 1',
-	F1.SALARY "Salário funcionário 1",
+F1.SALARY "Salário funcionário 1",
   F1.PHONE_NUMBER "Telefone funcionário 1",
   concat(F2.FIRST_NAME, ' ', F2.LAST_NAME) 'Nome completo funcionario 2',
   F2.SALARY "Salário funcionário 2",
   F2.PHONE_NUMBER "Telefone funcionário 2"
 from hr.employees F1, hr.employees F2
 where F1.JOB_ID = F2.JOB_ID
-	and F1.EMPLOYEE_ID <> F2.EMPLOYEE_ID
+and F1.EMPLOYEE_ID <> F2.EMPLOYEE_ID
 order by `Nome completo funcionario 1`, `Nome completo funcionario 2`;

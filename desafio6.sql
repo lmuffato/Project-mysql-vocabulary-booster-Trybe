2b75f7f663dfd4c5c6a74749231f07a0154f786a
Southier <patrick.southier@hotmail.com>
2021-08-18 01:30:37 -0300

    refactor: requirement 6 file trying to complete the requirement in evaluator

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,1 @@
-SELECT
-(SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) FROM hr.employees AS e WHERE e.EMPLOYEE_ID = h.EMPLOYEE_ID) AS 'Nome completo',
-(SELECT JOB_TITLE FROM hr.jobs AS j WHERE j.JOB_ID = h.JOB_ID) AS Cargo,
-h.START_DATE AS 'Data de início do cargo',
-(SELECT DEPARTMENT_NAME FROM hr.departments AS d WHERE d.DEPARTMENT_ID = h.DEPARTMENT_ID) AS Departamento
-FROM hr.job_history AS h
-ORDER BY (SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) FROM hr.employees AS e WHERE e.EMPLOYEE_ID = h.EMPLOYEE_ID) DESC, Cargo ASC;
+SELECT (SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) FROM hr.employees AS e WHERE e.EMPLOYEE_ID = h.EMPLOYEE_ID) AS 'Nome completo', (SELECT JOB_TITLE FROM hr.jobs AS j WHERE j.JOB_ID = h.JOB_ID) AS Cargo, h.START_DATE AS 'Data de início do cargo', (SELECT DEPARTMENT_NAME FROM hr.departments AS d WHERE d.DEPARTMENT_ID = h.DEPARTMENT_ID) AS Departamento FROM hr.job_history AS h ORDER BY (SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) FROM hr.employees AS e WHERE e.EMPLOYEE_ID = h.EMPLOYEE_ID) DESC, Cargo ASC;

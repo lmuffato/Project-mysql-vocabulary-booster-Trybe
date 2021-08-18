73b6b7e145b3595bbd74fc51a5ddd08968745fd3
nyxnil <nsmnilsonn@gmail.com>
2021-08-18 12:11:44 -0300

    requisitos refatorados e novos do 12 ao 18

--- desafio16.sql
+++ desafio16.sql
@@ -1,11 +1,12 @@
+
 delimiter $$
 create function BuscarQuantidadeEmpregosFuncionario(email_employeer varchar(50))
 returns int reads sql data begin
-declare history_jobs_qnt int;
+declare total_empregos int;
   select count(*)
   from hr.employees e
   join hr.job_history jh on e.EMPLOYEE_ID = jh.EMPLOYEE_ID
-  where e.EMAIL like email_employeer into history_jobs_qnt;
-return history_jobs_qnt;
+  where e.EMAIL like email_employeer into total_empregos;
+return total_empregos;
 end $$
 delimiter ;

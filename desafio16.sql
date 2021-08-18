91d93999e3529ca142c6babf83ebc798b6711106
nyxnil <nsmnilsonn@gmail.com>
2021-08-18 13:47:50 -0300

    requisito refatoro 16

--- desafio16.sql
+++ desafio16.sql
@@ -6,7 +6,8 @@ declare total_empregos int;
   select count(*)
   from hr.employees e
   join hr.job_history jh on e.EMPLOYEE_ID = jh.EMPLOYEE_ID
-  where e.EMAIL like email_employeer into total_empregos;
+  group by j.employee_id, e.email
+  having e.email = email_employeer into total_empregos;
 return total_empregos;
 end $$
 delimiter ;

9497462b42304154c22535ec379a985c280d8813
nyxnil <nsmnilsonn@gmail.com>
2021-08-18 13:52:33 -0300

    requisito refatoro 16

--- desafio16.sql
+++ desafio16.sql
@@ -1,13 +1,12 @@
 use hr
 delimiter $$
-create function BuscarQuantidadeEmpregosFuncionario(email_employeer varchar(50))
+create function buscar_quantidade_de_empregos_por_funcionario(email_employeer varchar(50))
 returns int reads sql data begin
 declare total_empregos int;
   select count(*)
   from hr.employees e
   join hr.job_history jh on e.EMPLOYEE_ID = jh.EMPLOYEE_ID
-  group by j.employee_id, e.email
-  having e.email = email_employeer into total_empregos;
+  where e.EMAIL like email_employeer into total_empregos;
 return total_empregos;
 end $$
 delimiter ;

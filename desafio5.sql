bb4be65129a9afd92ad9f806c4cb420fea7591ad
EdmilsonBernardo <ebedmilson48@gmail.com>
2021-08-18 21:50:31 -0300

    fix2x: requirement 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,5 +1,5 @@
 SELECT job_title AS 'Cargo',
-max_salary - min_salary AS 'Variação salarial',
+max_salary - min_salary AS 'Variação Salarial',
 ROUND((min_salary / 12),2) AS 'Média mínima mensal',
 ROUND((max_salary / 12),2) AS 'Média máxima mensal'
 FROM hr.jobs

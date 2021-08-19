56d60f0e2d1ff87b507a9c8300259f36a11c8469
EdmilsonBernardo <ebedmilson48@gmail.com>
2021-08-18 22:00:41 -0300

    fix: requirement 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,10 +1,8 @@
 SELECT CONCAT(UPPER(e.first_name), ' ', UPPER(e.last_name)) AS 'Nome completo',
-j.job_title AS 'Cargo',
-jh.start_date AS 'Data de início do cargo',
+jh.start_date AS 'Data de início',
 e.salary AS 'Salário'
 FROM hr.employees AS e
-INNER JOIN hr.jobs AS j
 INNER JOIN hr.job_history AS jh
 ON jh.employee_id = e.employee_id
 WHERE MONTH(jh.start_date) IN(1, 2, 3)
-ORDER BY `Nome Completo` DESC, `Cargo` ASC;
+ORDER BY `Nome Completo` DESC, `Data de início` ASC;

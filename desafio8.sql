84c880f746c975585d2d9ad1ea6345dfe82d2714
EdmilsonBernardo <ebedmilson48@gmail.com>
2021-08-18 22:14:08 -0300

    fix2x: requirement 7

--- desafio8.sql
+++ desafio8.sql
@@ -1,9 +1,8 @@
+SELECT c.ContactName AS 'Nome de contato',
 s.ShipperName AS 'Empresa que fez o envio',
 o.OrderDate AS 'Data do pedido'
-FROM w3schools.customers AS c
-INNER JOIN w3schools.shippers AS s
-INNER JOIN w3schools.orders AS o
-ON o.CustomerID = c.CustomerID
-AND o.ShipperID = s.ShipperID
-WHERE s.ShipperID IN(1, 2)
-ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;
+FROM w3schools.orders AS o
+INNER JOIN w3schools.customers AS c ON c.CustomerID = o.CustomerID
+INNER JOIN w3schools.shippers AS s ON s.ShipperID = o.ShipperID
+WHERE o.ShipperID = 1 OR o.ShipperID = 2
+ORDER BY 1, 2, 3;

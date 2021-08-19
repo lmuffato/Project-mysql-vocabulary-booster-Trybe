f3a6f6885de4d55f945039e3959670422fee4a0d
Southier <patrick.southier@hotmail.com>
2021-08-19 03:40:30 -0300

    fix: add the third order by option on requirement 8 file

--- desafio8.sql
+++ desafio8.sql
@@ -5,4 +5,4 @@ FROM w3schools.orders AS o
 INNER JOIN w3schools.customers AS c ON o.CustomerID = c.CustomerID
 INNER JOIN w3schools.shippers AS s ON o.ShipperID = s.ShipperID
 WHERE o.ShipperID IN (1, 2)
-ORDER BY c.ContactName, s.ShipperName;
+ORDER BY c.ContactName, s.ShipperName, o.OrderDate;

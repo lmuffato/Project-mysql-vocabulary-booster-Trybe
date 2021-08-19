9230fb8fd4fcf531a0d2e1cdef5dd2ce934d2565
Roberto Natividade <robertonnv@gmail.com>
2021-08-19 11:28:48 -0300

    Desafio 18

--- desafio18.sql
+++ desafio18.sql
@@ -2,7 +2,7 @@ select
 concat(emp.First_name, ' ', emp.Last_name) 'Nome completo',
 date_format(histo.start_date, '%d/%m/%y') 'Data de início',
 date_format(histo.end_date, '%d/%m/%y') 'Data de rescisão',
-round(datediff(histo.end_date, histo.start_date)/ 362,2) 'Anos trabalhados'
+round(datediff(histo.end_date, histo.start_date)/ 365,2) 'Anos trabalhados'
 from
 hr.employees emp
 inner join hr.job_history histo

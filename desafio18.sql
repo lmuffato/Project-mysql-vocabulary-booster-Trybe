92d2676e40dbc16d5617566f0702b902bae7255e
Roberto Natividade <robertonnv@gmail.com>
2021-08-19 12:05:51 -0300

    Desafio 18.1

--- desafio18.sql
+++ desafio18.sql
@@ -1,14 +1,11 @@
 SELECT
-  CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) 'Nome completo',
-  DATE_FORMAT(jobhist.START_DATE, '%d/%m/%Y') 'Data de início',
-  DATE_FORMAT(jobhist.END_DATE, '%d/%m/%Y') 'Data de rescisão',
-  
-  ROUND(DATEDIFF(jobhist.END_DATE, jobhist.START_DATE) / 365, 2) `Anos trabalhados`
+CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) 'Nome completo',
+DATE_FORMAT(jobhist.START_DATE, '%d/%m/%Y') 'Data de início',
+DATE_FORMAT(jobhist.END_DATE, '%d/%m/%Y') 'Data de rescisão',
+ROUND(DATEDIFF(jobhist.END_DATE, jobhist.START_DATE) / 365, 2) `Anos trabalhados`
 FROM
-  hr.employees emp
+hr.employees emp
 INNER JOIN hr.job_history jobhist
-  ON emp.EMPLOYEE_ID = jobhist.EMPLOYEE_ID
-
+ON emp.EMPLOYEE_ID = jobhist.EMPLOYEE_ID
 ORDER BY
-  `Nome completo`, `Anos trabalhados`;
-  
+`Nome completo`, `Anos trabalhados`;

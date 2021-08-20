9c692027430877a873fb2a60b123423bb74ab768
Southier <patrick.southier@hotmail.com>
2021-08-20 00:41:27 -0300

    fix: some logic errors in order to receive the expected returns

--- desafio11.sql
+++ desafio11.sql
@@ -1,6 +1,6 @@
 SELECT c1.ContactName AS Nome,
 c1.Country AS País,
-SUM(c1.Country = c2.Country) - 1 AS 'Número de compatriotas'
+CAST(SUM(c1.Country = c2.Country) AS UNSIGNED) - 1 AS 'Número de compatriotas'
 FROM w3schools.customers AS c1, w3schools.customers AS c2
 GROUP BY Nome, País
 HAVING (SUM(c1.Country = c2.Country) - 1) > 0
--- desafio12.sql
+++ desafio12.sql
@@ -5,5 +5,5 @@ CONCAT(e2.FIRST_NAME, ' ', e2.LAST_NAME) AS 'Nome completo funcionário 2',
 e2.SALARY AS 'Salário funcionário 2',
 e2.PHONE_NUMBER AS 'Telefone funcionário 2'
 FROM hr.employees AS e1, hr.employees AS e2
-WHERE (e1.JOB_ID = e2.JOB_ID) AND NOT (e1.FIRST_NAME = e2.FIRST_NAME)
+WHERE (e1.JOB_ID = e2.JOB_ID) AND NOT (CONCAT(e1.FIRST_NAME, ' ', e1.LAST_NAME) = CONCAT(e2.FIRST_NAME, ' ', e2.LAST_NAME))
 ORDER BY CONCAT(e1.FIRST_NAME, ' ', e1.LAST_NAME), CONCAT(e2.FIRST_NAME, ' ', e2.LAST_NAME);

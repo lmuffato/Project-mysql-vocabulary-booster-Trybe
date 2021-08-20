7005aa361c66da22fc3dac57e5daff88df977870
Southier <patrick.southier@hotmail.com>
2021-08-20 04:19:47 -0300

    fix: requirement 18 date format

--- desafio18.sql
+++ desafio18.sql
@@ -1,6 +1,18 @@
 SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
-CONCAT(DAY(h.START_DATE), '/', MONTH(h.START_DATE), '/', YEAR(h.START_DATE)) AS 'Data de início',
-CONCAT(DAY(h.END_DATE), '/', MONTH(h.END_DATE), '/', YEAR(h.END_DATE)) AS 'Data de rescisão',
+CONCAT(
+	IF(DAY(h.START_DATE) < 10, CONCAT('0', DAY(h.START_DATE)), DAY(h.START_DATE)),
+    '/',
+    IF(MONTH(h.START_DATE) < 10, CONCAT('0', MONTH(h.START_DATE)), MONTH(h.START_DATE)),
+    '/',
+    YEAR(h.START_DATE)
+) AS 'Data de início',
+CONCAT(
+	IF(DAY(h.END_DATE) < 10, CONCAT('0', DAY(h.END_DATE)), DAY(h.END_DATE)),
+	'/',
+	IF(MONTH(h.END_DATE) < 10, CONCAT('0', MONTH(h.END_DATE)), MONTH(h.END_DATE)),
+	'/',
+	YEAR(h.END_DATE)
+) AS 'Data de rescisão',
 ROUND((DATEDIFF(h.END_DATE, h.START_DATE)) / 365, 2) AS 'Anos trabalhados'
 FROM hr.job_history AS h
 INNER JOIN hr.employees AS e ON h.EMPLOYEE_ID = e.EMPLOYEE_ID

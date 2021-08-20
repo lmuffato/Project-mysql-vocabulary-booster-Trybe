40c3c22d98cebd5647ced6e6a49131de76f952f7
Southier <patrick.southier@hotmail.com>
2021-08-20 00:20:07 -0300

    feat: completes requirements 11 and 12

---
+++ desafio11.sql
@@ -0,0 +1,7 @@
+SELECT c1.ContactName AS Nome,
+c1.Country AS País,
+SUM(c1.Country = c2.Country) - 1 AS 'Número de compatriotas'
+FROM w3schools.customers AS c1, w3schools.customers AS c2
+GROUP BY Nome, País
+HAVING (SUM(c1.Country = c2.Country) - 1) > 0
+ORDER BY Nome;

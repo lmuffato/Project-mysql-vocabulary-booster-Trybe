c38878f2afa956ab85b1bb65ca725dfea5201a87
EdmilsonBernardo <ebedmilson48@gmail.com>
2021-08-21 17:08:55 -0300

    feat: requirement 16

---
+++ desafio16.sql
@@ -0,0 +1,17 @@
+USE hr;
+
+DELIMITER $$
+
+CREATE PROCEDURE buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(50)) RETURNS INT READS SQL DATA BEGIN
+DECLARE qnt_jobs INT;
+
+SELECT COUNT(*)
+FROM hr.job_history AS jobHistory
+INNER JOIN hr.employees AS employees
+ON jobHistory.EMPLOYEE_ID = employees.EMPLOYEE_ID
+WHERE employees.EMAIL LIKE email INTO qtd_jobs;
+RETURN qtd_jobs;
+
+END;
+
+DELIMITER ;

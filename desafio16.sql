b56263e795a1eb3e36eb7fae1a340eb5669afb6b
Southier <patrick.southier@hotmail.com>
2021-08-21 06:33:08 -0300

    fix: data type at requirement 16 return, in order to match the evaluator expected result

--- desafio16.sql
+++ desafio16.sql
@@ -4,7 +4,7 @@ CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(50))
 RETURNS VARCHAR(50) READS SQL DATA
 BEGIN
 DECLARE quantidade_de_empregos INT;
-SELECT COUNT(h.EMPLOYEE_ID)
+SELECT CAST(COUNT(h.EMPLOYEE_ID) AS UNSIGNED)
 FROM hr.employees AS e
 INNER JOIN hr.job_history AS h ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
 WHERE e.EMAIL = email

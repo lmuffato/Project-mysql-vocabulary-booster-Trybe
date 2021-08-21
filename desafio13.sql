b9400c7ea9a9862809e631afb13b43de9e9ec378
EdmilsonBernardo <ebedmilson48@gmail.com>
2021-08-21 10:45:43 -0300

    feat: requirement 14

--- desafio13.sql
+++ desafio13.sql
@@ -1,6 +1,6 @@
-SELECT p.ProductName AS Produto, 
-p.Price AS Preço
+SELECT p.ProductName AS 'Produto',
+p.Price AS 'Preço'
 FROM w3schools.products AS p
 INNER JOIN w3schools.order_details AS od
 ON p.ProductID = od.ProductID AND od.Quantity > 80
-ORDER BY Produto;
+ORDER BY `Produto`;

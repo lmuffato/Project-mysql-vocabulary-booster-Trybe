32afb55f0e763d542ff216a0d130ef80c66c74ab
Southier <patrick.southier@hotmail.com>
2021-08-21 06:25:48 -0300

    fix: errors pointed out by Code Climate

--- desafio16.sql
+++ desafio16.sql
@@ -3,13 +3,13 @@ DELIMITER $$
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(50))
 RETURNS VARCHAR(50) READS SQL DATA
 BEGIN
-	DECLARE quantidade_de_empregos INT;
-  SELECT COUNT(h.EMPLOYEE_ID)
-  FROM hr.employees AS e
-  INNER JOIN hr.job_history AS h ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
-  WHERE e.EMAIL = email
-  INTO quantidade_de_empregos;
-  RETURN quantidade_de_empregos;
+DECLARE quantidade_de_empregos INT;
+SELECT COUNT(h.EMPLOYEE_ID)
+FROM hr.employees AS e
+INNER JOIN hr.job_history AS h ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
+WHERE e.EMAIL = email
+INTO quantidade_de_empregos;
+RETURN quantidade_de_empregos;
 END $$
 
 DELIMITER ;
--- desafio17.sql
+++ desafio17.sql
@@ -1,10 +1,10 @@
 DELIMITER $$
 
 CREATE TRIGGER trigger_order_date_insert
-	BEFORE INSERT ON w3schools.orders
-  FOR EACH ROW
+BEFORE INSERT ON w3schools.orders
+FOR EACH ROW
 BEGIN
-	SET NEW.OrderDate = CURRENT_DATE();
+SET NEW.OrderDate = CURRENT_DATE();
 END $$
 
 DELIMITER ;

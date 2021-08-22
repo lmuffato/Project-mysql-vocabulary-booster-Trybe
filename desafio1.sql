6b493514cdf42840c3a6fb00ac0438eca69f3859
Paulo Henrique <ph.pereira.7@gmail.com>
2021-08-22 18:35:37 -0300

    Commit inicial com o primeiro desafio:fix

--- desafio1.sql
+++ desafio1.sql
@@ -1,5 +1,4 @@
-SELECT c.country_name AS 'País', 
-	IF(r.region_name = 'Europe', 'incluído', 'não incluído') AS 'Status Inclusão'
+SELECT c.country_name AS 'País', IF(r.region_name = 'Europe', 'incluído', 'não incluído') AS 'Status Inclusão'
 FROM countries AS c
 INNER JOIN regions AS r
 ON c.region_id = r.region_id

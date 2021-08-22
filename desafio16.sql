12da27ae08c7a7fffdbde1f9d49e9f0c1317b728
edersonrdg <edersonrodrigo31@gmail.com>
2021-08-22 19:52:31 -0300

    fix: requisito 16

--- desafio16.sql
+++ desafio16.sql
@@ -1,7 +1,15 @@
-USE w3schools;
-DELIMITER $$ CREATE TRIGGER insertDateNow BEFORE INSERT 
-        ON orders
-        FOR EACH ROW BEGIN   
-SET
-    NEW.OrderDate = NOW();    
+USE hr;
+DELIMITER $$ CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(emailValue VARCHAR(30)) RETURNS INT READS SQL DATA BEGIN   DECLARE qtd INT;
+SELECT
+    COUNT(*)   
+FROM
+    hr.employees AS e   
+INNER JOIN
+    hr.job_history AS jh 
+        ON e.EMPLOYEE_ID=jh.EMPLOYEE_ID   
+WHERE
+    e.EMAIL=emailValue   
+INTO
+    qtd;
+RETURN qtd; 
 END $$  DELIMITER ;

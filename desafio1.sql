fbfb82ba6efd4eb954cffe424f0e19826c62dd7e
Paulo Henrique <ph.pereira.7@gmail.com>
2021-08-22 18:33:45 -0300

    Commit inicial com o primeiro desafio

---
+++ desafio1.sql
@@ -0,0 +1,6 @@
+SELECT c.country_name AS 'País', 
+	IF(r.region_name = 'Europe', 'incluído', 'não incluído') AS 'Status Inclusão'
+FROM countries AS c
+INNER JOIN regions AS r
+ON c.region_id = r.region_id
+ORDER BY País;

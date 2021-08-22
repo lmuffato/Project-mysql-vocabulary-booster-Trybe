3755daecf1d3432cfe4fd323d7da8c3e1feec4e4
Paulo Henrique <ph.pereira.7@gmail.com>
2021-08-22 19:35:51 -0300

    Desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,9 @@
+SELECT job_title AS 'Cargo',
+CASE
+	WHEN max_salary BETWEEN 5000 AND 10000 THEN 'Baixo'
+    WHEN max_salary BETWEEN 10001 AND 20000 THEN 'Médio'
+    WHEN max_salary BETWEEN 20001 AND 30000 THEN 'Alto'
+    WHEN max_salary > 30000 THEN 'Altísimo'
+END AS 'Nível'
+FROM jobs
+ORDER BY job_title;

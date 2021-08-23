5ac18090f8e8e2a0ad957ed6e6556cfed1a1b224
Apoc35 <alexv_silveira@outlook.com>
2021-08-22 23:19:59 -0300

    Agora, finalmente de novo e novamente o fix dos exercícios 1 e 2. Envio do desafio 4 junto(Espero que seja o final da novela do 1 e 2 hahaha).

--- desafio1.sql
+++ desafio1.sql
@@ -1,6 +1,6 @@
 SELECT c1.COUNTRY_NAME AS "País",
- IF (r1.REGION_NAME = "Europe",
- "incluído", "não incluído") AS "Status Inclusão"
+  IF (r1.REGION_NAME = "Europe",
+  "incluído", "não incluído") AS "Status Inclusão"
 FROM hr.countries AS c1
 INNER JOIN hr.regions AS r1
 ON r1.REGION_ID = c1.REGION_ID

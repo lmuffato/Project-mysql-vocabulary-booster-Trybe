2154d240832588c0371d41c158e71a6d31395a20
Apoc35 <alexv_silveira@outlook.com>
2021-08-23 12:17:10 -0300

    Fix do desafio 18, continuo errando o nome das tabelas -.-

--- desafio18.sql
+++ desafio18.sql
@@ -1,7 +1,7 @@
-SELECT CONCAT(emp.first_name, " ", emp.last_name) AS "Nome Completo",
-DATE_FORMAT(his.START_DATE, '%d/%m/%Y') AS "Data de Início",
-DATE_FORMAT(his.END_DATE, '%d/%m/%Y') AS "Data de Rescisão",
-ROUND(DATEDIFF(his.END_DATE, his.START_DATE)/365, 2) AS "Anos Trabalhados"
+SELECT CONCAT(emp.first_name, " ", emp.last_name) AS "Nome completo",
+DATE_FORMAT(his.START_DATE, '%d/%m/%Y') AS "Data de início",
+DATE_FORMAT(his.END_DATE, '%d/%m/%Y') AS "Data de rescisão",
+ROUND(DATEDIFF(his.END_DATE, his.START_DATE)/365, 2) AS "Anos trabalhados"
 FROM hr.employees AS emp
 INNER JOIN hr.job_history AS his
 ON his.EMPLOYEE_ID = emp.EMPLOYEE_ID

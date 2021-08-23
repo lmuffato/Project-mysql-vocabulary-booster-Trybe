6d835e1478f4974b0fe20a39774891e61f89104e
Alan Albuquerque <alan.alb.flopes@gmail.comm>
2021-08-23 01:10:11 -0400

    fix: first column second world in lowercase

--- desafio6.sql
+++ desafio6.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT(empregados.FIRST_NAME,' ', empregados.LAST_NAME) AS `Nome Completo`,
+SELECT CONCAT(empregados.FIRST_NAME,' ', empregados.LAST_NAME) AS `Nome completo`,
 cargos.JOB_TITLE AS `Cargo`, historico_cargos.START_DATE AS `Data de início do cargo`,
 DEPARTMENT_NAME AS `Departamento`
 FROM hr.employees AS empregados
@@ -8,4 +8,4 @@ INNER JOIN hr.job_history AS historico_cargos
   ON empregados.EMPLOYEE_ID = historico_cargos.EMPLOYEE_ID
 INNER JOIN hr.departments AS departamento
   ON empregados.DEPARTMENT_ID = departamento.DEPARTMENT_ID
-ORDER BY `Nome Completo` DESC, `Cargo`;
+ORDER BY `Nome completo` DESC, `Cargo`;

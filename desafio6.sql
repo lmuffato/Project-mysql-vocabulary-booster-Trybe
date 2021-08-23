3fd5c4e6480b1d4275abc950e08978c2dd44cfa5
Alan Albuquerque <alan.alb.flopes@gmail.comm>
2021-08-23 07:53:50 -0400

    fix: realtionating departament id with job_history instead of employees

--- desafio6.sql
+++ desafio6.sql
@@ -5,5 +5,5 @@ FROM hr.employees AS empregados
 INNER JOIN hr.job_history AS historico_cargos
 INNER JOIN hr.jobs AS cargos 
 INNER JOIN hr.departments AS departamento
-  ON empregados.EMPLOYEE_ID = historico_cargos.EMPLOYEE_ID AND historico_cargos.JOB_ID = cargos.JOB_ID AND empregados.DEPARTMENT_ID = departamento.DEPARTMENT_ID
+  ON empregados.EMPLOYEE_ID = historico_cargos.EMPLOYEE_ID AND historico_cargos.JOB_ID = cargos.JOB_ID AND historico_cargos.DEPARTMENT_ID = departamento.DEPARTMENT_ID
 ORDER BY `Nome completo` DESC, `Cargo` ASC;

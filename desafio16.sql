ff7b20d8316ed7e0f538ac89b7cfff82f25d6ff8
Junior Portela <portelajr14@gmail.com>
2021-08-23 05:24:25 -0300

    desafio 16

---
+++ desafio16.sql
@@ -0,0 +1,15 @@
+DELIMITER $$
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(100))
+RETURNS INT READS SQL DATA 
+BEGIN
+DECLARE result INT;
+SELECT COUNT(j.EMPLOYEE_ID)
+FROM hr.job_history AS j
+INNER JOIN
+hr.employees AS e 
+ON j.EMPLOYEE_ID = e.EMPLOYEE_ID
+WHERE e.EMAIL = email INTO result;
+RETURN result;
+END $$
+DELIMITER ;

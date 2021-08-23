51ca39173ca8f90f6d4903b7a4e421f10b4c5e06
Alan Albuquerque <alan.alb.flopes@gmail.comm>
2021-08-23 10:00:19 -0400

    feat: added country in group by

--- desafio11.sql
+++ desafio11.sql
@@ -2,4 +2,4 @@ SELECT usual.ContactName AS `Nome`, usual.Country AS `País`,
 COUNT(*) AS `Número de compatriotas`
 FROM w3schools.customers usual, w3schools.customers diff
 WHERE usual.Country = diff.Country AND usual.ContactName != diff.ContactName
-GROUP BY `Nome` ORDER BY `Nome`;
+GROUP BY `Nome`, `País` ORDER BY `Nome`;

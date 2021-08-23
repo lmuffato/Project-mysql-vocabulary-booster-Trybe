65eb71658da4ff7602087513a55da89ec09fa8f6
Alan Albuquerque <alan.alb.flopes@gmail.comm>
2021-08-23 09:55:53 -0400

    fix: i forgot to save the changes in the .sql :)

--- desafio11.sql
+++ desafio11.sql
@@ -1,5 +1,5 @@
-SELECT usual.CustomerName AS `Nome`, usual.Country AS `País`,
+SELECT usual.ContactName AS `Nome`, usual.Country AS `País`,
 COUNT(*) AS `Número de compatriotas`
 FROM w3schools.customers usual, w3schools.customers diff
-WHERE usual.Country = diff.Country AND usual.ContactName <> diff.ContactName
-GROUP BY `País`, `Nome`;
+WHERE usual.Country = diff.Country AND usual.ContactName != diff.ContactName
+GROUP BY `Nome` ORDER BY `Nome`;

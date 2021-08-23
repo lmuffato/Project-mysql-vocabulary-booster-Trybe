96ca461e6c87e4a47986b7c90f3bd8d5a797dfee
Alan Albuquerque <alan.alb.flopes@gmail.comm>
2021-08-23 09:50:34 -0400

    fix: column select in name and where logistics

--- desafio10.sql
+++ desafio10.sql
@@ -6,5 +6,4 @@ FROM w3schools.products products
 INNER JOIN w3schools.order_details as order_details
     ON order_details.ProductID = products.ProductID
 GROUP BY products.ProductName
-HAVING `Média` > 20.00
-ORDER BY `Média`, `Produto`;
+HAVING `Média` > 20.00 ORDER BY `Média`, `Produto`;

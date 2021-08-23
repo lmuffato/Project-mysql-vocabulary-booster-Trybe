d071c1f7acd198677ac73246de5757ef5e89c8ad
Alan Albuquerque <alan.alb.flopes@gmail.comm>
2021-08-23 09:26:33 -0400

    fix: remove order details table cus was useless

--- desafio9.sql
+++ desafio9.sql
@@ -1,9 +1,7 @@
+SELECT * FROM w3schools.orders;
 SELECT CONCAT(employees.FirstName, ' ', employees.LastName) AS `Nome completo`,
-COUNT(order_details.Quantity) as `Total de pedidos`
+COUNT(orders.OrderID) as `Total de pedidos`
 FROM w3schools.employees employees
 INNER JOIN w3schools.orders orders
     ON orders.EmployeeID = employees.EmployeeID
-INNER JOIN w3schools.order_details order_details
-    ON order_details.OrderID = orders.OrderID
-GROUP BY `Nome completo`
-ORDER BY `Total de pedidos`;
+GROUP BY `Nome completo` ORDER BY `Total de pedidos`;

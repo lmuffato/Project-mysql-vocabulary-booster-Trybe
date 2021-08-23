5f4a98b7c76cdbbe53d5c8e0ad89162db47ee4cc
Apoc35 <alexv_silveira@outlook.com>
2021-08-22 22:48:00 -0300

    Desafios 1 e 2.

---
+++ desafio1.sql
@@ -0,0 +1,7 @@
+SELECT c1.COUNTRY_NAME AS "País",
+	IF (r1.REGION_NAME = "Europe",
+	"incluído", "não incluído") AS "Status Inclusão"
+FROM hr.countries AS c1
+INNER JOIN hr.regions AS r1
+ON r1.REGION_ID = c1.REGION_ID
+ORDER BY c1.COUNTRY_NAME;

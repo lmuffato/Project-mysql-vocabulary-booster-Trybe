d8a4ebb490f96a2dd0705c4f04e66a40802194e2
Alan Albuquerque <alan.alb.flopes@gmail.comm>
2021-08-23 08:47:02 -0400

    fix: order by in this query

--- desafio7.sql
+++ desafio7.sql
@@ -3,6 +3,7 @@ historico_cargos.START_DATE AS `Data de início`,
 empregados.SALARY AS `Salário`
 FROM hr.employees AS empregados
 INNER JOIN hr.job_history AS historico_cargos
-  ON empregados.EMPLOYEE_ID = historico_cargos.EMPLOYEE_ID
+Inner JOIN hr.jobs AS cargos
+  ON empregados.EMPLOYEE_ID = historico_cargos.EMPLOYEE_ID AND cargos.JOB_ID = historico_cargos.JOB_ID
 WHERE MONTH(historico_cargos.START_DATE) IN (1,2,3)
-ORDER BY `Nome Completo`;
+ORDER BY `Nome Completo` ASC, `Data de início` ASC;

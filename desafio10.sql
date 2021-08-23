54bc1dc4cde84a2503ebd60aabd3716ade15452d
Alan Albuquerque <alan.alb.flopes@gmail.comm>
2021-08-23 09:36:39 -0400

    fix: added only avg > 20

--- desafio10.sql
+++ desafio10.sql
@@ -6,4 +6,5 @@ FROM w3schools.products products
 INNER JOIN w3schools.order_details as order_details
     ON order_details.ProductID = products.ProductID
 GROUP BY products.ProductName
+HAVING `Média` > 20.00
 ORDER BY `Média`, `Produto`;

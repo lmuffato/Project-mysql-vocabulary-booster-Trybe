51500ddd42369407959cc39d1ddd988c2fff124a
Marcos Albuquerque <mpdsa20@gmail.com>
2021-08-23 09:22:59 -0300

    Desafio 7, fix desafio 6

---
+++ desafio7.sql
@@ -0,0 +1,8 @@
+SELECT UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) 'Nome completo',
+jh.START_DATE 'Data de início',
+e.SALARY Salário
+FROM hr.job_history jh
+JOIN hr.employees e
+ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+WHERE MONTH(jh.START_DATE ) IN (1,2,3)
+ORDER BY `Nome Completo`, `Data de início`;

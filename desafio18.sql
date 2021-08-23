d4ea3ec45206f0b146bf397b9811487d3edf26e5
Alan Albuquerque <alan.alb.flopes@gmail.comm>
2021-08-23 11:48:32 -0400

    fix: order by structure of string

--- desafio18.sql
+++ desafio18.sql
@@ -1,7 +1,8 @@
 SELECT CONCAT(employees.FIRST_NAME,' ', employees.LAST_NAME) AS 'Nome completo',
-DATE_FORMAT(jobHist.START_DATE, '%d/%m/%Y') AS 'Data de início', DATE_FORMAT(jobHist.END_DATE, '%d/%m/%Y') AS 'Data de rescisão',
+DATE_FORMAT(jobHist.START_DATE, '%d/%m/%Y') AS 'Data de início',
+DATE_FORMAT(jobHist.END_DATE, '%d/%m/%Y') AS 'Data de rescisão',
 ROUND(DATEDIFF(jobHist.END_DATE, jobHist.START_DATE)/365, 2) AS 'Anos trabalhados'
-FROM hr.employees employees
-INNER JOIN hr.job_history jobHist
+FROM hr.job_history jobHist
+INNER JOIN hr.employees employees
 ON jobHist.EMPLOYEE_ID = employees.EMPLOYEE_ID
-ORDER BY 'Nome completo', 'Anos Trabalhados';
+ORDER BY `Nome completo`, `Anos trabalhados`;

500dfbfc5fd7e652b6e95c3d5d2b3e6425a0a83a
pauloeliezerg <pauloeliezerg@gmail.com>
2021-08-23 06:24:10 -0300

    req15

--- desafio15.sql
+++ desafio15.sql
@@ -1,13 +1,17 @@
 DELIMITER $$
 
-CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(100))
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(50))
+RETURNS INT READS SQL DATA
 BEGIN
-  SELECT ROUND(AVG(employees.SALARY), 2) AS 'Média salarial'
-  FROM hr.jobs AS jobs
-  INNER JOIN hr.employees AS employees ON jobs.JOB_ID = employees.JOB_ID
-  WHERE jobs.JOB_TITLE = cargo;
+DECLARE quantidade_de_empregos INT;
+SELECT COUNT(employees_history.EMPLOYEE_ID)
+FROM hr.employees AS employees
+INNER JOIN hr.job_history AS employees_history ON employees.EMPLOYEE_ID = employees_history.EMPLOYEE_ID
+WHERE employees.EMAIL = email
+INTO quantidade_de_empregos;
+RETURN quantidade_de_empregos;
 END $$
 
 DELIMITER ;
 
-CALL buscar_media_por_cargo('Programmer');
+SELECT buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR');

c232fc7d6e168a549f0a09760f665a9b13cc9d30
Alan Albuquerque <alan.alb.flopes@gmail.comm>
2021-08-23 07:48:16 -0400

    feat: relationating job id with job_history instead employees

--- desafio6.sql
+++ desafio6.sql
@@ -2,10 +2,8 @@ SELECT CONCAT(empregados.FIRST_NAME,' ', empregados.LAST_NAME) AS `Nome completo
 cargos.JOB_TITLE AS `Cargo`, historico_cargos.START_DATE AS `Data de início do cargo`,
 DEPARTMENT_NAME AS `Departamento`
 FROM hr.employees AS empregados
-INNER JOIN hr.jobs AS cargos 
-  ON empregados.JOB_ID = cargos.JOB_ID
 INNER JOIN hr.job_history AS historico_cargos
-  ON empregados.EMPLOYEE_ID = historico_cargos.EMPLOYEE_ID
+INNER JOIN hr.jobs AS cargos 
 INNER JOIN hr.departments AS departamento
-  ON empregados.DEPARTMENT_ID = departamento.DEPARTMENT_ID
-ORDER BY `Nome completo` DESC, `Cargo`;
+  ON empregados.EMPLOYEE_ID = historico_cargos.EMPLOYEE_ID AND historico_cargos.JOB_ID = cargos.JOB_ID AND empregados.DEPARTMENT_ID = departamento.DEPARTMENT_ID
+ORDER BY `Nome completo` DESC, `Cargo` ASC;

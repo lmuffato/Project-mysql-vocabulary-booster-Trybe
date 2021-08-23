20666d99b5d53b8ac7a941631c6ebaec2bf6cf1b
Alan Albuquerque <alan.alb.flopes@gmail.comm>
2021-08-23 00:30:32 -0400

    fix: named second column in query number 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,3 +1,3 @@
 SELECT COUNTRY_NAME AS 'País',
-IF(REGION_ID=1, 'incluído', 'não incluído')
+IF(REGION_ID=1, 'incluído', 'não incluído') AS `Status inclusão`
 FROM hr.countries ORDER BY COUNTRY_NAME;
